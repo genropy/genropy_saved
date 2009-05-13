077d672d7b349ac93fc6f403560f147cac978e4c
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-05-13 10:52:06 +0000

    Deleting common
    git-svn-id: http://svn.genropy.org/genro/trunk@594 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/_static/lib_11/dojo/mywidgets/manifest.js
+++
@@ -1,8 +0,0 @@
-dojo.provide("mywidgets.manifest");
-dojo.require("dojo.string.extras");
-
-dojo.registerNamespaceResolver("mywidgets",
-	function(name){ 
-		return "mywidgets.widget."+dojo.string.capitalize(name);
-	}
-);
--- resources/common/_static/lib_11/dojo/mywidgets/post_dojo.js
+++
@@ -1,6 +0,0 @@
-// load widget handler code
-dojo.require("dojo.widget.*");
-
-// Load user custom widgets
-dojo.registerModulePath('mywidgets', '../mywidgets');
-dojo.widget.manager.registerWidgetPackage('mywidgets.widget');
