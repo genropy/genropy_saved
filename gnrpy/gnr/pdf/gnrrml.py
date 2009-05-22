2255f29b7c6af9dd76d2deb61fde1eaec6ffe21f
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-05-22 21:16:01 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@647 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/pdf/gnrrml.py
+++ gnrpy/gnr/pdf/gnrrml.py
@@ -530,8 +530,11 @@ class GnrRml(object):
     def __init__(self, filename='', invariant=1, **kwargs):
         self.root = GnrRmlSrc.makeRoot()
         self.document = self.root.document(filename=filename, invariant=invariant, **kwargs)
-    
-
+        self.stylesheets=self.document.stylesheet()
+        
+    def template(self,**kwargs):
+        self.templates=self.document.template(**kwargs)
+        
     def toRml(self,filename=None):
         if filename:
             filename=expandpath(filename)
@@ -550,39 +553,3 @@ class GnrRml(object):
             output.seek(0)
             return output.read()
         output.close()
-
-if __name__ =='__main__':
-    pdf = GnrRml(filename='a.pdf')
-    document=pdf.document
-    template=document.template(pageSize='letter', leftMargin=72, showBoundary=1)
-    pt=template.pageTemplate(id='main', pageSize='letter portrait')
-    pg =pt.pageGraphics()
-    pg.setFont(_name='Helvetica-BoldOblique', size=18)
-    pg.string('RML2PDF Test Suite',x=523, y=800,align='r')
-    ta=pg.textAnnotation()
-    ta.param(_name='Rect',content='0,0,1,1')
-    ta.param(_name='F',content='3')
-    ta.param(_name='escape',content='6')
-    ta.content(content='X::PDF PX(S) MT(PINK)')
-    pt.frame(id='first',x1='1in',y1='1in',width='6.26in',height='9.69in')
-    ss = document.stylesheet()
-    
-    ss.initialize().alias(id='style.normal',value='style.Normal')
-    ss.paraStyle(_name='h1', fontName="Helvetica-BoldOblique", fontSize="32", leading="36")
-    ss.paraStyle(_name="normal", fontName="Helvetica", fontSize="10", leading="12")
-    ss.paraStyle(_name="spaced", fontName="Helvetica", fontSize="10", leading="12",
-        spaceBefore="12", spaceAfter="12")
-    story=document.story()
-    para1=story.para(style='normal')
-    para1.content(content='Hello World.  This is a normal paragraph. Blah')
-    para1.font(color="red").content(content='IPO') 
-    para1.content(content="""blah blah blah blah growth forecast blah
-blah blah forecast blah.Blah blah blah blah blah blah blah blah blah blah blah profit blah blah blah blah blah
-blah blah blah blah blah IPO.Blah blah blah blah blah blah blah reengineering blah growth blah blah blah
-proactive direction strategic blah blah blah forward-thinking blah.Blah blah doubletalk blah blah blah blah
-blah profit blah blah growth blah blah blah blah blah profit.Blah blah blah blah venture capital blah blah blah
-blah blah forward-thinking blah. Here are some common characters
-        """)
-    story.para(style='spaced', content=u"""This is spaced.  There should be 12 points before and after.""")
-
-    print pdf.toPdf('~/a.pdf')
