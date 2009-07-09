48259ef6b057e9bb6cd3e173f61de919287e58fc
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-07-09 14:24:16 +0000

    Barcode
    git-svn-id: http://svn.genropy.org/genro/trunk@769 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ gnrpy/gnr/web/gnrbasewebtool.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#  untitled
+#
+#  Created by Giovanni Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+# --------------------------- GnrWebPage subclass ---------------------------
+class GnrBaseWebTool(object):
+    pass
+
