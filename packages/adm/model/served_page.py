589f0ce86abbcf79122a75d42021d3a8c45e5856
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-09-14 15:12:42 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@897 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/model/served_page.py
+++ packages/adm/model/served_page.py
@@ -13,18 +13,19 @@ class Table(object):
         tbl.column('end_reason',size=':12',name_long='!!End Reason')
         tbl.column('subscribed_tables',name_long='!!Subscribed tables')
         
-    def getPendingPages(self,connection_id=None):
-        where='$end_ts is null'
+    def getLivePages(self,connection_id=None,topic=None):
+        where=['$end_ts is null']
         if connection_id:
-            where='%s AND %s' % ('$connection_id=:connection_id',where)
-        return self.query(where=where,connection_id=connection_id).fetch()
+            where.append('$connection_id=:connection_id')
+        if topic:
+            where.append('$subscribed_tables ILIKE :topic')
+        return self.query(where=' AND '.join(where),connection_id=connection_id,topic="%%%s%%"%(topic or '')).fetch()
         
     def closePendingPages(self,connection_id=None,end_ts=None,end_reason=None):
-        for page in self.getPendingPages(connection_id=connection_id):
+        for page in self.getLivePages(connection_id=connection_id):
             self.closePage(page['page_id'],end_ts=end_ts,end_reason=end_reason)
             
     def closePage(self,page_id,end_ts=None,end_reason=None):
         self.update(dict(page_id=page_id,end_ts=end_ts or datetime.now(),end_reason=end_reason))
-        
-        
+
             
--- packages/adm/webpages/connection_explorer.py
+++ packages/adm/webpages/connection_explorer.py
@@ -8,9 +8,14 @@
 #
 
 # --------------------------- GnrWebPage subclass ---------------------------
+from gnr.core.gnrbag import Bag
+
 class GnrCustomWebPage(object):
     maintable='adm.connection'
     py_requires='public:Public,public:IncludedView'
+    
+    pageOptions=dict(subscribed_tables='adm.connection,adm.served_page')
+    
     def pageAuthTags(self, method=None, **kwargs):
         return 'user'
         
@@ -20,24 +25,27 @@ class GnrCustomWebPage(object):
     def main(self, rootBC, **kwargs):
         bc,top,bottom = self.pbl_rootBorderContainer(rootBC,'!!Connections')
         topBC = bc.borderContainer(region='top',height='50%')
-        self.connectedUser(topBC.borderContainer(region='left',width='50%',margin='5px'))
-        self.userConnections(topBC.borderContainer(region='center',margin_left=0,margin='5px'))
-        centerBC = bc.borderContainer(region='center',margin='5px',margin_top=0)
+        self.connectedUser(topBC.borderContainer(region='left',width='50%',margin='5px', datapath='user'))
+        self.userConnections(topBC.borderContainer(region='center',margin_left=0,margin='5px',datapath='connection'))
+        centerBC = bc.borderContainer(region='center',margin='5px',margin_top=0, datapath='page')
         self.userServedPages(centerBC)
         
     def connectedUser(self,bc):
-        topfb=bc.contentPane(region='top',height='40px').formBuilder(cols='2')
-        topfb.textBox(value='^messages.user.text',lbl='User message',width='30em')
-        topfb.button('send',lbl='',action='alert("vv")')
+        topfb=bc.contentPane(region='top',height='40px', datapath='.messages').formBuilder(cols='2')
+        topfb.textBox(value='^.text',lbl='User message',width='30em')
+        topfb.button('send',lbl='',fire='.send')
+        topfb.button('refresh',lbl='',fire='user.refresh')
+        topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', dest_user='=user.current')
         bc=bc.borderContainer(region='center')
         self.includedViewBox(bc,label='!!Live user connected',
-                            storepath='selection.connectedUsers', 
+                            storepath='.selection', 
                             struct=self._connectedUser_struct(), 
-                            autoWidth=True,selected_username='current.user',
+                            autoWidth=True,selected_username='.current',
                             nodeId='connected_users')
-        bc.dataSelection('selection.connectedUsers','adm.connection',
+        bc.dataSelection('.selection','adm.connection',
                          where='$end_ts IS NULL',distinct=True,
-                         columns='$username,@userid.fullname,$ip',_onStart=True)
+                         columns='$username,@userid.fullname,$ip',_onStart=True, _fired='.refresh')
+        
                             
     def _connectedUser_struct(self):
         struct = self.newGridStruct()
@@ -47,20 +55,25 @@ class GnrCustomWebPage(object):
         r.cell('ip', name='Remote addr.', width='15em')
         return struct
         
+    
     def userConnections(self,bc):
-        topfb=bc.contentPane(region='top',height='40px').formBuilder(cols='2')
-        topfb.textBox(value='^messages.connection.text',lbl='Connection message',width='30em')
-        topfb.button('send',lbl='',action='alert("vv")')
+        topfb=bc.contentPane(region='top',height='40px', datapath='.messages').formBuilder(cols='2')
+        topfb.textBox(value='^.text',lbl='Connection message',width='30em')
+        topfb.button('send',lbl='',fire='.send')
+        topfb.button('refresh',lbl='',fire='connection.refresh')
+        
+        topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', dest_connection='=connection.current')
+        
         bc=bc.borderContainer(region='center')
         self.includedViewBox(bc,label='!!User connections',
-                            storepath='selection.userConnections', 
+                            storepath='.selection', 
                             struct=self._userConnections_struct(), 
-                            autoWidth=True,selectedId='current.connection',
+                            autoWidth=True,selectedId='.current',
                             nodeId='user_connections')
-        bc.dataSelection('selection.userConnections','adm.connection',
+        bc.dataSelection('.selection','adm.connection',
                          where='$end_ts IS NULL AND username=:user',
-                         user='^current.user',
-                         columnsFromView='user_connections')
+                         user='^user.current',
+                         columnsFromView='user_connections', _fired='.refresh')
                          
     def _userConnections_struct(self):
         struct = self.newGridStruct()
@@ -70,19 +83,22 @@ class GnrCustomWebPage(object):
         return struct
         
     def userServedPages(self,bc):
-        topfb=bc.contentPane(region='top',height='40px').formBuilder(cols='2')
-        topfb.textBox(value='^messages.page.text',lbl='Page message',width='30em')
-        topfb.button('send',lbl='',action='alert("vv")')
+        topfb=bc.contentPane(region='top',height='40px', datapath='.messages').formBuilder(cols='3')
+        topfb.textBox(value='^.text',lbl='Page message',width='30em')
+        topfb.button('send',lbl='',fire='.send')
+        topfb.button('refresh',lbl='',fire='page.refresh')
+        
+        topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', dest_page='=page.current')
         bc=bc.borderContainer(region='center')
         self.includedViewBox(bc,label='!!User served pages',
-                            storepath='selection.servedPage', 
+                            storepath='.selection', 
                             struct=self._userServedPages_struct(), 
-                            autoWidth=True,selectedId='current.servedPage',
+                            autoWidth=True,selectedId='.current',
                            nodeId='user_servedpages')
-        bc.dataSelection('selection.servedPage','adm.served_page',
+        bc.dataSelection('.selection','adm.served_page',
                          where='connection_id=:connection',
-                         connection='^current.connection',
-                         columnsFromView='user_servedpages')
+                         connection='^connection.current',
+                         columnsFromView='user_servedpages', _fired='^.refresh')
                          
     def _userServedPages_struct(self):
         struct = self.newGridStruct()
@@ -94,3 +110,25 @@ class GnrCustomWebPage(object):
         r.cell('pagename',name='Page name',width='15em')
         r.cell('subscribed_tables',name='Subscribed table',width='20em')
         return struct
+        
+    def rpc_sendMessage(self,msg=None, dest_user=None, dest_connection=None, dest_page=None):
+        """
+        user and connection_id are mutually exclusive
+        """
+        if dest_user:
+            mode='user'
+        elif dest_connection:
+            mode='connection'
+        elif dest_page:
+            mode='page'
+        else:
+            mode='missing'
+        print dest_user, ',', dest_connection, ',', dest_page
+        print mode, msg
+        if ':' in msg:
+            message_type, msg = msg.split(':',1)
+        else:
+            message_type = 'servermsg'
+        self.site.writeMessage(body=Bag({message_type:msg}) ,user=dest_user, page_id=dest_page, 
+                               connection_id=dest_connection, message_type=message_type)
+        self.db.commit()
