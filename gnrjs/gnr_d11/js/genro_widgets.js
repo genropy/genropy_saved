a4a611103e20b7cd643b7a5067c9df946c769c5f
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-09-27 18:38:07 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@950 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrjs/gnr_d11/js/genro_widgets.js
+++ gnrjs/gnr_d11/js/genro_widgets.js
@@ -2490,9 +2490,7 @@ dojo.declare("gnr.widgets.VirtualStaticGrid",gnr.widgets.Grid,{
         this.gnrediting = true;
         if (this.sourceNode.currentEditedRow != row){
             var selectedDataPath = this.dataNodeByIndex(row).getFullpath(null, true);
-            //this.sourceNode.setRelativeData('.edit_datapath',selectedDataPath);
-            this.sourceNode.setRelativeData(this.sourceNode.attr.controllerPath+'.edit_datapath',
-                                             selectedDataPath);
+            this.sourceNode.setRelativeData('.edit_datapath',selectedDataPath);
             this.sourceNode.currentEditedRow = row;
             setTimeout(dojo.hitch(this, 'startEditCell', row, col), 1);
             return;
@@ -2599,9 +2597,7 @@ dojo.declare("gnr.widgets.GridEditor",gnr.widgets.baseHtml,{
     creating: function(attributes, sourceNode){
         attributes.display='none';
         sourceNode.attr.nodeId='grided_' + sourceNode.getStringId();
-        //sourceNode.attr.datapath = '^.edit_datapath';
-        sourceNode.attr.datapath = '^'+sourceNode.getParentNode().attr.controllerPath+'.edit_datapath';
-
+        sourceNode.attr.datapath = '^.edit_datapath';
         sourceNode.registerDynAttr('datapath');
         
         
@@ -2691,9 +2687,9 @@ dojo.declare("gnr.widgets.IncludedView",gnr.widgets.VirtualStaticGrid,{
         attributes.rowCount=0;
         attributes.datamode = datamode;
         sourceNode.attr.nodeId = sourceNode.attr.nodeId || 'grid_' + sourceNode.getStringId();
-        sourceNode.attr.controllerPath = sourceNode.attr.controllerPath || 'grids.' + sourceNode.attr.nodeId;
         if (attributes.query_columns){
-            sourceNode.setRelativeData(sourceNode.attr.controllerPath+'.columns', attributes.query_columns);
+            var controllerPath = sourceNode.absDatapath() || 'grids.' + sourceNode.attr.nodeId;
+            sourceNode.setRelativeData(controllerPath+'.columns', attributes.query_columns);
         }
     },    
     created: function(widget, savedAttrs, sourceNode){
