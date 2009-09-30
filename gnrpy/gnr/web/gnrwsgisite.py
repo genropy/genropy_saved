27524a03a817253db08ba993b8be342ffc567cd3
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-09-30 08:42:16 +0000

     
    git-svn-id: http://svn.genropy.org/genro/trunk@973 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/web/gnrwsgisite.py
+++ gnrpy/gnr/web/gnrwsgisite.py
@@ -730,7 +730,9 @@ class GnrWsgiSite(object):
         if not (fullpath and os.path.exists(fullpath)):
             return self.not_found(environ, start_response)
         if_none_match = environ.get('HTTP_IF_NONE_MATCH')
+        print 'pre if if_none_match'
         if if_none_match:
+            print 'if_none_match'
             print if_none_match
             mytime = os.stat(fullpath).st_mtime
             print mytime
