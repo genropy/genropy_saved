9840c09e9d8e3df50016ec2eae694f4e2536abb3
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-10-22 09:41:04 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1068 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/web/gnrwsgisite.py
+++ gnrpy/gnr/web/gnrwsgisite.py
@@ -58,6 +58,7 @@ class GnrWsgiSite(object):
             self.config = self.load_site_config()
         
         self.home_uri = self.config['wsgi?home_uri'] or '/'
+        self.beaker_type = self.config['wsgi?beaker_type'] or 'memory'
         if self.home_uri[-1]!='/':
             self.home_uri+='/'
         self.mainpackage = self.config['wsgi?mainpackage']
@@ -334,9 +335,9 @@ class GnrWsgiSite(object):
         wsgiapp=self.dispatcher
         if self.debug:
             wsgiapp = EvalException(wsgiapp, debug=True)
-        beaker_path = os.path.join(os.path.realpath(self.site_path),'session_data')
+        beaker_path = os.path.join(os.path.realpath(self.site_path),'session_data')        
         wsgiapp = SessionMiddleware(wsgiapp, dict(key=self.session_key, secret=self.secret, 
-                data_dir=beaker_path, type='memory', auto=True))
+                data_dir=beaker_path, type=self.beaker_type, auto=True))
         #wsgiapp = Gzipper(wsgiapp, compresslevel=8)
         return wsgiapp
         
