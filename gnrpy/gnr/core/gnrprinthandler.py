417c39d977ee44098666fdb7f782c0794533bb14
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-11-16 16:23:21 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1183 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/core/gnrprinthandler.py
+++ gnrpy/gnr/core/gnrprinthandler.py
@@ -89,7 +89,7 @@ class PrintHandler(object):
             baseName = os.path.splitext(os.path.basename(srcPath))[0]
             destPath=os.path.join(destPath, '%s.pdf' % baseName)
         if sys.platform.startswith('linux'):
-            result = call(['xvfb-run','-a','wk2pdf',srcPath,destPath])
+            result = call(['xvfb-run','-a','wk2pdf',srcPath,destPath],shell=True)
         else:
             result = call(['wk2pdf',srcPath,destPath])
         print result
@@ -103,7 +103,7 @@ class PrintHandler(object):
             if ext.lower() == '.html':
                 destPath=os.path.join(storeFolder, '%s.pdf' % baseName)
                 if sys.platform.startswith('linux'):
-                    result = call(['xvfb-run','-a','wk2pdf',filename,destPath])
+                    result = call(['xvfb-run','-a','wk2pdf',filename,destPath],shell=True)
                 else:
                     result = call(['wk2pdf',filename,destPath])
                 if result < 0:
