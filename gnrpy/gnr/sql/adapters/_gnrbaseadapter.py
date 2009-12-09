dd68be9a18529b7ff17433b7e232fab6c9ff7e37
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-12-09 10:27:30 +0000

    added numeriic and money types
    git-svn-id: http://svn.genropy.org/genro/trunk@1281 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/sql/adapters/_gnrbaseadapter.py
+++ gnrpy/gnr/sql/adapters/_gnrbaseadapter.py
@@ -31,11 +31,11 @@ class SqlDbAdapter(object):
     but only a few must be implemented in a specific adapter."""
     typesDict = {'character varying':'A', 'character':'A', 'text':'T', 
                  'boolean':'B', 'date':'D', 'time without time zone':'H', 'timestamp without time zone':'DH',
-                 'timestamp with time zone':'DH',
+                 'timestamp with time zone':'DH','numeric':'N','money':'M',
                  'integer':'I', 'bigint':'L','smallint':'I', 'double precision':'R', 'real':'R', 'bytea':'O'}
     
     revTypesDict = {'A':'character varying', 'C':'character', 'T':'text', 
-                    'X':'text','P':'text','Z':'text',
+                    'X':'text','P':'text','Z':'text','N':'numeric','M':'money',
                  'B':'boolean', 'D':'date', 'H':'time without time zone', 'DH':'timestamp without time zone',
                  'I':'integer', 'L':'bigint', 'R':'real',
                  'serial':'serial8','O':'bytea'}
