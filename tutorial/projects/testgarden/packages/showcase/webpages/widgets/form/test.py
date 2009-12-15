efc3ca6d353cab7d58fa078e40810d66d09477a8
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-12-15 19:49:54 +0000

    standard_tables refactoring:  parametric query
    git-svn-id: http://svn.genropy.org/genro/trunk@1303 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- tutorial/projects/testgarden/packages/showcase/webpages/widgets/form/test.py
+++ tutorial/projects/testgarden/packages/showcase/webpages/widgets/form/test.py
@@ -15,8 +15,8 @@ from gnr.web.gnrwebpage import GnrWebPage
 # --------------------------- GnrWebPage subclass ---------------------------
 class GnrCustomWebPage(object):    
     def main(self, root, **kwargs):
-        root.div('Hello assopy', font_size='40pt', 
-                        border='3px solid yellow', padding='20px')
-        root.button('aaa')
-def index(req, **kwargs):
-    return GnrWebPage(req, GnrCustomWebPage, __file__, **kwargs).index()
+        box = root.div(datapath='pluto')
+        box.textbox(value='^.pippo')
+        box.data('.pippo','antonio')
+        box.textbox(value='^.pippo?color')
+        
