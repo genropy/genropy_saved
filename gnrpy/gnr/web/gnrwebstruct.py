d6e4604b6211194818e91189b8fd064ddc38f6fa
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-12-20 08:52:37 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1339 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/web/gnrwebstruct.py
+++ gnrpy/gnr/web/gnrwebstruct.py
@@ -345,7 +345,7 @@ class GnrDomSrc_dojo_11(GnrDomSrc):
            #'Css','Script','Func','BagFilteringTable','DbTableFilter','TreeCheck']
     gnrNS=['DbSelect','DbComboBox','DbView','DbForm','DbQuery','DbField','dataFormula','dataScript','dataRpc', 'dataController',
            'dataRemote','gridView','viewHeader','viewRow','script','func',
-           'staticGrid','dynamicGrid','fileUploader','gridEditor']
+           'staticGrid','dynamicGrid','fileUploader','gridEditor','ckEditor']
     genroNameSpace=dict([(name.lower(),name) for name in htmlNS])
     genroNameSpace.update(dict([(name.lower(),name) for name in dijitNS]))
     genroNameSpace.update(dict([(name.lower(),name) for name in dojoxNS]))
