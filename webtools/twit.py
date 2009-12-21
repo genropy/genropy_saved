8f6e82bebf66315c0699196edfe52a67aedee174
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2009-12-21 18:12:38 +0000

    Twitter tool
    twitter lib required:
    sudo easy_install -U python-twitter
    
    git-svn-id: http://svn.genropy.org/genro/trunk@1358 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ webtools/twit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#  untitled
+#
+#  Created by Giovanni Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+# --------------------------- GnrBaseWebTool subclass ---------------------------
+
+
+from gnr.web.gnrbasewebtool import GnrBaseWebTool
+import twitter
+
+class Twitter(GnrBaseWebTool):
+    
+    def __call__(self,username=None, password=None,status=None):
+        twitter_api = twitter.Api(username=username,password=password)
+        self.content_type='application/text'
+        twitter_status=twitter_api.PostUpdate(status)
+        return twitter_status.text
+        
