3476799b5e2e21f95d46941ceb8aec7abfe37dc1
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-02 16:18:37 +0000

    a
    git-svn-id: http://svn.genropy.org/genro/trunk@1428 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ tutorial/projects/musiccd/packages/libcd/webpages/_resources/utility_music.py
@@ -0,0 +1,18 @@
+from gnr.web.gnrwebpage import BaseComponent
+from gnr.core.gnrbag import Bag
+class Common(BaseComponent):
+    def rpc_save(self,data,table,**kwargs):
+        tbl = self.db.table(table)
+        if data['id']:
+            tbl.update(data)
+        else:
+            tbl.insert(data)
+        self.db.commit()
+        return 'ok'
+    
+    def edit_record_dialog(self,root,title=None,height='400px',width='400px',
+                        nodeId=None,datapath=None):
+        dlg = root.dialog(title='Add Album',height='400px',width='400px',nodeId='album_dlg',datapath='album')
+        dlg.dataController("genro.wdgById('album_dlg').show()",_fired="^.show")
+        dlg.dataController("genro.wdgById('album_dlg').hide()",_fired="^.hide")
+        return dlg
--- tutorial/projects/musiccd/packages/libcd/webpages/artist.py
+++ tutorial/projects/musiccd/packages/libcd/webpages/artist.py
@@ -9,61 +9,53 @@
 
 # --------------------------- GnrWebPage subclass ---------------------------
 class GnrCustomWebPage(object):
+    py_requires='utility_music:Common'
     def windowTitle(self):
          return '!!Artist'
-
     def main(self, root, **kwargs):
         bc = root.borderContainer()
         top = bc.contentPane(region='left',width='50%',background='silver',
-                                splitter=True,datapath='record_artist')
+                                splitter=True,datapath='artist')
         center = bc.contentPane(region='center',background='black')
         self.formArtist(top)
         self.gridAlbum(center)
         self.formAlbum(root)
 
-    
     def formArtist(self,pane):
-        fb = pane.formbuilder(cols=1,border_spacing='4px')
+        fb = pane.formbuilder(cols=1,border_spacing='4px',datapath='.record')
         fb.dbSelect(value='^artist_id',dbtable='libcd.artist',lbl='Artist',hasDownArrow=True)
         fb.textbox(value='^.fullname',lbl='Name')
         fb.textbox(value='^.description',lbl='Description')
         fb.checkbox(value='^.is_band',label='Band')
         fb.button('Salva',fire='save_artist')
-        fb.button('Aggiungi disco',fire='add_album')
-        pane.dataController("genro.wdgById('album_dlg').show();",_fired="^add_album",
-                            _if='artist_id',artist_id='=artist_id')
-        
-        
-        pane.dataRecord('record_artist','libcd.artist',pkey='^artist_id',_onResult='FIRE record_loaded;')
+        fb.button('Aggiungi disco',action='SET album_id="*newrecord*"; FIRE #album_dlg.open')
+        pane.dataRecord('.record','libcd.artist',pkey='^artist_id',
+                        _onResult='FIRE record_loaded;',_onStart=True,
+                        ignoreMissing=True,default_is_band=False)
         pane.dataSelection('selection','libcd.album',where='$artist_id=:a_id',
                            a_id='=artist_id',_fired='^record_loaded')
-        pane.dataRpc('result','saveArtist',data='=record_artist',_fired='^save_artist')
+        pane.dataRpc('result','save',data='=.record',table='libcd.artist',_fired='^save_artist')
         pane.dataController("alert(msg)",msg="^result")
         
 
     def formAlbum(self,root):
-        dlg = root.dialog(title='Add Album',height='400px',width='400px',nodeId='album_dlg',datapath='record_album')
-        fb = dlg.formbuilder(cols=1,border_spacing='4px',dbtable='libcd.album')
-        fb.dataFormula("^.artist_id", "artist_id",artist_id="^artist_id")
+        dlg = self.edit_record_dialog(root,title='Add Album',nodeId='album_dlg',datapath='album')
+        dlg.dataController("FIRE .load; FIRE .show;",_fired="^.open")
+        dlg.dataRecord('.record','libcd.album',pkey='=album_id',_fired='^.load',
+                            default_artist_id='=artist_id',default_year=self.workdate.year,
+                            default_rating=3)
+        fb = dlg.formbuilder(cols=1,border_spacing='4px',dbtable='libcd.album',
+                            datapath='.record')
         fb.field('title') # fb.textbox(value='^.title',lbl='Title')
         fb.field('year') 
         fb.field('rating')
         fb.button('Salva',fire='save_album')
-        dlg.dataRpc('result','saveAlbum',data='=record_album',_fired='^save_album',
+        dlg.dataRpc('result','save',data='=.record',table='libcd.album',_fired='^save_album',
                     _onResult='FIRE record_loaded')
         
     def gridAlbum(self,pane):
-        pane.includedView(storepath='selection',table='libcd.album',columns='title:15,year:3,rating:3',
+        pane.includedView(storepath='selection',table='libcd.album',
+                        columns='title:15,year:3,rating:3', selectedId='album_id',
+                        connect_onRowDblClick="""FIRE #album_dlg.open;""",
                         autoWidth=True)
 
-    def rpc_saveArtist(self,data):
-        tblartisti = self.db.table('libcd.artist')
-        tblartisti.insert(data)
-        self.db.commit()
-        return 'ok'
-        
-    def rpc_saveAlbum(self,data):
-        tblalbum = self.db.table('libcd.album')
-        tblalbum.insert(data)
-        self.db.commit()
-        return 'ok'
