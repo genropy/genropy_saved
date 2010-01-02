9bbc93fef90060d7fc6ad71564db1d00e44bd74b
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-02 07:52:40 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1421 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- dojo_libs/dojo_14/dojo/dijit/_editor/nls/hu/LinkDialog.js
+++
@@ -1,1 +0,0 @@
-({"set":"Beállítás","text":"Leírás:","insertImageTitle":"Kép tulajdonságai","url":"URL:","createLinkTitle":"Hivatkozás tulajdonságai","newWindow":"New Window","topWindow":"Topmost Window","target":"Target:","parentWindow":"Parent Window","currentWindow":"Current Window"})
--- dojo_libs/dojo_14/dojo/dijit/_editor/nls/ru/LinkDialog.js
+++
@@ -1,1 +0,0 @@
-({"set":"Задать","text":"Описание:","insertImageTitle":"Свойства изображения","url":"URL:","createLinkTitle":"Свойства ссылки","newWindow":"New Window","topWindow":"Topmost Window","target":"Target:","parentWindow":"Parent Window","currentWindow":"Current Window"})
