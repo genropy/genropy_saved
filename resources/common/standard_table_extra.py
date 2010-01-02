af6a9211e5c7b190750993ef16bcc758ead6f537
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-02 20:14:48 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1433 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/standard_table_extra.py
+++ resources/common/standard_table_extra.py
@@ -25,7 +25,7 @@ class StatsHandler(BaseComponent):
         #fb.button('Run',fire='.tree.totalize')
         
     def stats_left(self,pane):
-        pane.tree(storepath='.root',inspect='shift',selectedPath='.currentTreePath',
+        pane.tree(storepath='.root.data',inspect='shift',selectedPath='.currentTreePath',
                  selectedItem='#_grid_total.data',isTree=True,margin='10px',_fired='^.reload_tree')
         pane.data('.root.data',Bag())
         pane.dataRpc('.root.data','stats_totalize',selectionName='=list.selectionName',
