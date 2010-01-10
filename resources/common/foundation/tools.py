08ebe2e2725e3753217d549c63b7f8830558405b
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-10 19:13:06 +0000

    remoteBuilder
    git-svn-id: http://svn.genropy.org/genro/trunk@1555 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ resources/common/foundation/tools.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+#--------------------------------------------------------------------------
+# Copyright (c) : 2004 - 2007 Softwell sas - Milano 
+# Written by    : Giovanni Porcari, Michele Bertoldi
+#                 Saverio Porcari, Francesco Porcari , Francesco Cavazzana
+#--------------------------------------------------------------------------
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#Lesser General Public License for more details.
+
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""
+Component for thermo:
+"""
+from gnr.web.gnrwebpage import BaseComponent
+
+class RemoteBuilder(BaseComponent):
+    def buildRemote(self,method,pane,**kwargs):    
+        """collocazione provvisoria"""
+        handler = getattr(self,'remote_%s' %method,None)
+        if handler:
+            parentAttr = pane.parentNode.getAttr()
+            parentAttr['remote'] = 'remoteBuilder'
+            parentAttr['remote_handler'] = method
+            for k,v in kwargs.items():
+                parentAttr['remote_%s' %k] = v
+                kwargs.pop(k)
+            handler(pane,**kwargs)
+        
+    def rpc_remoteBuilder(self,handler=None,**kwargs):
+        handler = getattr(self,'remote_%s' %handler,None)
+        if handler:
+            pane = self.newSourceRoot()
+            handler(pane,**kwargs)
+            return pane
