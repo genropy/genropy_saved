e3aefe05718be81eb137db6a623467cddbbd6778
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-10 13:43:02 +0000

    indexpublic
    git-svn-id: http://svn.genropy.org/genro/trunk@1548 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- projects/genropynet/packages/base/webpages/index.py
+++ projects/genropynet/packages/base/webpages/index.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
 #
-#  untitled
+#  Registrazione nuovo utente
 #
-#  Created by Giovanni Porcari on 2007-03-24.
-#  Copyright (c) 2007 Softwell. All rights reserved.
+#  Created by Francesco Cavazzana on 2008-01-23.
 #
 
+""" Registrazione nuovo utente """
+import datetime
+import os
+import hashlib
+
+from gnr.web.gnrwebpage import GnrWebPage
+from gnr.core.gnrbag import Bag
+# --------------------------- GnrWebPage subclass ---------------------------
 class GnrCustomWebPage(object):
-    py_requires='public:Public'
-    def pageAuthTags(self, method=None, **kwargs):
-        return ''
-        
-    def windowTitle(self):
-         return '!!Genropynet'
+    py_requires='publicsite:SiteLayout'
+    def main(self, root,**kwargs):
+        layout = root.borderContainer(_class='site_body')
+        layout.borderContainer(region='left',_class='site_left')
+        layout.borderContainer(region='right',_class='site_right')
+        header = layout.contentPane(region='top',_class='site_header')
+        footer = layout.contentPane(region='bottom',_class='site_footer')
+        self.site_header(header)
+        self.center(layout.contentPane(region='center',_class='site_center'))
+    
 
-    def main(self, root, **kwargs):
-        center, top, bottom = self.pbl_rootBorderContainer(root,
-                                                           title='gggggggggg')
-
-        tb=center.contentPane(region='top',height='30px',border_bottom='1px solid gray').toolbar()
-        center.contentPane(region='center').div('xxx')
-        menu=tb.dropdownbutton(label='Browse')
-        menu.menu(_class='browsemenu',action="function(attributes){genro.gotoURL(attributes.file)}",
-                      storepath='gnr.appmenu.root')
-      
+    def center(self,pane):
+        pane.div(_class='logobig')
+    
