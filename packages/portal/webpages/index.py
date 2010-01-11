972099404a036dd91f237630b5f801f40f23e698
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-11 08:50:41 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1559 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/portal/webpages/index.py
+++ packages/portal/webpages/index.py
@@ -13,26 +13,35 @@ from gnr.core.gnrbag import Bag
 class GnrCustomWebPage(object):
     py_requires='publicsite:SiteLayout,foundation/tools:RemoteBuilder'
     def main(self, root,**kwargs):
+        self.margin_default='7px'
         layout = root.borderContainer(_class='site_body')
         self.site_header(layout.contentPane(region='top',_class='site_header'))
-        self.site_footer(layout.contentPane(region='bottom',_class='site_footer'))
-        self.site_left(layout.borderContainer(region='left',_class='site_left site_pane'))
-        self.site_right(layout.borderContainer(region='right',_class='site_right site_pane'))
-        self.site_center(layout.contentPane(region='center',_class='site_pane'))
+        self.site_footer(layout.contentPane(region='bottom',_class='site_footer',height='15px'))
+        left=self.site_left(layout.borderContainer(region='left',width='25%'))
+
+        right=self.site_right(layout.borderContainer(region='right',width='25%'))
+        
+        self.site_center(layout.borderContainer(region='center'))
     
 
-    def site_center(self,pane):
-        pane.div(_class='logobig')
+    def site_center(self,bc):
+        bc.borderContainer(region='center',_class='site_pane',margin=self.margin_default)
+
         
     def site_footer(self,bc):
         pass
     def site_left(self,bc):
-        pass
+        
+        bc.borderContainer(region='top',_class='site_pane',margin=self.margin_default,height='60px')
+        bc.borderContainer(region='center',_class='site_pane',margin=self.margin_default)
+        
     def site_right(self,bc):
-        pane = bc.contentPane(region='top',height='100px')
-        fb = pane.formbuilder(cols=1,border_spacing='4px',margin_top='10px')
-        fb.textbox(value='^tabs',lbl='tabs',width='5em')        
-        self.buildRemote('tabContent',bc.tabContainer(region='center'),tabs='^tabs')
+        pane=bc.contentPane(region='top',_class='site_pane',margin=self.margin_default,height='60px')
+        bc.contentPane(region='bottom',_class='site_pane',margin=self.margin_default,height='60px')
+        center=bc.contentPane(region='center',_class='site_pane',margin=self.margin_default)
+        fb = pane.formbuilder(cols=1,border_spacing='4px',margin=self.margin_default)
+        fb.textbox(value='^tabs',lbl='Tabs',width='16em')        
+        self.buildRemote('tabContent',center.tabContainer(region='center',margin='8px'),tabs='^tabs')
 
     def remote_tabContent(self,tc,tabs='pippo,pluto'):
         tabs=tabs.split(',')
