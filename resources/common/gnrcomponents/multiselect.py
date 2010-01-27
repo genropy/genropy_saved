ff7308a038042d08b54c8ebd278bc6c21d9d89c7
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-27 14:22:54 +0000

    components
    git-svn-id: http://svn.genropy.org/genro/trunk@1752 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/gnrcomponents/multiselect.py
+++ resources/common/gnrcomponents/multiselect.py
@@ -19,21 +19,43 @@ import os
 
 class MultiSelect(BaseComponent):
     py_requires='foundation/includedview:IncludedView'
-    def multiSelect(self,bc,nodeId=None,table=None,datapath=None,struct=None,label=None,values=None,
+    js_requires='gnrcomponents/multiselect'
+    def multiSelect(self,bc,nodeId=None,table=None,datapath=None,struct=None,label=None,values=None,readColumns=None,
                              reloader=None,filterOn=None,hiddencolumns=None,selectionPars=None,order_by=None,
-                             showSelected=False,
-                             **kwargs):
+                             showSelected=False, readCol=None,**kwargs):
         assert not 'footer' in kwargs, 'remove footer par'
 
         assert struct, 'struct is mandatory'
         if callable(struct):
             struct = struct(self.newGridStruct(table))
+        values= values or '=.checkedList'           
+        readCol = readCol or '_pkey' 
         struct['#0']['#0'].cell('_checkedrow',name=' ',width='2em',
                     format_trueclass='checkboxOn',
                     styles='background:none!important;border:0;',
                     format_falseclass='checkboxOff',classes='row_checker',
-                    format_onclick='IVSelectionSearchComponent.check_row(kw.rowIndex, e, this,"tags");',
-                    dtype='B',calculated=True)
+                    #format_onclick='MultiSelectComponent.toggle_row(kw.rowIndex, e, this,"%s");' %values, 
+                    format_onclick="""var readCol = '%s';
+                                      var result = [];
+                                      var idx = kw.rowIndex;
+                                      var nodes = this.widget.storebag().getNodes();
+                                      var row = nodes[idx];
+                                      var rowattrs = row.getAttr();
+                                      rowattrs['_checkedrow'] = !rowattrs['_checkedrow'];
+                                      row.setAttr(rowattrs);
+                                      var cb = function(n){
+                                                        if(n.attr._checkedrow){
+                                                            if(readCol=='*'){
+                                                                result.push(n.attr);
+                                                            }else{
+                                                                result.push(n.attr[readCol]);
+                                                            }
+                                                        }
+                                                     };
+                                      dojo.forEach(nodes,cb);
+                                      this.setRelativeData('%s',result);
+                                      """ %(readCol,values),
+                    dtype='B',calculated=True,_pos=0)
         if not selectionPars:
             if order_by:
                 selectionPars=dict(order_by=order_by) 
@@ -43,18 +65,38 @@ class MultiSelect(BaseComponent):
                          hiddencolumns=hiddencolumns,reloader=reloader,autoWidth=True)
         
         checkboxGridBC = bc
+        checkboxGridDatapath = datapath
         if showSelected:
             footer = bc.contentPane(region='bottom',_class='pbl_roundedGroupBottom')
             footer.button('Back',action='SET %s.selectedGrid=0;' %datapath)
             footer.button('Next',action='FIRE #%s_result.reload; SET %s.selectedGrid=1;' %(nodeId,datapath))
-            checked= values or '=.#parent.checkedList'
             stack = bc.stackContainer(region='center',datapath=datapath,selected='^.selectedGrid')
             checkboxGridBC = stack.borderContainer()
-        self.includedViewBox(checkboxGridBC,datapath='.gridcheck',
+            checkboxGridDatapath = '.gridcheck'
+        
+        if 'applymethod' in selectionPars:
+            selectionPars['apply_callAfter'] = selectionPars['applymethod']
+        selectionPars['apply_checkedRows'] = '=%s' %values
+        selectionPars['applymethod'] = 'ms_setCheckedOnReload'
+        self.includedViewBox(checkboxGridBC,datapath=checkboxGridDatapath,
                             selectionPars=selectionPars,nodeId=nodeId,**viewpars)
-        selectionPars_result = dict(where='$%s IN :checked' %self.db.table(table).pkey,
-                                    checked=checked,_if='checked')
+        if showSelected:
+            selectionPars_result = dict(where='$%s IN :checked' %self.db.table(table).pkey,
+                                    checked=values,_if='checked')
 
-        self.includedViewBox(stack.borderContainer(_class='hide_row_checker'),nodeId='%s_result' %nodeId,
-                             datapath='.resultgrid',selectionPars=selectionPars_result,
-                             **viewpars)
+            self.includedViewBox(stack.borderContainer(_class='hide_row_checker'),nodeId='%s_result' %nodeId,
+                                    datapath='.resultgrid',selectionPars=selectionPars_result,
+                                    **viewpars)
+                                    
+    def rpc_ms_setCheckedOnReload(self,selection,checkedRows=None,callAfter=None,**kwargs):
+        def cb(row):
+            result = dict(_checkedrow=False)
+            if row['pkey'] in checkedRows:
+                result['_checkedrow'] = True
+            return result
+        selection.apply(cb)
+        if callAfter:
+            callAfter = getattr(self,'rpc_%s' %callAfter,None)
+            if callAfter:
+                callAfter(selection,**kwargs)
+        
