fbfd2db797d744df03473cf7be329a812114228f
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-01-31 19:51:10 +0000

    added a trigger to create the md5pass if the md5pass sent is less then 32 chars.
    git-svn-id: http://svn.genropy.org/genro/trunk@1804 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/model/user.py
+++ packages/adm/model/user.py
@@ -10,7 +10,7 @@ class Table(object):
         tbl =  pkg.table('user',  pkey='id', name_long='!!User', rowcaption='username,email:%s (%s)')
         self.sysFields(tbl,ins=True, upd=True, md5=True)
         tbl.column('id',size='22',group='_',readOnly='y',name_long='Id')
-        tbl.column('username', size=':32',name_long='!!Username', unique='y', 
+        tbl.column('username', size=':32',name_long='!!Username', unique='y', _sendback=True,
                     indexed='y',validate_notnull=True,validate_notnull_error='!!Mandatory field')
         tbl.column('email',name_long='Email',validate_notnull=True,
                     validate_notnull_error='!!Mandatory field')
@@ -32,6 +32,18 @@ class Table(object):
     def createPassword(self):
         password=getUuid()[0:6]
         return password
+        
+    def trigger_onUpdating(self, record,**kwargs):
+        self.passwordTrigger(record)
+        
+    def trigger_onInserting(self, record,**kwargs):
+        self.passwordTrigger(record)
+        
+    def passwordTrigger(self,record):
+        password =record['md5pwd']
+        if len(password)<32:
+            record['md5pwd']=self.db.application.changePassword(None, None, password, userid=record['username'])
+            
         
     def populate(self, fromDump=None):
         if fromDump:
