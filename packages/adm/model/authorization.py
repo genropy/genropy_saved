976cf3484669ec24cb310af261ca6dc8320c6049
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-02-03 17:57:56 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1840 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/model/authorization.py
+++ packages/adm/model/authorization.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 import random
 import string
-
+from datetime import datetime
 class Table(object):
     def config_db(self, pkg):
         tbl =  pkg.table('authorization',  pkey='code',name_long='!!Authorization',
@@ -10,7 +10,8 @@ class Table(object):
         tbl.column('code', size='8',validate_case='U', name_long='!!Code')
         tbl.column('user_id',size='22',name_long='!!Event').relation('user.id',mode='foreignkey')
         tbl.column('auth_tag',size=':10',name_long='!!Level')
-        tbl.column('redeemed','B',name_long='!!Redeemed')
+        tbl.column('use_datetime','DH',name_long='!!Used datetime')
+        tbl.column('used_by',size=':32',name_long='!!Used by')
         tbl.column('note',name_long='!!Note')     
         
     def generate_code(self):
@@ -19,16 +20,17 @@ class Table(object):
             code = code.replace(c[0],c[1])
         return code
                 
-    def use_auth(self,code):
+    def use_auth(self,code,username):
         record = self.record(pkey=code,for_update=True).output('bag')
-        record['redeemed'] = True
+        record['use_datetime'] =  datetime.now()
+        record['used_by'] =  username
         self.update(record)
     
-    def get_auth(self,code):
+    def check_auth(self,code):
         code = code.upper()
         coupon = self.db.record(pkey=code)
         if not coupon:
-            return None
-        if coupon['redeemed']:
-            return 'USED'
-        return coupon
+            return False
+        if coupon['use_datetime']:
+            return False
+        return True
