7bf21dfa1874a9c3d1bf333ffd5d86927fb1298a
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-02-09 10:54:01 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@1885 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/web/gnrwebpage.py
+++ gnrpy/gnr/web/gnrwebpage.py
@@ -164,7 +164,7 @@ class GnrWebPage(GnrBaseWebPage):
     def _get_db(self):
         if not hasattr(self, '_db'):
             self._db = self.app.db
-            self._db.currentEnv = dict(storename = self.storename)
+            self._db.currentEnv = dict(storename = getattr(self,'storename', None)
             # self._db.set_env(storename= self.storename, **kwargs)
         return self._db
     db = property(_get_db)
