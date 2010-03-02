35e67abd822aaffe17e51e71e9832c245f79e706
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-03-02 10:41:43 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2042 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrjs/gnr_d11/js/gnrstores.js
+++ gnrjs/gnr_d11/js/gnrstores.js
@@ -563,7 +563,7 @@ dojo.declare("gnr.GnrStoreQuery",gnr.GnrStoreBag,{
             var finalize=dojo.hitch(this,function(r){
                 var result;
                 var scope =  request.scope?request.scope:dojo.global;
-                result=r.getValue();
+                result=result.getValue();
                 if (result instanceof gnr.GnrBag){
                      result=result.getNode('#0');
                 }else{
