d4141531adf57236321d7e7c993e3d6e8516aa82
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-03-02 20:07:35 +0000

    operator between
    git-svn-id: http://svn.genropy.org/genro/trunk@2047 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/standard_tables/tablehandler_core.py
+++ resources/common/standard_tables/tablehandler_core.py
@@ -234,7 +234,7 @@ class LstQueryHandler(BaseComponent):
                     'less','lesseq','between','isnull','istrue','isfalse','nullorempty','in','regex')
         optype_dict = dict(alpha=['contains','startswith','equal','wordstart',
                             'startswithchars','isnull','nullorempty','in','regex',
-                            'greater','greatereq','less','lesseq'],
+                            'greater','greatereq','less','lesseq','between'],
                       date=['equal','in','isnull','greater','greatereq','less','lesseq','between'],
                       number=['equal','greater','greatereq','less','lesseq','isnull','in'],
                       boolean=['istrue','isfalse','isnull'],
