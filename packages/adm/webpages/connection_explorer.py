b03268cf6a141817607ca87f38de14ce57c464a3
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-03-11 14:01:01 +0000

    changes inside
    git-svn-id: http://svn.genropy.org/genro/trunk@2105 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/webpages/connection_explorer.py
+++ packages/adm/webpages/connection_explorer.py
@@ -21,7 +21,74 @@ class GnrCustomWebPage(object):
         
     def main(self, rootBC, **kwargs):
         rootBC.data('gnr.polling',3)
-        bc,top,bottom = self.pbl_rootBorderContainer(rootBC,'!!Connections')
+        tc,top,bottom = self.pbl_rootTabContainer(rootBC,'!!Connections')
+        self.overviewPane(tc.borderContainer(title='!!Overview',datapath='overviewPane'))
+        self.userPane(tc.borderContainer(title='!!Users',datapath='userPane'))
+        self.connectionPane(tc.borderContainer(title='!!Connections',datapath='connectionPane'))
+        self.pagePane(tc.borderContainer(title='!!Pages',datapath='pagesPane'))
+        
+    def overviewPane(self,bc):
+        bc.dataRpc(".total_connection", "countConnections",_fired='^gnr.dbevent.adm_connection',_onStart=True)
+        bc.dataRpc(".total_served_page", "countServedPages",_fired='^gnr.dbevent.adm_served_page',_onStart=True)
+
+        pane = bc.contentPane(region='center')
+        fb = pane.formbuilder(cols=1, border_spacing='4px')
+        fb.div('^.total_connection',lbl='!!Total active connections')
+        fb.div('^.total_served_page',lbl='!!Total active served pages')
+
+        
+    def rpc_countConnections(self):
+        return self.db.table('adm.connection').query(where='$end_ts IS NULL').count()
+        
+    def rpc_countServedPages(self):
+        return self.db.table('adm.served_page').query(where='$end_ts IS NULL').count()
+        
+    def connectionPane(self,bc):
+        def label(pane,**kwargs):
+            pane.checkbox(value='^.activeOnly',label='!!Only active connections')
+        bc.dataFormula(".connections.activeOnly", True,_onStart=True)
+        self.includedViewBox(bc,label=label,datapath='.connections',
+                             nodeId='connectionMainGrid',table='adm.connection',autoWidth=True,
+                             struct=self.connectionMainGrid_struct,filterOn='!!User:username,End reason:end_reason',
+                             reloader='^.activeOnly', externalChanges=True,tools_menu=self.toolsMenu(bc,'connectionMainGrid'),
+                             selectionPars=dict(where='($end_ts IS NULL AND :activeOnly) OR ($end_ts IS NOT NULL AND NOT :activeOnly)',
+                             activeOnly='=.activeOnly',order_by='$start_ts DESC'))
+                             
+                             
+    def connectionMainGrid_struct(self,struct):
+        r = struct.view().rows()
+        r.fieldcell('username', name='User', width='12em')
+        r.fieldcell('start_ts')
+        r.fieldcell('end_ts')
+        r.fieldcell('end_reason')
+        r.fieldcell('ip')
+        return struct
+        
+        
+    def pagePane(self,bc):
+        def label(pane,**kwargs):
+            pane.checkbox(value='^.activeOnly',label='!!Only active served pages')
+        bc.dataFormula(".connections.activeOnly", True,_onStart=True)
+        self.includedViewBox(bc,label=label,datapath='.connections',tools_menu=self.toolsMenu(bc,'pagesMainGrid'),
+                             nodeId='pagesMainGrid',table='adm.served_page',autoWidth=True,
+                             struct=self.servedPages_struct,filterOn='Username:@connection_id.username,Page:pagename,End reason:end_reason',
+                             reloader='^.activeOnly', externalChanges=True,
+                             selectionPars=dict(where='($end_ts IS NULL AND :activeOnly) OR ($end_ts IS NOT NULL AND NOT :activeOnly)',
+                             activeOnly='=.activeOnly',order_by='$start_ts DESC'))
+                             
+    def servedPages_struct(self,struct):
+        r = struct.view().rows()
+        r.cell('page_id',name='Served Page ID',width='20em')
+        r.cell('@connection_id.username',name='Username',width='15em')
+        r.cell('pagename',name='Page name',width='15em')
+        r.cell('start_ts',name='Start',dtype='DH',width='10em')
+        r.cell('end_ts',name='End',dtype='DH',width='10em')
+        r.cell('end_reason',name='End reason',width='10em')
+        r.cell('subscribed_tables',name='Subscribed table',width='20em')
+        return struct       
+                
+        
+    def userPane(self,bc):
         topBC = bc.borderContainer(region='top',height='50%')
         self.connectedUser(topBC.borderContainer(region='left',width='50%',margin='5px'))
         self.userConnections(topBC.borderContainer(region='center',margin_left=0,margin='5px'))
@@ -29,16 +96,16 @@ class GnrCustomWebPage(object):
         self.userServedPages(centerBC)
 
     def connectedUser(self,bc):
-        topfb=bc.contentPane(region='top',height='40px', datapath='messages.user').formBuilder(cols='3')
-        topfb.textBox(value='^.text',lbl='User message',width='15em')
-        topfb.button('send',lbl='',fire='.send')
-        topfb.button('refresh',lbl='',fire="users.reload")
-        topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', 
-                      dest_user='=users.selectedId?username')
-        bc=bc.borderContainer(region='center')
+       #topfb=bc.contentPane(region='top',height='40px', datapath='messages.user').formBuilder(cols='3')
+       #topfb.textBox(value='^.text',lbl='User message',width='15em')
+       #topfb.button('send',lbl='',fire='.send')
+       #topfb.button('refresh',lbl='',fire="users.reload")
+       #topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', 
+       #              dest_user='=users.selectedId?username')
+       #bc=bc.borderContainer(region='center')
         self.includedViewBox(bc,table='adm.connection',
-                            nodeId='connected_users',
-                            datapath='users',
+                            nodeId='connected_users',tools_menu=self.toolsMenu(bc,'connected_users'),
+                            datapath='.users',
                             label='!!Live user connected',
                             struct=self._connectedUser_struct, 
                             autoWidth=True,autoSelect=True,
@@ -62,19 +129,26 @@ class GnrCustomWebPage(object):
         return struct
         
     def userConnections(self,bc):
-        topfb=bc.contentPane(region='top',height='40px',datapath='messages.connection').formBuilder(cols='3')
-        topfb.textBox(value='^.text',lbl='Connection message',width='15em')
-        topfb.button('send',lbl='',fire='.send')
-        topfb.button('refresh',lbl='',fire="connections.reload")
-        topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', dest_connection='=connection.current')
-        bc=bc.borderContainer(region='center')
-        self.includedViewBox(bc,nodeId='user_connections',datapath='connections',table='adm.connection',
-                            struct=self._userConnections_struct, 
+       #topfb=bc.contentPane(region='top',height='40px',datapath='messages.connection').formBuilder(cols='3')
+       #topfb.textBox(value='^.text',lbl='Connection message',width='15em')
+       #topfb.button('send',lbl='',fire='.send')
+       #topfb.button('refresh',lbl='',fire="connections.reload")
+       #topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', dest_connection='=connection.current')
+       #bc=bc.borderContainer(region='center')
+        
+        self.includedViewBox(bc,nodeId='user_connections',datapath='.connections',table='adm.connection',
+                            struct=self._userConnections_struct, tools_menu=self.toolsMenu(bc,'user_connections'),
                             autoWidth=True,label='!!User connections',
-                            reloader='^users.selectedId',autoSelect=True,
+                            reloader='^userPane.users.selectedId',autoSelect=True,
                             selectionPars=dict(where='$end_ts IS NULL AND $username=:user AND $ip=:ip',
-                                                user='=users.selectedId?username',
-                                                ip='=users.selectedId?ip'))
+                                                user='=userPane.users.selectedId?username',
+                                                ip='=userPane.users.selectedId?ip'))
+    def toolsMenu(self,pane,gridId):
+        tools_menu = Bag()
+        tools_menu.setItem('reload',None,caption='Reload',action='FIRE .reload')
+        tools_menu.setItem('send_message',None,caption='Send Message',action='FIRE .send_message')
+        pane.data('#%s.tools_menu' %gridId,tools_menu)
+        return '.tools_menu'
 
     def _userConnections_struct(self,struct):
         r = struct.view().rows()
@@ -83,22 +157,23 @@ class GnrCustomWebPage(object):
         return struct
         
     def userServedPages(self,bc):
-        topfb=bc.contentPane(region='top',height='40px', datapath='messages.pages').formBuilder(cols='3')
-        topfb.textBox(value='^.text',lbl='Page message',width='30em')
-        topfb.button('send',lbl='',fire='.send')
-        topfb.button('refresh',lbl='',fire="pages.reload")
+        #topfb=bc.contentPane(region='top',height='40px', datapath='messages.pages').formBuilder(cols='3')
+        #topfb.textBox(value='^.text',lbl='Page message',width='30em')
+        #topfb.button('send',lbl='',fire='.send')
+        #topfb.button('refresh',lbl='',fire="pages.reload")
+        #
+        #topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', dest_page='=pages.selectedId')
+        #bc=bc.borderContainer(region='center')
+        #bc.dataController("console.log(selectedId)",selectedId="^connections.selectedId")
         
-        topfb.dataRpc('.result','sendMessage',_fired='^.send', msg='=.text', dest_page='=pages.selectedId')
-        bc=bc.borderContainer(region='center')
-        bc.dataController("console.log(selectedId)",selectedId="^connections.selectedId")
         self.includedViewBox(bc,label='!!User served pages',table='adm.served_page', 
                             struct=self._userServedPages_struct, autoWidth=True,
-                           nodeId='user_servedpages',autoSelect=True,
-                           datapath='pages',
-                           externalChanges='connection_id=connections.selectedId:UID',
-                           reloader='^connections.selectedId',
+                           nodeId='user_servedpages',autoSelect=True,tools_menu=self.toolsMenu(bc,'user_servedpages'),
+                           datapath='.pages',
+                           externalChanges='connection_id=userPane.connections.selectedId:UID',
+                           reloader='^userPane.connections.selectedId',
                            selectionPars=dict(where='$connection_id=:connection_id AND $end_ts IS NULL',
-                                                connection_id='=connections.selectedId',
+                                                connection_id='=userPane.connections.selectedId',
                                                 order_by='$start_ts desc',_if='connection_id'))
                            
 
