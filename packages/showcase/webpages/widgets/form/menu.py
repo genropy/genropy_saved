f3572f7e8bdb5e02dc27499fde533ed3980c55e5
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-03-14 12:09:44 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2129 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/menu.py
+++ packages/showcase/webpages/widgets/form/menu.py
@@ -18,7 +18,7 @@ class GnrCustomWebPage(object):
         root.data('values.states', self.tableData_states())
         fb = root.formbuilder(cols=1, border_spacing='4px')
         x = fb.div(height='30px',width='50px',background='lime')
-        x.menu(storepath='values.states',action='alert($1.caption);')
+        x.menu(modifiers='*',storepath='values.states',action='alert($1.caption);')
         fb.button('addItem to menu',action='genro.setData("values.states.r6",null,{caption:"Washington",id:"WA"})')
         fb.button('delItem to menu',action='genro._data.pop("values.states.r6");')
         
