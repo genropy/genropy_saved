188acc6b8428de5903c5bc79e1261b49a9c96240
pcamera <pcamera@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-04-22 07:54:30 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2312 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/rss/model/feed.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+from gnr.core.gnrbag import Bag
+class Table(object):
+    def config_db(self, pkg):
+        tbl =  pkg.table('feed',pkey='id',name_long='!!Feed Rss')
+        self.sysFields(tbl) 
+        tbl.column('topic',name_long='!!Topic',validate_notnull=True,
+                    validate_notnull_error='!!Required',validate_case='c'
+        )   
+        tbl.column('title',name_long='!!Title',validate_notnull=True,validate_notnull_error='!!Required',validate_case='c'
+        )    
+        tbl.column('description',name_long='!!Description')
+        tbl.column('url',name_long='!!Address',validate_notnull=True,validate_notnull_error='!!Required')
+        tbl.column('username',name_long='!!User').relation('adm.user.username',mode='foreignkey',onDelete='cascade')
+
+        
+    def getFeeds(self,user=None):
+        b = self.query(where='$username=:user',user=user).selection().totalize(group_by=['topic','title'],keep=['id','url'],key='id')     
+        def epuratore(node):
+            if node.value:
+                node.attr=None
+        b.walk(epuratore)
+        return b   
+            
+            
+
