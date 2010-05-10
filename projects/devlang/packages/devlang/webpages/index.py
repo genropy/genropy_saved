939da6a2b1ac4f44eed6ab84a013e4f73ebd71aa
jeffedwa <jeffedwa@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-10 20:49:07 +0000

    devlang
    git-svn-id: http://svn.genropy.org/genro/trunk@2431 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ projects/devlang/packages/devlang/webpages/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#  untitled
+#
+#  Created by Giovanni Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+""" index.py """
+
+# --------------------------- GnrWebPage subclass ---------------------------
+class GnrCustomWebPage(object):
+    py_requires = 'public:Public,public:StandardIndex'
+    
+    def windowTitle(self):
+         return '!!dev App'
+
+
+    def main(self, root, **kwargs):
+        center, top, bottom = self.pbl_rootBorderContainer(root, '',datapath='form')
+        center.div('Developer Information', align='center', margin_top='20px')
