5b524b5961d1da12b7d4b6319322f1a713c650c6
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-11 08:23:55 +0000

    remove from portal imps solving demo
    git-svn-id: http://svn.genropy.org/genro/trunk@2437 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/portal/model/procedura.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-class Table(object):
-    def config_db(self, pkg):
-        tbl =  pkg.table('procedura',pkey='codice',name_long='!!Procedura',
-                      name_plural='!!Procedura',rowcaption='$descrizione')
-        self.sysFields(tbl,id=False)
-        tbl.column('codice',name_long='!!Codice')
-        tbl.column('descrizione',name_long='!!Descrizione')        
