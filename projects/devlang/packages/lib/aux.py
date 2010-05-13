679eb834ca74d637cba19669409a3712ba4cd3ad
jeffedwa <jeffedwa@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-13 21:05:15 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2464 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- projects/devlang/packages/lib/aux.py
+++ projects/devlang/packages/lib/aux.py
@@ -26,8 +26,9 @@ def test():
     for k, v in lang_dict.items():
         x = dict(v)
         print '%s : %s' %(k,x)
-    
-    
+
+
+
 def populateDevelopers():
     app=GnrApp('devlang') # create the app starting from the instance config
     db=app.db # app.db is the instance of the db
@@ -35,7 +36,15 @@ def populateDevelopers():
     tbl_devlang = db.table('devlang.dev_lang')
     tbl_lang = db.table('devlang.language')
     lang_dict = tbl_lang.query(columns='name').fetchAsDict(key='name')
-    
+
+    def populatedevlang(frmSp,frmDb):
+        if  temprecord[frmSp]:
+            record_devlang = Bag()
+            record_dev['id'] = tbl_devlang.newPkeyValue()
+            record_devlang['developer_id'] = developer_id
+            record_devlang['language_id'] = dict(lang_dict[frmDb])['pkey']
+            record_devlang['level'] = int(temprecord[frmSp])
+            tbl_devlang.insert(record_devlang)
 
     print "... developer import"
 
@@ -61,70 +70,14 @@ def populateDevelopers():
         tbl_developer.insert(record_dev)
         
         # python	java	javascript	csharp	cplus	objectivec	smalltalk	html
-        if  temprecord['java']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['Java'])['pkey']
-            record_devlang['level'] = int(temprecord['java'])
-            tbl_devlang.insert(record_devlang)
-        
-        if  temprecord['python']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['Python'])['pkey']
-            record_devlang['level'] = int(temprecord['python'])
-            tbl_devlang.insert(record_devlang)
-
-        if  temprecord['javascript']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['Javascript'])['pkey']
-            record_devlang['level'] = int(temprecord['javascript'])
-            tbl_devlang.insert(record_devlang)
-
-        if  temprecord['cplus']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['C++'])['pkey']
-            record_devlang['level'] = int(temprecord['cplus'])
-            tbl_devlang.insert(record_devlang)
-
-        if  temprecord['objectivec']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['Smalltalk'])['pkey']
-            record_devlang['level'] = int(temprecord['objectivec'])
-            tbl_devlang.insert(record_devlang)
-
-        if  temprecord['html']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['C#'])['pkey']
-            record_devlang['level'] = int(temprecord['html'])
-            tbl_devlang.insert(record_devlang)
-
-        if  temprecord['smalltalk']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['Objective C'])['pkey']
-            record_devlang['level'] = int(temprecord['smalltalk'])
-            tbl_devlang.insert(record_devlang)
-
-        if  temprecord['csharp']:
-            record_devlang = Bag()
-            record_dev['id'] = tbl_devlang.newPkeyValue()
-            record_devlang['developer_id'] = developer_id
-            record_devlang['language_id'] = dict(lang_dict['C#'])['pkey']
-            record_devlang['level'] = int(temprecord['csharp'])
-            tbl_devlang.insert(record_devlang)
-
+        populatedevlang('java','Java')
+        populatedevlang('python','Python')
+        populatedevlang('javascript','Javascript')
+        populatedevlang('cplus','C++')
+        populatedevlang('objectivec','Objective C')
+        populatedevlang('html','html')
+        populatedevlang('smalltalk','Smalltalk')
+        populatedevlang('csharp','C#')
 
     db.commit()
     print "... developer import complete"
