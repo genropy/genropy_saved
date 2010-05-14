f61f9a0ad28e92a72ea3c5f78b295a0e940fa31e
jeffedwa <jeffedwa@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-14 21:25:10 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2474 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- projects/devlang/packages/devlang/main.py
+++ projects/devlang/packages/devlang/main.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter, Table_userobject,Table_recordtag,Table_recordtag_link
+import os
+from gnr.core.gnrbag import Bag
+from gnr.app.gnrdbo import GnrDboTable,GnrDboPackage, Table_counter,Table_userobject,Table_recordtag,Table_recordtag_link
+from gnr.core.gnrstring import templateReplace,splitAndStrip
 
 class Package(object):
     def config_attributes(self):
-        return dict(comment='devlang package',sqlschema='devlang',
-                name_short='Devlang', name_long='Developers languages', name_full='Demo: Developers languages')
+        return dict(comment='devlang package',
+                    sqlschema='devlang',
+                    name_short='Devlang', 
+                    name_long='Developers languages', 
+                    name_full='Demo: Developers languages')
+
     def config_db(self, pkg):
         pass
 
     def loginUrl(self):
         return 'devlang/login'
+
+    def newUserUrl(self):
+        return 'adm/new_user.py'
+
+    def modifyUserUrl(self):
+        return 'adm/modify_user.py'
 
 class Table(GnrDboTable):
     pass
