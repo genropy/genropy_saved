1c051acd8b781d19d071b2f7d1b73da85b4abb29
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-19 10:47:17 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2515 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/showcase/webpages/dev/touch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#  untitled
+#
+#  Created by Giovanni Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+from gnr.core.gnrbag import Bag
+class GnrCustomWebPage(object):
+    def pageAuthTags(self, method=None, **kwargs):
+        return ''
+        
+    def windowTitle(self):
+         return '!! touch test'
+
+    def main(self, root, **kwargs):
+        #root.tree(storepath='gnr',margin_top='30px',margin_left='30px')
+        fb=root.formbuilder(cols=1,margin_top='30px',margin_left='30px')
+        fb.div('^gnr.touch.orientation',lbl='Orientation')
+        fb.div('^gnr.touch.gesture.scale',lbl='Gesture Scale')
+        fb.div('^gnr.touch.gesture.rotation',lbl='Gesture rotation')
