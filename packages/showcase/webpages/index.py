08e651207118c3098d5978c401c7a7ac1a7d5bde
pcamera <pcamera@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-24 09:36:13 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2558 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/index.py
+++ packages/showcase/webpages/index.py
@@ -32,9 +32,9 @@ class GnrCustomWebPage(object):
         
     def main(self,rootBC,**kwargs):
         self.pageController(rootBC)
-        self.editorDialog(rootBC)
         self.left_menu(rootBC.contentPane(region='left',width='230px',splitter=True,_class='leftpane'))
-        self.top(rootBC.contentPane(region='top',height='20px',_class='header',padding='5px'))
+        self.top(rootBC.borderContainer(region='top',height='30px',splitter=False,background_color='silver'))
+        #self.top(rootBC.contentPane(region='top',height='30px',_class='header',padding='5px'))
         center = rootBC.borderContainer(region='center')
         buttons = center.contentPane(region='bottom',height='30px',_class='centerfooter').div(position='absolute',right='15px',top='2px')
         buttons.button('Page',baseClass='indexbutton',action='SET stack.selected=0')
@@ -43,9 +43,32 @@ class GnrCustomWebPage(object):
         sc = center.stackContainer(region='center',selected='^stack.selected')
         sc.contentPane(overflow='hidden').iframe(height='100%',width='100%',border='0',src='^iframe.selected_page')
         sc.contentPane(overflow='auto',background_color='white').div(value='^demo.current.source')
-        self.docPane(sc.borderContainer(region='center'))
+        self.docPane(sc.contentPane(overflow='auto',background_color='white'))
+        self.docPaneEdit(sc.borderContainer(region='center'))
+
+    def left_menu(self,pane):
+        pane.data('menubag',self.diskDirectory())
+        pane.tree(storepath='menubag',hideValues=True,inspect='shift',labelAttribute='name',isTree=False,
+                    selected_path='tree.current_path')
+        pane.dataFormula("iframe.selected_page", "current_path", current_path="^tree.current_path",_if='current_path')
+
+    def top(self,bc):
+        leftpane = bc.contentPane(overflow='hidden',region='left',style='font-size:20px;')
+        leftpane.span("TestGarden > ")
+        leftpane.span().a('^demo.current.relpath',href='^demo.current.relpath',color='#dfcfa4')
+        loginstack='login'
+        if self.user:
+            loginstack='logout'
+        rightpane = bc.stackContainer(region='right',selectedPage='^loginstack')
+        rightpane.dataFormula('loginstack','"%s"' %loginstack,_onStart=True)
+        self.loginbox(rightpane.contentPane(pageName='login',datapath='login',width="600px"))
+        self.logoutbox(rightpane.contentPane(pageName='logout'))
         
-    def docPane(self,bc):
+    def docPane(self,pane):
+        pane.div(value='^demo.current.docdata')
+
+    def docPaneEdit(self,bc):
+        top=bc.contentPane(height='100%',region='top',splitter=False)
         toolbar="""[
                    ['Source','-','Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'],
                    ['Image','Table','HorizontalRule','PageBreak'],
@@ -54,11 +77,32 @@ class GnrCustomWebPage(object):
                    ['Styles','Format','Font','FontSize'],
                    ['TextColor','BGColor'],['Maximize', 'ShowBlocks']
                    ]"""
-        
-        top=bc.contentPane(height='100%',region='top',splitter=False)
         top.ckeditor(value='^demo.current.docdata',nodeId='editor',config_toolbar='Basic',
-        config_uiColor= '#9AB8F3', toolbar=toolbar,height='700px')
-        top.button('Save',fire='save.doc')
+        config_uiColor= '#D1DBE4', toolbar=toolbar,height='700px')
+        top.button('Save',action="FIRE pippo.doc;")
+
+    def loadRecord(self,kwargs):
+        print x
+        
+    def loginbox(self,pane):
+        pane.dataRpc('.result', 'doLogin', login='=.form', btn='^.enter',_onResult='FIRE .afterLogin')
+        pane.dataController("genro.dom.effect('bottomMsg','fadeout',{duration:3000,delay:3000});", 
+                          msg='^error_msg',_if='msg')
+        pane.dataController("SET loginstack='logout';SET stack.selected=3;" , message='=.result.message',
+                            _if="message==''", _else="FIRE .error_msg = badUserMsg; SET .form = null;",
+                             badUserMsg="!!Incorrect Login",_fired='^.afterLogin')
+        fb = pane.formbuilder(cols=4,border_spacing='4px',_class='login',
+                            onEnter='FIRE .enter',datapath='.form',style="float:right;")
+        fb.textbox(value='^.user',ghost='User',width='10em')
+        fb.textbox(value='^.password',ghost='Password',lbl_width='1em',type='password',
+                 width='10em')
+        fb.button('!!Login',baseClass='loginbutton',fire='login.enter')
+        pane.span('^.error_msg',nodeId='bottomMsg',_class='disclaimer',style="float:right;margin:10px;")
+
+    def logoutbox(self,pane):
+        pane.dataRecord('user','showcase.user',username='^login.result.user')
+        pane.button('logout',float='right',action='genro.logout();')
+        pane.div('^login.result.user',float='right',margin='10px')
 
     def pageController(self,root):
         """The data controller on the page"""
@@ -74,60 +118,17 @@ class GnrCustomWebPage(object):
         root.dataScript('dummy','SET panel = 0', _fired='^demo.current.name')
         root.dataRpc('demo.current.syspath','fileSysPath',relpath='^demo.current.relpath')
         root.dataRpc('demo.current.source','getSourceFile',syspath='^demo.current.syspath')
-        #root.dataRpc('demo.current.doc','getDocFile',abspage='^iframe.current_path')
         root.dataRpc('demo.current.docdata','getDocData',syspath='^demo.current.syspath')
-        root.dataRpc('funcEdited','saveDocData',_fired='^save.doc',data='^demo.current.docdata',syspath="^demo.current.syspath",name='^demo.current.name')
+        root.dataRpc('funcEdited','saveDocData',_fired='^pippo.doc',data='=demo.current.docdata',
+                                        syspath="=demo.current.syspath",title='=demo.current.name')
 
-    def editorDialog(self,pane):
-        """docstring for categoryDialog"""
-        dlg = pane.dialog(nodeId='doc_edit',title='Edit documentation',_class='edit_dlg')
-        fb = dlg.formbuilder(cols=1,border_spacing='3px',font_size='8pt',datapath='demo.current.doc.description')
-        fb.textbox(value='^.short',lbl='Title')
-        fb.simpleTextarea(value='^.full',lbl='Abstract',lbl_vertical_align='top')
-        fb.simpleTextarea(value='^.children',lbl='Children',lbl_vertical_align='top')
-        fb.simpleTextarea(value='^.params',lbl='Params',lbl_vertical_align='top')
-        fb.button('Save',action='FIRE aux.doSave=true')
-       
     #----------  Rpc custom Calls ------------    
     def diskDirectory(self):         
         pages = self.site.sitemap['showcase']
         return pages
-        
-    def rpc_saveDocumentation(self, docbag, currpath):
-        """docstring for rpc_saveDocumentation"""
-        
-        if docbag and currpath:
-            docpath, ext = os.path.splitext(currpath)
-            docpath = '%s.xdoc' %docpath
-            docpath = docpath.split('/')
-            filename = docpath.pop()
-            docpath.append('_doc')
-            docpath.append(filename.replace('.py','.xdoc'))
-            docpath = '/'.join(docpath)   
-            docbag.toXml(docpath)
-            return 'ok'
-        else:
-            return 'error'
-            
+
     def getUserMenu(self):
-
         result=self.application.config['menu']
-
-        return result
-        
-    def rpc_getDocFile(self,abspage):
-        """docstring for rpc_getDocFile"""
-     
-        docpath = abspage.split('/')
-        filename = docpath.pop()
-        docpath.append('_doc')
-        docpath.append('/%s.xdoc' %filename)
-        docpath = '/'.join(docpath)
-        print docpath
-
-        result = Bag()
-        if os.path.isfile(docpath):
-            result=Bag(docpath)
         return result
 
     def rpc_fileSysPath(self,relpath=None):
@@ -147,7 +148,7 @@ class GnrCustomWebPage(object):
             from pygments.lexers import PythonLexer
             from pygments.formatters import HtmlFormatter
             code = unicode(result)
-            parsed = highlight(code, PythonLexer(), HtmlFormatter())
+            parsed = highlight(code, PythonLexer(), HtmlFormatter(linenos='table'))
             return parsed
         except:
             return '<div>error: %s</div>'  %syspath
@@ -157,24 +158,23 @@ class GnrCustomWebPage(object):
         return name
 
     def rpc_getDocData(self,syspath):
-        tbl = self.db.table('showcase.function')
+        tbl = self.db.table('showcase.document')
         func = tbl.query(where='path=:func_path',func_path=syspath).fetch()
         if func:
             return func[0]['data']
         return ''
 
-    def rpc_saveDocData(self,data='',syspath='',name=''):
-        tbl = self.db.table('showcase.function')
-        func = tbl.query(where='path=:func_path',func_path=syspath).fetch()
-        pkey=None
-        if func:
-            func[0]['doc']=data
-            pkey = func[0]['id']
-        else:
+    def rpc_saveDocData(self,data='',syspath='',title=''):
+        tbl = self.db.table('showcase.document')
+        #records = tbl.query(where='path=:func_path',func_path=syspath,addPkeyColumn=False,for_update=True).fetch()
+        record_id=''
+        tbl.batchUpdate(dict(doc=data),where='path=:func_path',func_path=syspath)
+        self.db.commit()
+        if title:
             try:
                 pkey = tbl.newPkeyValue()
-                tbl.insert(dict(id=pkey,name=name,doc=data,path=syspath))
+                tbl.insert(dict(id=pkey,name=title,data=data,path=syspath))
+                self.db.commit()
             except:
                 return None
-        self.db.commit()
-        return pkey
+        return record_id
