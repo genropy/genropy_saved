1d5e89232bfd8383c39ad1c363ab49a6085a1e56
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-28 12:38:58 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2595 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/editing/mailtest.py
+++ packages/showcase/webpages/widgets/editing/mailtest.py
@@ -29,13 +29,18 @@ class GnrCustomWebPage(object):
         center=bc.borderContainer(region='center')
         fb=center.formbuilder(cols=3)
         fb.data('mail.from','michele@vico10.com')
+        fb.data('mail.async',False)
         fb.textBox(value='^mail.from', lbl='From:')
         fb.textBox(value='^mail.to', lbl='To:')
         fb.textBox(value='^mail.subject', lbl='Subject:')
+        fb.checkBox(value='^mail.async', lbl='Async:')
         fb.button('Send', action='FIRE mail.send')
-        center.dataRpc('dummy','sendMail', to_address='=mail.to', from_address='=mail.from', subject='=mail.subject', body='=mail.body', _fired='^mail.send')
+        center.dataRpc('dummy','sendMail', to_address='=mail.to', from_address='=mail.from',async='=mail.async', subject='=mail.subject', body='=mail.body', _fired='^mail.send')
         
-    def rpc_sendMail(self, to_address=None, from_address=None, subject=None, body=None):
+    def rpc_sendMail(self, to_address=None, from_address=None, subject=None, body=None, async=False):
+        import time
+        t=time.time()
         self.site.mail_handler.sendmail(to_address, subject, body,from_address=from_address,
-                            host='smtp.gmail.com',tls=True, user='michele.bertoldi@gmail.com', password='rotring', html=True)
+                            host='smtp.gmail.com',tls=True, user='michele.bertoldi@gmail.com', password='rotring', html=True, async=async)
+        print time.time()-t
         
