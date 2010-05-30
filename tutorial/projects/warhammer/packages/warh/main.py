99efc7ccaf7cb60700454502532c100d4deb0b74
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-05-30 12:38:40 +0000

    warhammer
    git-svn-id: http://svn.genropy.org/genro/trunk@2599 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ tutorial/projects/warhammer/packages/warh/main.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env python
+# encoding: utf-8
+from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter, Table_userobject
+
+class Package(GnrDboPackage):
+    def config_attributes(self):
+        return dict(comment='warh package',sqlschema='warh',
+                name_short='Warh', name_long='Warh', name_full='Warh')
+
+    def config_db(self, pkg):
+        pass
+
+    def loginUrl(self):
+        return 'warh/login'
+
+class Table(GnrDboTable):
+    pass
