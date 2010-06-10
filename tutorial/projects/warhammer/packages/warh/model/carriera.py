8d56e849d5e06dec8128dae45e7e63c0e81826ad
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-06-10 15:45:51 +0000

    ppp
    git-svn-id: http://svn.genropy.org/genro/trunk@2653 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ tutorial/projects/warhammer/packages/warh/model/carriera.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+class Table(object):
+    def config_db(self, pkg):
+        tbl =  pkg.table('carriera',pkey='id',name_long='!!Carriera',name_plural='!!Carriere')
+        self.sysFields(tbl)
+        tbl.column('sigla',name_long='!!Sigla Carriera')
+        tbl.column('nome',name_long='!!Nome Carriera')
+        tbl.column('razza_codice',size=':2',name_long='!!Razza').relation('warh.razza.codice',mode='foreignkey')
+        tbl.column('ac','L',default=0,name_long='!!AC')
+        tbl.column('ab','L',default=0,name_long='!!AB')
+        tbl.column('forza','L',default=0,name_long='!!Forza')
+        tbl.column('resistenza','L',default=0,name_long='!!Resistenza')
+        tbl.column('agilita','L',default=0,name_long='!!Agilita')
+        tbl.column('intelligenza','L',default=0,name_long='!!Intelligenza')
+        tbl.column('volonta','L',default=0,name_long='!!Volonta')
+        tbl.column('simpatia','L',default=0,name_long='!!Simpatia')
+        tbl.column('attacchi','L',default=0,name_long='!!Attacchi')
+        tbl.column('ferite','L',default=0,name_long='!!Ferite')
+        tbl.column('bonus_forza','L',default=0,name_long='!!Bonus Forza')
+        tbl.column('bonus_res','L',default=0,name_long='!!Bonus Resistenza')
+        tbl.column('mov','L',default=0,name_long='!!Movimento')
+        tbl.column('magia','L',default=0,name_long='!!Magia')
+        tbl.column('follia','L',default=0,name_long='!!Punti Follia')
+        tbl.column('fato','L',default=0,name_long='!!Punti Fato')
---
+++ tutorial/projects/warhammer/packages/warh/webpages/carriere.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by Softwell on 2008-07-10.
+Copyright (c) 2008 Softwell. All rights reserved.
+"""
+
+# --------------------------- GnrWebPage Standard header ---------------------------
+class GnrCustomWebPage(object):
+    maintable='warh.carriera'
+    py_requires='public:Public,standard_tables:TableHandlerLight,public:IncludedView'
+
+######################## STANDARD TABLE OVERRIDDEN METHODS ################ 
+    def windowTitle(self):
+        return '!!Carriere Personaggi'
+
+    def barTitle(self):
+        return '!!Carriere Personaggi'
+
+    def lstBase(self,struct):
+        """!!Vista base"""
+        r = struct.view().rows()
+        r.fieldcell('sigla',width='5em')
+        r.fieldcell('nome',width='15em')
+        r.fieldcell('ac',width='4em')
+        r.fieldcell('ab',width='4em')
+        r.fieldcell('forza',width='4em')
+        r.fieldcell('resistenza',width='4em')
+        r.fieldcell('agilita',width='4em')
+        r.fieldcell('intelligenza',width='4em')
+        r.fieldcell('volonta',width='4em')
+        r.fieldcell('simpatia',width='4em')
+        r.fieldcell('attacchi',width='4em')
+        r.fieldcell('ferite',width='4em')
+        r.fieldcell('bonus_forza',width='4em')
+        r.fieldcell('bonus_res',width='4em')
+        r.fieldcell('mov',width='4em')
+        r.fieldcell('magia',width='4em')
+        r.fieldcell('follia',width='4em')
+        r.fieldcell('fato',width='4em')
+        return struct
+
+    def printActionBase(self):
+        return True
+
+    def exportActionBase(self):
+        return True
+
+    def orderBase(self):
+        return 'nome'
+
+    def queryBase(self):
+        return dict(column='nome',op='contains', val='')
+
+    def userCanWrite(self):
+        return True
+
+    def userCanDelete(self):
+        return True
+
+    def formBaseDimension(self): 
+        return dict(height='250px',width='800px')
+
+############################## FORM METHODS ##################################
+        
+    def formBase(self, parentBC, disabled=False, **kwargs):
+        pane = parentBC.contentPane(**kwargs)
+        fb = pane.formbuilder(cols=8, border_spacing='4px', disabled=disabled)
+        fb.field('sigla', width='2em')
+        fb.field('nome', width='10em')
+        fb.br()
+        fb.br()
+        fb.br()
+        fb.br()
+        fb.br()
+        fb.br()        
+        fb.field('ac',width='2em')
+        fb.field('ab',width='2em')
+        fb.field('forza',width='2em')
+        fb.field('resistenza',width='2em')
+        fb.field('agilita',width='2em')
+        fb.field('intelligenza',width='2em')
+        fb.field('volonta',width='2em')
+        fb.field('simpatia',width='2em')
+        fb.field('attacchi',width='2em')
+        fb.field('ferite',width='2em')
+        fb.field('bonus_forza',width='2em')
+        fb.field('bonus_res',width='2em')
+        fb.field('mov',width='2em')
+        fb.field('magia',width='2em')
+        fb.field('follia',width='2em')
+        fb.field('fato',width='2em')
+
