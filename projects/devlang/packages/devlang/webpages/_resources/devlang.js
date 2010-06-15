26dbb1e54d11a30f7923c2382053f42113f64fda
jeffedwa <jeffedwa@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-06-15 02:06:14 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2677 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- projects/devlang/packages/devlang/webpages/_resources/devlang.js
+++ projects/devlang/packages/devlang/webpages/_resources/devlang.js
@@ -1,11 +1,12 @@
 function formatLevel(value){
     var result = ''; //this is javascript callback you can put inside a separate module.
     value = parseInt(value);
-    var colors = ['red','orange','yellow','darkgreen','green','lightgreen']
+    var colors = ['#6352b6','#9552b6','#bc5491','#cd5c5c','#ef0d0e'] 
     for (var i=0; i<value ; i++){
         var style = "width:10px;height:14px;float:left;background:"+colors[i]+";";
+        console.log(i)
         result = result+'<div style="'+style+'"> </div>';
     }
     return result;
 }    
-    
+//#['#527cb6','red','orange','yellow','darkgreen','green','lightgreen']
