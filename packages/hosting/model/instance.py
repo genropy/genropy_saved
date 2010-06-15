de85b9cf28bcc9a9edf3e844e1e33475a56df952
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-06-15 17:56:20 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2682 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/hosting/model/instance.py
+++ packages/hosting/model/instance.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from gnr.app.gnrdeploy import InstanceMaker, SiteMaker
+import os.path
+from gnr.core.gnrbag import Bag
+
 class Table(object):
 
     def config_db(self, pkg):
@@ -9,6 +13,7 @@ class Table(object):
         tbl.column('name',size=':10',name_long='!!Instance Name')
         tbl.column('repository_name', dtype='T', name_long='!!Repository Name') # Optional
         tbl.column('path',dtype='T',name_long='!!Instance Path')
+        tbl.column('site_path',dtype='T',name_long='!!Site Path')
         
         tbl.column('client_id',size=':22',name_long='!!Client id')
         # dtype -> sql
@@ -16,3 +21,22 @@ class Table(object):
         #   R       float
         #   DH      datetime
         #   H       time
+        # 
+    def create_instance(self, name, path, instanceconfig):
+        instanceconfig=Bag(instanceconfig)
+        instanceconfig.pop('application')
+        instanceconfig.pop('menu')
+        instanceconfig.pop('packages.hosting')
+        base_path=os.path.dirname(os.path.realpath(path))
+        im=InstanceMaker(name, base_path=base_path, config=instanceconfig)
+        im.do()
+        return im.instance_path
+        
+    def create_site(self, name, path, siteconfig):
+        siteconfig=Bag(siteconfig)
+        siteconfig.pop('secret')
+        siteconfig.pop('instances')
+        base_path=os.path.dirname(os.path.realpath(path))
+        sm=SiteMaker(name, base_path=base_path, config=siteconfig)
+        sm.do()
+        return sm.site_path
