6416bcfa13dedc9107e6c6789f47d96a4e1d6cd7
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-06-21 10:19:18 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2726 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/hosting/main.py
+++ packages/hosting/main.py
@@ -2,6 +2,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter, Table_userobject
+from gnr.app.gnrapp import GnrApp
 import os
 
 class Package(GnrDboPackage):
@@ -29,6 +30,45 @@ class Package(GnrDboPackage):
         """check if the site exists"""
         return instance_name and os.path.isfile(os.path.join(self.site_folder(instance_name),'siteconfig.xml'))
                 
+    def db_synced(self, instance_name, storename=None, verbose=False):
+        if self.instance_exists(instance_name):
+            changes_dict=dict()
+            app=GnrApp(os.path.join(self.instance_folder(instance_name),'instanceconfig.xml'))
+            if storename=='*':
+                stores = [None]+app.db.dbstores.keys()
+            else:
+                stores = [storename]
+            for storename in stores:
+                app.db.use_store(storename)
+                changes = app.db.model.check()
+                if not verbose and changes:
+                    return False
+                else:
+                    changes_dict[storename or 'Main']=changes
+            if not verbose:
+                return True
+            else:
+                return changes
+        else:
+            raise Exception('Instance %s does not exists'%instance_name)
+            
+    def db_setup(self, instance_name, storename=None):
+        if self.instance_exists(instance_name):
+            app=GnrApp(os.path.join(self.instance_folder(instance_name),'instanceconfig.xml'))
+            if storename=='*':
+                stores = [None]+app.db.dbstores.keys()
+            else:
+                stores = [storename]
+            for storename in stores:
+                app.db.use_store(storename)
+                changes = app.db.model.check()
+                if changes:
+                    app.db.model.applyModelChanges()
+        else:
+            raise Exception('Instance %s does not exists'%instance_name)
+
+            for storename in stores:
+                app.db.use_store(storename)
 
 class Table(GnrDboTable):
     pass
