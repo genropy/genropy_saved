da0880079c18c8ddea0cdc4b054c630165b0fa39
robertolupi <robertolupi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-06-22 15:07:47 +0000

    Updated TextMate bundle from ghigo
    git-svn-id: http://svn.genropy.org/genro/trunk@2746 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ bundles/Genropy.tmbundle/Templates/standard table.tmTemplate/template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by Softwell on 2008-07-10.
+Copyright (c) 2008 Softwell. All rights reserved.
+"""
+-- GnrWebPage Standard header ---------------------------
+class GnrCustomWebPage(object):
+    maintable=''
+    py_requires='public:Public,standard_tables:TableHandler,public:IncludedView'
+
+######################## STANDARD TABLE OVERRIDDEN METHODS ###############
+    def windowTitle(self):
+        return '!!'
+         
+    def pageAuthTags(self, method=None, **kwargs):
+        return 'user'
+        
+    def tableWriteTags(self):
+        return 'user'
+        
+    def tableDeleteTags(self):
+        return 'user'
+        
+    def barTitle(self):
+        return '!!'
+        
+    def columnsBase(self,):
+        return """"""
+                  
+            
+    def orderBase(self):
+        return ''
+        
+    def conditionBase(self):
+        pass
+    
+    def queryBase(self):
+        return dict(column='',op='contains', val='%')
+
+############################## FORM METHODS ##################################
+
+    def formBase(self, parentBC,disabled=False, **kwargs):
+        pass
