ed88b8daa4f4f5c09c4fa67e137155f880aec0ab
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-06-24 15:13:06 +0000

    button demo made cleaner
    git-svn-id: http://svn.genropy.org/genro/trunk@2765 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/button.py
+++ packages/showcase/webpages/widgets/form/button.py
@@ -10,7 +10,6 @@
 """ Buttons """
 import os
 from gnr.core.gnrbag import Bag
-from gnr.web.gnrwsgisite import cache
 
 class GnrCustomWebPage(object):    
     css_requires='index,button'
@@ -21,38 +20,21 @@ class GnrCustomWebPage(object):
         
         root.dataFormula('width_calc','Math.ceil(w)+umw',w='^width',umw='^um_width')
         bc = root.borderContainer(height='100%')
-        top = bc.contentPane(height='400px',region='top')
-        top.button("Save it",action="alert('Saving!')",nodeId='cbtn',iconClass='^icon',
+        top = bc.contentPane(height='100px',region='top')
+        top.button("Save it",action="FIRE msg='Saving!';",nodeId='cbtn',iconClass='^icon',
                    tooltip='click me',font_size='^font_size',width='^width_calc',font_family='^fontfam')
         center = bc.contentPane(region='center')
-        titlepane = center.titlepane(title='pippo')
+        titlepane = center.titlepane(title='Center')
         titlepane.div(height='200px',width='250px')
+        center.dataController("""alert(msg);""",msg='^msg')
+        self.makeDropDown(center,'Drop')
+        self.checkBoxGroup(center,'First,Second,Third,Fourth,Fifth,Sixth')
         
-        fb = center.formbuilder(cols=3,margin_top='10px')
-        fb.button('!!Calcola', action='FIRE ^calcola')
-        fb.textbox(value='^risultato')
-        fb.dataRpc('risultato','calcola',_fired='^calcola')
-        fb.br()
-        fb.horizontalslider(lbl='!!width', value = '^width', width='200px', 
-                                         minimum=3, maximum=50,intermediateChanges=True)
-        fb.numberTextBox(value ='^width',width='5em')
-        fb.comboBox(width='5em',values='em,px,%',value='^um_width',default='em')
-        fb.horizontalslider(lbl='!!font', value = '^font', width='200px', 
-                                         minimum=4, maximum=50,intermediateChanges=True)
-        fb.numberTextBox(value = '^font',width='5em')
-        
-        fb.comboBox(width='5em',values='pt,px',value='^um_font',default='pt')
-        fb.filteringSelect(width='15em',value='^icon',colspan=3,lbl='Icon',
-                           values='icnBaseAdd:Add,icnBaseCancel:Cancel,icnBaseDelete:Delete,icnBaseOk:Ok')
-    
-        fb.filteringSelect(width='15em',value='^fontfam',colspan=3,lbl='Font',
-                           values='Verdana:Verdana,Courier:Courier,mono:Mono,"Comic Sans MS":Comic')
-        
-    def makeDropDown(self,fb,label):
-        dropdown=fb.dropdownbutton(label)
+    def makeDropDown(self,pane,label):
+        dropdown=pane.dropdownbutton(label)
         dmenu=dropdown.menu()
-        dmenu.menuline('Open...',action="alert('Opening...')")
-        dmenu.menuline('Close',action="alert('Closing...')")
+        dmenu.menuline('Open...',action="FIRE msg='Opening!';")
+        dmenu.menuline('Close',action="FIRE msg='Closing!';")
         dmenu.menuline('-')
         submenu=dmenu.menuline('I have submenues').menu()
         submenu.menuline('To do this',action="alert('Doing this...')")
@@ -66,8 +48,3 @@ class GnrCustomWebPage(object):
         for label in labels:
             pane.checkbox(label) 
         
-    @cache.cached_call()
-    def rpc_calcola(self):
-        for i in range(1000000):
-            j=i*2
-        return j
