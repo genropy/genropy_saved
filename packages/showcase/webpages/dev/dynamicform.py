1f2ee3fc3be33baffa9c258008cca79d9d795212
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-03 10:14:07 +0000

    demo dynamicForm
    git-svn-id: http://svn.genropy.org/genro/trunk@2875 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/dev/dynamicform.py
+++ packages/showcase/webpages/dev/dynamicform.py
@@ -28,15 +28,40 @@ class GnrCustomWebPage(object):
 
          
     def main(self, root, **kwargs):
-        self.dynamicForm(root,nodeId='myform',storepath='test',reloader='^gnr.onStart',
-                        row_types={'phone':'!!Phone','email':"Email",'fax':'Fax'},
-                        type_field='contact_type',height='300px',width='400pz')
-        root.button('Save',action='var values = GET test; alert(values.toXml());')
+        self.dynamicForm(root,nodeId='myform',storepath='contacts',reloader='^gnr.onStart',
+                        row_types={'phone':'!!Phone','email':"Email",'fax':'Fax','post_address':'Postal'},
+                        type_field='contact_type')
+                        
+                        
+        #data d'esempio
+        egData = Bag()
+        egData['r_1.contact_type'] = 'phone'
+        egData['r_1.location'] = 'Work'
+        egData['r_1.content'] = '44422444'
+        
+        egData['r_2.contact_type'] = 'email'
+        egData['r_2.content'] = 'jeffedwa@goodsoftware.com'
+        
+        egData['r_3.contact_type'] = 'post_address'
+        egData['r_3.suburb'] = 'Sydney'
+        egData['r_3.state'] = 'NSW' #thanks
+        egData['r_3.address'] =  '1 Lucinda Ave'
+        root.data('contacts',egData)
+        
                         
     def myform_row(self,row,disabled=None,**kwargs):
+        #inside remote
         fb = row.formbuilder(cols=2, border_spacing='2px',disabled=disabled)
         fb.combobox(value='^.location',values='Home,Work,Main',lbl='Loc.')
         fb.textbox(value='^.content')
+        
     def myform_email_row(self,row,disabled=None,**kwargs):
         fb = row.formbuilder(cols=1, border_spacing='2px',disabled=disabled)
         fb.textbox(value='^.content',lbl='Url')
+    
+    def myform_post_address_row(self,row,disabled=None,**kwargs):
+        fb = row.formbuilder(cols=2, border_spacing='2px',disabled=disabled)
+        fb.textbox(value='^.suburb',lbl='Suburb',width='12em')
+        fb.textbox(value='^.state',lbl='State',width='3em')
+        fb.simpleTextArea(value='^.address',colspan=2,width='100%')
+        
