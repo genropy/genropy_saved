470225983cf0b1ea878659687b7a2158e05a27e0
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-06 21:13:22 +0000

    Slider demo page fix
    git-svn-id: http://svn.genropy.org/genro/trunk@2939 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/sliders.py
+++ packages/showcase/webpages/widgets/form/sliders.py
@@ -14,7 +14,8 @@ import os
 class GnrCustomWebPage(object):
     def main(self, root, **kwargs):       
         fb=root.formbuilder(cols=2)
-        fb.horizontalSlider(lbl="People",width='400px')
-
+        fb.horizontalslider(lbl='!!People', value = '^people', width='400px', 
+                                         minimum=4, maximum=50,intermediateChanges=True)
+        fb.numberTextBox(value = '^people',width='5em')
 
        
