bedf5877abf11559ba25ecf42446cc53a5fff014
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-09 11:40:41 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@2964 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/button.py
+++ packages/showcase/webpages/widgets/form/button.py
@@ -23,23 +23,23 @@ class GnrCustomWebPage(object):
         titlepane = center.titlepane(title='Center')
         titlepane.div(height='200px',width='250px')
         
-        self.makeDropDown(center,'Drop')
+        self.makeDropDown(center,'Menù')
         self.checkBoxGroup(center,'First,Second,Third,Fourth,Fifth,Sixth')
         
     def makeDropDown(self,pane,label):
-        dropdown=pane.dropdownbutton(label)
-        dmenu=dropdown.menu()
-        dmenu.menuline('Open...',action="FIRE msg='Opening!';")
-        dmenu.menuline('Close',action="FIRE msg='Closing!';")
+        ddb=pane.dropdownbutton(label)
+        dmenu=ddb.menu()
+        dmenu.menuline('Open...', action="FIRE msg='Opening!';")
+        dmenu.menuline('Close', action="FIRE msg='Closing!';")
         dmenu.menuline('-')
         submenu=dmenu.menuline('I have submenues').menu()
-        submenu.menuline('To do this',action="alert('Doing this...')")
-        submenu.menuline('Or to do that',action="alert('Doing that...')")       
+        submenu.menuline('To do this', action="alert('Doing this...')")
+        submenu.menuline('Or to do that', action="alert('Doing that...')")       
         dmenu.menuline('-')
         dmenu.menuline('Quit',action="alert('Quitting...')")
         
-    def checkBoxGroup(self,pane,labels,cols=1):
+    def checkBoxGroup(self,pane,labels,columns=1):
         labels=labels.split(',')
-        pane=pane.formbuilder(cols=cols)
+        pane=pane.formbuilder(cols=columns)
         for label in labels:
             pane.checkbox(label)
---
+++ packages/showcase/webpages/widgets/form/button_2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#  Created by Giovanni Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+
+""" Buttons """
+import os
+from gnr.core.gnrbag import Bag
+from gnr.web.gnrwsgisite import cache
+
+class GnrCustomWebPage(object):
+    css_requires='index'
+    def main(self, root, **kwargs):
+        root.data('icon','icnBaseOk')
+        root.data('fontfam','Courier')
+        root.dataFormula('width_calc','w + umw',w='^width',umw='^um_width')
+        root.dataFormula('font_size','font + umf',font='^font',umf='^um_font')
+        
+        bc = root.borderContainer()
+        top = bc.contentPane(region='top')
+        fb = top.formbuilder(cols=4, padding='10px')
+        fb.horizontalslider(lbl='!!width',value='^width',width='200px',discreteValues='48',
+                            minimum=3,maximum=50,intermediateChanges=True, default=8)
+        fb.numberTextBox(value='^width', width='4em')
+        fb.comboBox(width='5em',value='^um_width',values='em,px,%',default='em')
+        fb.br()
+        
+        fb.horizontalslider(lbl='!!font',value='^font',width='200px',discreteValues='47',
+                            minimum=4,maximum=50,intermediateChanges=True, default=11)
+        fb.numberTextBox(value='^font',width='4em')
+        fb.comboBox(width='5em',values='pt,px',value='^um_font',default='pt')
+        fb.filteringSelect(width='8em',value='^fontfam',lbl='Font',
+                           values='Verdana:Verdana,Courier:Courier,mono:Mono,"Comic Sans MS":Comic')
+                           
+        fb.filteringSelect(width='5em',value='^icon',colspan=4,lbl='icon',
+                           values='icnBaseAdd:Add,icnBaseCancel:Cancel,icnBaseDelete:Delete,icnBaseOk:Ok')
+                           
+        center = bc.contentPane(height='200px',region='center')
+        center.button("Save it", action="alert('Saving!')", iconClass='^icon', tooltip='click me',
+                       font_size='^font_size', width='^width_calc', font_family='^fontfam')
