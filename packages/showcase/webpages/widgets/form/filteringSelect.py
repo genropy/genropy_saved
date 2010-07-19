e04ad68e5a5ded4849ec1cac20db8a5a0fbd0a83
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-19 08:51:08 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3016 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/filteringSelect.py
+++ packages/showcase/webpages/widgets/form/filteringSelect.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
-#
+
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
 
-""" Buttons """
-import os
 from gnr.core.gnrbag import Bag
 
 class GnrCustomWebPage(object):
     def main(self, root, **kwargs):
-        root.data('values.sport',self.sports(),id='.pkey', caption='.Description')
-        tbl=root.table()
-        r=tbl.tr()
-        r.td('Sport')
-        r.td().div('loaded from bag')
-        r.td().filteringSelect(lbl='Sport', value = '^record.sport',
-                              storepath='values.sport',ignoreCase=True)
-        r.td().div('loaded from values')
-        r.td().filteringSelect(lbl='Sport', 
-                               values='so:Soccer,bs:Basket,hk:Hockey,ts:Tennis,bb:Baseball,sb:Snowboard')
+        root.data('bag',self.sports(),id='.pkey',caption='.Description')
+        bc = root.borderContainer(datapath='record.sport',padding='1em')
+        fb = bc.formbuilder(cols=6)
+        fb.div('------ Sport ------',colspan=6)
+        fb.div('loaded from bag')
+        fb.filteringSelect(value='^.bag_values',storepath='bag',ignoreCase=True,colspan=2)
+        fb.div('loaded from values')
+        fb.filteringSelect(value='^.loaded_values',colspan=2,
+                           values='SC:Soccer,BK:Basket,HK:Hockey,TE:Tennis,BB:Baseball,SB:Snowboard')
         
     def sports(self,**kwargs):
         mytable=Bag()
-        mytable.setItem('r1.pkey','SC')
-        mytable.setItem('r1.Description','Soccer')
-        mytable.setItem('r2.pkey','BK')
-        mytable.setItem('r2.Description','Basket')
-        mytable.setItem('r3.pkey','TE')
-        mytable.setItem('r3.Description','Tennis')
-        mytable.setItem('r4.pkey','HK')
-        mytable.setItem('r4.Description','Hockey')
-        mytable.setItem('r5.pkey','BB')
-        mytable.setItem('r5.Description','Baseball')
-        mytable.setItem('r6.pkey','SB')
-        mytable.setItem('r6.Description','Snowboard')
+        mytable['r1.pkey']='SC'
+        mytable['r1.Description']='Soccer'
+        mytable['r2.pkey']='BK'
+        mytable['r2.Description']='Basket'
+        mytable['r3.pkey']='TE'
+        mytable['r3.Description']='Tennis'
+        mytable['r4.pkey']='HK'
+        mytable['r4.Description']='Hockey'
+        mytable['r5.pkey']='BB'
+        mytable['r5.Description']='Baseball'
+        mytable['r6.pkey']='SB'
+        mytable['r6.Description']='Snowboard'
         return mytable
