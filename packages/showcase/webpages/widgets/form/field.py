26fc3d10a3b7548b8f582111e30a13721f2e7e0e
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-19 08:47:13 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3015 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/field.py
+++ packages/showcase/webpages/widgets/form/field.py
@@ -4,9 +4,6 @@
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
 
-""" dbSelect """
-import os
-from gnr.core.gnrbag import Bag
 from gnr.core.gnrstring import templateReplace, splitAndStrip, countOf
 
 class GnrCustomWebPage(object):
