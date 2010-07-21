4d0d1dda6622751ca08ed08c373b1343a13071ec
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-21 10:34:55 +0000

    Fixed some bugs, improved GUI, improved code
    git-svn-id: http://svn.genropy.org/genro/trunk@3030 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/menu.py
+++ packages/showcase/webpages/widgets/form/menu.py
@@ -4,119 +4,73 @@
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
 
-""" GnrDojo Hello World """
-import os
-
-import datetime
 from gnr.core.gnrbag import Bag, DirectoryResolver
 
 class GnrCustomWebPage(object):
-    def main_(self,root):
-        root.css('.colorpalettemenu .dijitMenuItemHover','background-color:transparent;')
-        root.css('.colorpalettemenu .dijitMenuItem td','padding:0;')
-        root.css('.colorpalettemenu .dijitMenuItemIcon','display:none;')
-        root.data('tempcolor','emptypath')
-        root.div().menu(modifiers='*',id='colorPalettemenu',_class='colorPaletteMenu',
-                            connect_onOpen="""
-                                            var path= this.widget.originalContextTarget.sourceNode.absDatapath();
-                                            SET tempcolor=path;""",
-                        ).menuItem(datapath='^tempcolor').colorPalette(value='^.color')
-        root.dataController("sourceNode=dijit.byId('colorPalettemenu').originalContextTarget.sourceNode;",color="^temp.color")
-        root.div(width='20px',height='20px',border='1px solid gray',background_color='^.color',
-                                connectedMenu='colorPalettemenu',datapath='c1')
-        root.div(width='20px',height='20px',border='1px solid gray',background_color='^.color',connectedMenu='colorPalettemenu',datapath='c2')
-        root.div(width='20px',height='20px',border='1px solid gray',background_color='^.color',connectedMenu='colorPalettemenu',datapath='c3')
-        root.div(width='20px',height='20px',border='1px solid gray',background_color='^.color',connectedMenu='colorPalettemenu',datapath='c4')
-        tb=root.textbox(background_color='^pippo',color='^pluto',nodeId='pier')
-       # self.colorPaletteMenu('pier','pippo')
-        
-    def main(self,root):
-        root.data('values.states', self.tableData_states())
-        fb = root.formbuilder(cols=1, border_spacing='4px')
-        x = fb.div(height='30px',width='50px',background='lime')
-        x.menu(modifiers='*',storepath='values.states',action='alert($1.caption);',selected_id='aaa')
-        x.div('^aaa')
-        fb.filteringSelect(storepath='values.states',selected_caption='aaa')
-        fb.button('addItem to menu',action='genro.setData("values.states.r6",null,{caption:"Washington",id:"WA"})')
-        fb.button('delItem to menu',action='genro._data.pop("values.states.r6");')
-        
-    def main___(self, root, **kwargs):
-        root.data('values.states', self.tableData_states())
-        root.data('values.sex', self.tableData_sex(), id='#k', caption='#v')
-        root.data('records', self.myRecords())
-        root.div(width='20px',height='20px',border='1px solid gray',background_color='^color').menu(modifiers='*').colorPalette()
-        fb = root.formbuilder(cols='4', datapath='records',
-                              cellspacing='10', background_color='silver', margin='30px')
-        for r in range (4):
+    def main(self, root, **kwargs):
+        root.data('values.states',self.tableData_states())
+        root.data('values.sex',self.tableData_sex(),id='#k',caption='#v')
+        root.data('records',self.myRecords())
+        fb = root.formbuilder(cols='4',datapath='records',background_color='silver',margin='30px')
+        for r in range(4):
             self.makeRow(fb,r)
             
-        self.pageCtrlMenu(root.menu('pagemenu'))
-        self.pageShiftMenu(root.menu('shiftmenu',modifiers='shift'))
-        
+        self.pageCtrlMenu(fb.dropdownbutton('pagemenu'))
         self.dropDownContent(fb.dropdownbutton('Open me'))
         self.connectionDialog(fb.dropdownbutton('Connection mode'))
-        self.savingDialog(root,'savedlg')
+        self.saveDialog(root,'savedlg')
         fb.button('Save as...',action='genro.savedlg.show()')
         
-        dlg=root.dialog(title='Saving all',datapath='saving',gnrId='savedlg').formbuilder(cols='2',border_spacing='8px')
-        dlg.textbox(lbl='Filename',value='^.filename')
-        dlg.checkbox('Make a copy',value='^.docopy')
-        dlg.button('Ok',action='genro.savedlg.hide()')
+    def makeRow(self,fb,r):
+        name = fb.textBox(lbl='Name',value='^.r%i.name' % r)
+        age = fb.numberSpinner(lbl='Age',value='^.r%i.age' % r)
+        sex = fb.filteringSelect(lbl='Sex',value='^.r%i.sex' % r,storepath='values.sex')
+        state = fb.filteringSelect(lbl='State',value='^.r%i.state' % r,storepath='values.states')
         
-    def savingDialog(self,root,gnrId): 
-        dlg=root.dialog(title='Saving all',datapath='saving',gnrId=gnrId).formbuilder(cols='2',cellspacing='8')
+    def pageCtrlMenu(self,dropdownbutton):
+        menu = dropdownbutton.menu()
+        menu.menuline('Open...',gnrIcon='Save',action="alert('Opening now...')")
+        menu.menuline('Close...',action="alert('Closing now')")
+        menu.menuline('-')
+        submenu=menu.menuline('Print to').menu()
+        submenu.menuline('Laser first floor',background_color='green',action="alert('Printing on the laser')")
+        submenu.menuline('Ink Jet',color='red',action="alert('Wasting ink...')")
+        
+    def dropDownContent(self,dropdownbutton):
+        menu=dropdownbutton.menu(action="function(menuline){alert('Selected: '+menuline.label)}")
+        menu.menuline('Do This')
+        menu.menuline('Do That')
+        submenu=menu.menuline('Print to').menu()
+        submenu.menuline('Printer')
+        submenu.menuline('Fax')
+        submenu.menuline('-')
+        submenu.menuline('File')
+        menu.menuline('-')
+        menu.menuline('Do nothing',disabled=True)
+        
+    def connectionDialog(self,dropdownbutton):
+        tooltip=dropdownbutton.tooltipDialog(datapath='connection').formbuilder(cols='2',background_color='yellow')
+        tooltip.textbox(lbl='Url',lbl_color='red',value='^.url')
+        tooltip.textbox(lbl='User',value='^.user')
+        tooltip.numberTextBox(lbl='Min time',value='^.mintime')
+        tooltip.numberTextBox(lbl='Max time',value='^.maxtime')
+        
+    def saveDialog(self,root,gnrId):
+        dlg=root.dialog(title='Save',datapath='saving',gnrId=gnrId).formbuilder(cols='2')
         dlg.textbox(lbl='Filename',value='^.filename')
         dlg.checkbox('Make a copy',value='^.docopy')
         dlg.button('Ok',action='genro.%s.hide()' % gnrId)
         
-    def connectionDialog(self,dlg):
-        tt=dlg.tooltipDialog(datapath='connection').formbuilder(cols='2',cellspacing='8',background_color='yellow')
-        tt.textbox(lbl='Url',lbl_color='red',value='^.url')
-        tt.textbox(lbl='User',value='^.user')
-        tt.numberTextBox(lbl='Min time',value='^.maxtime')
-        tt.numberTextBox(lbl='Min time',value='^.mintime')
+############################# filling Bag methods ####################################################
         
-    def dropDownContent(self,x):
-        x=x.menu(action="function(x){alert('Selected:'+x.label)}")
-        x.menuline('Do This')
-        x.menuline('Do That',action="alert('Doing That')")
-        p=x.menuline('Print to').menu()
-        p.menuline('Printer')
-        p.menuline('Fax')
-        p.menuline('-')
-        p.menuline('File')
-        x.menuline('-')
-        x.menuline('Do nothing',disabled=True)
-        
-    def pageCtrlMenu(self,m):
-        m.menuline('Open...',gnrIcon='Save',action="alert('Opening now...')")
-        m.menuline('Close...',action="alert('Closing now')")
-        m.menuline('-')
-        mz=m.menuline('Print to').menu()
-        mz.menuline('Laser first floor',gnrIcon='Paste',background_color='green',action="alert('Printing on the laser')")
-        mz.menuline('Ink Jet',color='red',gnrIcon='Cut',action="alert('Wasting ink...')")
-        
-    def pageShiftMenu(self,m):
-        m.menuline('gggg',gnrIcon='Save',action="alert('ggggggg')")
-        m.menuline('hhhhh',action="alert('hhhhhhhhh')")
-        
-    def ageMenu(self,m):
-        m.menuline('Over 18', specialattr='24', action="function(item){alert(item.specialattr)}")
-        m.menuline('Over 30')
-        m.menuline('-')
-        mz = m.menuline('Older').menu()
-        mz.menuline('Over 50', action="alert('Senior uh ?')")
-        mz.menuline('Over 100', disabled=True)
-
-    def makeRow(self,fb,r): 
-        name = fb.textBox(lbl='Name', value='^.r%i.name' % r )
-        #age=fb.numberTextBox(lbl='Age', value='.r%i.age' % r)
-        age = fb.numberSpinner(lbl='Age', value='^.r%i.age' % r)
-        menu = age.menu(action="function(item){alert(item.label)}")
-        self.ageMenu(menu)
-        
-        sex = fb.filteringSelect(lbl='Sex', value='^.r%i.sex' % r,storepath='values.sex')
-        state = fb.filteringSelect(lbl='State', value = '^.r%i.state' % r, storepath='values.states')
+    def tableData_states(self):
+        mytable=Bag()
+        mytable.setItem('r1',None,id='CA',caption='California')
+        mytable.setItem('r2',None,id='IL',caption='Illinois')
+        mytable.setItem('r3',None,id='NY',caption='New York')
+        mytable.setItem('r4',None,id='TX',caption='Texas')
+        mytable.setItem('r5',None,id='AL',caption='Alabama')
+        return mytable
         
     def myRecords(self):
         data=Bag()
@@ -134,13 +88,4 @@ class GnrCustomWebPage(object):
         mytable=Bag()
         mytable.setItem('M','Male')
         mytable.setItem('F','Female')
-        return mytable
-        
-    def tableData_states(self):
-        mytable=Bag()
-        mytable.setItem('r1',None,id='CA',caption='California')
-        mytable.setItem('r2',None,id='IL',caption='Illinois')
-        mytable.setItem('r3',None,id='NY',caption='New York')
-        mytable.setItem('r4',None,id='TX',caption='Texas')
-        mytable.setItem('r5',None,id='AL',caption='Alabama')        
         return mytable
