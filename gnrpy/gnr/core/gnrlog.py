44094a724fd51ab65df59447fe4de4e6e5a3b108
robertolupi <robertolupi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-21 15:27:55 +0000

    logging level for gnrwsgiserve se to WARN
    git-svn-id: http://svn.genropy.org/genro/trunk@3035 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/core/gnrlog.py
+++ gnrpy/gnr/core/gnrlog.py
@@ -42,14 +42,13 @@ class ColoredFormatter(logging.Formatter):
 FORMAT = "[$BOLD%(name)-20s$RESET][%(levelname)-18s]  %(message)s ($BOLD%(filename)s$RESET:%(lineno)d)"
 COLOR_FORMAT = formatter_message(FORMAT, True)
 
-def enable_colored_logging(stream=sys.stderr, **kwargs):
+def enable_colored_logging(stream=sys.stderr, level=None):
     """Enable colored logging."""
     root = logging.getLogger()
     if len(root.handlers) == 0:
         hdlr = logging.StreamHandler(stream)
         if hasattr(stream, 'isatty') and stream.isatty():
             hdlr.setFormatter(ColoredFormatter(COLOR_FORMAT))
-        level = kwargs.get("level")
         if level is not None:
             root.setLevel(level)
         root.addHandler(hdlr)
