7a0a40fd20b94ce015b6d5d7eec94943642e2b26
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-30 09:06:48 +0000

    Improved example
    git-svn-id: http://svn.genropy.org/genro/trunk@3040 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/textbox.py
+++ packages/showcase/webpages/widgets/form/textbox.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
-#
+
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
 
-"""textbox"""
-import os
-
-import datetime
-from gnr.core.gnrbag import Bag, DirectoryResolver
-
 class GnrCustomWebPage(object):
     def main(self, root, **kwargs):
-        fb = root.form().formbuilder(datapath='form',cols=3)
-        fb.textBox(value='^.r0.name',lbl='Name one')
-        fb.textbox(value='^code',connect_onkeyup="""if($1.target.value.length==5){
+        #   dateTextBox - DEFAULT parameters:
+        #       popup=True      --> show calendar dialog
+        
+        fb = root.form().formbuilder(datapath='form',cols=2)
+        root.dataController("console.log('load record '+code)",code="^code")
+        fb.textBox(value='^.r0.name',lbl='Name')
+        fb.textBox(value='^.r0.surname',lbl='Surname')
+        fb.div('date format: GG/MM/AAAA')
+        fb.dateTextBox(value='^.r0.birthday',lbl='Birthday')
+        fb.numberTextBox(value='^.r0.age',lbl='Age')
+        fb.dateTextBox(value='^.r0.date',lbl='Date (no popup)',popup=False)
+        
+        root.div('- In magic box the sixth character is written onto the box with \"Text\" label')
+        fb.textbox(value='^code',lbl='Magic box',connect_onkeyup="""if($1.target.value.length==5){
                                                         var form = $1.target.form;
                                                         var pos = dojo.indexOf(form.elements,$1.target)
                                                         form.elements[pos+1].focus();
                                                     }""")
-        root.dataController("console.log('load record '+code)",code="^code")
-        fb.numberTextBox(value='^.r0.age',lbl='age')
-        fb.dateTextBox(value='^.r0.birthday',lbl='Birthday')
-        fb.dateTextBox(value='^.r0.date',lbl='date')
-        fb.dateTextBox(value='^.r0.dob',lbl='DOB',popup=False)
+                                                    
+        fb.textBox(value='^.r0.text',lbl='Text',width='5em')
