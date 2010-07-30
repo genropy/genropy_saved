1fc9f0878cce6e7de214870b5cf2b36ca50701f2
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-07-30 08:06:13 +0000

    Improved example
    git-svn-id: http://svn.genropy.org/genro/trunk@3039 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/togglebutton.py
+++ packages/showcase/webpages/widgets/form/togglebutton.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
-#
+
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
 
-""" Component for referto """
-
 class GnrCustomWebPage(object):
-    #py_requires='public:Public'
-    
     def main(self, rootBC, **kwargs):
-        center = rootBC.contentPane(**kwargs)
-        center.data('ppp',True)
-        center.radiobutton(value='^ppp',label='Prova radiobutton')
-        center.checkbox(value='^ppp',label='Prova checkbox')
+        fb=rootBC.contentPane(title='Buttons',datapath='buttons').formbuilder(cols=3,border_spacing='10px')
+        
+        fb.checkbox('checkbox',value='^.checkbox')
+        fb.toggleButton('Toggle',iconClass="dijitRadioIcon",value='^.toggle1')
+        fb.toggleButton(iconClass="dijitRadioIcon",value='^.toggle2')
+        
+        fb.radiobutton('Jazz',value='^.radio.jazz',group='genre1')
+        fb.radiobutton('Rock',value='^.radio.rock',group='genre1')
+        fb.radiobutton('Blues',value='^.radio.blues',group='genre1')
+        
+        fb.div('Sex')
+        fb.radiobutton('M',value='^.sex.male',group='genre2')
+        fb.radiobutton('F',value='^.sex.female',group='genre2')
