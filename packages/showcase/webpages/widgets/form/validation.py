4fd29d3baf5f8485cf0cca74b81bcc9b78ded079
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-08-03 08:47:16 +0000

    Fixed some bugs, still some work to do...
    git-svn-id: http://svn.genropy.org/genro/trunk@3041 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/validation.py
+++ packages/showcase/webpages/widgets/form/validation.py
@@ -1,92 +1,75 @@
 #!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
-#
+
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
-
-""" GnrDojo Hello World """
-import os
 
 from gnr.core.gnrbag import Bag
 
 class GnrCustomWebPage(object):
     def main(self, root, **kwargs):
-        tc = root.tabContainer(height='100%',margin='5px',formId='testform', datapath='test')
+        tc = root.tabContainer(margin='5px',formId='testform',datapath='test')
         self.genroValidation(tc)
         tc2 = tc.tabContainer(title='Dojo Validation')
         self.numberTextBox(tc2)
         self.currencyTextBox(tc2)
         self.dateTextBox(tc2)
         self.timeTextBox(tc2)
-        self.validationTextBox(tc2)
         self.textArea(tc2)
-        #self.buttons(tc2)
         
     def genroValidation(self,tc):
-        tc.dataController("""var b = new gnr.GnrBag();
-                             b.setItem('name','pippo');
-                             b.setItem('notnull', true);
-                             b.setItem('iswarning', true);
-                             b.setItem('len', '2:4');
-                             b.setItem('case', 'upper');
-                             b.setItem('min', 10);
-                             b.setItem('max', 100);
-                             SET test.textBox=b;""", nodeId='testform_loader')
-        tc.dataController("var result = GET test;alert(result.toXml());", nodeId='testform_saver')
-        fb=tc.contentPane(title='Genro Validation', datapath='.textBox').formbuilder(cols=2,cellspacing='10')
+   #    tc.dataController("""var bag = new gnr.GnrBag();
+   #                         bag.setItem('name','pippo');
+   #                         bag.setItem('notnull',true);
+   #                         bag.setItem('iswarning',true);
+   #                         bag.setItem('len','2:4');
+   #                         bag.setItem('case','upper');
+   #                         bag.setItem('min',10);
+   #                         bag.setItem('max',100);
+   #                         SET test.textBox=bag;""", nodeId='testform_loader')
+   #    tc.dataController("var result = GET test;alert(result.toXml());", nodeId='testform_saver')
+        fb=tc.contentPane(title='Genro Validation',datapath='.textBox').formbuilder(cols=2,border_spacing='10px')
+   #    
+   #    tc.dataController('genro.formById("testform").load();',_fired='^test.doload')
+   #    fb.button('Load',fire='^test.doload')
+   #    
+   #    fb.button('Save',action='genro.formById("testform").save()',disabled='== !(_changed && _valid)', 
+   #              _changed='^gnr.forms.testform.changed',_valid='^gnr.forms.testform.valid')
+        fb.button('Set Value',action='SET .name="John"',colspan=2)
+        fb.textBox(lbl="Len",value='^.len',tooltip='Here you define the size of \'Name(Len)\' textbox. (Format --> 4:7)')
+        fb.textBox(lbl="Name (Len)",value='^.name4',validate_len='^.len',
+                   validate_len_min='alert("too much short!")',validate_len_max='alert("too much long!")')
+   #    fb.textBox(lbl="Set case validation",value='^.case',colspan=2)
+        fb.textBox(lbl="Name",value='^.name',validate_len='4:',tooltip='Insert 4 or more characters',
+                   validate_onReject='alert("The name "+"\'"+value+"\'"+" is too short")',
+                   validate_onAccept='alert("correct lenght of "+"\'"+value+"\'")')
+        fb.textBox(lbl="Name echo",value='^.name') 
+        fb.textBox(lbl="Name up",value='^.nameup',validate_case='upper')
+        fb.textBox(lbl="Name low",value='^.namelow',validate_case='lower')
+        fb.textBox(lbl="Name capitalized",value='^.namecap',validate_case='capitalize')
+   #    fb.checkbox(lbl="notnull",value='^.notnull')
+   #    fb.textBox(lbl="Name notrim notnull",value='^.namenotrim',trim=False,
+   #               validate_notnull='^.notnull',validate_notnull_error='required!')
+        fb.textBox(lbl="Email",value='^.email',validate_email=True,validate_email_iswarning='^',
+                        validate_email_warning='uncorrected e-mail format')
+        fb.textBox(lbl="contains abc",value='^.nameregex',validate_regex='abc',validate_regex_iswarning='^.iswarning')
+        fb.checkbox(lbl="iswarning",value='^.iswarning',tooltip='if selected, the error in \'contains abc\' box is a warning, not an error') 
+        fb.span('Validation using Callback and Remote function',colspan=2,color='== _email ? "red" : "green"',_email='^.email')
         
-        tc.dataController('genro.formById("testform").load(true);', _fired='^doload',
-                           _onstart='^gnr.onStart')
-        fb.button('Load', fire='doload')
-        fb.button('Save', action='genro.formById("testform").save()',
-                  disabled='== !(_changed && _valid)', 
-                  _changed='^gnr.forms.testform.changed', 
-                  _valid='^gnr.forms.testform.valid')
-        fb.button('Set Value', action='SET .name="giangino"')
-        
-        fb.TextBox(lbl="Len",value='^.len') 
-        fb.TextBox(lbl="Set case validation",value='^.case') 
-        
-        fb.TextBox(lbl="Name", value='^.name', validate_len='4:', nodeId='name', 
-                                        validate_onReject='alert("troppo corto: "+value)',
-                                        validate_onAccept='alert("giusto: "+value)')
-        fb.TextBox(lbl="Name echo",value='^.name') 
-        fb.TextBox(lbl="Name up",value='^.nameup', validate_case='upper')
-        fb.TextBox(lbl="Name low",value='^.namelow', validate_case='lower')
-        fb.TextBox(lbl="Name case",value='^.namecap', validate_case='^.case')
-        fb.TextBox(lbl="Name 4",value='^.name4', validate_len='^.len', validate_len_max='much too long!', validate_len_min='too short !')
-        
-        fb.checkbox(lbl="notnull",value='^.notnull') 
-        fb.TextBox(lbl="Name notrim notnull",value='^.namenotrim', trim=False, 
-                   validate_notnull='^.notnull', validate_notnull_error='required!')
-        fb.TextBox(lbl="Email", value='^.email', validate_email=True, validate_email_iswarning='^', 
-                        validate_email_warning='Email non corretta')
-        
-        fb.checkbox(lbl="iswarning",value='^.iswarning') 
-        fb.TextBox(lbl="contains abc", value='^.nameregex', colspan=2, 
-                        validate_regex='abc', 
-                        validate_regex_iswarning='^.iswarning', 
-                        validate_regex_error='MUST contain "abc"',
-                        validate_regex_warning='SHOULD contain "abc"')
-        fb.span('Validation using Callback and Remote function',colspan=2, color='== _email ? "red" : "green"', _email='^.email')
-        
-        fb.NumberTextBox(lbl="Min",value='^.min') 
-        fb.NumberTextBox(lbl="Max",value='^.max') 
-        
-        fb.NumberTextBox(lbl="between min and max", value='^.namecall', 
-                        validate_max='^.max', validate_min='^.min',
+        fb.numberTextBox(lbl="Min",value='^.min') 
+        fb.numberTextBox(lbl="Max",value='^.max') 
+        fb.numberTextBox(lbl="between min and max",value='^.minmax', 
+                        validate_min='^.min',validate_max='^.max',
                         validate_call="""   
                                        if (value < min){
-                                           return 'min';
+                                           return 'the value inserted is too small';
                                        } else if (value > max){
-                                           return 'max';
-                                       }""",
-                    validate_call_min='!! small!', validate_call_max='!!Big!')
-        
-        fb.TextBox(lbl="remote Name up", value='^.nameremote', 
-                    validate_remote="nameremote", validate_name='^.name', 
-                    validate_remote_error='different from the name field value entered')
-        
+                                           return 'the value inserted is too large';
+                                       }""")
+        fb.textBox(lbl="remote Name",value='^.remote', 
+                    validate_remote="nameremote",validate_name='^.name', 
+                    validate_remote_error='different from name field\'s value inserted')
+                    
     def rpc_nameremote(self, value=None, name=None, **kwargs):
         if not value:
             return
@@ -95,53 +78,29 @@ class GnrCustomWebPage(object):
             result['value'] = value.upper()
             return result
         else:
-            return 'nameremote'
-        
-    def rpc_checkZ(self, value, noletters, **kwargs):
-        for letter in noletters:
-            if letter in value:
-                return False
-        return True
-        
+            return 'remote'
+            
     def numberTextBox(self,tc):
-        fb=tc.contentPane(title='Number',datapath='.numberTextBox').formbuilder(cols=2,cellspacing='10')
-        fb.NumberTextBox(lbl="Age",default=36,value='^.age')
-        fb.NumberTextBox(lbl="Age echo",value='^.age')
+        fb=tc.contentPane(title='numberTextBox',datapath='.numberTextBox').formbuilder(cols=2,border_spacing='10px')
+        fb.numberTextBox(lbl="Age",default=36,value='^.age')
+        fb.numberTextBox(lbl="Age echo",value='^.age')
         
     def currencyTextBox(self,tc):
-        fb=tc.contentPane(title='CurrencyTextBox',datapath='.currencyTextBox').formbuilder(cols=2,cellspacing='10')
-        fb.CurrencyTextBox(lbl="Amount",default=1123.34, currency='EUR', locale='it',value='^.amount')
-        fb.CurrencyTextBox(lbl="Amount echo", currency='EUR', locale='it',value='^.amount')
+        fb=tc.contentPane(title='currencyTextBox',datapath='.currencyTextBox').formbuilder(cols=2,border_spacing='10px')
+        fb.currencyTextBox(lbl="Age",default=1123.34, currency='EUR', locale='it',value='^.amount')
+        fb.currencyTextBox(lbl="Age echo", currency='EUR', locale='it',value='^.amount')
         
     def dateTextBox(self,tc):
-        fb=tc.contentPane(title='DateTextBox',datapath='.dateTextBox').formbuilder(cols=2,cellspacing='10')
-        fb.DateTextBox(lbl='Birthday echo',value='^.birthday')
+        fb=tc.contentPane(title='dateTextBox',datapath='.dateTextBox').formbuilder(cols=2,border_spacing='10px')
+        fb.dateTextBox(lbl='Birthday',value='^.birthday')
+        fb.dateTextBox(lbl='Birthday echo',value='^.birthday')
         
     def timeTextBox(self,tc):
-        fb=tc.contentPane(title='TimeTextBox',datapath='.timeTextBox').formbuilder(cols=2,cellspacing='10')
-        fb.TimeTextBox(lbl='Meeting Time',value='^.meetingAt')
-        fb.TimeTextBox(lbl='Meeting Time echo',value='^.meetingAt')
-        
-    def validationTextBox(self,tc):
-        fb=tc.contentPane(title='ValidationTextBox',datapath='.validationTextBox').formbuilder(cols=2,cellspacing='10')
-        fb.ValidationTextBox(lbl='Email',default='john.brown@somewhere.com',value='^.email')
-        fb.ValidationTextBox(lbl='Email echo',value='^.email')
+        fb=tc.contentPane(title='timeTextBox',datapath='.timeTextBox').formbuilder(cols=2,border_spacing='10px')
+        fb.timeTextBox(lbl='Meeting Time',value='^.meetingAt')
+        fb.timeTextBox(lbl='Meeting Time echo',value='^.meetingAt')
         
     def textArea(self,tc):
-        fb=tc.contentPane(title='TextArea',datapath='.textArea').formbuilder(cols=2,cellspacing='10')
+        fb=tc.contentPane(title='textArea',datapath='.textArea').formbuilder(cols=2,border_spacing='10px')
         fb.textArea(lbl='Remarks',value='^.remarks')
         fb.textArea(lbl='Remarks echo',value='^.remarks')
-        
-    def inLineEditBox(self,tc):
-        fb=tc.contentPane(title='InLineEditBox',datapath='.inLineEditBox').formbuilder(cols=2,cellspacing='10')
-        fb.inlineEditBox(lbl='EditThis',onChange="function(x){alert('xxxx')}::JS").div(value='^.ed1')
-        fb.inlineEditBox(lbl='EditThat',onChange="function(x){alert('xxxx')}::JS",
-                                   autoSave=False,buttonSave='OK',buttonCance='Cancel',
-                                   editor="dijit.form.DateTextBox").div(value='^.ed2')
-        
-    def buttons(self,tc):
-        fb=tc.contentPane(title='Buttons',datapath='.buttons').formbuilder(cols=2,cellspacing='10')
-        fb.button("Load me", action="alert('No way...')",fire='^.button1')
-        fb.toggleButton('Toggle',iconClass="dijitRadioIcon",value='^.toggle1')
-        fb.checkBoxGroup("Rugby,Soccer,Baseball,Tennis",cols=1,border='1px solid silver',padding='5px',value='^.cb')
-        fb.radioGroup('Jazz,Rock,Punk,Metal','genre',cols=4,border='1px solid red',padding='5px',value='^.rb')
