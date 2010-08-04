6565722d0ee869581aa2b6982c815ada331b68b2
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-08-04 08:41:51 +0000

    some cleaning!
    git-svn-id: http://svn.genropy.org/genro/trunk@3046 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/dropdownbutton.py
+++ packages/showcase/webpages/widgets/form/dropdownbutton.py
@@ -9,14 +9,14 @@ from gnr.core.gnrbag import Bag
 class GnrCustomWebPage(object):
     def main(self, root, **kwargs):
         root.data('contents', self.receiptBag())
-        ddb=root.toolbar().dropdownbutton(label='!!Menu',id='string').menu(storepath='contents',action=("alert($1.label)"))
+        ddb=root.toolbar().dropdownbutton(label='!!Menu').menu(storepath='contents',action=("alert($1.label)"))
         
     def receiptBag(self):
         bag = Bag()
         bag2 = Bag()
         bag2['drag']=['foo']
         bag.setItem('r1',None,page=0,label='Credit Card')
-        bag.setItem('r1.aaa','A Bag value ',label='Buy')
+        bag.setItem('r1.aaa','A Bag value',label='Buy')
         bag.setItem('r2',None,label='EFTPOS')
         bag.setItem('r3',None,label='Cheque')
         bag.setItem('r4',None,label='Cash')
