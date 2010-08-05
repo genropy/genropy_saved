377b40e09746f919e59927bb0376df451ecd6d95
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-08-05 09:08:13 +0000

    corrected some bugs, added some tooltips and a text hint to discover them
    git-svn-id: http://svn.genropy.org/genro/trunk@3050 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/validation.py
+++ packages/showcase/webpages/widgets/form/validation.py
@@ -11,10 +11,10 @@ class GnrCustomWebPage(object):
         tc = root.tabContainer(margin='5px',formId='testform',datapath='test')
         self.genroValidation(tc)
         tc2 = tc.tabContainer(title='Dojo Validation')
-        self.numberTextBox(tc2)
-        self.currencyTextBox(tc2)
-        self.dateTextBox(tc2)
-        self.timeTextBox(tc2)
+        self.numberTextbox(tc2)
+        self.currencyTextbox(tc2)
+        self.dateTextbox(tc2)
+        self.timeTextbox(tc2)
         self.textArea(tc2)
         
     def genroValidation(self,tc):
@@ -29,21 +29,21 @@ class GnrCustomWebPage(object):
                             SET test.textBox=bag;""", nodeId='testform_loader')
         tc.dataController("var result = GET test;alert(result.toXml());",nodeId='testform_saver')
         fb=tc.contentPane(title='Genro Validation',datapath='.textBox').formbuilder(cols=2,border_spacing='10px')
-        
         tc.dataController('genro.formById("testform").load({sync:true});',_fired='^test.doload')
+        fb.div('The boxes with (*) have a tooltip for helping code comprehension',colspan=2)
         fb.button(fire='^test.doload',label='Load')
         fb.button(action='genro.formById("testform").save()',disabled='== !(_changed && _valid)', 
                   _changed='^gnr.forms.testform.changed',_valid='^gnr.forms.testform.valid',label='Save')
                   
         fb.button(action='SET .name="John"',colspan=2,label='Set Value')
-        fb.textBox(value='^.len',tooltip='Here you define the size of \'Name(Len)\' textbox. (Format --> 4:7)',lbl="Len")
+        fb.textBox(value='^.len',tooltip='Here you define the size of \'Name(Len)\' textbox. (Format --> 4:7)',lbl="Len (*)")
         fb.textBox(value='^.name4',validate_len='^.len',
                    validate_len_min='alert("too much short!")',validate_len_max='alert("too much long!")',
                    lbl="Name (Len)")
    #    fb.textBox(value='^.case',colspan=2,lbl="Set case validation")
         fb.textBox(value='^.name',validate_len='4:',tooltip='Insert 4 or more characters',
                    validate_onReject='alert("The name "+"\'"+value+"\'"+" is too short")',
-                   validate_onAccept='alert("correct lenght of "+"\'"+value+"\'")',lbl="Name")
+                   validate_onAccept='alert("correct lenght of "+"\'"+value+"\'")',lbl="Name (*)")
         fb.textBox(value='^.name',lbl="Name echo") 
         fb.textBox(value='^.nameup',validate_case='upper',lbl="Name up")
         fb.textBox(value='^.namelow',validate_case='lower',lbl="Name low")
@@ -54,7 +54,8 @@ class GnrCustomWebPage(object):
         fb.textBox(value='^.email',validate_email=True,validate_email_iswarning='^',
                         validate_email_warning='uncorrected e-mail format',lbl="Email")
         fb.textBox(value='^.nameregex',validate_regex='abc',validate_regex_iswarning='^.iswarning',lbl="contains abc")
-        fb.checkbox(value='^.iswarning',tooltip='if selected, the error in \'contains abc\' box is a warning, not an error',lbl="iswarning") 
+        fb.checkbox(value='^.iswarning',lbl="iswarning (*)",tooltip="""if selected, the tooltip in box labelled \'contains
+                                                                       abc\' is \'warning\'; else you get error""") 
         fb.span('Validation using Callback and Remote function',colspan=2,color='== _email ? "red" : "green"',_email='^.email')
         
         fb.numberTextBox(value='^.min',lbl="Min") 
@@ -67,10 +68,10 @@ class GnrCustomWebPage(object):
                                        } else if (value > max){
                                            return 'the value inserted is too large';
                                        }""")
-        fb.textBox(value='^.remote',lbl="remote Name",
-                   validate_remote="nameremote",validate_name='^.name', 
-                   validate_remote_error='different from name field\'s value inserted')
-                    
+        fb.textBox(value='^.remote',validate_remote="nameremote",validate_name='^.name',
+                   validate_remote_error='different from name field\'s value inserted',
+                   tooltip='for validation, insert the same words of box labelled \'Name\'',lbl='remote Name (*)')
+                   
     def rpc_nameremote(self, value=None, name=None, **kwargs):
         if not value:
             return
@@ -81,25 +82,25 @@ class GnrCustomWebPage(object):
         else:
             return 'remote'
             
-    def numberTextBox(self,tc):
-        fb=tc.contentPane(title='numberTextBox',datapath='.numberTextBox').formbuilder(cols=2,border_spacing='10px')
-        fb.numberTextBox(value='^.age',lbl="Age",default=36)
-        fb.numberTextBox(value='^.age',lbl="Age echo")
+    def numberTextbox(self,tc):
+        fb=tc.contentPane(title='numberTextbox',datapath='.numberTextbox').formbuilder(cols=2,border_spacing='10px')
+        fb.numberTextbox(value='^.age',lbl="Age",default=36)
+        fb.numberTextbox(value='^.age',lbl="Age echo")
         
-    def currencyTextBox(self,tc):
-        fb=tc.contentPane(title='currencyTextBox',datapath='.currencyTextBox').formbuilder(cols=2,border_spacing='10px')
-        fb.currencyTextBox(value='^.amount',default=1123.34, currency='EUR', locale='it',lbl="Age")
-        fb.currencyTextBox(value='^.amount',currency='EUR',locale='it',lbl="Age echo")
+    def currencyTextbox(self,tc):
+        fb=tc.contentPane(title='currencyTextbox',datapath='.currencyTextbox').formbuilder(cols=2,border_spacing='10px')
+        fb.currencyTextbox(value='^.amount',default=1123.34, currency='EUR', locale='it',lbl="Age")
+        fb.currencyTextbox(value='^.amount',currency='EUR',locale='it',lbl="Age echo")
         
-    def dateTextBox(self,tc):
-        fb=tc.contentPane(title='dateTextBox',datapath='.dateTextBox').formbuilder(cols=2,border_spacing='10px')
-        fb.dateTextBox(value='^.birthday',lbl='Birthday')
-        fb.dateTextBox(value='^.birthday',lbl='Birthday echo')
+    def dateTextbox(self,tc):
+        fb=tc.contentPane(title='dateTextbox',datapath='.dateTextbox').formbuilder(cols=2,border_spacing='10px')
+        fb.dateTextbox(value='^.birthday',lbl='Birthday')
+        fb.dateTextbox(value='^.birthday',lbl='Birthday echo')
         
-    def timeTextBox(self,tc):
-        fb=tc.contentPane(title='timeTextBox',datapath='.timeTextBox').formbuilder(cols=2,border_spacing='10px')
-        fb.timeTextBox(value='^.meetingAt',lbl='Meeting Time')
-        fb.timeTextBox(value='^.meetingAt',lbl='Meeting Time echo')
+    def timeTextbox(self,tc):
+        fb=tc.contentPane(title='timeTextbox',datapath='.timeTextbox').formbuilder(cols=2,border_spacing='10px')
+        fb.timeTextbox(value='^.meetingAt',lbl='Meeting Time')
+        fb.timeTextbox(value='^.meetingAt',lbl='Meeting Time echo')
         
     def textArea(self,tc):
         fb=tc.contentPane(title='textArea',datapath='.textArea').formbuilder(cols=2,border_spacing='10px')
