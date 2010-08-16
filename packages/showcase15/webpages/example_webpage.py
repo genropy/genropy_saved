7ab598f2b4b72e9da26f1c86aee7bad63764d0ae
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-08-16 15:37:28 +0000

    moving packages showcase15 and test15
    git-svn-id: http://svn.genropy.org/genro/trunk@3090 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/showcase15/webpages/example_webpage.py
@@ -0,0 +1,6 @@
+
+# -*- coding: UTF-8 -*-
+
+class GnrCustomWebPage(object):
+    def main(self, root, **kwargs):
+        pass
---
+++ packages/test15/webpages/dojo/layout/bordercontainer.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+# bordercontainer.py
+# Created by Francesco Porcari on 2010-08-16.
+# Copyright (c) 2010 Softwell. All rights reserved.
+
+class GnrCustomWebPage(object):
+    py_requires="testhandler:TestHandlerBase"
+    dojo_theme='claro'
+
+    def windowTitle(self):
+        return ''
+         
+    def test_bordercontainer(self,pane):
+        bc = pane.borderContainer(height='300px')
+        bc.contentPane(region='top',height='20px',background='red',splitter=True)
+        tc = bc.tabContainer(region='left',width='400px')
+        tc.contentPane(title='aa')
+        tc.contentPane(title='bb')
+        ac = bc.accordionContainer(region='right',width='400px')
+        bc2= ac.borderContainer(title='aa',background='black')
+        bc2.contentPane(region='bottom',height='30px',background='silver')
+        bc2.contentPane(region='center',background='lime')
+        ac.contentPane(title='bb')
+        bc.contentPane(region='center',background='yellow')
