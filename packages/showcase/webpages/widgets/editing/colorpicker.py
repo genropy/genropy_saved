f11827aaa575bdd26ee8ee4dd72a83db87ee383f
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-08-17 08:58:16 +0000

    some cleaning...
    git-svn-id: http://svn.genropy.org/genro/trunk@3092 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/editing/colorpicker.py
+++ packages/showcase/webpages/widgets/editing/colorpicker.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
-#
-#  untitled
-#
+
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
-#
-
-""" GnrDojo Hello World """
-
 
 class GnrCustomWebPage(object):
     def main(self,root,**kwargs):
@@ -18,10 +12,11 @@ class GnrCustomWebPage(object):
                         zoom = zoom || 100;
                         SET aux.zoom_factor= zoom+'%';
                         """,zoom='^aux.zoom', _onStart=True)
-        bottomleft=center.contentPane(region='top',height="225",splitter=True)
-        bottomleft.ColorPicker(value='^aux.color',showHsv=False,showRgb=False,webSafe=False,showHex=False)
-        bottomleft.horizontalslider(lbl='!!Zoom', value = '^aux.zoom', width='200px', 
-                                         minimum=50, maximum=200, float='left',discreteValues=31)
-        bottomleft.numberTextBox(value='^aux.zoom', places=0)
-        square=center.contentPane(region='center').div(padding='20px',height='200px', width='200px',background_color='^aux.color',zoomFactor='^aux.zoom_factor')
-        square.div(height='200px', width='200px', border='1px solid black;').div('Testo')
+        bottomleft=center.contentPane(region='top',height=225,splitter=True)
+        bottomleft.colorPicker(value='^aux.color',showHsv=False,showRgb=False,webSafe=False,showHex=False)
+        bottomleft.horizontalSlider(lbl='!!Zoom',value='^aux.zoom',width='200px', 
+                                         minimum=50,maximum=200,float='left',discreteValues=31)
+        bottomleft.numberTextbox(value='^aux.zoom',places=0)
+        square=center.contentPane(region='center').div(padding='20px',height='200px',width='200px',
+                                                       background_color='^aux.color',zoomFactor='^aux.zoom_factor')
+        square.div(height='200px',width='200px',border='1px solid black;').div('Testo')
