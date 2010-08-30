50db07e19e7629d09636f60461318b89957b6d96
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-08-30 09:23:59 +0000

    register user register connection
    git-svn-id: http://svn.genropy.org/genro/trunk@3183 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/tools/registered_users.py
+++ packages/test15/webpages/tools/registered_users.py
@@ -1,8 +1,6 @@
 # -*- coding: UTF-8 -*-
 # 
 """Registered users tester"""
-import datetime
-from gnr.core.gnrbag import Bag,BagResolver
 class GnrCustomWebPage(object):
     py_requires="testhandler:TestHandlerFull,storetester:StoreTester"
     dojo_theme='claro'
@@ -31,105 +29,4 @@ class GnrCustomWebPage(object):
             
         
     def rpc_curr_users(self):
-        return UserListResolver()
-        
-class UserListResolver(BagResolver):
-    classKwargs={'cacheTime':1,
-                 'readOnly':False,
-                 'connectionId':None,
-                 'user':None}
-    classArgs=['user']
-        
-
-    def load(self):   
-        if not self.user:
-            return self.list_users()
-        else:
-            return self.one_user()
-            
-    def one_user(self):        
-        register = self._page.site.register_user
-        item_user = register.get_register_item(self.user)
-        item = Bag()
-        data = item_user.pop('data',None)
-        item['info'] = Bag([('%s:%s' %(k,str(v).replace('.','_')),v) for k,v in item_user.items()])
-        item['data'] = data
-        item.setItem('connections',ConnectionListResolver(),cacheTime=2)
-        return item
-        
-    def list_users(self):
-        usersDict = self._page.site.register_user.users()
-        result = Bag()
-        for user,item_user in usersDict.items():
-            delta = (datetime.datetime.now()-item_user['start_ts']).seconds
-            user = user or 'Anonymous'
-            itemlabel = user
-            resolver = UserListResolver(user)
-            result.setItem(itemlabel,resolver,cacheTime=1,user=user)
-        return result 
-
-class ConnectionListResolver(BagResolver):
-    classKwargs={'cacheTime':1,
-                 'readOnly':False,
-                 'connectionId':None}
-    classArgs=['connectionId']
-    def load(self):
-        if not self.connectionId:
-            return self.list_connections()
-        else:
-            return self.one_connection()
-            
-    def one_connection(self):        
-        register = self._page.site.register_connection
-        page = register.get_register_item(self.connectionId)
-        item = Bag()
-        data = page.pop('data',None)
-        item['info'] = Bag([('%s:%s' %(k,str(v).replace('.','_')),v) for k,v in page.items()])
-        item['data'] = data
-        item.setItem('pages',PageListResolver(),cacheTime=2)
-        return item
-        
-    def list_connections(self):
-        connectionsDict = self._page.site.register_connection.connections()
-        result = Bag()
-        for connection_id,connection in connectionsDict.items():
-            delta = (datetime.datetime.now()-connection['start_ts']).seconds
-            user = connection['user'] or 'Anonymous'
-            ip =  connection['user_ip'].replace('.','_')
-            connection_name=connection['connection_name']
-            user_agent=connection['user_agent']
-            itemlabel = '%s (%i)' %(connection_name,delta)
-            resolver = ConnectionListResolver(connection_id)
-            result.setItem(itemlabel,resolver,cacheTime=1,connection_id=connection_id)
-        return result 
-
-class PageListResolver(BagResolver):
-    classKwargs={'cacheTime':1,
-                 'readOnly':False,
-                 'pageId':None}
-    classArgs=['pageId']
-    def load(self):
-        if not self.pageId:
-            return self.list_pages()
-        else:
-            return self.one_page()
-    
-    def one_page(self):
-        register = self._page.site.register_page
-        page = register.get_register_item(self.pageId)
-        item = Bag()
-        data = page.pop('data',None)
-        item['info'] = Bag([('%s:%s' %(k,str(v).replace('.','_')),v) for k,v in page.items()])
-        item['data'] = data
-        return item
-        
-    def list_pages(self):
-        pagesDict = self._page.site.register_page.pages()
-        result = Bag()
-        for page_id,page in pagesDict.items():
-            delta = (datetime.datetime.now()-page['start_ts']).seconds
-            pagename= page['pagename'].replace('.py','')
-            itemlabel = '%s (%i)' %(pagename,delta)
-            resolver = PageListResolver(page_id)
-            result.setItem(itemlabel,resolver,cacheTime=1)
-        return result 
+        return self.site.register_user.user_tree()
