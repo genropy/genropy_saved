f9ae657ed14cb981ec235c121b109ac93fe4f039
robertolupi <robertolupi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-09-09 10:57:41 +0000

    includedView sample in showcase
    git-svn-id: http://svn.genropy.org/genro/trunk@3246 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/components/public/includedView.py
+++ packages/showcase/webpages/components/public/includedView.py
@@ -4,18 +4,38 @@
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
 
-from gnr.sql.gnrsql_exceptions import RecordNotExistingError
+from gnr.core.gnrbag import Bag
 
 class GnrCustomWebPage(object):
-    py_requires = 'public:Public'
-    def main(self, root, **kwargs):
-        box = root.div('press me',connect_onclick='FIRE aux.showDialog')
-        dlgBC = self.hiddenTooltipDialog(root, dlgId='dlgProdChar', 
-                                        close_action='FIRE aux_job.charChanged',
-                                        title="!!showDialog",
-                                        width="27em",height="15ex",
-                                        fired='^aux_job.showDialog')
-        dlgPane = dlgBC.contentPane(region='center',_class='pbl_roundedGroup',margin='2px',padding='5px')                                     
-        fb = dlgPane.formbuilder(border_spacing='6px',margin_left='1em',datapath='dlg.form')
-        fb.textbox(value='^.a',lbl='Foo')
-        fb.textbox(value='^.b',lbl='Spam')
+    py_requires="foundation/includedview"
+    def main(self, root, **kwargs):        
+        tc = root.tabContainer()
+        # self.ivDbSelection(tc, title="Db Selection")
+        self.ivInline(tc, title="Inline")
+    
+    def ivInline(self, parent, **kwargs):
+        """An includedview on a datastore's path."""
+        bc = parent.borderContainer(**kwargs)
+        
+        b = Bag()
+        b.addItem('r0',Bag(dict(name="name", description="Full name", size=40, type="U")))
+        b.addItem('r1',Bag(dict(name="birth date", description="Birth date", size=10, type="D")))
+        
+        bc.data('inline_data', b)
+        
+        def structInlineData(struct):
+            r = struct.view().rows()
+            r.cell('name', name='!!Name', width='20ex')
+            r.cell('description', name='!!Description', width='40ex')
+            r.cell('size', name='!!Size', width='5ex')
+            r.cell('type', name='!!Type', width='20ex')
+        
+        iv = self.includedViewBox(bc,label='!!Inline IncludedView', locked=False,
+                            datamode="bag",
+                            storepath='inline_data', struct=structInlineData,
+                            autoWidth=True, add_action=True,del_action=True)
+        ge = iv.gridEditor()
+        ge.textbox(gridcell="name")
+        ge.textbox(gridcell="description")
+        ge.numberspinner(gridcell="size", min=1, max=100)
+        ge.filteringselect(gridcell="type", values="I:Int,R:Real,S:String,U:Unicode,D:Date,H:Time,DH:Date and Time,X:Bag")
