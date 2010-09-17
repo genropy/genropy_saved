e1dd65875ee2325f8b202c190ad08620acbda28b
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-09-17 07:20:18 +0000

    added __init__ to showcase webpages to include them in sphinx
    git-svn-id: http://svn.genropy.org/genro/trunk@3366 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/sys/webpages/index.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+# untitled.py
+# Created by Giovanni Porcari on 2010-08-29.
+# Copyright (c) 2010 Softwell. All rights reserved.
+
+class GnrCustomWebPage(object):
+    #py_requires='public:Public'
+
+    def pageAuthTags(self, method=None, **kwargs):
+        return ''
+        
+    def windowTitle(self):
+        return ''
+         
+    def main(self, root, **kwargs):
+        print x
+        root.div('not existing')
+        return
