04b9fe0561e9bf43e307fab91954649116acb28d
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-09-17 13:45:50 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3396 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/slider.py
+++ packages/showcase/webpages/widgets/form/slider.py
@@ -10,19 +10,37 @@ class GnrCustomWebPage(object):
     py_requires="gnrcomponents/testhandler:TestHandlerFull"
     # dojo_theme='claro'    # !! Uncomment this row for Dojo_1.5 usage
     
-    """ Here we introduce the sliders. It is a scale with a handle you can drag left/right for
-    horizontal slider (or up/down for vertical one) to select a value.
-    They are form widgets inherit from Dojo.
-    
-    - slider's attributes (and default values)
-        default='NUMBER'        Add a default value in your slider (0)
-        minimum='NUMBER'        Add the minimum value of a slider (0)
-        maximum='NUMBER'        Add the maximum value of a slider (100)
-        intermediateChanges     (Boolean) If True, changes value of slider DURING the mouse move (False)
-        
-    It is strongly advised to use:
-        - "width" attribute for horizontal slider.
-        - "height" attribute for vertical slider. """
+    """
+    =========
+     Sliders
+    =========
+
+    .. currentmodule:: form
+
+    .. class:: Sliders -  Genropy sliders
+
+    	Here we introduce the sliders, form widgets inherit from Dojo. It is a scale with a handle you can drag left/right for horizontal slider (or up/down for vertical one) to select a value.
+
+    	+-------------------------+---------------------------------------------------------+-------------+
+    	|   Attribute             |          Description                                    |   Default   |
+    	+=========================+=========================================================+=============+
+    	| ``default='NUMBER'``    | Add a default value in your slider                      |  ``0``      |
+    	+-------------------------+---------------------------------------------------------+-------------+
+    	| ``intermediateChanges`` | (Boolean) If True, it allows to changes value of slider |  ``False``  |
+    	|                         | during slider move                                      |             |
+    	+-------------------------+---------------------------------------------------------+-------------+
+    	| ``maximum='NUMBER'``    | Add the maximum value of a slider                       |  ``100``    |
+    	+-------------------------+---------------------------------------------------------+-------------+
+    	| ``minimum='NUMBER'``    | Add the minimum value of a slider                       |  ``0``      |
+    	+-------------------------+---------------------------------------------------------+-------------+
+
+    	It is strongly advised to use "width" attribute for horizontal slider and "height" attribute for vertical slider.
+
+    		Example::
+
+    			pane.horizontalSlider(value='^integer_number',width='200px',
+    								  maximum=50,discreteValues=51)
+    """
     
     #   - Other forms, attributes and items:
     #       In this section we report forms/attributes that have been used in this example
@@ -36,7 +54,7 @@ class GnrCustomWebPage(object):
     #       comboBox        --> combobox.py
     #       filteringSelect --> filteringSelect.py
     #       formbuilder     --> formbuilder.py
-    #       numberTextBox   --> box.py
+    #       numberTextBox   --> textbox.py
     #       splitter        --> webpages/widgets/layout/border.py
     #       value           --> datapath.py
     
