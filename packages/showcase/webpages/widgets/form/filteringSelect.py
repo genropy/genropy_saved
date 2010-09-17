7cb4b826a23fcb96be536967d57d96eee3078575
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-09-17 14:10:44 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3400 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/form/filteringSelect.py
+++ packages/showcase/webpages/widgets/form/filteringSelect.py
@@ -12,24 +12,68 @@ class GnrCustomWebPage(object):
     py_requires="gnrcomponents/testhandler:TestHandlerFull"
     # dojo_theme='claro'    # !! Uncomment this row for Dojo_1.5 usage
     
-    """ The filteringSelect is a text field who suggests to user the possible (and unique!) entries 
-    of his selection.
+    """
+    =================
+     filteringSelect
+    =================
+
+    .. currentmodule:: form
+
+    .. class:: filteringSelect -  Genropy filteringSelect
+
+    	The filteringSelect is a text field who suggests to user the possible (and unique!) entries of his selection.
+
+    	FilteringSelect's values are composed by a key and a value (like the Python dictionary's elements): user can chooses from values, while in database the user's choice is saved through keys. User can also freely type text and partially matched values will be shown in a pop-up menu below the input text box.
+
+    		+----------------+----------------------------------------------------------+-------------+
+    		|   Attribute    |          Description                                     |   Default   |
+    		+================+==========================================================+=============+
+    		| ``ignoreCase`` | If True, user can write in filteringSelect ignoring case |  ``True``   |
+    		+----------------+----------------------------------------------------------+-------------+
+    		| ``values``     | Contains all the entries from which users have to choose |  ``None``   |
+    		+----------------+----------------------------------------------------------+-------------+
+
+    	The main two modes to fill a filteringSelect are:
+
+    	    - through a bag
+
+    	        Example::
+
+    	            def main(self,root,**kwargs):
+    	                fb = root.formbuilder(datapath='test2',cols=2)
+    	                fb.filteringSelect(value='^.bag',storepath='bag')
+
+    	            def sports(self,**kwargs):
+    	                mytable=Bag()
+    	                mytable['r1.pkey']='SC'
+    	                mytable['r1.Description']='Soccer'
+    	                mytable['r2.pkey']='BK'
+    	                mytable['r2.Description']='Basket'
+    	                mytable['r3.pkey']='TE'
+    	                mytable['r3.Description']='Tennis'
+    	                mytable['r4.pkey']='HK'
+    	                mytable['r4.Description']='Hockey'
+    	                mytable['r5.pkey']='BB'
+    	                mytable['r5.Description']='Baseball'
+    	                mytable['r6.pkey']='SB'
+    	                mytable['r6.Description']='Snowboard'
+    	                return mytable
+
+    	    See "datapath" for more details.
+
+    	    - through the "values" attributes
+
+    	        Example::
+
+    	            def main(self,root,**kwargs):
+    	                pane.filteringSelect(value='^sports',
+    	                                 values='SC:Soccer,BK:Basket,HK:Hockey,TE:Tennis,BB:Baseball,SB:Snowboard')
+
+    	Pay attention not to confuse the "value" attribute with the "values" attribute: "value" is used to allocate user data in a well determined datapath, while "values" is used to fill the filteringSelect.
+
+    	Warning: unlike Dojo, actually filteringSelect doesn't warn user for its wrong insertion. You can add a warning for the user through a "validate" attribute.
     
-    FilteringSelect's values are composed by a key and a value (like the Python dictionary's elements):
-    user can chooses from values, while in database the user's choice is saved through keys.
-    User can also freely type text and partially matched values will be shown in a pop-up menu below the 
-    input text box.
-    
-    FilteringSelect widgets are dojo.data-enabled. This means rather than embedding all the OPTION tags within 
-    the page, you can have dojo.data fetch them from a server-based store. The unified dojo.data architecture can get 
-    its data from various places such as databases and web services.
-    
-    Warning: actually filteringSelect doesn't warn user for its wrong insertion. You can
-    add a warning for the user through a "validate" attribute.
-    
-    - filteringSelect's attributes (and default values)
-        ignoreCase (True)       If True, user can write in filteringSelect ignoring case.
-        values                  Contains all the entries from which users have to choose. """
+    """
     
     #   - Other forms and attributes:
     #       In this section we report forms/attributes that have been used in this example
