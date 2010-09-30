e0b703fc14f5e6976d8f4ca43b541b68d5989a78
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-09-30 18:00:39 +0000

    subscribtion and publish pattern in DataControllers, dataFormula and dataRpc
    PUBLISH 
    
    git-svn-id: http://svn.genropy.org/genro/trunk@3751 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/controllers/publish.py
+++ packages/test15/webpages/controllers/publish.py
@@ -13,7 +13,7 @@ class GnrCustomWebPage(object):
     def test_1_publish_subscribe(self,pane):
         pane.textbox(value='^test')
         pane.button('publish',action='genro.publish("pressed",arg,"miao")',arg='=test')
-        pane.dataController("console.log(pressed[0]); console.log(pressed[1]);",
+        pane.dataController("console.log(arguments);console.log(pressed[0]); console.log(pressed[1]);",
                             subscribe_pressed=True)
         #pane.div(subscribe_test_pressed='var args =arguments; genro.bp(args);')
     
@@ -21,3 +21,10 @@ class GnrCustomWebPage(object):
         pane.button('publish',action='PUBLISH pressed_2 = "bau","miao";')
         pane.dataController("console.log(pressed_2[0]); console.log(pressed_2[1]);",
                             subscribe_pressed_2=True)
+
+    def test_3_publish_double_subscription(self,pane):
+        pane.button('publish a',action='PUBLISH pressed_a = "bau","miao"')
+        pane.button('publish b',action='PUBLISH pressed_b = "tau"')
+
+        pane.dataController("console.log(arguments)",
+                            subscribe_pressed_a=True,subscribe_pressed_b=True)
