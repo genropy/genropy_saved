5291c3a179dc6e06ea517130c9598d0faee3ef6d
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-10-05 21:09:25 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3841 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/tools/test_htable_picker.py
+++ packages/test15/webpages/tools/test_htable_picker.py
@@ -10,13 +10,57 @@ class GnrCustomWebPage(object):
     py_requires='gnrcomponents/testhandler:TestHandlerFull,gnrcomponents/htablehandler:HTablePicker'
     
     htable = 'base.category'
-    grid_table = 'base.pt_product'
-    grid_where = '$category_code IN :codes'
+    grid_table = 'base.pr_product'
+    grid_where = '@category_id.code IN :codes'
     
     def test_1_testpicker(self,pane):
         """Picker on htable"""
-        pane.button('Show',fire='#struct_picker_dlg.open')
+        fb = pane.formbuilder(cols=1, border_spacing='3px')
+        fb.textbox(value='^.codes',lbl='Codes')
+        fb.button('Show',action='PUBLISH picker_1_open;')
+        self.htablePicker(pane,table=self.htable, 
+                            nodeId='picker_1',datapath='.struct_picker',
+                            input_codes='=.#parent.codes',
+                            onAccept='console.log(pkeys); console.log(codes);',
+                            onChange='console.log(pkeys); console.log(codes);',
+                            dialogPars=dict(width='600px'))
+    
+    def __test_2_testpicker(self,pane):
+        """Picker on related table"""
+        pane.button('Show',fire='.struct_picker.open')
+        self.htablePicker(pane,table=self.htable,
+                            grid_columns=self.db.table(self.grid_table).baseViewColumns(),
+                            grid_where=self.grid_where,grid_table=self.grid_table,
+                            resultpath=None,nodeId='struct_picker2',datapath='.struct_picker',
+                            dialogPars=dict(width='600px'))
+                            
+    def __test_3_testpicker(self,pane):
+        fb = pane.formbuilder(cols=1, border_spacing='3px')
+        fb.textbox(value='^.codes',lbl='Codes')
+        fb.button('Show',fire='.struct_picker.open')
+        
+        self.htablePicker(pane,table=self.htable,
+                            grid_columns=self.db.table(self.htable).baseViewColumns(),
+                            input_codes='=.#parent.codes',
+                            resultpath=None,nodeId='struct_picker',datapath='.struct_picker',
+                            dialogPars=dict(width='600px'))
+                            
+
+
+    def __test_4_testpicker(self,pane):
+        """Picker on related table"""
+        pane.button('Show',fire='.struct_picker.open')
+        
+        
         self.htablePicker(pane,table=self.htable,column='id', 
                             grid_columns=self.db.table(self.grid_table).baseViewColumns(),
                             grid_where=self.grid_where,grid_table=self.grid_table,
-                            resultpath=None,nodeId='struct_picker',datapath='.struct_picker',dialogPars=dict(width='400px'))
+                            input_pkeys='=',
+                            onAccept='console.log(pkeys); console.log(codes);',
+                            onChange='console.log(pkeys); console.log(codes);',
+                            nodeId='struct_picker2',datapath='.struct_picker',
+                            dialogPars=dict(width='600px'))
+                            
+                            
+                            
+                            
