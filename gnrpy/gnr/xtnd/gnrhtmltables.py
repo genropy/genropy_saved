abd57d5cff7ec013e03524ab0aaaa133fe96883f
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-10-05 14:23:06 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3837 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/xtnd/gnrhtmltables.py
+++ gnrpy/gnr/xtnd/gnrhtmltables.py
@@ -220,7 +220,7 @@ class PageTableBuilder(TableBuilder):
         cssname = self.page.getResourceUri('html_tables/html_tables_print','css')
         all_css.append('@import url("%s") %s;' % (cssname, 'print'))
         
-        for cssname in self.page.get_css_requires():
+        for cssname in self.page.get_css_path():
             all_css.append('@import url("%s");' % cssname)
         
         html = gnrstring.templateReplace(html, dict(all_css='\n'.join(all_css), 
