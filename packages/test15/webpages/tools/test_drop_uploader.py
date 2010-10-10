149d7c62af40fc5ecabe17fda5b32568a34b63d6
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-10-10 17:47:50 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3880 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/test15/webpages/tools/test_drop_uploader.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+# batch_handler.py
+# Created by Francesco Porcari on 2010-10-01.
+# Copyright (c) 2010 Softwell. All rights reserved.
+
+
+"""Test drop uploader"""
+class GnrCustomWebPage(object):
+    py_requires="""gnrcomponents/testhandler:TestHandlerFull,
+                   gnrcomponents/drop_uploader/drop_uploader:DropUploader"""
+
+        
+    def test_1_uploader(self,pane):
+        """File Uploader"""
+        self.dropUploader(pane)
+        
+        
