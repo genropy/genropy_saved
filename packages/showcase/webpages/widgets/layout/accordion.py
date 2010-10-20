57ea853b7c5c8cbbd9fe1d5d00f7a23e51bf86cf
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-10-20 10:12:50 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@3978 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/widgets/layout/accordion.py
+++ packages/showcase/webpages/widgets/layout/accordion.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
 
-#  Created by Giovanni Porcari on 2007-03-24.
-#  Copyright (c) 2007 Softwell. All rights reserved.
+# accordion.py
+# Created by Niso on 2010-09-01.
+# Copyright (c) 2010 Softwell. All rights reserved.
+
+"""Accordion container"""
 
 class GnrCustomWebPage(object):
-    def main(self, root, **kwargs):
-        ac = root.accordionContainer(margin='1em')
-        ac.accordionPane(title='Pane one')
-        ac.accordionPane(title='Pane two')
-        ac.accordionPane(title='Pane three')
+    py_requires="gnrcomponents/testhandler:TestHandlerFull"
+    # dojo_theme='claro'    # !! Uncomment this row for Dojo_1.5 usage
+    
+    # For an exhaustive documentation, please see http://docs.genropy.org/widgets/layout/accordioncontainer.html
+    
+    def test_1_basic(self,pane):
+        """Basic accordion container"""
+        pane.div("""In this example we will show you an example of an accordion container.""",
+                    font_size='.9em',text_align='justify',margin_bottom='10px')
+        ac = pane.accordionContainer(height='300px')
+        ap1 = ac.accordionPane(title='Pane one')
+        ap1.div("""Click on the "Pane three"!""",
+                font_size='.9em',text_align='justify',margin='10px')
+        ap2 = ac.accordionPane(title='Pane two')
+        ap3 = ac.accordionPane(title='Pane three')
+        ap3.div("""The content of a pane will be showned when user chooses the corresponding pane.""",
+                font_size='.9em',text_align='justify',margin='10px')
