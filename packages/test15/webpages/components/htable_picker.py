633b5c2e3326dfb15fc4f221465cbb0e4f3f85f3
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-10-20 15:49:39 +0000

    plainView in htablehandler
    git-svn-id: http://svn.genropy.org/genro/trunk@3982 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/components/htable_picker.py
+++ packages/test15/webpages/components/htable_picker.py
@@ -9,7 +9,7 @@
 class GnrCustomWebPage(object):
     py_requires='gnrcomponents/testhandler:TestHandlerFull,gnrcomponents/htablehandler:HTablePicker'
     htable = 'base.category'
-    related_table = 'base.pr_product'
+    related_table = 'base.ct_catalog'
     relation_path = '@category_id.code'
     
     def test_1_testpicker(self,pane):
@@ -22,21 +22,35 @@ class GnrCustomWebPage(object):
                             input_codes='=.#parent.codes',
                             output_codes='.#parent.codes')
     
+
+    
     def test_2_testpicker(self,pane):
         """Picker on related table"""
         fb = pane.formbuilder(cols=1, border_spacing='3px')
         fb.textbox(value='^.pkeys',lbl='Related Pkeys',width='30em')
         fb.div(value='^.output_pkeys',lbl='Related Output Pkeys',width='30em',height='20px',background='red')
         fb.button('Show',action='PUBLISH picker_2_open;')
+        def struct(struct):
+            r = struct.view().rows()
+            r.fieldcell('code', name='Code', width='12em')
+            r.fieldcell('description', name='Description', width='20em')
+            r.fieldcell('rec_type',name='Type',width='4em')
+
         self.htablePickerOnRelated(pane,table=self.htable,
                             related_table=self.related_table,
                             input_pkeys='=.#parent.pkeys',
                             output_pkeys='.#parent.output_pkeys',
-                            relation_path=self.relation_path,leaf_type='livello_due',
-                            nodeId='picker_2',datapath='.struct_picker')
+                            relation_path=self.relation_path,
+                            grid_struct=struct,
+                            condition='$rec_type IN :rec_types',
+                            condition_pars=dict(rec_types=['I','P','L']),
+                            nodeId='picker_2',datapath='.struct_picker',grid_applymethod='rec_type_differences')
+    
+    def rpc_rec_type_differences(self,selection):
+        print x
                             
                             
-    def test_3_testpicker_bc(self,pane):
+    def __test_3_testpicker_bc(self,pane):
         """Picker on bc"""
 
         bc = pane.borderContainer(height='400px')
@@ -48,9 +62,10 @@ class GnrCustomWebPage(object):
                             grid_columns=self.db.table(self.htable).baseViewColumns(),
                             input_codes='=.#parent.codes',output_codes='.#parent.codes',
                             resultpath=None,nodeId='picker_3',
+                            
                             datapath='.struct_picker',editMode='bc')
     
-    def test_4_testpicker_bc(self,pane):
+    def __test_4_testpicker_bc(self,pane):
         """Picker on bc"""
 
         bc = pane.borderContainer(height='400px')
