c4afc2bc504e1f6993c17204f625f7f4d1cb1dab
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-10-29 15:08:43 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4072 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/flib/webpages/item_uploader.py
+++ packages/flib/webpages/item_uploader.py
@@ -11,22 +11,43 @@ class GnrCustomWebPage(object):
     
     def main(self, root, **kwargs):
         bc,top, bottom =self.pbl_rootBorderContainer(root,title='!!Upload file',datapath='uploader')
-        self.htablePicker(bc.contentPane(region='left',width='150px'),nodeId='category_picker',
-                            table='flib.category',
+        left = bc.contentPane(region='left',width='150px',_class='pbl_roundedGroup',margin='2px',splitter=True)
+        left.div('!!Categories',_class='pbl_roundedGroupLabel')
+        self.htablePicker(left,nodeId='category_picker',
+                            table='flib.category',datapath='category_picker',
                             output_pkeys='selected_categories',editMode='bc')
-
-        self.uploader_pane(bc.contentPane(region='center'))
+        left.dataController("FIRE reload_saved_files",_fired="^selected_categories")
+        self.uploader_pane(bc.contentPane(region='center',margin='2px'))
        
+        
+       
+    def saved_files_struct(self):
+        struct = self.newGridStruct()
+        r = struct.view().rows()
+        r.cell("title",width='10em',title='!!Title')
+        r.cell("description",width='15em',title='!!Description')
+        r.cell("thumb",width='5em',title='!!Thumb')
+        return struct
+    
     def uploader_pane(self,pane):
         def footer(footer,**kwargs):
             footer.button('Upload',action='PUBLISH flib_uploader_upload',float='right')
-                                
+        
+        savedFilesGrid=dict(method='get_uploaded_files',
+                                                selected_categories='=selected_categories',
+                                                reloader='^reload_saved_files',
+                                                current_range='=current_range')
+        savedFilesGrid['struct'] = self.saved_files_struct()            
+            
         self.dropFileGrid(pane,uploaderId='flib_uploader',datapath='.drop_filegrid',
-                            label='!!Uploaded files',uploadPath='site:flib/items',
+                            label='!!Upload files',uploadPath='site:flib/items',
                             metacol_title=dict(name='!!Title',width='10em'),
                             metacol_description=dict(name='!!Descripton',width='15em'),
+                            external_fileaction_resize48=dict(command='resize',dir='48x48',width=48),
+                            external_fileaction_resize64=dict(command='resize',filetype='jpg'),
                             external_categories='=selected_categories',preview=True,
-                            footer=footer)
+                            footer=footer,onResult='FIRE reload_saved_files;',
+                            savedFilesGrid=savedFilesGrid)
         pane.dataController("console.log('fatto');",subscribe_flib_uploader_done=True)
         
     def onUploading_flib_uploader(self,file_url=None,file_path=None,categories=None,
@@ -47,3 +68,19 @@ class GnrCustomWebPage(object):
         for category_id in categories:
             cat_table.insert(dict(category_id=category_id,item_id=item_record['id']))
         self.db.commit()
+    
+    def rpc_get_uploaded_files(self,selected_categories=None,current_range=None):
+        tblobj = self.db.table('flib.item_category')
+        categories = selected_categories.split(',')
+        current_range = current_range or 20
+        data = tblobj.query(columns='@item_id.url AS url, @item_id.title AS title, @item_id.description AS description',
+                        where='$category_id IN :categories', 
+                        categories=categories,limit=current_range,
+                        order_by='$__ins_ts').selection().output('grid')
+        for n in data:
+           n.attr['thumb']  = '<img border=0 width="40px" height="40px" src="%s" />' %n.attr['url']
+           print n.attr
+        return data
+        
+        
+        
