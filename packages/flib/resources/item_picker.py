371cdbaf0b6bd3104dcf97760145bf1ed133601f
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-11-03 13:11:29 +0000

    drop uploader
    git-svn-id: http://svn.genropy.org/genro/trunk@4085 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/flib/resources/item_picker.py
+++ packages/flib/resources/item_picker.py
@@ -39,7 +39,8 @@ class FlibPicker(BaseComponent):
             r = struct.view().rows()
             r.fieldcell("@item_id.title",width='10em',title='!!Title')
             r.fieldcell("@item_id.description",width='15em',title='!!Description')
-            r.cell("thumb",width='5em',title='!!Thumb',calculated=True)
+            r.cell("_thumb",width='5em',title='!!Thumb',calculated=True)
+            
         pickerGridId = '%s_grid' %pickerId
         
         self.includedViewBox(center.borderContainer(region='top',height='50%'),label='!!Items',
@@ -47,70 +48,24 @@ class FlibPicker(BaseComponent):
                              nodeId=pickerGridId,table='flib.item_category',autoWidth=True,
                              struct=saved_files_struct,hiddencolumns='$item_id,@item_id.ext AS ext,@item_id.metadata AS meta',
                              reloader='^.#parent.tree.code',
+                             drag_value_cb='var dataNode = event.widget.dataNodeByIndex(event.rowIndex); return {flib_element:dataNode.attr.item_id};',
                              filterOn='@item_id.title,@item_id.description',
                              selectionPars=dict(where="@category_id.code LIKE :cat_code || '%%'",
                                                 cat_code='=.#parent.tree.code',
-                                                applymethod='flibUpdateThumb',apply_checked_pkeys='=.#parent.checked_pkeys',
+                                                applymethod='flibPickerThumb',
                                                 order_by='@item_id.title'))
-       #center.dataController("""
-       #                        
-       #                        var params = %s_row_checked;
-       #                        var checked = params[1];
-       #                        var item_id = params[2]['item_id'];
-       #                        var checked_pkeys = checked_pkeys || [];
-       #                        if(checked){
-       #                        
-       #                            checked_pkeys.push(item_id);
-       #                            console.log('adding')
-       #
-       #                            console.log(checked_pkeys)
-       #                        }
-       #                        else{
-       #                            var ind = dojo.indexOf(checked_pkeys,item_id);
-       #                            if(ind>=0){
-       #                                checked_pkeys.splice(ind,1);
-       #                                console.log('removing')
-       #                                console.log(checked_pkeys)
-       #                            }
-       #                        }
-       #                        SET .checked_pkeys =checked_pkeys;
-       #                        FIRE .checked_pkeys_changed;
-       #                      """ %pickerGridId,checked_pkeys='=.checked_pkeys',
-       #                      
-       #                    **{'subscribe_%s_row_checked' %pickerGridId:True})
-                            
-      # def item_struct(struct):
-      #     r = struct.view().rows()
-      #     r.fieldcell("title",width='10em',title='!!Title')
-      #     r.fieldcell("description",width='15em',title='!!Description')
-      #     r.cell("thumb",width='5em',title='!!Thumb',calculated=True)
-      #     r.cell('delete',format_isbutton=True, name=' ',calculated=True,width='18px',
-      #             format_buttonclass='icnBaseDelete',
-      #             format_onclick='console.log(arguments);')
-      #     
-      # self.includedViewBox(center.borderContainer(region='center'),datapath='.picked_grid',
-      #                      label='!!Picked items',
-      #                      nodeId='%s_pickedgrid' %pickerId,table='flib.item',autoWidth=True,
-      #                      struct=item_struct,hiddencolumns='$ext AS ext,$metadata AS meta',
-      #                      reloader='^.#parent.checked_pkeys_changed',
-      #                      selectionPars=dict(where="$id IN :checked_pkeys",
-      #                                         checked_pkeys='=.#parent.checked_pkeys',
-      #                                         applymethod='flibUpdateThumb'
-      #                                         ))
-      # 
+
                                                 
-    def rpc_flibUpdateThumb(self,selection,checked_pkeys=None):
-        checked_pkeys = checked_pkeys or []
+    def rpc_flibPickerThumb(self,selection):
         def apply_thumb(row):
-            _checked = False
-            ext_img = self.getResourceUri('filetype_icons/%s.png'%row['ext'].lower()) \
+            ext_img = self.getResourceUri('filetype_icons/%s.png'%row['ext'][1:].lower()) \
                       or self.getResourceUri('filetype_icons/_blank.png')
             if row['meta']:
                 metabag = Bag(row['meta'])
-            if row['item_id'] in checked_pkeys:
-                _checked = True
-            return dict(thumb= '<img border=0 src="%s" height="32px" />' %(metabag['thumb32_url'] or ext_img),_checked=_checked)
+            return dict(_thumb= '<img border=0 src="%s" height="32px" />' %(metabag['thumb32_url'] or ext_img))
         selection.apply(apply_thumb)
+    
+    
     
     
 
