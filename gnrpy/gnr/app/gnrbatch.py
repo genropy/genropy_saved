f00c8e29f07a0602daae0536fc3b01d426212c57
mbertoldi <mbertoldi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-11-15 17:10:41 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4210 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/app/gnrbatch.py
+++ gnrpy/gnr/app/gnrbatch.py
@@ -11,6 +11,54 @@ from time import time
 from collections import defaultdict
 import os
 import datetime
+'''
+Character width dictionary and convenience functions for column sizing
+with xlwt when Arial 10 is the standard font.  Widths were determined
+experimentally using Excel 2000 on Windows XP.  I have no idea how well
+these will work on other setups.  For example, I don't know if system
+video settings will affect the results.  I do know for sure that this
+module won't be applicable to other fonts in general.
+
+//John Yeung  2009-09-02
+'''
+
+charwidths = {
+    '0': 262.637,'1': 262.637,'2': 262.637,'3': 262.637,'4': 262.637,'5': 262.637,'6': 262.637,
+    '7': 262.637,'8': 262.637,'9': 262.637,'a': 262.637,'b': 262.637,'c': 262.637,'d': 262.637,
+    'e': 262.637,'f': 146.015,'g': 262.637,'h': 262.637,'i': 117.096,'j': 88.178,'k': 233.244,
+    'l': 88.178,'m': 379.259,'n': 262.637,'o': 262.637,'p': 262.637,'q': 262.637,'r': 175.407,
+    's': 233.244,'t': 117.096,'u': 262.637,'v': 203.852,'w': 321.422,'x': 203.852,'y': 262.637,
+    'z': 233.244,'A': 321.422,'B': 321.422,'C': 350.341,'D': 350.341,'E': 321.422,'F': 291.556,
+    'G': 350.341,'H': 321.422,'I': 146.015,'J': 262.637,'K': 321.422,'L': 262.637,'M': 379.259,
+    'N': 321.422,'O': 350.341,'P': 321.422,'Q': 350.341,'R': 321.422,'S': 321.422,'T': 262.637,
+    'U': 321.422,'V': 321.422,'W': 496.356,'X': 321.422,'Y': 321.422,'Z': 262.637,' ': 146.015,
+    '!': 146.015,'"': 175.407,'#': 262.637,'$': 262.637,'%': 438.044,'&': 321.422,'\'': 88.178,
+    '(': 175.407,')': 175.407,'*': 203.852,'+': 291.556,',': 146.015,'-': 175.407,'.': 146.015,
+    '/': 146.015,':': 146.015,';': 146.015,'<': 291.556,'=': 291.556,'>': 291.556,'?': 262.637,
+    '@': 496.356,'[': 146.015,'\\': 146.015,']': 146.015,'^': 203.852,'_': 262.637,'`': 175.407,
+    '{': 175.407,'|': 146.015,'}': 175.407,'~': 291.556}
+
+
+def colwidth(n):
+    '''Translate human-readable units to BIFF column width units'''
+    if n <= 0:
+        return 0
+    if n <= 1:
+        return n * 456
+    return 200 + n * 256
+
+def fitwidth(data, bold=False):
+    '''Try to autofit Arial 10'''
+    units = 220
+    for char in str(data):
+        if char in charwidths:
+            units += charwidths[char]
+        else:
+            units += charwidths['0']
+    if bold:
+        units *= 1.1
+    return max(units, 700) # Don't go smaller than a reported width of 2
+
 class GnrBatch(object):
     thermo_rows = 1
     def __init__(self,data=None,runKwargs=None, thermocb = None, thermoId=None,thermofield='*',thermodelay=0.5,**kwargs):
@@ -132,6 +180,7 @@ class SelectionToXls(GnrBatch):
         super(SelectionToXls,self).__init__(data=data,**kwargs)
         self.tblobj = self.page.db.table(table)
         self.thermo_maximum[0] = self.data_counter()
+        self.colsizes=dict()
         
     def thermo_chunk_message(self, chunk, row):
         if self.thermofield=='*':
@@ -158,6 +207,7 @@ class SelectionToXls(GnrBatch):
         hstyle.font = font0
         for c,header in enumerate(self.columns):
             self.sheet.write(0, c, self.colHeaders[header], hstyle)
+            self.colsizes[c]=max(self.colsizes.get(c,0),fitwidth(str(self.colHeaders[header])))
         self.current_row=1
         
     def process_chunk(self, chunk):
@@ -174,6 +224,7 @@ class SelectionToXls(GnrBatch):
                 style=self.date_style
             else:
                 style=None
+                self.colsizes[c]=max(self.colsizes.get(c,0),fitwidth(str(value)))
             if style:
                 self.sheet.write(self.current_row, c, value,style)
             else:
@@ -181,6 +232,8 @@ class SelectionToXls(GnrBatch):
         self.current_row += 1
     
     def post_process(self):
+        for colindex,colsize in self.colsizes.items():
+            self.sheet.col(colindex).width=colsize
         self.filePath=self.page.temporaryDocument(self.filename)
         self.fileUrl=self.page.temporaryDocumentUrl(self.filename)
         self.workbook.save(self.filePath)
