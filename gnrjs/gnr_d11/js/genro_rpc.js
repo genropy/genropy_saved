a8508b920b57c96bd632892945fdd78cc2a449ee
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-11-18 14:42:39 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4250 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrjs/gnr_d11/js/genro_rpc.js
+++ gnrjs/gnr_d11/js/genro_rpc.js
@@ -355,7 +355,7 @@ dojo.declare("gnr.GnrRpcHandler",null,{
             var fired = attr.fired;
             var reason = attr.reason;
             var change_ts = attr.ts;
-            attr = attr.attr;
+            attr = attr.attr || {};
             attr._change_ts = change_ts;
             var updater = function(path,value,attr,reason){
                 if(genro._data.getItem(path)!=value){
