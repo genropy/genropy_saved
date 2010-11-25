b80dbebf05e0922fb029db348322241db036259f
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-11-25 09:20:11 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4337 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/model/datacatalog.py
+++ packages/adm/model/datacatalog.py
@@ -6,4 +6,18 @@ class Table(GnrHTable):
                       name_plural='!!DC Elements',rowcaption='$description')
         self.sysFields(tbl)
         self.htableFields(tbl)
+        tbl.column('dtype',name_long='!!Dtype')
+        tbl.column('name_long',name_long='!!Name long')
+        tbl.column('name_short',name_long='!!Name short')
+        tbl.column('format',name_long='!!Format')
+        tbl.column('options',name_long='!!Options')
+        tbl.column('maxvalue',name_long='!!Max')
+        tbl.column('minvalue',name_long='!!Min')
+        tbl.column('dflt',name_long='!!Default')
+        tbl.column('tip',name_long='!!Tip')
+        tbl.column('purpose',name_long='!!Purpose')
+        tbl.column('ext_ref',name_long='!!External referenece')
+        tbl.column('pkg',name_long='!!Package')
+        tbl.column('tbl',name_long='!!Table')
+        
         
