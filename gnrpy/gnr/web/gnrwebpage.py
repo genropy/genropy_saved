f6b88aec7d19ddf0f74b9e257ef87ff08f978892
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-11-28 10:12:48 +0000

    New dragdrop: can be still buggy !!!
    git-svn-id: http://svn.genropy.org/genro/trunk@4361 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/web/gnrwebpage.py
+++ gnrpy/gnr/web/gnrwebpage.py
@@ -770,8 +770,8 @@ class GnrWebPage(GnrBaseWebPage):
                 self.debugger.right_pane(root)
                 self.debugger.bottom_pane(root)
                 self.mainLeftContent(root,region='left',splitter=True, nodeId='gnr_main_left')
-                root.div(_class='trash_drop',droppable=True,dropTypes='trashable/json',id='trash_drop',
-                         onDrop="""genro.publish('trashedObject',drop_data,event)""")
+                root.div(_class='trash_drop',droppable=True,dropTypes='trashable',id='trash_drop',
+                         onDrop_trashable="""genro.publish('trashedObject',data,dropInfo)""")
                 root.dataController("""
                                        var new_status = main_left_set_status[0];
                                        new_status = new_status=='toggle'? !current_status:new_status;
