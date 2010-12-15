abbe85a415c63faf591e47797e918f3675c9cbba
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-12-15 18:50:04 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4486 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/gnrcomponents/explorer_manager.py
+++ resources/common/gnrcomponents/explorer_manager.py
@@ -5,6 +5,7 @@
 # Copyright (c) 2010 Softwell. All rights reserved.
 
 from gnr.web.gnrbaseclasses import BaseComponent
+from gnr.core.gnrstring import splitAndStrip
 from gnr.core.gnrbag import DirectoryResolver
 
 class ExplorerManager(BaseComponent):
@@ -18,28 +19,37 @@ class ExplorerManager(BaseComponent):
         return ','.join(['%s_item' %n.split('_',1)[1] for n in dir(self) if n.startswith('onDroppedItem_')])
        
     def expmng_main(self,explorers):
-        explorer_to_create = explorers.split(',')
+        explorer_to_create = splitAndStrip(explorers,',')
         pane = self.pageSource('pbl_bottomBarCenter').value
         pane.dock(id='explorer_dock',width='100px',background='none',border=0,float='right')
         floating = pane.floatingPane(title='!!Explorers',height='400px',width='300px',
                         top='100px',right='100px',closable=False,resizable=True,persist=True,
+                        nodeId='gnr_explorer_floating',
                         dockable=True,dockTo='explorer_dock',_class='shadow_4',visibility='hidden',
                         datapath='gnr.explorers')
-        tc = floating.tabContainer(margin='2px')
-                        
-        for explorer_name in explorer_to_create:
+        tc = floating.tabContainer(margin='2px',selectedPage='^.selected_explorer')
+        tc.dataController("SET .selected_explorer=show_explorer[0];",subscribe_show_explorer=True)
+        for explorer in explorer_to_create:
             explorer_pars=None
-            if ':' in explorer_name:
-                explorer_name,explorer_pars=explorer_name.split(':')
-            handler= getattr(self,'explorer_'+explorer_name,None)
-            pane=tc.contentPane(title=explorer_name,datapath='.%s' %explorer_name.replace('.','_'))
-            if handler:
-                handler(pane,explorer_pars)
+            if ' AS ' in explorer:
+                explorer,explorer_code = splitAndStrip(explorer,' AS ')
+            elif ' as ' in explorer:
+                explorer,explorer_code = splitAndStrip(explorer, ' as ')
             else:
-                self.expmng_htableExplorer(pane,explorer_table=explorer_name,rootpath=explorer_pars)
+                explorer_code = None
+            if ':' in explorer:
+                explorer,explorer_pars=explorer.split(':')
+            if not explorer_code:
+                explorer_code = explorer_code.replace('.','_').replace('@','_')
+            handler= getattr(self,'explorer_'+explorer,None)
+            pane=tc.contentPane(title=explorer,pageName=explorer_code,datapath='.%s' %explorer_code)
+            if handler:
+                handler(pane,explorer_pars,explorer_code=explorer_code)
+            else:
+                self.expmng_htableExplorer(pane,explorer_table=explorer,explorer_code=explorer_code,rootpath=explorer_pars)
         
     
-    def expmng_htableExplorer(self,pane,explorer_table=None,rootpath=None):
+    def expmng_htableExplorer(self,pane,explorer_table=None,explorer_code=None,rootpath=None):
         tblobj = self.db.table(explorer_table)
         title=tblobj.name_long
         pane.attributes['title'] = title
@@ -54,7 +64,7 @@ class ExplorerManager(BaseComponent):
                                                 related_table=related_table,
                                                 relation_path=related_field,
                                                 rootpath=rootpath,rootcaption=tblobj.name_plural)
-        self.expmng_make_explorer(pane,data,explorer_code=explorer_table.replace('.','_'))        
+        self.expmng_make_explorer(pane,data,explorer_code=explorer_code)        
                 
     def expmng_make_explorer(self,pane,data,explorer_code=None):
         pane.data('.data',data)
@@ -71,8 +81,8 @@ class ExplorerManager(BaseComponent):
                             dragValues['text/plain']=treeItem.attr.caption;
                            dragValues['explorer_%s']=treeItem.attr;""" %explorer_code)
                            
-    def explorer_directory(self,pane,path=None):
+    def explorer_directory(self,pane,path=None,exporer_code=None):
         path = path or '/'
         pane.attributes['title'] = 'Directory:"%s"'%path
-        self.expmng_make_explorer(pane,DirectoryResolver(path)(),explorer_code='directory')
+        self.expmng_make_explorer(pane,DirectoryResolver(path)(),explorer_code=exporer_code)
         
