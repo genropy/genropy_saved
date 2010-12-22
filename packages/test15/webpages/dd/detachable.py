cecef87ce4935739c56406297c65d0fdee5dc4b9
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-12-22 22:18:50 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4562 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/dd/detachable.py
+++ packages/test15/webpages/dd/detachable.py
@@ -17,9 +17,9 @@ class GnrCustomWebPage(object):
         one=tc.contentPane(title='One').contentPane(background_color='pink',detachable=True)
         one.div('one')
         one.div('pippo',background='blue')
-        two=tc.contentPane(title='Two',background_color='yellow',detachable=True)
-        two.div('two',draggable=True)
-        three=tc.contentPane(title='Three',background_color='lime',detachable=True)
+        two=tc.contentPane(title='Two').contentPane(tbackground_color='yellow',detachable=True)
+        two.div('two')
+        three=tc.contentPane(title='Three').contentPane(background_color='lime',detachable=True)
         three.div('three')
 
     
