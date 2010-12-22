53c333b9770b5a4d4b73c02a8a0ee945bf052266
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-12-22 14:38:27 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4552 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/test15/webpages/dd/detachable.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+# batch_handler.py
+# Created by Francesco Porcari on 2010-10-01.
+# Copyright (c) 2010 Softwell. All rights reserved.
+# 
+
+
+"""Test drag & drop detachable pane"""
+
+class GnrCustomWebPage(object):
+
+    py_requires="""gnrcomponents/testhandler:TestHandlerFull"""
+    def test_0_tabContainer(self,pane):
+        """Drop Boxes"""
+        tc=pane.tabContainer(height='300px',width='200px')
+        one=tc.contentPane(title='One',background_color='pink',detachable=True)
+        one.div('one')
+        two=tc.contentPane(title='Two',background_color='yellow')
+        two.div('two',draggable=True)
+        three=tc.contentPane(title='Three',background_color='lime')
+        three.div('three')
+
+    
