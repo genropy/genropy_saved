3e22a132cf4f65f6b8cd34f24364ae84d0de4bef
jeffedwa <jeffedwa@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2010-12-28 05:10:30 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4575 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/components/dynamicform.py
+++ packages/test15/webpages/components/dynamicform.py
@@ -17,7 +17,7 @@ class GnrCustomWebPage(object):
         egData = Bag()
         egData['r_1.contact_type'] = 'phone'
         egData['r_1.location'] = 'Work'
-        egData['r_1.content'] = '44422444'
+        egData['r_1.content'] = '02 4442 2444'
         
         egData['r_2.contact_type'] = 'email'
         egData['r_2.content'] = 'jeffedwa@goodsoftware.com'
@@ -29,7 +29,7 @@ class GnrCustomWebPage(object):
         pane.data('data',egData)
         
     def test_1_dynamicform(self,pane):
-        """Dinamycform"""        
+        """DynamicForm"""        
         self.dynamicForm(pane,nodeId='myform',storepath='contacts',reloader='^gnr.onStart',
                         row_types={'phone':'!!Phone','email':"Email",'fax':'Fax','post_address':'Postal'},
                         type_field='contact_type')
@@ -37,16 +37,18 @@ class GnrCustomWebPage(object):
         
     def myform_row(self,row,disabled=None,**kwargs):
         #inside remote
-        fb = row.formbuilder(cols=2, border_spacing='2px',disabled=disabled)
+        fb = row.formbuilder(cols=2, border_spacing='2px',disabled=disabled,tdl_width='6em')
         fb.combobox(value='^.location',values='Home,Work,Main',lbl='Loc.')
         fb.textbox(value='^.content')
         
     def myform_email_row(self,row,disabled=None,**kwargs):
-        fb = row.formbuilder(cols=1, border_spacing='2px',disabled=disabled)
+        fb = row.formbuilder(cols=1, border_spacing='2px',disabled=disabled,tdl_width='6em')
         fb.textbox(value='^.content',lbl='Url')
         
     def myform_post_address_row(self,row,disabled=None,**kwargs):
-        fb = row.formbuilder(cols=2, border_spacing='2px',disabled=disabled)
+        fb = row.formbuilder(cols=2, border_spacing='2px',disabled=disabled,tdl_width='6em')
         fb.textbox(value='^.suburb',lbl='Suburb',width='12em')
         fb.textbox(value='^.state',lbl='State',width='3em')
         fb.simpleTextArea(value='^.address',colspan=2,width='100%')
+
+
