3c891ff47b23811cc52a9b5a6c531b8eb431780d
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-01-04 16:36:20 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4651 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/components/palette_manager.py
+++ packages/test15/webpages/components/palette_manager.py
@@ -10,35 +10,38 @@ from gnr.core.gnrbag import Bag
 class GnrCustomWebPage(object):
     py_requires = "gnrcomponents/testhandler:TestHandlerFull,gnrcomponents/palette_manager"
     css_requires = 'public'
-
+    auto_polling = 0
+    user_polling = 0
+    
     def windowTitle(self):
         return 'Palette manager'
 
     def test_0_default_dock(self, pane):
         pane.div(height='30px').dock(id='default_dock')
 
-    def test_1_palette(self, pane):
-        pane.div(height='30px').dock(id='mydock_1')
-        pg = pane.paletteGroup('first', dockTo='mydock_1')
-        pg.palettePane('aa', title='aa', background_color='pink').div('aaa')
-        pg.palettePane('bb', title='bb', background_color='red').div('bb')
-        pg = pane.paletteGroup('second', dockTo='mydock_1')
-        pg.palettePane('uu', title='uuu', background_color='yellow').div('uuu')
-        pg.palettePane('ee', title='eeee', background_color='cream').div('eeeee')
-        pane.palettePane('xx', title='xx', background_color='orange').div('xx')
-        pane.palettePane('zz', title='zz', background_color='lime', dockTo='mydock_1').div('zz')
+    #def test_1_palette(self, pane):
+    #    pane.div(height='30px').dock(id='mydock_1')
+    #    pg = pane.paletteGroup('first', dockTo='mydock_1')
+    #    pg.palettePane('aa', title='aa', background_color='pink').div('aaa')
+    #    pg.palettePane('bb', title='bb', background_color='red').div('bb')
+    #    pg = pane.paletteGroup('second', dockTo='mydock_1')
+    #    pg.palettePane('uu', title='uuu', background_color='yellow').div('uuu')
+    #    pg.palettePane('ee', title='eeee', background_color='cream').div('eeeee')
+    #    pane.palettePane('xx', title='xx', background_color='orange').div('xx')
+    #    pane.palettePane('zz', title='zz', background_color='lime', dockTo='mydock_1').div('zz')
 
-    def test_2_treepalette(self, pane):
-        pane.div(height='30px').dock(id='mydock_2')
-        pg = pane.paletteGroup('second', dockTo='mydock_2')
-        pg.paletteTree('mytree', title='State', data=self.treedata())
-        pg.palettePane('blue', title='aa', background_color='blue').div('blu')
+   #def test_2_treepalette(self, pane):
+   #    pane.div(height='30px').dock(id='mydock_2')
+   #    pg = pane.paletteGroup('second', dockTo='mydock_2')
+   #    pg.paletteTree('mytree', title='State', data=self.treedata())
+   #    pg.palettePane('blue', title='aa', background_color='blue').div('blu')
 
     def test_3_gridpalette(self, pane):
         pane.div(height='30px').dock(id='mydock_3')
         pg = pane.paletteGroup('third', dockTo='mydock_3')
-        pg.paletteGrid('mygrid', title='States', data=self.treedata(), struct=self.gridstruct,
-                       filterOn='Caption:caption')
+        pg.paletteGrid('mygrid', title='States', data=self.griddata(), struct=self.gridstruct
+                       #filterOn='Caption:caption'
+                       )
         #pg.palettePane('blue',title='aa',background_color='blue').div('blu')
 
 
