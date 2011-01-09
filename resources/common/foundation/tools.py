5900413db307a284c9f3423c5f6f779f68baf16e
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-01-09 14:15:16 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4699 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/foundation/tools.py
+++ resources/common/foundation/tools.py
@@ -18,9 +18,7 @@
 #License along with this library; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-"""
-Component for thermo:
-"""
+
 from gnr.web.gnrbaseclasses import BaseComponent
 
 class RemoteBuilder(BaseComponent):
