38d70f6268e61cdd7334b6fa9d8e0edaf3bcde8a
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-01-14 19:06:14 +0000

    formhandler
    git-svn-id: http://svn.genropy.org/genro/trunk@4754 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ resources/common/gnrcomponents/formhandler.py
@@ -0,0 +1,128 @@
+# -*- coding: UTF-8 -*-
+#--------------------------------------------------------------------------
+# Copyright (c) : 2004 - 2007 Softwell sas - Milano 
+# Written by    : Giovanni Porcari, Michele Bertoldi
+#                 Saverio Porcari, Francesco Porcari , Francesco Cavazzana
+#--------------------------------------------------------------------------
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#Lesser General Public License for more details.
+
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+from gnr.web.gnrbaseclasses import BaseComponent
+from gnr.web.gnrwebstruct import struct_method
+from gnr.core.gnrstring import splitAndStrip
+
+class FormHandler(BaseComponent):
+    css_requires='public'
+    py_requires='foundation/macrowidgets:SlotToolbar'
+    
+    @struct_method
+    def fh_paletteFormPane(self,pane,paletteCode=None,table=None,title=None,
+                        formId=None,nodeId=None,datapath=None,height=None,width=None,saveKwargs=None,
+                        loadKwargs=None,disabled=None,**kwargs):
+        nodeId = nodeId or '%s_palette' %paletteCode
+        formId = formId or '%s_form' %paletteCode
+        title = title or '^.record?caption'
+        palette = pane.palettePane(paletteCode,datapath=datapath,
+                                        height=height,width=width,title=title)
+        return palette.formPane(formId=formId,table=table,datapath=datapath,
+                                     disabled=disabled)
+        
+    @struct_method
+    def fh_formPane(self,pane,formId=None,table=None,datapath=None,disabled=None,loadKwargs=None):
+        sqlContextName = 'sqlcontext_%s' % formId
+        sqlContextRoot = '#%s.record' % formId    
+        loadKwargs = loadKwargs or dict()    
+        form = pane.borderContainer(datapath=datapath,formDatapath='.record',controllerPath='.form',pkeyPath='.pkey',
+                                    formId=formId,sqlContextName=sqlContextName,sqlContextRoot=sqlContextRoot)
+        top = form.contentPane(region='top',overflow='hidden',_attachname='top')
+        center = form.contentPane(region='center',datapath='.record',_attachname='content')
+        return form
+        
+    #def sqlLoader(pane,table=None,formId=None,recordPath=None):
+    #    attr = pane.attributes
+    #    formId = attr.get('formId',None)
+    #    formId = attr.get('formId',None)
+    #    if formId:
+    #        self.formLoader()
+    
+    #'navigation,selectrecord,formcommands'
+    # 
+
+        
+    @struct_method
+    def fh_sltb_navigation(self,pane):
+        pane = pane.div(width='120px')
+        pane.button('!!First', fire_first='.navbutton', iconClass="tb_button icnNavFirst",
+                  disabled='^.atBegin', showLabel=False)
+        pane.button('!!Previous', fire_prev='.navbutton', iconClass="tb_button icnNavPrev",
+                  disabled='^.atBegin', showLabel=False)
+        pane.button('!!Next', fire_next='.navbutton', iconClass="tb_button icnNavNext",
+                  disabled='^.atEnd', showLabel=False)
+        pane.button('!!Last', fire_last='.navbutton', iconClass="tb_button icnNavLast",
+                  disabled='^.atEnd', showLabel=False)
+    
+    @struct_method               
+    def fh_sltb_semaphore(self,pane):
+        pane.div(width='20px',_class='greenLight',
+                 formsubscribe_onStatusChange="""
+                                    var status = $1.status;
+                                    genro.dom.setClass(this.domNode,"redLight",status=='error');
+                                    genro.dom.setClass(this.domNode,"yellowLight",status=='changed');
+                                    genro.dom.setClass(this.domNode,"greenLight",status=='ok');
+                                    genro.dom.setClass(this.domNode,"icnBaseReadOnly",status=='readOnly');
+                                    """)
+    
+    @struct_method          
+    def fh_sltb_formcommands(self,pane):
+        buttons = pane.div(width='100px')
+        buttons.button(label='^.status.lockLabel', fire='.status.changelock',
+                      iconClass="^.status.statusClass", showLabel=False)
+                      
+        #buttons.semaphore(formId=formId)
+        buttons.button('!!Save', action='genro.publish(_formId+"_save")', float='right',
+                       iconClass="tb_button db_save", showLabel=False)
+        buttons.button('!!Revert', action='genro.publish(_formId+"_save")',
+                        iconClass="tb_button db_revert",
+                       float='right',
+                       showLabel=False)
+        buttons.button('!!Delete', fire=".edit.delete", iconClass='db_del tb_button',
+                       showLabel=False,float='right')
+    @struct_method 
+    def fh_sltb_locker(self,pane):
+        pane.button('!!Locker',width='20px',iconClass='icnBaseLocked',showLabel=False,
+                    action='this.form.publish("setLocked","toggle");',
+                    formsubscribe_onLockChange="""var locked= $1.locked;
+                                                  this.widget.setIconClass(locked?'icnBaseLocked':'icnBaseUnlocked');""")
+    
+    def xxx(self,toolbar):
+        toolbar = pane.toolbar(_class='standard_toolbar')
+        toolbar.dataFormula('.status.locked', True, _onStart=True)
+        toolbar.dataController("""
+                            if(isLocked){
+                            //if not unlockable return
+                                isLocked = isLocked //if unlocable 
+                            }
+                            SET .status.locked=!isLocked
+                            """,
+                               _fired='^.status.changelock',
+                               isLocked='=.status.locked')
+        toolbar.dataController("""
+                             SET .status.statusClass = isLocked?'tb_button icnBaseLocked':'tb_button icnBaseUnlocked';
+                             SET .status.lockLabel = isLocked?unlockLabel:lockLabel;
+                               """, isLocked="^.status.locked", lockLabel='!!Lock',
+                               unlockLabel='!!Unlock')
+
+        
+        
