268bf532c5f50281942b700319b973264082c5fd
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-01-22 17:29:50 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4874 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/gnrwdg/formHandler.py
+++ packages/test15/webpages/gnrwdg/formHandler.py
@@ -94,7 +94,7 @@ class GnrCustomWebPage(object):
         dialog.formTester('form_dialog')
                    
     @struct_method('mytoolbar_selectrecord')
-    def mytoolbar_selectrecord(self,pane):
+    def mytoolbar_selectrecord(self,pane,**kwargs):
         fb=pane.formbuilder(cols=1, border_spacing='0px',width='200px')
         fb.dbselect(value="^.prov",dbtable="glbl.provincia",parentForm=False,
                     validate_onAccept="this.form.publish('load',{destPkey:value});",lbl='Provincia',width='100%')
