c9687ec69c870ef75095ec9c4a0113e9b7bcc147
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-01-22 17:27:01 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4873 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/gnrwdg/panegrid.py
+++ packages/test15/webpages/gnrwdg/panegrid.py
@@ -4,24 +4,37 @@
 # Created by Francesco Porcari on 2011-01-21.
 # Copyright (c) 2011 Softwell. All rights reserved.
 
+from gnr.web.gnrwebstruct import struct_method
 "Test page description"
 class GnrCustomWebPage(object):
     py_requires="gnrcomponents/testhandler:TestHandlerFull,gnrcomponents/palette_manager:PaletteManager"
 
     def windowTitle(self):
         return ''
-         
+        
     def test_0_panegrid(self,pane):
+        """Pane grid """
+        pane = pane.framePane(height='200px')
+        pane.contentPane(height='30px',side='top',background='blue')
+        pane.contentPane(margin='10px',background='red')
+        
+         
+    def _test_0_panegrid(self,pane):
         """Pane grid """
         bc = pane.borderContainer(height='200px')
         bc.contentPane(region='left',width='200px',background_color='red')
-        gridPane = bc.paneGrid('province',struct='regione',table='glbl.provincia',region='center',searchOn=True)
+        gridPane = bc.paneGrid('province_1',struct='regione',table='glbl.provincia',region='center')
+        gridPane.slotToolbar('province',slots='*,piero',piero='*A,T,D',side='top')
         gridPane.selectionStore(storepath='.grid.store',table='glbl.provincia',
-                                where="$regione='LOM'",gridId='province_grid',_onStart=True)   
+                                where="$regione='LOM'",gridId='province_1_grid',_onStart=True)   
     
-    def test_1_palettegrid(self,pane):
-        gridPane = pane.paletteGrid('province',title='Province',
+    @struct_method('province_piero')
+    def province_piero(self,pane,piero=None,wdgNodeId=None,**kwargs):
+        return pane.div(width='205px').searchBox(nodeId='%s_searchbox' %wdgNodeId,searchOn=piero,datapath='.searchbox')
+        
+    def _test_1_palettegrid(self,pane):
+        gridPane = pane.paletteGrid('province_2',title='Province',
                                     struct='regione',dockTo='*',
                                     table='glbl.provincia',searchOn='*A,T,D')
         gridPane.selectionStore(storepath='.grid.store',table='glbl.provincia',
-                                where="$regione='LOM'",gridId='palette_province_grid') 
+                                where="$regione='LOM'",gridId='province_2_grid') 
