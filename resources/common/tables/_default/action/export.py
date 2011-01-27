25210362a2c948115f9323a412775716acf0cc4d
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-01-27 15:17:18 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4946 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/tables/_default/action/export.py
+++ resources/common/tables/_default/action/export.py
@@ -15,11 +15,11 @@ class Main(BaseResourceExport):
     batch_title = 'Export'
     batch_cancellable = False
     batch_delay = 0.5
+    batch_immediate = True
 
     def table_script_parameters_pane(self, pane, **kwargs):
         fb = pane.formbuilder(cols=1, border_spacing='3px')
         fb.data('.export_mode', 'xls')
         fb.filteringSelect(value='^.export_mode', values='xls:Excel,csv:Csv,html:Html', lbl='!!Mode')
         fb.textbox(value='^.filename', lbl='!!Save as')
-        
         
