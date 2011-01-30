f8c80765bd27fa9374003019e2d3c0dd32d7243d
gporcari <gporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-01-30 09:32:34 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4959 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/tools/css3make.py
+++ packages/test15/webpages/tools/css3make.py
@@ -30,24 +30,35 @@ class GnrCustomWebPage(object):
                  shadow_blur='^.blur',shadow_inset='^.inset')
       
     def test_3_gradient_fixed(self, pane):
-        sl = pane.slotBar('deg,fld,tan,*,test,*')
+        sl = pane.slotBar('deg,fld,tan,*,test,*,test1,*')
         
         sl.deg.verticalSlider(value='^.deg',minimum=0,maximum=360,intermediateChanges=True,height='100px')
         fb = sl.fld.formbuilder(cols=6, border_spacing='2px')
         fb.numbertextbox(value='^.deg',lbl='deg')
-        fb.numbertextbox(value='^.tng',lbl='tng')
-        fb.numbertextbox(value='^.tng_corr',lbl='tng_corr')
-        fb.textbox(value='^.result',lbl='res')
-        fb.dataFormula(".tng", "Math.tan(d * Math.PI/180)",d='^.deg')
-        fb.dataFormula(".tng_corr", "tng",tng='^.tng')
-        fb.dataFormula(".tng_corr", "tng",tng='^.tng')
-        fb.dataFormula(".result", "conCss2(d)",d='^.deg')
-
-        pane.div(margin='5px', display='inline-block',
+        sl.test.div(margin='5px', display='inline-block',
                  border='1px solid gray', width='100px', height='80px',
                  gradient_from='white',gradient_to='navy',gradient_degrees='^.deg')
-    
                  
+        sl.test1.div(margin='5px', display='inline-block',
+                border='1px solid gray', width='100px', height='80px',
+                gradient_color_0='pink,15',gradient_color_1='yellow,50' ,gradient_color_2='red,100',gradient_degrees='^.deg')
+       
+    def test_4_transform(self, pane):
+        sl = pane.slotBar('rotate,translatex,translatey,scalex,scaley,skewx,skewy,*,test,*')
+        sl.rotate.verticalSlider(value='^.rotate',minimum=0,maximum=360,intermediateChanges=True,height='100px',default_value=0)
+        sl.translatex.verticalSlider(value='^.translate_x',minimum=-100,maximum=100,intermediateChanges=True,height='100px',default_value=0)
+        sl.translatey.verticalSlider(value='^.translate_y',minimum=-100,maximum=100,intermediateChanges=True,height='100px',default_value=0)
+        sl.scalex.verticalSlider(value='^.scale_x',minimum=0,maximum=1,intermediateChanges=True,height='100px',default_value=1)
+        sl.scaley.verticalSlider(value='^.scale_y',minimum=0,maximum=1,intermediateChanges=True,height='100px',default_value=1)
+        sl.skewx.verticalSlider(value='^.skew_x',minimum=0,maximum=360,intermediateChanges=True,height='100px',default_value=0)
+        sl.skewy.verticalSlider(value='^.skew_y',minimum=0,maximum=360,intermediateChanges=True,height='100px',default_value=0)
+        sl.test.div(margin='5px', display='inline-block', border='1px solid gray', width='50px', height='70px'
+                 ,transform_rotate='^.rotate'
+                 ,transform_translate_x='^.translate_x',transform_translate_y='^.translate_y'
+                 ,transform_scale_x='^.scale_x',transform_scale_y='^.scale_y'
+                 ,transform_skew_x='^.skew_x',transform_skew_y='^.skew_y'
+                 )
+                  
 
    #def test_3_gradient_fixed(self, pane):
    #    "firefox"
