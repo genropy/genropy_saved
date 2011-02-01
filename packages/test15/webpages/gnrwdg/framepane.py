5c43e66c673a554f14e340131de82d69e3229697
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-02-01 11:22:21 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@4988 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/gnrwdg/framepane.py
+++ packages/test15/webpages/gnrwdg/framepane.py
@@ -13,13 +13,30 @@ class GnrCustomWebPage(object):
         return 'SlotBar test'
          
     def test_0_slotbar_base(self,pane):
-        """First test description"""
-        frame = pane.framePane(frameCode='frameOne',height='200px',width='200px',shadow='3px 3px 5px gray',
-                                border='1px solid #bbb',rounded=10,margin='10px')
-        top = frame.top.slotToolbar(slots='30,foo,*,bar,30',height='20px',rounded_top=10)
-        bottom = frame.bottom.slotToolbar(slots='30,foo,*,bar,30',height='20px',rounded_bottom=10)
-        left = frame.left.slotToolbar(slots='30,foo,*,bar,30',height='20px',orientation='V')
-        right = frame.right.slotToolbar(slots='30,foo,*,bar,30',height='20px',orientation='V')
+        """Design headline"""
+        pane.makeDemoFrame()
+    
+    def test_1_slotbar_sidebar(self,pane):
+        """Design sidebar"""
+        pane.makeDemoFrame(design='sidebar')
+    
+    def test_2_slotbar_sidebar(self,pane):
+        """Change gradients"""
+        pane.makeDemoFrame(design='sidebar',gradient_from='blue',gradient_to='navy')
+        
+    
+    @struct_method
+    def makeDemoFrame(self,pane,design=None,gradient_from=None,gradient_to=None):
+        frame = pane.framePane(frameCode='frameOne',height='200px',width='300px',shadow='3px 3px 5px gray',
+                                border='1px solid #bbb',margin='10px',center_border='1px solid #bbb',
+                                center_background='gray',
+                                side_gradient_from=gradient_from,
+                                side_gradient_to=gradient_to,
+                                rounded=10,design=design)
+        top = frame.top.slotToolbar(slots='30,foo,*,bar,30',height='20px')
+        bottom = frame.bottom.slotToolbar(slots='30,foo,*,bar,30',height='20px')
+        left = frame.left.slotToolbar(slots='30,foo,*,bar,30',width='20px')
+        right = frame.right.slotToolbar(slots='30,foo,*,bar,30',width='20px')
         top.foo.button(label='Add',iconClass='icnBaseAdd',showLabel=False)
         top.bar.button(label='Del',iconClass='icnBaseOk',showLabel=False)
         bottom.foo.button(label='Add',iconClass='icnBaseAdd',showLabel=False)
@@ -28,5 +45,5 @@ class GnrCustomWebPage(object):
         left.bar.button(label='Del',iconClass='icnBaseOk',showLabel=False)
         right.foo.button(label='Add',iconClass='icnBaseAdd',showLabel=False)
         right.bar.button(label='Del',iconClass='icnBaseOk',showLabel=False)
-        frame.div(height='100%',width='100%',background_color='red')
+        
         
