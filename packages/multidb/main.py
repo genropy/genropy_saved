e29a44e1e1ddfe8e6a37ff4b4cedeb5e9e1dffb9
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-02-02 15:50:34 +0000

    multistore
    git-svn-id: http://svn.genropy.org/genro/trunk@5018 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/multidb/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter, Table_userobject
+
+class Package(GnrDboPackage):
+    def config_attributes(self):
+        return dict(comment='multidb package',sqlschema='multidb',
+                name_short='Multidb', name_long='Multidb', name_full='Multidb')
+
+    def config_db(self, pkg):
+        pass
+
+
+class Table(GnrDboTable):
+    pass
