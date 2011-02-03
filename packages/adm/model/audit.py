ff6a338f435b7444c07e474a179b2498b2a54db7
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-02-03 16:29:23 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5033 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/model/audit.py
+++ packages/adm/model/audit.py
@@ -1,22 +1,48 @@
 # encoding: utf-8
-
+from gnr.core.gnrbag import Bag
 class Table(object):        
     def config_db(self, pkg):
         tbl =  pkg.table('audit',pkey='id',name_long='!!Audit',name_plural='!!Audit')
         self.sysFields(tbl)
-        tbl.column('table_fullname',name_long='!!Table name')
-        tbl.column('change_type',size='1',name_long='!!Change type',
+        tbl.column('tablename',name_long='!!Table name')
+        tbl.column('event',size='1',name_long='!!Change type',
                     values='I:Insert,U:Update,D:Delete')
-        tbl.column('user',name_long='!!User')
+        tbl.column('username',name_long='!!User')
         tbl.column('record_pkey',name_long='!!Record Pkey')
         tbl.column('version','L',name_long='!!Version')
         tbl.column('data','X',name_long='!!Data')
     
-    def audit_I(self,tblobj,record):
-        pass
+    def getXmlRecord(self,record=None):
+        if not isinstance(record,Bag):
+            record = Bag(record)
+        return record.toXml()
     
-    def audit_U(self,tblobj,record,old_record):
-        pass
-    
-    def audit_D(self,tblobj,record):
-        pass
+
+    def audit(self,tblobj,event,audit_mode=None,record=None,old_record=None,username=None,page=None):
+        if event=='I' and audit_mode=='lazy':
+            return
+        if not username:
+            username = self.db.currentEnv.get('user') 
+        version = record.get('__version',0)
+        audit_record = dict(tablename=tblobj.fullname,event=event,username=username,
+                      record_pkey=record[tblobj.pkey],version=version)
+        if event == 'I' or event=='D':
+            audit_record['data'] = self.getXmlRecord(record)
+        if event == 'U':
+            assert old_record, 'Missing old_record in an update that uses audit feature tbl: %s' %tblobj.fullname
+            changes = Bag()
+            for k in record.keys():
+                print k
+                if k in ('__version','__mod_ts'):
+                    continue
+                if record[k] != old_record.get(k):
+                    changes[k] = record[k]
+            print 'before xml changes'
+            audit_record['data'] = changes.toXml()
+            if audit_mode=='lazy' and version==1:
+                first_audit = dict(audit_record)
+                first_audit['version'] = 0
+                first_audit['event'] = 'I'
+                first_audit['data'] = self.getXmlRecord(old_record)
+                self.insert(first_audit)
+        self.insert(audit_record)
