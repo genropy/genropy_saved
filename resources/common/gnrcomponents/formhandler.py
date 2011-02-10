91341118908b452e420b5da6cf2d3ba6f2c82d6c
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-02-10 11:06:33 +0000

    prefix change
    git-svn-id: http://svn.genropy.org/genro/trunk@5108 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/gnrcomponents/formhandler.py
+++ resources/common/gnrcomponents/formhandler.py
@@ -29,57 +29,57 @@ class FormHandler(BaseComponent):
     py_requires='foundation/macrowidgets:SlotBar'
 
     @struct_method
-    def fh_sltb_navigation(self,pane,**kwargs):
+    def fh_slotbar_navigation(self,pane,**kwargs):
         pane = pane.div(lbl='!!Navigation',_class='slotbar_group')
-        pane.sltb_first()
-        pane.sltb_prev()
-        pane.sltb_next()
-        pane.sltb_last()
+        pane.slotbar_first()
+        pane.slotbar_prev()
+        pane.slotbar_next()
+        pane.slotbar_last()
     
     @struct_method               
-    def fh_sltb_semaphore(self,pane,**kwargs):
+    def fh_slotbar_semaphore(self,pane,**kwargs):
         pane.div(_class='fh_semaphore')
     
     @struct_method          
-    def fh_sltb_formcommands(self,pane,**kwargs):
+    def fh_slotbar_formcommands(self,pane,**kwargs):
         pane = pane.div(lbl='!!Form Commands',_class='slotbar_group')
-        pane.sltb_save()
-        pane.sltb_revert()
-        pane.sltb_delete()
+        pane.slotbar_save()
+        pane.slotbar_revert()
+        pane.slotbar_delete()
 
     
     @struct_method          
-    def fh_sltb_save(self,pane,**kwargs):
+    def fh_slotbar_save(self,pane,**kwargs):
         pane.formButton('!!Save',topic='save',iconClass="tb_button db_save", parentForm=True)
 
     @struct_method          
-    def fh_sltb_revert(self,pane,**kwargs):
+    def fh_slotbar_revert(self,pane,**kwargs):
         pane.formButton('!!Revert',topic='load',iconClass="tb_button db_revert", parentForm=True)
     
     @struct_method          
-    def fh_sltb_delete(self,pane,**kwargs):
+    def fh_slotbar_delete(self,pane,**kwargs):
         pane.formButton('!!Delete',topic='delete',iconClass="tb_button db_del",parentForm=True)
 
     @struct_method          
-    def fh_sltb_first(self,pane,**kwargs):
+    def fh_slotbar_first(self,pane,**kwargs):
         pane.formButton('!!First',iconClass="tb_button icnNavFirst",
                     topic='navigationEvent',command='first',
                     formsubscribe_navigationStatus="this.widget.setAttribute('disabled',$1.first || false);")
     
     @struct_method          
-    def fh_sltb_prev(self,pane,**kwargs):
+    def fh_slotbar_prev(self,pane,**kwargs):
         pane.formButton('!!Prev',iconClass="tb_button icnNavPrev",
                     topic='navigationEvent',command='prev',
                     formsubscribe_navigationStatus="this.widget.setAttribute('disabled',$1.first || false);")
     
     @struct_method          
-    def fh_sltb_next(self,pane,**kwargs):
+    def fh_slotbar_next(self,pane,**kwargs):
         pane.formButton('!!Next',iconClass="tb_button icnNavNext",
                     topic='navigationEvent',command='next',
                     formsubscribe_navigationStatus="this.widget.setAttribute('disabled',$1.last || false);")
     
     @struct_method          
-    def fh_sltb_last(self,pane,**kwargs):
+    def fh_slotbar_last(self,pane,**kwargs):
         pane.formButton('!!Last',iconClass="tb_button icnNavLast",
                     topic='navigationEvent',command='last',
                     formsubscribe_navigationStatus="this.widget.setAttribute('disabled',$1.last || false);")
@@ -91,7 +91,7 @@ class FormHandler(BaseComponent):
                     showLabel=False,**kwargs)
     
     @struct_method 
-    def fh_sltb_locker(self,pane,**kwargs):
+    def fh_slotbar_locker(self,pane,**kwargs):
         pane.button('!!Locker',width='20px',iconClass='icnBaseUnlocked',showLabel=False,
                     action='this.form.publish("setLocked","toggle");',
                     formsubscribe_onLockChange="""var locked= $1.locked;
