6ba1e90f38874ba93f0a0573ecb1f1665f5047d2
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-04 13:56:23 +0000

    login_component update
    git-svn-id: http://svn.genropy.org/genro/trunk@5451 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/resources/login_component.py
+++ packages/adm/resources/login_component.py
@@ -2,9 +2,10 @@
 
 # login_component.py
 # Created by Niso on 2011-03-03.
-# Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+# Copyright (c) 2011 Softwell. All rights reserved.
 
 from gnr.web.gnrwebpage import BaseComponent
+from gnr.web.gnrwebstruct import struct_method
 
 class LoginPage(BaseComponent):
     css_requires='public'
@@ -12,30 +13,47 @@ class LoginPage(BaseComponent):
     def loginTitle(self):
         return '!!Login'
         
-    def rootWidget(self,root,**kwargs): # def rootWidget(self,root,region='center',nodeId='_pageRoot')
-        return root.framePane(frameCode='login',datapath='logindata',height='130px',width='250px',
-                              margin='auto',shadow='2px 2px 2px gray',rounded=12,
-                              gradient_from='black',gradient_to='white',gradient_deg=90,**kwargs)
-    
-    def main(self, frame, fromPage=None, **kwargs):
+    def main(self, root, **kwargs):
+        self.rootAttributes(root.attributes)
+        root.loginFrame(title=self.loginTitle(),shadow='2px 2px 2px gray',rounded=12,
+                        gradient_from='black',gradient_to='white',gradient_deg=90,**kwargs)
+                        
+    def rootAttributes(self,attributes):
+        attributes['height'] = '130px'
+        attributes['width'] = '250px'
+        attributes['margin'] = 'auto'
+        attributes['padding'] = '5px'
+        
+    @struct_method
+    def lg_loginFrame(self,pane,title=None,fromPage=None,**kwargs):
+        frame = pane.framePane(frameCode='login',datapath='logindata',**kwargs)
         self.layoutAttributes(frame.attributes)
-        self.loginTop(frame.top)
-        self.loginCenter(frame)
-        self.loginBottom(frame.bottom)
-        self.loginController(frame,fromPage=fromPage)
-                              
-    def layoutAttributes(self,attributes):
-        #attributes[height] = '1000px'
+        frame.top.loginTop(title=title)
+        frame.loginCenter()
+        frame.bottom.loginBottom()
+        self._loginController(frame,fromPage=fromPage)
+        
+    def layoutAttributes(self, attributes):
         pass
         
-    def loginTop(self,pane):
-        pane.slotToolbar('*,title,*',title=self.loginTitle(),height='20px')
+    @struct_method
+    def lg_loginTop(self,pane,title=None):
+        pane.slotToolbar('*,title,*',title=title,height='20px')
         
-    def loginBottom(self,pane):
+    @struct_method
+    def lg_loginBottom(self,pane):
         pane.slotBar('*,messageBox,*',background='transparent',
                         messageBox_subscribeTo='failed_login_msg',color='white',height='12px')
                         
-    def loginController(self,frame,fromPage=None):
+    @struct_method
+    def loginCenter(self,frame):
+        fb = frame.formbuilder(cols=2, border_spacing='3px',onEnter='PUBLISH login;',margin='auto',
+                                    margin_top='15px',fld_line_height='20px',fld_padding='2px',fld_rounded=6)
+        fb.textbox(value='^.user',ghost='Username',colspan=2)
+        fb.textbox(value='^.password',ghost='Password',type='password')
+        frame.button('Entra',action='PUBLISH login;',padding='1px',rounded=10,bottom='3px',right='32px',position='absolute')
+        
+    def _loginController(self,frame,fromPage=None):
         rpc = frame.dataRpc('dummy','doLogin',subscribe_login=True,login='=logindata',_POST=True)
         rpc.addCallback("""var msg = result.getValue().getItem('message');
                             if(msg==''){
@@ -50,10 +68,3 @@ class LoginPage(BaseComponent):
                             }
                             return result;
                             """,badUserMsg="!!User unknown or wrong password",fromPage=fromPage)
-    
-    def loginCenter(self,frame):
-        fb = frame.formbuilder(cols=2, border_spacing='3px',onEnter='PUBLISH login;',margin='auto',
-                                    margin_top='15px',fld_line_height='20px',fld_padding='2px',fld_rounded=6)
-        fb.textbox(value='^.user',ghost='Username',colspan=2)
-        fb.textbox(value='^.password',ghost='Password',type='password')
-        frame.button('Entra',action='PUBLISH login;',padding='1px',rounded=10,bottom='3px',right='32px',position='absolute')
