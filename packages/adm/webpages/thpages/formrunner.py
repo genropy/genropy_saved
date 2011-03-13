689bf2020ea719911fae016375c21fad3232f245
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-13 10:47:32 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5521 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/adm/webpages/thpages/formrunner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#   form _runner
+#
+#  Created by Giovanni Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+# --------------------------- GnrWebPage subclass ---------------------------
+class GnrCustomWebPage(object):
+    py_requires='tablehandler/th_iframepage:RecordPage'
---
+++ packages/adm/webpages/thpages/viewrunner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#  view runner
+#
+#  Created by Giovanni Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+""" generic_frame"""
+
+# --------------------------- GnrWebPage subclass ---------------------------
+class GnrCustomWebPage(object):
+    py_requires='tablehandler/th_iframepage:ViewPage'
