d21403818af73fdc10f57ff93e7b49256471e052
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-14 10:59:02 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5525 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/glbl/webpages/test/thframe.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+# thframe.py
+# Created by Francesco Porcari on 2011-03-13.
+# Copyright (c) 2011 Softwell. All rights reserved.
+
+"Tablehandler"
+class GnrCustomWebPage(object):
+    py_requires="gnrcomponents/testhandler:TestHandlerFull,tablehandler/th_iframepage:TableHandler"
+
+    def windowTitle(self):
+        return ''
+         
+    def test_0_firsttest(self,pane):
+        """First test description"""
+        bc = pane.borderContainer(height='600px')
+        gridpane = bc.contentPane(region='top',height='50%')
+        formpane = bc.contentPane(region='center')
+        gridpane.iframeTableHandler(table='glbl.localita',inputPane=formpane) 
