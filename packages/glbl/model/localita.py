6e75b9bf1bbbe6280fc10c83ba86d9cda1a5da23
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-15 16:09:18 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5550 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/glbl/model/localita.py
+++ packages/glbl/model/localita.py
@@ -3,7 +3,7 @@
 
 class Table(object):
     def config_db(self, pkg):
-        tbl = pkg.table('localita', pkey='id', name_long='Localita', rowcaption='nome,@provincia.sigla:%s (%s)')
+        tbl = pkg.table('localita', pkey='id', name_long='Localita', rowcaption='nome,@provincia.sigla:%s (%s)',broadcast=True)
         tbl.column('id', size='22', group='_', readOnly=True, name_long='!!Id')
         tbl.column('nome', size=':52', name_long='Nome', indexed=True)
         tbl.column('provincia', size='2', name_long='Provincia').relation('glbl.provincia.sigla', mode='foreignkey',
