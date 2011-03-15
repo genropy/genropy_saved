89a3f9fb8e60a6e8c087e3ff484557e022007550
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-15 16:09:02 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5549 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/glbl/webpages/test/thframe.py
+++ packages/glbl/webpages/test/thframe.py
@@ -7,13 +7,11 @@
 "Tablehandler"
 class GnrCustomWebPage(object):
     py_requires="gnrcomponents/testhandler:TestHandlerFull,tablehandler/th_iframepage:TableHandler"
-
-    def windowTitle(self):
-        return ''
          
     def test_0_firsttest(self,pane):
         """First test description"""
         bc = pane.borderContainer(height='600px')
         gridpane = bc.contentPane(region='top',height='50%')
         formpane = bc.contentPane(region='center')
+        formpane.dataController("",dbevent="^gnr.dbevent.glbl_localita.id")
         gridpane.iframeTableHandler(table='glbl.localita',inputPane=formpane) 
