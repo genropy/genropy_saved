1e7c31572732f5c7c2575a0b9d8c02fe91db62ab
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-18 08:49:27 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5573 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- projects/devlang/packages/devlang/model/dev_lang.py
+++ projects/devlang/packages/devlang/model/dev_lang.py
@@ -3,7 +3,7 @@
 class Table(object):
     def config_db(self, pkg):
         tbl = pkg.table('dev_lang', pkey='id', name_long='!!Developer languages',
-                        name_plural='!!Developer languages', broadcast=True)
+                        name_plural='!!Developer languages', broadcast='old')
         tbl.column('id', size='22', group='_', name_long='Id')
         tbl.column('developer_id', size='22', group='_', name_long='!!Developer').relation('developer.id',
                                                                                            mode='foreignkey',
