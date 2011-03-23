bcc81fef887b5709924e0f743260a7e982259f4c
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-23 14:07:52 +0000

    showcase update - framePane, slotToolbar and CSS 3
    git-svn-id: http://svn.genropy.org/genro/trunk@5628 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/webpage_elements/toolbar.py
+++ packages/showcase/webpages/webpage_elements/toolbar.py
@@ -10,7 +10,7 @@ import datetime
 
 class GnrCustomWebPage(object):
     py_requires = 'gnrcomponents/testhandler:TestHandlerFull'
-    dojo_theme = 'soria'
+    #dojo_theme = 'soria'
     
     def windowTitle(self):
          return 'slotBar and slotToolbar'
@@ -19,7 +19,7 @@ class GnrCustomWebPage(object):
         """simple example"""
         workdate = str(datetime.datetime.now().date())
         frame = pane.framePane(frameCode='dummy',height='150px',margin='10px',
-                               center_background='gray',rounded=10,design='headline') # design='sidebar'
+                               rounded=10,design='headline') # design='sidebar'
         top = frame.top.slotToolbar(slotbarCode='dummy1',slots='*,foo,*',height='20px')
         top.foo.div('Hello!')
         left = frame.left.slotToolbar(slotbarCode='dummy2',slots='*,foo,*',width='30px')
@@ -30,9 +30,9 @@ class GnrCustomWebPage(object):
         bottom.foo.div('Hello! (from the bottom slotBar)')
         
     def test_2_features(self,pane):
-        """Some added features"""
+        """framPane, slotToolbar and CSS 3"""
         workdate = str(datetime.datetime.now().date())
-        pane.data('color','yellow')
+        pane.data('color','white')
         pane.data('from','#4BA21A')
         pane.data('to','#7ED932')
         
@@ -52,6 +52,8 @@ class GnrCustomWebPage(object):
                          action="alert('Sended your schedule by email')")
         top.dummy.button(label='pdf',iconClass='icnBasePdf',showLabel=False,
                          action="alert('PDF created')")
+        top.dummy.button(label='',iconClass='icnBaseExport',showLabel=False,
+                         action="alert('Exported in an Excel file')")
         top.dummy.button(label='print',iconClass='icnBasePrinter',showLabel=False,
                          action="alert('Printed')")
                          
@@ -66,7 +68,8 @@ class GnrCustomWebPage(object):
                                         width='200px',gradient_from='^from',gradient_to='^to')
         fb = right.dummy.formbuilder(lbl_color='^color')
         fb.div('Settings',font_size='12pt',color='^color')
-        fb.comboBox(lbl='color',values='black,white,yellow',value='^color',width='90px')
+        fb.comboBox(lbl='color',value='^color',width='90px',
+                    values='black,white,yellow,red,brown,grey,green,blue')
         fb.filteringSelect(lbl='gradient_from',value='^from',width='90px',
                            values="""#0065E7:dark Blue,#4BA21A:dark Green,
                                      #E3AA00:dark Orange,#C413A9:dark Pink,
@@ -80,5 +83,11 @@ class GnrCustomWebPage(object):
                                           gradient_from='^from',gradient_to='^to')
         bottom.bar.div('Here goes the messages for user',color='^color')
         
+        sb = frame.div('Remember: a slotToolbar (or a slotBar) can be attached to any div!',
+                        margin='20px',color='black').slotToolbar(slotbarCode='top',slots='10,hello,*,dummy',
+                                                                 height='25px',gradient_from='^from',gradient_to='^to')
+        sb.hello.button('Click me!',action='alert("Hello!!!")')
+        sb.dummy.button(label='',iconClass='icnBasePref',showLabel=False,
+                        action="alert('A wonderful action!')")
         frame.div('Here goes the \"center\" content',margin='20px')
                            
