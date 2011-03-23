fdd5e9fc9d85fd917a551c292516734303bd136a
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-23 16:05:13 +0000

    showcase update - "gradient_deg" Slider-modifiable
    git-svn-id: http://svn.genropy.org/genro/trunk@5635 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/showcase/webpages/webpage_elements/toolbar.py
+++ packages/showcase/webpages/webpage_elements/toolbar.py
@@ -32,18 +32,18 @@ class GnrCustomWebPage(object):
     def test_2_features(self,pane):
         """framPane, slotToolbar and CSS 3"""
         workdate = str(datetime.datetime.now().date())
-        pane.data('color','white')
-        pane.data('from','#4BA21A')
-        pane.data('to','#7ED932')
+        pane.data('.color','white')
+        pane.data('.from','#4BA21A')
+        pane.data('.to','#7ED932')
         
         frame = pane.framePane(frameCode='framecode',height='400px',
                                shadow='3px 3px 5px gray',rounded=10,
                                border='1px solid #bbb',margin='10px',
                                center_background='#E1E9E9')
         top = frame.top.slotToolbar(slotbarCode='top',slots='10,hello,*,foo,*,dummy',
-                                    height='25px',gradient_from='^from',gradient_to='^to')
-        top.hello.div(workdate,color='^color')
-        top.foo.div('Schedule',font_size='14pt',color='^color')
+                                    height='25px',gradient_from='^.from',gradient_to='^.to')
+        top.hello.div(workdate,color='^.color')
+        top.foo.div('Schedule',font_size='14pt',color='^.color')
         top.dummy.button(label='add',iconClass='icnBaseAdd',showLabel=False,
                          action="alert('Added a row in your grid')")
         top.dummy.button(label='del',iconClass='icnBaseDelete',showLabel=False,
@@ -57,37 +57,41 @@ class GnrCustomWebPage(object):
         top.dummy.button(label='print',iconClass='icnBasePrinter',showLabel=False,
                          action="alert('Printed')")
                          
-        left = frame.left.slotToolbar(slotbarCode='left',slots='10,foo,*',width='40px',
-                                      gradient_from='^from',gradient_to='^to')
+        left = frame.left.slotBar(slotbarCode='left',slots='10,foo,*',width='40px',
+                                  gradient_from='^.from',gradient_to='^.to',gradient_deg='0')
         left.foo.button('new grid',action="alert('New schedule!')")
         left.foo.button('save grid',action="alert('Saved!')")
         left.foo.button('load grid',action="alert('Loaded!')")
         left.foo.button('exit', action="alert('Exited!')")
         
-        right = frame.right.slotToolbar(slotbarCode='left',slots='20,dummy,*',
-                                        width='200px',gradient_from='^from',gradient_to='^to')
-        fb = right.dummy.formbuilder(lbl_color='^color')
-        fb.div('Settings',font_size='12pt',color='^color')
-        fb.comboBox(lbl='color',value='^color',width='90px',
-                    values='black,white,yellow,red,brown,grey,green,blue')
-        fb.filteringSelect(lbl='gradient_from',value='^from',width='90px',
+        right = frame.right.slotBar(slotbarCode='left',slots='20,dummy,*',width='130px',
+                                    gradient_from='^.from',gradient_to='^.to',gradient_deg='^.deg')
+        fb = right.dummy.formbuilder(lbl_color='^.color',cols=2)
+        fb.div('Settings',font_size='12pt',color='^.color',colspan=2)
+        fb.comboBox(lbl='color',value='^.color',width='90px',colspan=2,
+                    values="""aqua,black,blue,fuchsia,gray,green,lime,maroon,
+                              navy,olive,purple,red,silver,teal,white,yellow
+                              """) # A complete list of CSS 3 basic color keywords
+        fb.filteringSelect(lbl='from',value='^.from',width='90px',colspan=2,
                            values="""#0065E7:dark Blue,#4BA21A:dark Green,
                                      #E3AA00:dark Orange,#C413A9:dark Pink,
                                      #960000:Dark Red""")
-        fb.filteringSelect(lbl='gradient_to',value='^to',width='90px',
+        fb.filteringSelect(lbl='to',value='^.to',width='90px',colspan=2,
                            values="""#29DFFA:light Blue,#7ED932:light Green,
                                      #F4DC7F:light Orange,#FFCCED:light Pink,
                                      #FD4042:light Red""")
+        fb.verticalSlider(value='^.deg',minimum=0,maximum=360,discreteValues=361,
+                          intermediateChanges=True,height='100px',lbl='Deg')
+        fb.numbertextbox(value='^.deg',lbl='deg',width='3em')
         
         bottom = frame.bottom.slotToolbar(slots='300,bar,*,searchOn',height='20px',
-                                          gradient_from='^from',gradient_to='^to')
-        bottom.bar.div('Here goes the messages for user',color='^color')
+                                          gradient_from='^.from',gradient_to='^.to')
+        bottom.bar.div('Here goes the messages for user',color='^.color')
         
         sb = frame.div('Remember: a slotToolbar (or a slotBar) can be attached to any div!',
                         margin='20px',color='black').slotToolbar(slotbarCode='top',slots='10,hello,*,dummy',
-                                                                 height='25px',gradient_from='^from',gradient_to='^to')
+                                                                 height='25px',gradient_from='^.from',gradient_to='^.to')
         sb.hello.button('Click me!',action='alert("Hello!!!")')
         sb.dummy.button(label='',iconClass='icnBasePref',showLabel=False,
                         action="alert('A wonderful action!')")
-        frame.div('Here goes the \"center\" content',margin='20px')
-                           
+        frame.div('Here goes the \"center\" content.',margin='20px')
