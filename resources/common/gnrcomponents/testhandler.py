5e05528ee1447b295c95a2a07eb9dac154afba46
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-30 15:44:35 +0000

    CSS3 on testhandler
    git-svn-id: http://svn.genropy.org/genro/trunk@5688 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/gnrcomponents/testhandler.py
+++ resources/common/gnrcomponents/testhandler.py
@@ -10,16 +10,16 @@ class TestHandler(BaseComponent):
     testOnly=False
     def windowTitle(self):
         return 'Test: %s' % self.__module__.__doc__ or 'unnamed'
-
+        
     def testHandler(self, pane):
         self.testHandler_headers(pane)
-
+        
         #pane.div(height='12px',width='12px',position='absolute',top='2px',right='2px',background='red',nodeId='rpcpulser',opacity=0)
-        title = pane.div(width='900px', text_align='center', color='gray', font_size='20pt')
+        title = pane.div(width='900px', text_align='center', color='#63B8F7', font_size='20pt')
         title.span(self.__module__.__doc__ or '...missing docline in module...')
         pane = pane.div(width='900px')
         self.testHandler_loop(pane)
-
+        
     def testHandler_headers(self, pane):
         header = pane.div(width='900px', margin='5px')
         header.div('Dojo version :', float='left', margin_top='7px', color='gray')
@@ -36,7 +36,7 @@ class TestHandler(BaseComponent):
         m.menuline('Claro')
         m.menuline('Tundra')
         m.menuline('Soria')
-
+        
     def testHandler_loop(self, pane):
         def skip_test(test_name):
             if not self.testOnly:
@@ -53,12 +53,15 @@ class TestHandler(BaseComponent):
             if skip_test(test_name):
                 continue
             test_handler = getattr(self, test_name)
-            element = pane.div(border='1px solid gray', margin='5px', datapath='test.%s' % test_name)
+            element = pane.div(border='1px solid gray',margin='5px',
+                               rounded=5,shadow='3px 3px 5px gray',
+                               datapath='test.%s' % test_name)
             h = element.div()
             h.a('Source', href='', float='right', onclick='alert("ggg")', color='white', font_size='10px',
                 margin_right='5px', margin_top='3px', style='cursor:pointer')
-            h.div(test_handler.__doc__ or test_name, background_color='gray', color='white', padding='3px')
-
+            h.div(test_handler.__doc__ or test_name,background_color='#63B8F7',
+                  color='white',padding='3px')
+            
             element = element.div(padding='5px')
             test_handler(element)
 
