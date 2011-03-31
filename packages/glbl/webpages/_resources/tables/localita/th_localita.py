0909df2b6bfd7f86ab5cebdd9161ecf79c887acc
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-31 13:28:39 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5700 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/glbl/webpages/_resources/tables/localita/th_localita.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+
+# th_localita.py
+# Created by Francesco Porcari on 2011-03-31.
+# Copyright (c) 2011 Softwell. All rights reserved.
+from gnr.web.gnrwebpage import BaseComponent
+
+class Form(BaseComponent):
+    def formCb(self,pane,**kwargs):
+        #pane=parent.contentPane(padding='5px',**kwargs).div(_class='pbl_roundedGroup', height='100%')
+        pane.div(u'!!Località',_class='pbl_roundedGroupLabel')
+        fb = pane.formbuilder(cols=1, margin_left='2em',border_spacing='7px',margin_top='1em')
+        fb.field('nome', width='20em')
+        fb.field('provincia',width='15em')
+        fb.field('prefisso_tel',width='3em')
+        fb.field('cap',width='3em')
+        fb.field('codice_istat',width='7em')
+        fb.field('codice_comune',width='4em')
+
+class View(BaseComponent):
+    def lstBase(self,struct):
+        r = struct.view().rows()
+        r.fieldcell('nome',width='50%')
+        r.fieldcell('provincia',width='20%')
+        r.fieldcell('codice_istat',width='10%') 
+        r.fieldcell('codice_comune',width='10%')
+        r.fieldcell('prefisso_tel',width='5%')
+        r.fieldcell('cap',width='5%')
+        
+    def orderBase(self):
+        return 'nome'
+        
+    def queryBase(self):
+        return dict(column='nome',op='contains', val=None)
+        
