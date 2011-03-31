4506be7cd58606280a643b584b9368054d99f1f3
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-31 11:53:09 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5697 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/glbl/webpages/_resources/tables/localita/thform/default.py
+++ packages/glbl/webpages/_resources/tables/localita/thform/default.py
@@ -7,8 +7,8 @@
 from gnr.web.gnrwebpage import BaseComponent
 
 class Main(BaseComponent):
-    def formCb(self,parent,**kwargs):
-        pane=parent.contentPane(padding='5px',**kwargs).div(_class='pbl_roundedGroup', height='100%')
+    def formCb(self,pane,**kwargs):
+        #pane=parent.contentPane(padding='5px',**kwargs).div(_class='pbl_roundedGroup', height='100%')
         pane.div(u'!!Località',_class='pbl_roundedGroupLabel')
         fb = pane.formbuilder(cols=1, margin_left='2em',border_spacing='7px',margin_top='1em')
         fb.field('nome', width='20em')
