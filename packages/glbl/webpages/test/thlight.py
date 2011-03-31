cc66c8a54f399d1d7e626de14f24c76421c2b03b
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-03-31 13:31:14 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5704 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/glbl/webpages/test/thlight.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+
+# thlight.py
+# Created by Francesco Porcari on 2011-03-30.
+# Copyright (c) 2011 Softwell. All rights reserved.
+
+"Test page description"
+class GnrCustomWebPage(object):
+    py_requires="gnrcomponents/testhandler:TestHandlerFull,tablehandler/th_core:StackTableHandler"
+
+    def windowTitle(self):
+        return ''
+         
+    def test_0_firsttest(self,pane):
+        """First test description"""
+        sc = pane.stackTableHandler(height='400px',table='glbl.localita',inputForm='thform/default')
+        sc.form.store.handler('load',default_provincia='MI')
