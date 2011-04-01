c22fad2b9e6806103a2111cedce5a3d0b210cfee
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-01 14:42:18 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5739 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/webpages/th/thrunner.py
+++ packages/adm/webpages/th/thrunner.py
@@ -5,31 +5,35 @@
 # Copyright (c) 2011 Softwell. All rights reserved.
 
 class GnrCustomWebPage(object):
+    py_requires = """tablehandler/th_components:StackTableHandler"""
     """
-    http//mysite/adm/th/thrunner/package/table/pkey? widget='se diverso da stack' && formName='' && viewName= && iframe='v||f||b'
+    http//mysite/adm/th/thrunner/package/table/pkey? th_widget='se diverso da stack' && th_formName='' && th_viewName= && th_iframe='v||f||b'
     """
-
     def onIniting(self, url_parts, request_kwargs):
         self.mixin_path = '/'.join(url_parts)
-        pkg = url_parts.pop(0)
-        tablename = url_parts.pop(0)
-        table = '%s.%s' %(pkg,tablename)
-        name = request_kwargs.pop('formName','default')
-        self.maintable = table
-        self.mixinComponent(pkg,'tables',tablename,'thform','%s:Main' %name)
-        if url_parts:
-            request_kwargs['pkey'] = url_parts.pop(0)
+        if request_kwargs.get('method') == 'main':
+            request_kwargs['th_pkg'] = url_parts.pop(0)
+            request_kwargs['th_table'] = url_parts.pop(0)
+            if url_parts:
+                request_kwargs['th_pkey'] = url_parts.pop(0)
         while len(url_parts)>0: 
             url_parts.pop(0)
+    
+    def main(self,root,**kwargs):
+        th_widget = kwargs.pop('th_widget','sc')
+        handler = getattr(self,'main_%s' %th_widget)
+        assert handler, 'not exitsting handler for widget %s' %th_widget
+        #pane = root.rootContentPane(title='Test',datapath='test')
+        handler(root,**kwargs)
 
-
-    def main_stack(self, rootBC, **kwargs):
-        sc = pane.stackTableHandler(height='400px',table='glbl.localita',inputForm='thform/default')
-        sc.form.store.handler('load',default_provincia='MI')
+    def main_sc(self, root,th_pkg=None,th_table=None, **kwargs):
+        table = '%s.%s' %(th_pkg,th_table)
+        sc = root.stackTableHandler(table=table,datapath='th',**kwargs)
+        #sc.form.store.handler('load',default_provincia='MI')
         
-    def main_frame(self, rootBC, **kwargs):
-        bc = pane.borderContainer(height='600px')
-        gridpane = bc.contentPane(region='top',height='50%')
-        formpane = bc.contentPane(region='center')
-        formpane.dataController("",dbevent="^gnr.dbevent.glbl_localita.id")
-        gridpane.iframeTableHandler(table='glbl.localita',inputPane=formpane) 
+  # def main_frame(self, rootBC, **kwargs):
+  #     bc = root.borderContainer(height='600px')
+  #     gridpane = bc.contentPane(region='top',height='50%')
+  #     formpane = bc.contentPane(region='center')
+  #     formpane.dataController("",dbevent="^gnr.dbevent.glbl_localita.id")
+  #     gridpane.iframeTableHandler(table='glbl.localita',inputPane=formpane) 
