f12017833efc99efa4be73dfe95841088f6ab2a3
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-01 06:36:31 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5706 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- gnrpy/gnr/web/gnrwebstruct.py
+++ gnrpy/gnr/web/gnrwebstruct.py
@@ -324,6 +324,9 @@ class GnrDomSrc(GnrStructData):
         self.child('callBack',childcontent=cb,**kwargs)
         return self
         
+    def htmlform(self,childcontent=None,**kwargs):
+        return self.htmlChild('form',  childcontent=childcontent, **kwargs)
+
     def h1(self, childcontent=None, **kwargs):
         return self.htmlChild('h1',  childcontent=childcontent, **kwargs)
         
@@ -664,7 +667,7 @@ class GnrDomSrc_dojo_11(GnrDomSrc):
     """add???"""
     htmlNS = ['a', 'abbr', 'acronym', 'address', 'area', 'b', 'base', 'bdo', 'big', 'blockquote',
               'body', 'br', 'button', 'caption', 'cite', 'code', 'col', 'colgroup', 'dd', 'del',
-              'div', 'dfn', 'dl', 'dt', 'em', 'fieldset', 'form', 'frame', 'frameset',
+              'div', 'dfn', 'dl', 'dt', 'em', 'fieldset', 'frame', 'frameset',
               'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'hr', 'html', 'i', 'iframe', 'img', 'input',
               'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'meta', 'noframes', 'noscript',
               'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'q', 'samp',
