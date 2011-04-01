65cf0ffb05f5315e8b625c546949ede3815318e4
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-01 09:38:42 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5727 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/glbl/webpages/_resources/tables/provincia/th_provincia.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+# th_localita.py
+# Created by Francesco Porcari on 2011-03-31.
+# Copyright (c) 2011 Softwell. All rights reserved.
+from gnr.web.gnrwebpage import BaseComponent
+
+class Form(BaseComponent):
+    def formCb(self,pane,**kwargs):
+        pane.div(u'!!Provincia',_class='pbl_roundedGroupLabel')
+        fb = pane.formbuilder(cols=1, margin_left='2em',border_spacing='7px',margin_top='1em')
+        fb.field('nome', width='20em')
+        fb.field('sigla',width='3em')
+        fb.field('codice_istat',width='7em')
+        fb.field('regione',width='15em')
+
+class View(BaseComponent):
+    def lstBase(self,struct):
+        r = struct.view().rows()
+        r.fieldcell('nome', width='20em')
+        r.fieldcell('sigla',width='3em')
+        r.fieldcell('codice_istat',width='7em')
+        r.fieldcell('regione',width='15em')
+        
+    def orderBase(self):
+        return 'nome'
+        
+    def queryBase(self):
+        return dict(column='nome',op='contains', val=None)
+        
