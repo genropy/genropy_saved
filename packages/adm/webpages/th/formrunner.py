c0219d1d32ca1499418e9dc7c6f7c9d904a035f2
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-05 07:43:16 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5759 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/webpages/th/formrunner.py
+++ packages/adm/webpages/th/formrunner.py
@@ -9,4 +9,23 @@
 
 # --------------------------- GnrWebPage subclass ---------------------------
 class GnrCustomWebPage(object):
-    py_requires='tablehandler/th_iframepage:RecordPage'
+    py_requires = """tablehandler/th_form:TableHandlerFormBase"""
+
+    def onIniting(self, url_parts, request_kwargs):
+        self.mixin_path = '/'.join(url_parts)
+        if request_kwargs.get('method') == 'main':
+            request_kwargs['th_pkg'] = url_parts.pop(0)
+            request_kwargs['th_table'] = url_parts.pop(0)
+            if url_parts:
+                request_kwargs['th_pkey'] = url_parts.pop(0)
+        while len(url_parts)>0: 
+            url_parts.pop(0)
+
+    def main(self,root,th_pkg=None,th_table=None,th_pkey='*norecord*',th_formName=None,th_storeType=None,**kwargs):
+        fulltable = '.'.join([th_pkg,th_table])
+        tableCode = fulltable.replace('.','_')
+        defaultName = 'th_%s' %th_table
+        formName = th_formName or defaultName
+        root.attributes['datapath'] = tableCode
+        self.mixinComponent(th_pkg,'tables',th_table,'%s:Form' %formName,mangling_th=tableCode)
+        form = root.formPage(frameCode='%s_form' %tableCode,table=fulltable,startKey=th_pkey,**kwargs)
--- packages/adm/webpages/th/viewrunner.py
+++ packages/adm/webpages/th/viewrunner.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env pythonw
 # -*- coding: UTF-8 -*-
 #
-#  view runner
+#   form _runner
 #
 #  Created by Giovanni Porcari on 2007-03-24.
 #  Copyright (c) 2007 Softwell. All rights reserved.
 #
 
-""" generic_frame"""
-
 # --------------------------- GnrWebPage subclass ---------------------------
 class GnrCustomWebPage(object):
-    py_requires='tablehandler/th_iframepage:ViewPage'
+    py_requires = """tablehandler/th_list:TableHandlerListBase"""
+
+    def onIniting(self, url_parts, request_kwargs):
+        self.mixin_path = '/'.join(url_parts)
+        if request_kwargs.get('method') == 'main':
+            request_kwargs['th_pkg'] = url_parts.pop(0)
+            request_kwargs['th_table'] = url_parts.pop(0)
+        while len(url_parts)>0: 
+            url_parts.pop(0)
+
+    def main(self,root,th_pkg=None,th_table=None,th_pkey=None,th_viewName=None,th_loadEvent=None,**kwargs):
+        fulltable = '.'.join([th_pkg,th_table])
+        tableCode = fulltable.replace('.','_')
+        defaultName = 'th_%s' %th_table
+        viewName = th_viewName or defaultName
+        self.mixinComponent(th_pkg,'tables',th_table,'%s:View' %viewName,mangling_th=tableCode)
+        root.attributes['datapath'] = tableCode
+        listpage = root.listPage(frameCode='%s_list' %tableCode,table=fulltable,pageName='view')
