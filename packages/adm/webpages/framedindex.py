af31f8055ec685d7aa05ac718c406ceed76c7649
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-06 12:25:49 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5777 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/adm/webpages/framedindex.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+
+# framedindex.py
+# Created by Francesco Porcari on 2011-04-05.
+# Copyright (c) 2011 Softwell. All rights reserved.
+
+class GnrCustomWebPage(object):
+    dojo_source=True
+    py_requires='public:Public,foundation/menu:MenuIframes'
+    plugin_list = 'iframemenu_plugin,batch_monitor,chat_plugin'    
+         
+
+    def rootWidget(self, root, **kwargs):
+        return root.stackContainer(selectedPage='^selectedFrame',nodeId='center_stack',region='center')
+    
+    def main(self, root, **kwargs):
+        #root = root.rootContentPane(title='ciao')
+        root.dataController("""
+            var sc = genro.nodeById("center_stack");
+            var page = sc.getValue().getItem(name);
+            if(!page){
+                 page = sc._('ContentPane',name,{pageName:name,title:label,_lazyBuild:true,overflow:'hidden'});
+                 var url = file;
+                 if(table){
+                    url = '/adm/th/thrunner/'+table.replace('.','/');
+                 }
+                 else{
+                    url = file;
+                    if(url.indexOf('?')>0){
+                        url+='&&inframe=true'
+                    }else{
+                        url+='?inframe=true'
+                    }
+                 }
+                 page._('iframe',{'height':'100%','width':'100%','border':0,src:url});
+             }
+            SET selectedFrame = name;
+        """,subscribe__menutree__selected=True)
+
+    
