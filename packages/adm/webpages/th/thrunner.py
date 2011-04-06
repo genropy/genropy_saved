ede2cd5fec2902a34fa2085bd062c9c31e495ba1
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-06 12:26:14 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5780 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/webpages/th/thrunner.py
+++ packages/adm/webpages/th/thrunner.py
@@ -5,34 +5,21 @@
 # Copyright (c) 2011 Softwell. All rights reserved.
 
 class GnrCustomWebPage(object):
-    py_requires = """tablehandler/th_components:StackTableHandler"""
-    """
-    http//mysite/adm/th/thrunner/package/table/pkey? th_widget='se diverso da stack' && th_formName='' && th_viewName= && th_iframe='v||f||b'
-    """
-    def onIniting(self, url_parts, request_kwargs):
-        self.mixin_path = '/'.join(url_parts)
-        if request_kwargs.get('method') == 'main':
-            request_kwargs['th_pkg'] = url_parts.pop(0)
-            request_kwargs['th_table'] = url_parts.pop(0)
-            if url_parts:
-                request_kwargs['th_pkey'] = url_parts.pop(0)
-        while len(url_parts)>0: 
-            url_parts.pop(0)
+    py_requires = """public:Public,tablehandler/th_components:StackTableHandler"""
+    plugin_list=''
+    def onMain_pbl(self):
+        pass
     
     def main(self,root,**kwargs):
+        kwargs.update(self.getCallArgs('th_pkg','th_table'))
         th_widget = kwargs.pop('th_widget','sc')
         handler = getattr(self,'main_%s' %th_widget)
         assert handler, 'not exitsting handler for widget %s' %th_widget
         handler(root,**kwargs)
 
-    def main_sc(self, root,th_pkg=None,th_table=None, **kwargs):
+    def main_sc(self, root,th_pkg=None,th_table=None, th_public=True,**kwargs):
         table = '%s.%s' %(th_pkg,th_table)
+        if th_public:
+            tblobj = self.db.table(table)
+            root = root.rootContentPane(title=tblobj.name_long)
         root.stackTableHandler(table=table,datapath=table.replace('.','_'),**kwargs)
-
-        
-  # def main_frame(self, rootBC, **kwargs):
-  #     bc = root.borderContainer(height='600px')
-  #     gridpane = bc.contentPane(region='top',height='50%')
-  #     formpane = bc.contentPane(region='center')
-  #     formpane.dataController("",dbevent="^gnr.dbevent.glbl_localita.id")
-  #     gridpane.iframeTableHandler(table='glbl.localita',inputPane=formpane) 
