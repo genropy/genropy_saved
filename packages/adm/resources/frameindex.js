a7b26162569a256474b9f0fcd798f6ba1d73c7fe
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-11 09:01:16 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5833 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ packages/adm/resources/frameindex.js
@@ -0,0 +1,26 @@
+var frameIndex = {};
+
+frameIndex.selectIframePage=function(sourceNode,name,label,file,table,formResource,viewResource){
+    var sc = genro.nodeById("center_stack");
+    var page = sc.getValue().getItem(name);
+    var url;
+    if(!page){
+         page = sc._('ContentPane',name,{pageName:name,title:label,overflow:'hidden',nodeId:name});
+         url = file;
+         var urlPars = {inframe:true};
+         if(table){
+            url = '/adm/th/thrunner/'+table.replace('.','/');
+            if(formName){
+                urlPars['th_formResource'] = formResource;
+            }
+            if(viewName){
+                urlPars['th_viewResource'] = viewResource;
+            }
+         }
+         url = genro.addParamsToUrl(url,urlPars);
+    }
+    sourceNode.setRelativeData('selectedFrame',name);
+    if(url){
+        setTimeout(function(){page._('iframe',{'height':'100%','width':'100%','border':0,src:url});},1);
+    }
+}
