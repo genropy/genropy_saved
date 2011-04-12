c85cc84daa40b08e579e1685c59a768d8c3ad03c
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-12 09:08:51 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5851 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/glbl/webpages/test/thlight.py
+++ packages/glbl/webpages/test/thlight.py
@@ -6,14 +6,19 @@
 
 "Test page description"
 class GnrCustomWebPage(object):
-    py_requires="gnrcomponents/testhandler:TestHandlerFull,tablehandler/th_components:StackTableHandler"
+    py_requires="gnrcomponents/testhandler:TestHandlerFull,tablehandler/th_components:TableHandlerBase"
          
     def test_0_localita(self,pane):
         """First test description"""
-        sc = pane.stackTableHandler(height='400px',table='glbl.localita')
+        sc = pane.stackTableHandler(widget_height='400px',table='glbl.localita')
         sc.form.store.handler('load',default_provincia='MI')
 
     def test_1_provincia(self,pane):
         """First test description"""
-        sc = pane.stackTableHandler(height='400px',table='glbl.provincia')
+        sc = pane.stackTableHandler(widget_height='400px',table='glbl.provincia')
         sc.form.store.handler('load',default_regione='LOM')
+    
+   #def test_2_provincia2(self,pane):
+   #    """First test description"""
+   #    sc = pane.stackTableHandler(widget_height='400px',nodeId='provinciali',table='glbl.provincia')
+   #    sc.form.store.handler('load',default_regione='LOM')
