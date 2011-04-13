c6ad7db5b33349111dc36080430f214bce6881c8
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-13 10:56:47 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5881 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/tablehandler/th_components.py
+++ resources/common/tablehandler/th_components.py
@@ -113,7 +113,7 @@ class StackTableHandlerRunner(BaseComponent):
         formResource = th_formResource or self.formResource
         viewResource = th_viewResource or self.viewResource
         root = root.rootContentPane(title=self.tblobj.name_long)
-        root.stackTableHandler(table=self.maintable,datapath=self.maintable.replace('.','_'),formResource=formResource,viewResource=viewResource,virtualStore=True,**kwargs)
+        root.stackTableHandler(table=self.maintable,datapath=self.maintable.replace('.','_'),formResource=formResource,viewResource=viewResource,virtualStore=False,**kwargs)
         
     
      
