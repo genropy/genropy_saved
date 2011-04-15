3e3ce7c8d03ec0e58a182f6d6a8183d6d051e5ae
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-15 09:28:57 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5916 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- resources/common/tablehandler/th_list.py
+++ resources/common/tablehandler/th_list.py
@@ -293,7 +293,7 @@ class TableHandlerListBase(TableHandlerList):
         pane.dataController("""
             this.setRelativeData(".query.where",baseQuery.deepCopy(),{objtype:"query", tbl:maintable});
             genro.querybuilder(maintable).buildQueryPane(); 
-        """,_fired='^.query.new',baseQuery='=.baseQuery', maintable=table or self.maintable)
+        """,_fired='^.query.new',baseQuery='=.baseQuery', maintable=table)
         
        #else:
        #    iv.selectionStore(table=table,childname='store',
