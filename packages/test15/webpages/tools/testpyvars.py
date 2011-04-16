146528c8f32b8c1506fe178cbea46be56476fa16
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-16 15:01:18 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5944 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/test15/webpages/tools/testpyvars.py
+++ packages/test15/webpages/tools/testpyvars.py
@@ -5,13 +5,23 @@
 class GnrCustomWebPage(object):
     py_requires = "gnrcomponents/testhandler:TestHandlerFull,"
 
-    def test_1_(self, pane):
+    def test_1_a(self, pane):
         """Set in external store"""
         fb=pane.formbuilder(cols=1)
         tb1=fb.textbox(value='^aaa',lbl='aaa',)
         fb.textbox(value='^bbb',lbl='bbb')
         fb.div('^ccc')
         pane.dataFormula('ccc','a+" - "+b',a='^aaa',b='^bbb')
-        pane.button(label='ffff',action="console.log(tb1)",tb1=tb1)
+        
+        btn = pane.button(label='ffff',action="console.log(tb1)",tb1=tb1)
 
+    def test_2_b(self, pane):
+        xxx = pane.button(label='ttt')
+        box = pane.div(height='12px',width='12px',background='red',
+                        selfsubscribe_test='genro.bp();console.log(btn)',
+                        btn=xxx,
+                        connect_onclick='console.log(btn);'
+                        )
+        
+        pane.button('test 2',action='box.publish("test")',box=box)
 
