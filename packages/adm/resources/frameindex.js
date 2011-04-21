83a046ae3153337860f98b8fb0cb6d58447562e2
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-21 10:42:37 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@5983 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- packages/adm/resources/frameindex.js
+++ packages/adm/resources/frameindex.js
@@ -1,11 +1,17 @@
 var frameIndex = {};
 
 frameIndex.selectIframePage=function(sourceNode,name,label,file,table,formResource,viewResource){
-    var sc = genro.nodeById("center_stack");
-    var page = sc.getValue().getItem(name);
-    var url;
-    if(!page){
-         page = sc._('ContentPane',name,{pageName:name,title:label,overflow:'hidden',nodeId:name});
+    var sc = genro.nodeById("center_stack").getValue();
+    var page = sc.getItem(name);
+    if (page){
+        sourceNode.setRelativeData('selectedFrame',name);
+    }
+    else{
+         var root = genro.src.newRoot();
+         var bc = root._('BorderContainer',name,{pageName:name,title:label});
+         var bottom = bc._('ContentPane',{'height':'12px',region:'bottom',background:'red'});
+         var center = bc._('ContentPane',{'region':'center','overflow':'hidden'});
+         var iframe = center._('iframe',{'height':'100%','width':'100%','border':0});
          url = file;
          var urlPars = {inframe:true};
          if(table){
@@ -18,9 +24,9 @@ frameIndex.selectIframePage=function(sourceNode,name,label,file,table,formResour
             }
          }
          url = genro.addParamsToUrl(url,urlPars);
-    }
-    sourceNode.setRelativeData('selectedFrame',name);
-    if(url){
-        setTimeout(function(){page._('iframe',{'height':'100%','width':'100%','border':0,src:url});},1);
+         var node = root.popNode('#0');
+         sc.setItem(node.label,node);
+         sourceNode.setRelativeData('selectedFrame',name);
+         iframe.getParentNode().domNode.src = url;
     }
 }
