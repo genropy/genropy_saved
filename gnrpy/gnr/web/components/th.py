237a56d0cdea7c32ae288d51cbba493518353331
fporcari <fporcari@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-04-28 17:00:51 +0000

    git-svn-id: http://svn.genropy.org/genro/trunk@6074 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

---
+++ gnrpy/gnr/web/components/th.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+th.py
+
+Created by Francesco Porcari on 2011-04-28.
+Copyright (c) 2011 Softwell. All rights reserved.
+"""
+
+from gnr.web.gnrbaseclasses import BaseClasses
+
+
+class BaseView(BaseClasses):
+    def th_struct(self,struct):
+        r = struct.view().rows()
+        self.th_columns(r)
+        
+    def th_columns(self,r):
+        """
+            r.fieldcell('col1', name='Col1',width='10em')
+            r.fieldcell('col2', name='Col2',width='100%')
+        """
+        pass
+
+    def th_order(self):
+        "return 'fieldname,..'"
+        return
+        
+    def th_query(self):
+        #rename as th_default_query
+        """return dict(column='xxx',op='contains',val='')"""
+        return         
+    
+    def th_condition(self):
+        """return dict()"""
+        return
+
+
+class BaseForm(BaseClasses):
+    def th_form(self, pane):
+        pass
+    
+    def th_dialog(self):
+        """return dict()"""
+        return
