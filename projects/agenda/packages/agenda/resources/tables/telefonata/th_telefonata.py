dcb80fe2ed2a2d5d1e65bc0c8691f6345c738453
fastolfi <fastolfi@c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8>
2011-05-04 14:59:56 +0000

    agenda update
    git-svn-id: http://svn.genropy.org/genro/trunk@6170 c8d40aac-abfe-4c07-9ed8-ca0f8f1311d8

--- projects/agenda/packages/agenda/resources/tables/telefonata/th_telefonata.py
+++ projects/agenda/packages/agenda/resources/tables/telefonata/th_telefonata.py
@@ -9,16 +9,16 @@ from gnr.web.gnrbaseclasses import BaseComponent
 class View(BaseComponent):
     def th_struct(self,struct):
         r = struct.view().rows()
-        r.fieldcell('@anagrafica_id.nome', width='8%')
-        r.fieldcell('@anagrafica_id.cognome', width='8%')
-        r.fieldcell('@anagrafica_id.email', name='!!Mail personale', width='14%')
-        r.fieldcell('@anagrafica_id.telefono', name='!!Telefono personale', width='8%')
-        r.fieldcell('@anagrafica_id.codice_fiscale', width='10%')
-        r.fieldcell('@anagrafica_id.partita_iva', name='!!P.IVA personale', width='7%')
-        r.fieldcell('@anagrafica_id.fax', name='!!Fax personale', width='7%')
-        r.fieldcell('interno', width='6%')
-        r.fieldcell('ruolo', width='7%')
-        r.fieldcell('@anagrafica_id.note', name='!!Note personali', width='9%')
+        r.fieldcell(field='giorno',width='5%')
+        r.fieldcell('ora',width='3%')
+        r.fieldcell('username', name='!!Utente', width='8%')
+        r.fieldcell('destinatario', name='!!Chi ha cercato', width='9%')
+        r.fieldcell('cognome', name='!!Chi ha chiamato', width='8%')
+        r.fieldcell('@contatto_id.@anagrafica_id.telefono', name='!!Telefono personale', width='7%')
+        r.fieldcell('@contatto_id.@anagrafica_id.email', name='Mail personale', width='12%')
+        r.fieldcell('@azienda_id.@anagrafica_id.ragione_sociale', name='!!Azienda', width='18%')
+        r.fieldcell('@azienda_id.@anagrafica_id.telefono', name='!!Telefono azienda', width='8%')
+        r.fieldcell('descrizione', width='22%')
         return struct
         
     def th_order(self):
@@ -29,21 +29,24 @@ class View(BaseComponent):
         
 class Form(BaseComponent):
     def th_form(self, form):
-        pane = form.record
-        pane.dataFormula("form.title", '"Scheda contatto: " + nome + " " + cognome',
-                        nome='^.@anagrafica_id.nome', cognome='^.@anagrafica_id.cognome',
-                        _if='nome && cognome',_else='"Scheda contatto: nuovo contatto"')
-        fb = pane.formbuilder(cols=2,border_spacing='6px',fld_width='15em',lbl_color='teal')
-        fb.field('@anagrafica_id.nome')
-        fb.field('@anagrafica_id.cognome')
-        fb.field('@anagrafica_id.email',
-                  validate_email=True,validate_email_error='!!Formato email non corretto')
-        fb.field('@anagrafica_id.telefono', ghost='esempio: 347/1212123')
-        fb.field('@anagrafica_id.codice_fiscale',
-                  validate_case='u')
-        fb.field('@anagrafica_id.partita_iva')
-        fb.field('@anagrafica_id.fax')
-        fb.field('interno', ghost='esempio: 202')
-        fb.field('@anagrafica_id.note', tag='textarea', colspan=2, width='100%')
-        fb.field('ruolo',tag='combobox', lbl='Ruolo nell\'azienda',
-                  values='dipendente, libero professionista, manager')
+        pane = parentBC.contentPane(**kwargs)
+        fb = pane.formbuilder(cols=3,fld_width='15em',
+                              disabled=disabled,lbl_color='teal')
+        fb.field('giorno',readOnly=True,width='6em',tooltip='Campo non modificabile')
+        fb.field('ora',readOnly=True,width='6em',tooltip='Campo non modificabile')
+        fb.field('username',readOnly=True,lbl='Utente',tooltip='Campo non modificabile')
+        fb.field('contatto_id',zoom=True,lbl='Chiamante',colspan=2,
+                 selected_azienda_id='.azienda_id',
+                 validate_notnull=True,validate_notnull_error='!!Campo obbligatorio')
+        pane.dataFormula("aux.mostra_contatto",'true',_if='contatto_id',_else='false',
+                          contatto_id='^.contatto_id')
+        fb.button('Mostra dati chiamante',action='genro.wdgById("info_chiamante").show()',
+                  visible='^aux.mostra_contatto')
+        
+        fb.field('destinatario_id',lbl='!!Destinatario',colspan=2)
+        pane.dataFormula("aux.mostra_destinatario",'true',_if='destinatario_id',_else='false',
+                          destinatario_id='^.destinatario_id')
+        fb.button('Mostra dati destinatario',action='genro.wdgById("info_destinatario").show()',
+                  visible='^aux.mostra_destinatario')
+        
+        fb.field('descrizione',tag='textarea',colspan=3,width='100%',height='60px')
