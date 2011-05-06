a70357b404b31931464a43683a0372369df79e5a
Francesco Porcari <francesco.porcari@softwell.it>
2011-05-06 14:52:07 +0200

    test

--- packages/adm/resources/frameindex.py
+++ packages/adm/resources/frameindex.py
@@ -3,6 +3,7 @@
 # frameindex.py
 # Created by Francesco Porcari on 2011-04-06.
 # Copyright (c) 2011 Softwell. All rights reserved.
+# Frameindex component
 
 from gnr.web.gnrwebpage import BaseComponent
 class Mixin(BaseComponent):
