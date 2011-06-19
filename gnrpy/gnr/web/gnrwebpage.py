3f73d923f47612634c3a9f456dbfd306cf4a8680
Jeff Edwards <jeffedwa@goodsoftware.com.au>
2011-06-19 13:47:59 +1000

    just fixing parameter passing - please check that this is what you meant

--- gnrpy/gnr/web/gnrwebpage.py
+++ gnrpy/gnr/web/gnrwebpage.py
@@ -332,8 +332,8 @@ class GnrWebPage(GnrBaseWebPage):
         :param \*\*kwargs: add???"""
         self.site.resource_loader.mixinPageComponent(self, pkg, *path,**kwargs)
         
-    def tableTemplate(self,table=None,tplname=None):
-        return self.getTableResourceContent(table=table,path='tpl/%s' %tplname,ext='html')
+    def tableTemplate(self,table=None,tplname=None, ext='html'):
+        return self.getTableResourceContent(table=table,path='tpl/%s' %tplname,ext=ext)
         
     @property
     def isGuest(self):
@@ -1118,6 +1118,7 @@ class GnrWebPage(GnrBaseWebPage):
         
         :param \_auth: add???. Default value is ``AUTH_OK``
         :param debugger: add???. Default value is ``None``"""
+        err = None
         page = self.domSrcFactory.makeRoot(self)
         self._root = page
         pageattr = {}
@@ -1469,7 +1470,7 @@ class GnrWebPage(GnrBaseWebPage):
     def userDocumentUrl(self, *args, **kwargs):
         """add???"""
         if kwargs:
-            return self.site.getStatic('user').kwargs_url(self.user, *args, **kwargs_url)
+            return self.site.getStatic('user').kwargs_url(self.user, *args, **kwargs)
         else:
             return self.site.getStatic('user').url(self.user, *args)
             
