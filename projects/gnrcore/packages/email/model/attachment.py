e307e93448dfaf29924e3e46c4a05dae1bcaed59
Michele Bertoldi <michele.bertoldi@softwell.it>
2011-08-23 14:28:13 +0200

    email

--- projects/gnrcore/packages/email/model/attachment.py
+++ projects/gnrcore/packages/email/model/attachment.py
@@ -5,7 +5,8 @@ class Table(object):
     def config_db(self, pkg):
         tbl =  pkg.table('attachment', rowcaption='title', pkey='id', name_long='!!Attachment', name_plural='!!Attachments')
         self.sysFields(tbl)
-        tbl.column('title',name_long='!!Title')
+        #tbl.column('title',name_long='!!Title')
+        tbl.column('filename',name_long='!!Filename')
         tbl.column('mime_type',name_long='!!Mime Type')
         tbl.column('size',name_long='!!Size')
         tbl.column('path',name_long='!!path')
