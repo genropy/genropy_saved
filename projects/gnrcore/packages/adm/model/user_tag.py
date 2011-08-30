0fbb9fafed9a8ed514430bfe565275788a06e1b3
Francesco Porcari <francesco.porcari@softwell.it>
2011-08-30 12:00:43 +0200

    a

--- projects/gnrcore/packages/adm/model/user_tag.py
+++ projects/gnrcore/packages/adm/model/user_tag.py
@@ -5,9 +5,9 @@ class Table(object):
         tbl =  pkg.table('user_tag',pkey='id',name_long='!!User tag',
                       name_plural='!!User tags')
         self.sysFields(tbl)
-        tbl.column('user_id',size='22',group='_',name_long='User').relation('user.id', mode='foreignkey', 
+        tbl.column('user_id',size='22',group='_',name_long='User',_sendback=True).relation('user.id', mode='foreignkey', 
                                                                             onDelete='cascade',relation_name='tags')
-        tbl.column('tag_id',size='22',group='_',name_long='Tag').relation('htag.id', mode='foreignkey', onDelete='raise',
+        tbl.column('tag_id',size='22',group='_',name_long='Tag id').relation('htag.id', mode='foreignkey', onDelete='raise',
                                                                           relation_name='users')
         tbl.aliasColumn('user',relation_path='@user_id.username')
         tbl.aliasColumn('fullname',relation_path='@user_id.fullname')
@@ -17,5 +17,21 @@ class Table(object):
         tbl.aliasColumn('tag_note',relation_path='@tag_id.note')
 
         #tbl.aliases(relation='@user_id',user='username')
+    
+    def trigger_onInserted(self, record_data):
+        self.setUserAuthTags(record_data)
+    
+    def trigger_onUpdated(self, record_data, old_record):
+        self.setUserAuthTags(record_data)
+    
+    def trigger_onDeleted(self, record):
+        self.setUserAuthTags(record)
+    
+    def setUserAuthTags(self,record):
+        user_id = record['user_id']
+        rows = self.query(where='$user_id=:u_id',u_id=user_id,columns='$tag_code',addPkeyColumn=False).fetch()
+        tags = ','.join([r['tag_code'] for r in rows])
+        self.db.table('adm.user').batchUpdate(dict(auth_tags=tags),where='$id=:pkey',pkey=user_id)
+
 
         
