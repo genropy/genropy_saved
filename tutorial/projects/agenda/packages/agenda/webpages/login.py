f8b2db002a3d609ddbb0a32e8bd5a4abe13080ed
fastolfi <filippo.astolfi@softwell.it>
2011-09-14 16:24:49 +0200

    tutorials update

--- tutorial/projects/agenda/packages/agenda/webpages/login.py
+++ tutorial/projects/agenda/packages/agenda/webpages/login.py
@@ -18,6 +18,6 @@ class GnrCustomWebPage(object):
     def loginCenter(self,frame):
         fb = frame.formbuilder(border_spacing='3px',onEnter='PUBLISH login;',margin='auto',
                                margin_top='15px',fld_line_height='20px',fld_padding='2px',fld_rounded=6)
-        fb.textbox(value='^.user',ghost='Username: agenda')
+        fb.textbox(value='^.user',ghost='Username: agenda',autofocus=True)
         fb.textbox(value='^.password',ghost='Password: guest',type='password')
         frame.button('Entra',action='PUBLISH login;',padding='1px',rounded=10,bottom='3px',right='32px',position='absolute')
