a74fca6077c3ce8018c3351bba3b9a888c83a486
Saverio Porcari <saverioporcari@Pussobook.local>
2011-09-21 10:35:43 +0200

    a

---
+++ projects/gnrcore/packages/flib/resources/tables/item/th_item.py
@@ -0,0 +1,56 @@
+# -*- coding: UTF-8 -*-
+# Created by Saverio Porcari on 2011-03-13.
+# Copyright (c) 2011 Softwell. All rights reserved.
+
+from gnr.web.gnrbaseclasses import BaseComponent
+from gnr.core.gnrdecorator import public_method
+class View(BaseComponent):
+    def th_struct(self,struct):
+        r = struct.view().rows()
+        
+        r.fieldcell('title', name='!!Title')
+        r.fieldcell('description', name='!!Description')
+        r.fieldcell('url', name='!!Url')
+        r.fieldcell('path', name='!!Path')
+        r.fieldcell('thumb_url', name='!!Url')
+        r.fieldcell('thumb_path', name='!!Path')
+        r.fieldcell('file_type', name='!!File type')
+        r.fieldcell('ext', name='!!Extension')
+        r.fieldcell('username', name='!!User')
+        
+    def th_order(self):
+        return 'description'
+        
+    def th_query(self):
+        return dict(column='description',op='contains',val='',runOnStart=True)
+    
+    @public_method
+    def th_applymethod(self,selection=None,**kwargs):
+        print x
+        
+class LoadedFilesView(BaseComponent):
+    def th_struct(self,struct):
+        r = struct.view().rows()
+        
+        r.fieldcell("title", width='10em', zoom=True)
+        r.fieldcell("description", width='15em', zoom=True)
+        r.cell("_thumb", width='5em', name='!!Thumb', calculated=True)
+        
+
+        
+        
+class Form(BaseComponent):
+    def th_form(self, form):
+        pane = form.record
+        return
+        fb = pane.formbuilder(cols=1, margin_left='2em',border_spacing='7px',
+                              margin_top='1em')
+        r.field('title', lbl='!!Title')
+        r.field('description', lbl='!!Description')
+        r.field('url', lbl='!!Url')
+        r.field('path', lbl='!!Path')
+        r.field('thumb_url', lbl='!!Url')
+        r.field('thumb_path', lbl='!!Path')
+        r.field('file_type', lbl='!!File type')
+        r.field('ext', lbl='!!Extension')
+        r.field('username', lbl='!!User')
