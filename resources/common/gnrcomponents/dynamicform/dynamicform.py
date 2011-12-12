a8d1158a41cc3350678d86faab262bf0f356d556
Francesco Porcari <francesco.porcari@softwell.it>
2011-12-12 16:18:32 +0100

    a

--- resources/common/gnrcomponents/dynamicform/dynamicform.py
+++ resources/common/gnrcomponents/dynamicform/dynamicform.py
@@ -35,20 +35,25 @@ class DynamicForm(BaseComponent):
     def df_fieldsGrid(self,pane,storepath=None,standard_range=False,title=None,**kwargs):   
         def struct(struct):
             r = struct.view().rows()
+            r.cell('_row_count',hidden=True,counter=True)
             r.cell('code', name='!!Code', width='5em')
             r.cell('description', name='!!Description', width='15em')
             r.cell('field_type', name='!!Type', width='10em')
         
             r.cell('field_source', name='!!Source', width='10em')
-            r.cell('range', name='!!Min:Max',width='10em')
+            r.cell('range', name='!!Min:Max',width='5em')
             #r.cell('max', name='!!Max',dtype='N', width='5em')
             if standard_range:
                 r.cell('standard_range', name='Std.Range', width='10em')
-            r.cell('formula', name='!!Formula', width='10em')  
+            r.cell('formula', name='!!Formula', width='15em') 
+            r.cell('condition', name='!!Condition',width='10em')  
+ 
             r.checkboxcell('do_summary', name='!!Summary',width='6em')       
             r.checkboxcell('mandatory', name='!!Mandatory',width='7em')  
+
             
-        frame = pane.frameGrid(storepath=storepath,struct=struct,datamode='bag',datapath='#FORM.fieldsGrid_%i' %id(pane),**kwargs)
+        frame = pane.frameGrid(storepath=storepath,struct=struct,datamode='bag',datapath='#FORM.fieldsGrid_%i' %id(pane),
+                              grid_selfDragRows=True,**kwargs)
         ge = frame.grid.gridEditor()
         ge.textbox(gridcell='code')
         ge.textbox(gridcell='description')
@@ -58,6 +63,7 @@ class DynamicForm(BaseComponent):
         if standard_range:      
             ge.textbox(gridcell='standard_range')
         ge.textbox(gridcell='formula')
+        ge.textbox(gridcell='condition')
         frame.top.slotToolbar('3,gridtitle,*,delrow,addrow,2',gridtitle=title or '!!Fields',delrow_parentForm=True,addrow_parentForm=True)
         return frame 
     @struct_method
@@ -68,26 +74,25 @@ class DynamicForm(BaseComponent):
 
     
     @public_method
-    def df_remoteDynamicForm(self,pane,df_table=None,df_pkey=None,df_folders=None,datapath=None,**kwargs):
+    def df_remoteDynamicForm(self,pane,df_table=None,df_pkey=None,df_folders=None,**kwargs):
         if not df_pkey:
             pane.div('!!No Form descriptor')
             return
-        fb_kwargs = dictExtract(kwargs,'fb_',pop=True)
         dbstore_kwargs = dictExtract(kwargs,'dbstore_',pop=True)
         pane.attributes.update(kwargs)
         df_tblobj = self.db.table(df_table)
         formDescriptor = df_tblobj.getFormDescriptor(pkey=df_pkey,folders=df_folders)
         fields = formDescriptor[df_tblobj.attributes.get('df_fields','fields')]
-        fb_kwargs.setdefault('datapath',datapath)
         fielddict = {'T':'Textbox','L':'NumberTextBox','D':'DateTextBox','B':'Checkbox','N':'NumberTextBox', 'TL':'Simpletextarea'}
-        fb = pane.formbuilder(**fb_kwargs)
+        fb = pane.div(margin_right='10px').formbuilder(cols=1)
         for fnode in fields:
             attr = dict(fnode.attr)
             field_type = attr.pop('field_type')
             attr['tag'] = fielddict[field_type]
-            attr['colspan'] = 2 if field_type == 'TL' else 1
+            #attr['colspan'] = col_max if field_type == 'TL' else 1
             self._df_handleFieldSource(attr,dbstore_kwargs=dbstore_kwargs)
             self._df_handleFieldFormula(attr,fb=fb,fields=fields)
+            self._df_handleFieldValidation(attr,fb,fields=fields)
             fb.child(value='^.%s' %attr.pop('code'), lbl='%s' %attr.pop('description'),**attr)
         
     def _df_handleFieldSource(self,attr,dbstore_kwargs=None):
@@ -98,6 +103,7 @@ class DynamicForm(BaseComponent):
             tag = 'dbSelect'
             attr['dbtable'] = field_source
             pkg,tblname = field_source.split('.')
+            attr['hasDownArrow'] =True
             if pkg in dbstore_kwargs.get('pkg','').split(','):
                 attr['_storename'] = '=%(name)s' %dbstore_kwargs
         else:
@@ -113,11 +119,30 @@ class DynamicForm(BaseComponent):
         if not formula:
             return
         formulaArgs = dict([(str(x),'^.%s' %x) for x in fields.digest('#a.code') if x in formula])
-        #formula = re.sub('[^\s,()*/%+-]+',lambda v:  v.group(0).replace(',','.') if v.group(0).startswith('Math.')else 'parseFloat(%s)' % v.group(0).replace(',','.'),formula)                                                        
-        #re.sub('[^\s()*/%+-]+',lambda v: 'parseFloat(%s)' % v.group(0),formula)
-        #print bazinga
         fb.dataFormula(".%s" %attr['code'], formula,**formulaArgs)
         attr['readOnly'] =True 
     
-    
+    def _df_handleFieldValidation(self,attr,fb,fields):
+        if 'range' in attr:
+            r = attr.pop('range')
+            min_v,max_v = r.split(':')
+            if min_v or max_v:
+                attr['validate_min'] = min_v or None
+                attr['validate_max'] = max_v or None
+                attr['validate_min_error'] = '!!Under min value %s' %min_v
+                attr['validate_max_error'] = '!!Over max value %s' %max_v
+        if 'condition' in attr:
+            condition = attr.pop('condition')
+            attr['row_hidden'] = """==function(sourceNode){
+                                        if(%s){
+                                            return false;
+                                        }else{
+                                            sourceNode.setRelativeData('.%s',null);
+                                            return true;
+                                        }
+                                    }(this);""" %(condition,attr['code'])
+            conditionArgs = dict([('row_%s' %str(x),'^.%s' %x) for x in fields.digest('#a.code') if x in condition])
+            attr.update(conditionArgs)
+            #fb.dataFormula('.%(code)s' %attr,'v',_if=condition,_else='return null',v='=.%(code)s' %attr,**conditionArgs)
+            
     
