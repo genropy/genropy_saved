84b5d78cb72efc9d0fb8e1d2f4b86f8cbe890c5e
Jeff Edwards <jeffedwa@goodsoftware.com.au>
2012-01-28 14:54:04 +1100

    j

--- gnrpy/gnr/core/gnrvobject.py
+++ gnrpy/gnr/core/gnrvobject.py
@@ -146,15 +146,15 @@ if __name__ == '__main__':
     x['nickname.nickname']='Eddie'
     x['bday.bday']='1961-10-21'
     x['org.org']='Goodsoftware Pty Ltd'
-    x.setItem('email.email','jeffsmith@me.com', param_list=['Home','INTERNET','pref'])
-    x.addItem('email.email','jeffsmith@mac.com', param_list=['Work','INTERNET'])
+    x.setItem('email.email','jeffsmith@me.com', param_list=['Home','type=INTERNET','type=pref'])
+    x.addItem('email.email','jeffsmith@mac.com', param_list=['Work','type=INTERNET'])
     x['adr.street']='32 Smith Waters Rd'
     x['adr.city']='Kincumber'
     x['adr.code']='2251'
     x['adr.country']='Australia'
-    x.setItem('tel.tel','02 4332 0368', param_list=['Home','pref'])
+    x.setItem('tel.tel','02 4332 0368', param_list=['Home','type=pref'])
     x.addItem('tel.tel','0421 232 249', param_list=['CELL'])
-    x.setItem('url.url','02 4332 0368', param_list=['INTERNET'])
+    x.setItem('url.url','02 4332 0368', param_list=['Work'])
     
 
     c = VCard(x)
