a3d2aaafdc92b3d2885cc2913aab30031cd4aca4
Michele Bertoldi <michele.bertoldi@softwell.it>
2012-04-15 19:54:05 +0200

    Soap example with gnrsoappagenew

--- projects/gnrcore/packages/test15/webpages/soap/soap.py
+++ projects/gnrcore/packages/test15/webpages/soap/soap.py
@@ -2,15 +2,17 @@
 # -*- coding: UTF-8 -*-
 #
 
-from gnr.web.gnrsoappage import GnrSoapPage as page_factory
+from gnr.web.gnrsoappagenew import GnrSoapPage as page_factory
 
-from soaplib.core.service import soap
 
-from soaplib.core.model.primitive import String, Integer
 
+from rpclib.decorator import srpc
+from rpclib.model.complex import Iterable
+from rpclib.model.primitive import Integer
+from rpclib.model.primitive import String
 class GnrCustomWebPage(object):
     
-    @soap(String,String,_returns=String)
+    @srpc(String,String,_returns=String, _no_ctx=False)
     def test(self, pippo, pluto):
         print pippo
         print x
