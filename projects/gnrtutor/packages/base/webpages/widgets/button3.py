0b67eb7dc3abae70aaed7b11af24e71194f5ed35
Jeff Edwards <jeffedwa@me.com>
2012-04-26 00:03:09 +1000

    j

--- projects/gnrtutor/packages/base/webpages/widgets/button3.py
+++ projects/gnrtutor/packages/base/webpages/widgets/button3.py
@@ -1,12 +1,13 @@
 # -*- coding: UTF-8 -*-
 
 
-"""Buttons Examples : why not this docline ???"""
+"""Buttons Examples"""
+
 from gnr.core.gnrdecorator import example
 class GnrCustomWebPage(object):
     py_requires = "gnrcomponents/examplehandler:ExampleHandlerFull"    
     
-    @example(code='1',height=300,description='xxxxxxx')
+    @example(code='1',height=150,description='xxxxxxx')
     def alert(self, pane):
         """This is a button with an action parameter that has a js alert"""
         pane.button('Alert',action='alert("Alert")')
@@ -16,7 +17,7 @@ class GnrCustomWebPage(object):
         """This is a button with an action parameter that has a js alert"""
         pane.button('Alert',action='alert("Alert")')
     
-    @example(code='3',height=400,description='uuuuu')
+    @example(code='3',height=200,description='uuuuu')
     def baaaaar(self, pane):
         """This is a button with an action parameter that has a js alert"""
         pane.button('Alert',action='alert("Alert")')
--- resources/common/gnrcomponents/examplehandler.py
+++ resources/common/gnrcomponents/examplehandler.py
@@ -55,26 +55,17 @@ class ExampleHandler(BaseComponent):
             self.exampleBody(pane,example_name=example_name,example_handler=example_handler)
 
     def exampleBody(self,pane,example_name=None,example_handler=None):
-        bc=pane.borderContainer(border='1px solid gray', margin='5px',height='%spx' %example_handler.example_height,background_color='white',
+        bc=pane.borderContainer(border='1px solid gray', margin='2em',height='%spx' %example_handler.example_height,background_color='white',
                            rounded=5, shadow='3px 3px 5px gray',
                            datapath='example.%s' % example_name)
         top=bc.contentPane(region='top',border_bottom='1px solid silver')
-        
-        
-        top.div(example_handler.__doc__ or '...missing example docline...',margin_left='1em')
 
-        ##bottom=bc.contentPane(region='bottom')
-       # bottom.div("Here we will put commands as show source or other stuff",margin_left='1em')
+        top.div(example_handler.__doc__ or '...missing example docline...',_class='pbl_roundedGroupLabel')
+
         center=bc.tabContainer(region='center')
-        p1=center.contentPane(title='Example')
+        p1=center.contentPane(title='Example',margin='3px')
         p2=center.contentPane(title='Source Code')
-       # h = element.div()
-       # h.a('Show Source',href='',float='right',onclick='alert("ggg")',style='cursor:pointer',
-       #      color='white',font_size='11px',margin_right='5px',margin_top='3px')
-       # h.div(example_handler.__doc__ or example_name, background_color='#ACACAC',
-       #       color='white', padding='3px')
-       #       
-       # element = element.div(padding='5px')
+
         example_handler(p1)
         source=inspect.getsource(example_handler).split('\n')
         source.pop(0)
