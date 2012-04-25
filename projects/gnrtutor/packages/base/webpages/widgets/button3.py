893ad57a62c9652ca75c4a592df36b67156185d4
Jeff Edwards <jeffedwa@me.com>
2012-04-26 07:05:21 +1000

    j

--- projects/gnrtutor/packages/base/webpages/widgets/button3.py
+++ projects/gnrtutor/packages/base/webpages/widgets/button3.py
@@ -7,12 +7,12 @@ from gnr.core.gnrdecorator import example
 class GnrCustomWebPage(object):
     py_requires = "gnrcomponents/examplehandler:ExampleHandlerFull"    
     
-    @example(code='1',height=150,description='xxxxxxx')
+    @example(code='1',height=150,description='xxxxxxx',background_color='red')
     def alert(self, pane):
         """This is a button with an action parameter that has a js alert"""
         pane.button('Alert',action='alert("Alert")')
         
-    @example(code='2',height=200,description='zzzz')
+    @example(code='2',height=200,description='zzzz',background_color='red')
     def fooooooo(self, pane):
         """This is a button with an action parameter that has a js alert"""
         pane.button('Alert',action='alert("Alert")')
