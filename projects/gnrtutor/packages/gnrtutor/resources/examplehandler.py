3899e39790903d1153b8c84fd45df7b1a4e8848e
Jeff Edwards <jeffedwa@me.com>
2012-04-26 08:05:45 +1000

    j

--- projects/gnrtutor/packages/gnrtutor/resources/examplehandler.py
+++ projects/gnrtutor/packages/gnrtutor/resources/examplehandler.py
@@ -52,26 +52,31 @@ class ExampleHandler(BaseComponent):
         for example_name,example_handler in example_to_do:
             if skip_example(example_name):
                 continue
-            self.exampleBody(pane,example_name=example_name,example_handler=example_handler)
+            example_doc_handler=getattr(self,'doc_%s'%example_name,None)
+            example_doc= example_doc_handler.__doc__ or '...missing example doc...' if example_doc_handler else '...missing example doc...'
+            
+            self.exampleBody(pane,example_name=example_name,example_handler=example_handler,example_doc=example_doc)
 
-    def exampleBody(self,pane,example_name=None,example_handler=None):
+    def exampleBody(self,pane,example_name=None,example_handler=None,example_doc=None):
         bc=pane.borderContainer(border='1px solid gray', margin='2em',height='%spx' %example_handler.example_height,background_color='white',
                            rounded=5, shadow='3px 3px 5px gray',
                            datapath='example.%s' % example_name)
         top=bc.contentPane(region='top',border_bottom='1px solid silver')
 
-        top.div(example_handler.__doc__ or '...missing example docline...',_class='pbl_roundedGroupLabel')
+        top.div(example_handler.example_description,_class='pbl_roundedGroupLabel')
 
         center=bc.tabContainer(region='center')
         p1=center.contentPane(title='Example',margin='3px')
         p2=center.contentPane(title='Source Code')
+        p3=center.contentPane(title='Documantation')
 
         example_handler(p1)
         source=inspect.getsource(example_handler).split('\n')
         source.pop(0)
         source='\n'.join(source)
         source=highlight(source, PythonLexer(), HtmlFormatter(linenos='table'))
-        p2.div(source,_class='codehilite')
+        p2.div(source,_class='codehilite',margin='3px')
+        p3.div(example_doc,white_space='pre',overflow='auto',margin='3px')
 
 
         
--- projects/gnrtutor/packages/gnrtutor/webpages/widgets/button3.py
+++ projects/gnrtutor/packages/gnrtutor/webpages/widgets/button3.py
@@ -7,12 +7,24 @@ from tutorlib import example
 class GnrCustomWebPage(object):
     py_requires = "examplehandler:ExampleHandlerFull"    
     
-    @example(code='1',height=150,description='xxxxxxx',background_color='red')
+    @example(code='1',height=150,description='xxxxxxx')
     def alert(self, pane):
-        """This is a button with an action parameter that has a js alert"""
+        pane=pane.contentPane(background_color='pink')
         pane.button('Alert',action='alert("Alert")')
         
-    @example(code='2',height=200,description='zzzz',background_color='red')
+    def doc_alert(self):
+        """
+This is a button with an action parameter that has a js alert
+blkzsd THIS COULD GO IN TAB 3 BUT WE HAVE IT ALSO IN THE CODE
+dsfhdgf  h dfgh dfg df h dfgh d gh dfgh dfg hdgd
+ghdfghdfghdfghdfghd
+dfg
+sdfg
+sdf
+gsdfgsdfgsdfgsfdfghdfgjfghjfghfghjfgh
+            """
+        
+    @example(code='2',height=200,description='zzzz')
     def fooooooo(self, pane):
         """This is a button with an action parameter that has a js alert"""
         pane.button('Alert',action='alert("Alert")')
