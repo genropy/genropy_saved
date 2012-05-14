46506949dc63579dcc8d3e97c075d866288f3ee2
Francesco Porcari <francesco.porcari@softwell.it>
2012-05-14 14:54:40 +0200

    a

--- projects/gnrcore/packages/uke/resources/tables/people/th_people.py
+++ projects/gnrcore/packages/uke/resources/tables/people/th_people.py
@@ -29,11 +29,18 @@ class View(BaseComponent):
 class Form(BaseComponent):
 
     def th_form(self, form):
-        pane = form.record
-        fb = pane.formbuilder(cols=2, border_spacing='4px')
+        bc = form.center.borderContainer(datapath='.record')
+        top = bc.borderContainer(region='top',height='110px')
+        top.contentPane(region='right',width='200px').linkerBox('user_id',formUrl='/adm/user_page',dialog_height='400px',
+                                                                 dialog_width='650px',
+                                                                 default_firstname='=#FORM.record.name',
+                                                                 default_lastname='=#FORM.record.name',
+                                                                 default_email='=#FORM.record.email',
+                                                                 default_status='C',template='default',
+                                                                 openIfEmpty=True)
+        fb = top.contentPane(region='center').formbuilder(cols=2, border_spacing='4px')
         fb.field('name')
         fb.field('company_id')
-        fb.field('user_id')
         fb.field('customer_id')
         fb.field('role')
         fb.field('email')
