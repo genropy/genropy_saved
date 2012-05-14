7e8d0020eccfe64011b723e9e8942a8802ebadf3
Francesco Porcari <francesco.porcari@softwell.it>
2012-05-14 12:38:19 +0200

    aa

--- projects/gnrcore/packages/uke/model/instance.py
+++ projects/gnrcore/packages/uke/model/instance.py
@@ -3,7 +3,7 @@
 class Table(object):
     def config_db(self, pkg):
         tbl =  pkg.table('instance',pkey='id',name_long='!!Instance',
-                      name_plural='!!Instances')
+                      name_plural='!!Instances',rowcaption='$name',caption_field='name')
         self.sysFields(tbl)
         tbl.column('name',name_long='!!Name')
         tbl.column('company_id',size='22',group='_',name_long='Company id').relation('company.id', mode='foreignkey', onDelete='raise',relation_name='instances')
--- projects/gnrcore/packages/uke/model/people.py
+++ projects/gnrcore/packages/uke/model/people.py
@@ -3,7 +3,7 @@
 class Table(object):
     def config_db(self, pkg):
         tbl =  pkg.table('developer',pkey='id',name_long='!!Developer',
-                      name_plural='!!Developer')
+                      name_plural='!!Developer',rowcaption='$name',caption_field='name')
         self.sysFields(tbl)
         tbl.column('name',name_long='!!Name')
         tbl.column('company_id',size='22',group='_',name_long='Company id').relation('company.id', mode='foreignkey', onDelete='raise')
