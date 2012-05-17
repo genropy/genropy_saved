9ead49752437621bbc15c5551c91de6483e5f328
Jeff Edwards <jeffedwa@me.com>
2012-05-17 20:21:58 +1000

    jeff mods on test

--- gnrpy/gnr/xtnd/gnrtimestamp.py
+++ gnrpy/gnr/xtnd/gnrtimestamp.py
@@ -5,20 +5,20 @@
 # Copyright (c) : 2004 - 2007 Softwell sas - Milano 
 # Written by    : Giovanni Porcari, Michele Bertoldi
 #                 Saverio Porcari, Francesco Porcari , Francesco Cavazzana
-#--------------------------------------------------------------------------
-#This library is free software; you can redistribute it and/or
-#modify it under the terms of the GNU Lesser General Public
-#License as published by the Free Software Foundation; either
-#version 2.1 of the License, or (at your option) any later version.
+# --------------------------------------------------------------------------
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 
-#This library is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#Lesser General Public License for more details.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
 
-#You should have received a copy of the GNU Lesser General Public
-#License along with this library; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 import datetime
 from gnr.core import gnrstring
@@ -66,15 +66,16 @@ class GnrTimeStamp(object):
 
 if __name__ == '__main__':
     s = GnrTimeStamp()
-    print s.gnrTimeStampGet(station=1, base=100)
-    print s.gnrTimeStampGet(station=1, base=100)
-    print s.gnrTimeStampGet(station=1, base=100)
-    print s.gnrTimeStampGet(station=1, base=100)
-    print s.gnrTimeStampGet(station=2, base=100)
-    print s.gnrTimeStampGet(station=3, base=100)
-    print s.gnrTimeStampGet(station=1, base=100)
-    print s.gnrTimeStampGet(station=2, base=100)
-    print s.gnrTimeStampGet(station=3, base=100)
-    print s.gnrTimeStampGet()
+    print s.get(station=1, base=100)
+    print s.get(station=1, base=100)
+    print s.get(station=1, base=100)
+    print s.get(station=1, base=100)
+    print s.get(station=2, base=100)
+    print s.get(station=3, base=100)
+    print s.get(station=1, base=100)
+    print s.get(station=2, base=100)
+    print s.get(station=3, base=100)
+    print s.get()
+    print s.getDate(s.get())
 
 	
