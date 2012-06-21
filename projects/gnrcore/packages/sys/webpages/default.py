bfc8d4d198e81983b86473ee7a59d588e8f2b4b3
gporcari <giovanni.porcari@gmail.com>
2012-06-21 19:55:59 +0200

    defaultpage

--- projects/gnrcore/packages/sys/webpages/default.py
+++ projects/gnrcore/packages/sys/webpages/default.py
@@ -5,7 +5,7 @@
 # Copyright (c) 2010 Softwell. All rights reserved.
 
 class GnrCustomWebPage(object):
-    #py_requires='public:Public'
+    css_requires='public'
 
     def pageAuthTags(self, method=None, **kwargs):
         return ''
@@ -21,15 +21,18 @@ class GnrCustomWebPage(object):
         else:
             root.data('pages_tree',node.value)
             bc=root.borderContainer()
-            top=bc.contentPane(region='top',height='120px')
-            top.div('^tree.current_path')
+            left=bc.contentPane(region='left',width='200px',splitter=True,background='#eee',overflow_y='auto')
             center=bc.contentPane(region='center')
             prefix=''.join(self.getCallArgs())
-            center.dataController("""
-                                    genro.gotoURL(prefix+'/'+x);
-                                """,x='^tree.current_path',prefix=prefix)
-            center.tree(storepath='pages_tree', hideValues=True, inspect='shift', labelAttribute='name',
+            left.dataFormula("current_url","'/'+prefix+'/'+x",x='^tree.current_path',prefix=prefix)
+            left.tree(storepath='pages_tree', hideValues=True, inspect='shift', labelAttribute='name',
+                  getIconClass='return node.attr.iconClass || "treeNoIcon"',
+                  getLabelClass="""var _class= (node._resolver || node._value) ? 'menu_shape menu_level_0' :  'menu_shape menu_level_2';
+                                   console.log(node,_class);return _class""",
                   isTree=False, selected_path='tree.current_path',  _class='menutree',
+                  openOnClick=True,
+                  autoCollapse=True,
                   selected_name='tree.name')
+            center.iframe(border='0px',width='100%',height='100%',src='^current_url')
             
         return
