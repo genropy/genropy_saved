0d2731a735b5f80b9ac5f4e854e036f95d98211b
Francesco Porcari <francesco.porcari@softwell.it>
2012-09-18 09:22:52 +0200

    a

--- projects/gnrcore/packages/test15/webpages/gnrwdg/includedview_externalchanges.py
+++ projects/gnrcore/packages/test15/webpages/gnrwdg/includedview_externalchanges.py
@@ -8,24 +8,38 @@
 
 class GnrCustomWebPage(object):
     py_requires="gnrcomponents/testhandler:TestHandlerFull"
+    css_requires='public'
     
     def windowTitle(self):
         return 'includedview: externalchanges'
          
+    def isDeveloper(self):
+        return True
+
+    def mystruct(self,struct):
+        r = struct.view().rows()
+        r.fieldcell('nome',width='20em')
+        r.fieldcell('provincia',width='10em')
+        r.fieldcell('codice_istat',width='7em') 
+        r.fieldcell('codice_comune',width='7em')
+        r.fieldcell('prefisso_tel',width='4em')
+        r.fieldcell('cap',width='7em')
+        r.checkboxcolumn(checkedId='.checked_localita')
+
     def test_0_iv_standard(self,pane):
         "standard"
         pane = pane.framePane(frameCode='loc_stat',height='250px')
-        view = pane.includedView(_newGrid=True)
-        struct = view.gridStruct('min')
+        view = pane.includedView(_newGrid=True,struct=self.mystruct)
         view.selectionStore(table='glbl.localita',where="$provincia='AN'",
                             _onStart=True,order_by='$nome',
                             externalChanges=True)
-                            
+
+
+                
     def test_1_iv_virtual(self,pane):
         "virtual"
         pane = pane.framePane(frameCode='loc_virt',height='250px')
-        view = pane.includedView(_newGrid=True)
-        struct = view.gridStruct('min')
+        view = pane.includedView(_newGrid=True,struct=self.mystruct)
         view.selectionStore(table='glbl.localita',#where="$provincia='AN'",
                             _onStart=True,order_by='$nome',
                             externalChanges=True,chunkSize=10)
