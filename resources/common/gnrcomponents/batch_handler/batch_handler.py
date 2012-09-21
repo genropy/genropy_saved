cfe0a002f980c698ebcd69bdb3c53cd08df6234d
Francesco Porcari <francesco.porcari@softwell.it>
2012-09-21 12:42:23 +0200

    m

--- resources/common/gnrcomponents/batch_handler/batch_handler.py
+++ resources/common/gnrcomponents/batch_handler/batch_handler.py
@@ -295,7 +295,7 @@ class TableScriptRunner(TableScriptHandler):
         plugin_main.dataRpc('dummy', self.table_script_run,
                             _fired='^.run',
                             _onCalling='=.onCalling',
-                            _onResult="""if(kwargs._publishOnResult){genro.publish(kwargs._publishOnResult);}""",
+                            _onResult="""if(kwargs._publishOnResult){genro.publish({topic:kwargs._publishOnResult),broadcast:'*'};}""",
                             parameters='=.parameters',
                             resource='=.resource',
                             res_type='=.res_type',
