d8ce2e11770d97de1864d91de8fae691ebdeaa17
Francesco Porcari <francesco.porcari@softwell.it>
2013-01-24 10:48:43 +0100

    added onStart

--- resources/common/th/th_view.py
+++ resources/common/th/th_view.py
@@ -351,7 +351,7 @@ class TableHandlerView(BaseComponent):
                         #view_title='=.title',
                         _sections='^.sections',
                         sub_title='==_sections?th_sections_manager.getSectionTitle(_sections):"";',
-                        _onBuilt=True)
+                        _onBuilt=True,_onStart=True)
         condPars = {}
         if isinstance(condition,dict):
             condPars = condition
