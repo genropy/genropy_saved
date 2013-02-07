03f50ce02bb82a6aaf3aefbd7b9cf685c31965d8
Francesco Porcari <francesco.porcari@softwell.it>
2013-02-07 10:31:30 +0100

    a

--- projects/gnrcore/packages/sys/webpages/html_mixablepage.py
+++ projects/gnrcore/packages/sys/webpages/html_mixablepage.py
@@ -7,7 +7,6 @@ from gnr.web.gnrhtmlpage import GnrHtmlDojoPage as page_factory
 class GnrCustomWebPage(object):
     dojo_version='18'
     dojo_theme='tundra'
-    js_requires='html_genro'
     def onIniting(self, url_parts, request_kwargs):
         component_path = '%s:%s' %('/'.join(url_parts[0:-1]),url_parts[-1])
         url_parts[:] = []
