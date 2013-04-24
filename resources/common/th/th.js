7eb15ec76fcec46907eddce6a53c1d287198aae2
Francesco Porcari <francesco.porcari@softwell.it>
2013-04-24 15:36:28 +0200

    fix regex sections

--- resources/common/th/th.js
+++ resources/common/th/th.js
@@ -88,7 +88,7 @@ var th_sections_manager = {
                     for(var k in condpars){
                         var newcondkey = k+'_'+cn.attr.code;
                         kwargs[newcondkey] = condpars[k];
-                        cond = cond.replace(':'+k,':'+newcondkey);
+                        cond = cond.replace(new RegExp(':'+k,'g'),':'+newcondkey);
                     }
                     orlist.push(' ('+cond+') ');
                 }
