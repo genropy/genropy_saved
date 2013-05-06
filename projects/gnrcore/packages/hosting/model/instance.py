db5284d1f72d9bda3f6df58a73d32260e622da4a
Francesco Porcari <francesco.porcari@softwell.it>
2013-05-06 15:48:56 +0200

    hosting

--- projects/gnrcore/packages/hosting/model/instance.py
+++ projects/gnrcore/packages/hosting/model/instance.py
@@ -19,7 +19,8 @@ class Table(object):
         tbl.column('site_path', dtype='T', name_long='!!Site Path')
         tbl.column('slot_configuration', 'X', name_long='!!Slot configuration', _sendback=True)
         tbl.column('hosted_data', 'X', name_long='!!Hosted data', _sendback=True)
-        tbl.column('client_id', size=':22', name_long='!!Client id').relation('client.id', mode='foreignkey')
+        tbl.column('client_id', size=':22', name_long='!!Client id').relation('hosting.client.id', mode='foreignkey',
+                                                                            relation_name='instances')
 
     def create_instance(self, code):
         instanceconfig = Bag(self.pkg.instance_template())
