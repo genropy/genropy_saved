76f7141f28e324db50b8ff64a98d7ff01a3218c6
Francesco Porcari <francesco.porcari@softwell.it>
2013-06-06 13:04:52 +0200

    raw_update fix

--- gnrpy/gnr/app/gnrapp.py
+++ gnrpy/gnr/app/gnrapp.py
@@ -245,7 +245,7 @@ class GnrSqlAppDb(GnrSqlDb):
         :param old_record: the old record to be updated
         :param pkey: the record :ref:`primary key <pkey>`"""
         self.checkTransactionWritable(tblobj)
-        GnrSqlDb.update(self, tblobj, record, pkey=pkey,**kwargs)
+        GnrSqlDb.raw_update(self, tblobj, record, pkey=pkey,**kwargs)
         if self.systemDbEvent():
             return
         old_record = record or dict(record)
