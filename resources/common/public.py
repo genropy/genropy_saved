b3278acdc37374403b0ec17758501fd08ce3ec8e
Francesco Porcari <francesco.porcari@softwell.it>
2013-06-07 12:07:54 +0200

    a

--- resources/common/public.py
+++ resources/common/public.py
@@ -82,7 +82,8 @@ class PublicBase(BaseComponent):
                             **kwargs)
                             
     def public_frameBottomBar(self,pane,slots=None,**kwargs):
-        pane.slotBar('',_class='gnr_footer')
+        pane.slotBar('',_class='pbl_root_bottom')
+
 
     @struct_method
     def public_roundedGroup(self, container, title=None,frame=False,top=None,bottom=None,left=None,right=None,**kwargs):
