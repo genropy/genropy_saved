1f86df3de21d098a49ac13d2954803f08a810d77
gporcari <giovanni.porcari@gmail.com>
2013-06-25 09:42:41 +0200

    video and canvas refactoring

--- projects/gnrcore/packages/test15/webpages/html/webRTC.py
+++ projects/gnrcore/packages/test15/webpages/html/webRTC.py
@@ -12,23 +12,37 @@ class GnrCustomWebPage(object):
 
     def test_0_video(self,pane):
         """Video"""
-        v = pane.div(height='310px').video(autoplay=True,height='300px',width='400px')
+        video = pane.div(height='310px').video(autoplay=True,height='300px',width='400px')
         fb = pane.formbuilder(cols=3,border_spacing='3px')
         fb.checkbox(value='^.video',label='Video',default_value=True)
         fb.checkbox(value='^.audio',label='Audio')
-        fb.button('Start',action='v.startCapture({video:video,audio:audio})',v=v,video='=.video',audio='=.audio')
+        fb.button('Start',action='video.startCapture({video:videoCapture,audio:audioCapture})',video=video,videoCapture='=.video',audioCapture='=.audio')
 
 
 
     def test_1_video_canvas(self,pane):
         """Video and Canvas"""
-        v = pane.div(height='310px',display='inline-block').video(autoplay=True,height='300px',width='400px')
-        c = pane.div(height='310px',display='inline-block',margin_left='10px').canvas(height='300px',width='400px',border='1px solid silver')
+        video = pane.div(height='310px',display='inline-block').video(autoplay=True,height='300px',width='400px')
+        canvas = pane.div(height='310px',display='inline-block',margin_left='10px').canvas(height='300px',width='400px',border='1px solid silver')
         pane.br()
         fb = pane.formbuilder(cols=5,border_spacing='3px')
         fb.checkbox(value='^.video',label='Video',default_value=True)
         fb.checkbox(value='^.audio',label='Audio')
-        fb.button('Start',action='v.startCapture({video:video,audio:audio})',v=v,video='=.video',audio='=.audio')
-        fb.button('Snapshot',action='v.takePhoto(c)',v=v,c=c)
-        fb.button('Save',action='c.savePhoto()',c=c)
+        fb.button('Start',action='video.startCapture({video:videoCapture,audio:audioCapture})',video=video,videoCapture='=.video',audioCapture='=.audio')
 
+        fb.button('Snapshot',action='canvas.takePhoto(video)',video=video,canvas=canvas)
+        fb.button('Save',action='canvas.savePhoto()',canvas=canvas)
+
+    def test_2_video_copytocanvas(self,pane):
+        """Video and Canvas"""
+        video = pane.div(height='310px',display='inline-block').video(autoplay=True,height='300px',width='400px')
+        canvas = pane.div(height='310px',display='inline-block',margin_left='10px').canvas(height='300px',width='400px',border='1px solid silver')
+        pane.br()
+        fb = pane.formbuilder(cols=5,border_spacing='3px')
+        fb.checkbox(value='^.video',label='Video',default_value=True)
+        fb.checkbox(value='^.audio',label='Audio')
+        fb.button('Start',action='video.startCapture({video:videoCapture,audio:audioCapture})',video=video,videoCapture='=.video',audioCapture='=.audio')
+
+        fb.button('Copy',action='canvas.takePhoto(video,{"sync":sync})',video=video,canvas=canvas,sync=True)
+        fb.button('Save',action='canvas.savePhoto()',canvas=canvas)
+  
