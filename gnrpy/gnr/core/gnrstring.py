7885061b4d83f75bbe9e5bfb9b48d545a36f1385
Francesco Porcari <francesco.porcari@softwell.it>
2013-07-11 14:35:07 +0200

    fix

--- gnrpy/gnr/core/gnrstring.py
+++ gnrpy/gnr/core/gnrstring.py
@@ -438,8 +438,7 @@ def templateReplace(myString, symbolDict=None, safeMode=False,noneIsBlank=True,l
     
     >>> templateReplace('$foo loves $bar but she loves $aux and not $foo', {'foo':'John','bar':'Sandra','aux':'Steve'})
     'John loves Sandra but she loves Steve and not John'"""
-    if not myString:
-        return ''
+    myString = myString or ''
     templateBag=None
     if hasattr(myString, '_htraverse'):
         templateBag = myString.deepcopy()
