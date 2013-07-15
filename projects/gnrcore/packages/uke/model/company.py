412a9bb00a6398d3af4d9933dc20672b07f06837
Francesco Porcari <francesco.porcari@softwell.it>
2013-07-15 11:29:40 +0200

    fix uke

--- projects/gnrcore/packages/uke/model/company.py
+++ projects/gnrcore/packages/uke/model/company.py
@@ -2,8 +2,8 @@
 
 class Table(object):
     def config_db(self, pkg):
-        tbl =  pkg.table('company',pkey='code',name_long='!!Company',
+        tbl =  pkg.table('company',pkey='id',name_long='!!Company',
                       name_plural='!!Company',rowcaption='$code',caption_field='code')
-        self.sysFields(tbl,id=False)
+        self.sysFields(tbl)
         tbl.column('code',name_long='!!Code')
         tbl.column('description',name_long='!!Description')
