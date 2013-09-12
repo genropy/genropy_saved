d05c045c607aefa32e81eea070552f0f8eec5155
Francesco Porcari <francesco.porcari@softwell.it>
2013-09-12 13:47:16 +0200

    lastRpc

--- gnrjs/gnr_d11/js/genro_rpc.js
+++ gnrjs/gnr_d11/js/genro_rpc.js
@@ -599,12 +599,19 @@ dojo.declare("gnr.GnrRpcHandler", null, {
             'timeout': 10000,
             'load': dojo.hitch(this, function(response, ioArgs) {
                 var result = genro.rpc.resultHandler(response, ioArgs);
+                genro.callAfter(function(){
+                    genro.dom.removeClass(dojo.body(),'ping_start');
+                },1000);
+                
                 genro.rpc.setPollingStatus(false);
                 return result;
             }),
             'error': dojo.hitch(this, function(response, ioArgs) {
                 genro.rpc.setPollingStatus(false);
+                genro.dom.removeClass(dojo.body(),'ping_start');
+                genro.dom.addClass(dojo.body(),'ping_error');
                 genro.rpc.errorHandler(response, ioArgs);
+
             }),
             'sync': false,
             'preventCache': false
@@ -619,6 +626,8 @@ dojo.declare("gnr.GnrRpcHandler", null, {
         pingKw._pageProfilers = genro.getTimeProfilers();
         pingKw.sysrpc = true;
         genro.lastPing = new Date();
+        genro.dom.removeClass(dojo.body(),'ping_error');
+        genro.dom.addClass(dojo.body(),'ping_start');
         this._serverCall(pingKw, xhrKwargs, 'POST');
     },
     setPollingStatus:function(status) {
