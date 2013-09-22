c4bec38155009e50dc5d9b4e28561f0e7cb01c7c
Jeff Edwards <jeffedwa@me.com>
2013-09-21 20:19:24 -0600

    needing to fix a bug, please review this . . it is temporary

--- gnrpy/gnr/web/gnrwsgisite_proxy/gnrobjectregister.py
+++ gnrpy/gnr/web/gnrwsgisite_proxy/gnrobjectregister.py
@@ -573,7 +573,8 @@ class BaseRegister(object):
         last_used = last_used or self.sd.get(self.lastused_key(register_item['register_item_id']))
         if not last_used:
             return
-        register_item['last_ts'], register_item['last_user_ts'],register_item['last_rpc_ts'],register_item['profile'] = last_used
+        print 'last_used: ',last_used
+        register_item['last_ts'], register_item['last_user_ts'],register_item['last_rpc_ts'],register_item['profile'],temp = last_used
         register_item['age'] = age('start_ts')
         if register_item['last_rpc_ts']:
             register_item['last_rpc_age'] = age('last_rpc_ts')
