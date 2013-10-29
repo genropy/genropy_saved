25e83128bb686991e0e364ba624fb1add3ad411d
Saverio Porcari <saverio.porcari@softwell.it>
2013-10-29 18:28:34 +0100

    a

---
+++ resources/common/services/doctopdf/abiword.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env pythonw
+# -*- coding: UTF-8 -*-
+#
+#  Created by Saverio Porcari on 2013-04-06.
+#  Copyright (c) 2013 Softwell. All rights reserved.
+
+
+from gnr.core.gnrbaseservice import GnrBaseService
+from subprocess import call
+
+
+class Main(GnrBaseService):
+    def __init__(self, parent=None):
+        self.parent = parent
+
+    def convert(self,path):
+        try:
+            call(['abiword', '--to=pdf',path])
+        except Exception:
+            return path
+        return path.replace('.doc','pdf')
