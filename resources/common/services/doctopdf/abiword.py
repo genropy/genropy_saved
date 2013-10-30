678d7d09e931bfa358bcd278651698736d98ba4b
Saverio Porcari <saverio.porcari@softwell.it>
2013-10-30 14:31:04 +0100

    m

--- resources/common/services/doctopdf/abiword.py
+++ resources/common/services/doctopdf/abiword.py
@@ -28,7 +28,7 @@ class Main(GnrBaseService):
         while os.path.exists(dest_path):
             dest_path = '%s_%i%s'%(name,counter,ext)
             counter +=1
-        call_list = ['abiword', '--to=pdf', src_path, '-o', dest_path]
+        call_list = ['abiword', '--to=pdf', os.path.abspath(src_path), '-o', os.path.abspath(dest_path)]
         print call_list
         result = call(call_list)
         if result !=0:
