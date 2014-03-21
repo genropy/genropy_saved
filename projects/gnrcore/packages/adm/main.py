fa7f826acfea2fcbf5f50ea09e8095185019ca9a
Francesco Porcari <francesco.porcari@softwell.it>
2014-03-21 10:19:31 +0100

    removed userobject table in all packages. Userobject belong only to adm

--- projects/gnrcore/packages/adm/main.py
+++ projects/gnrcore/packages/adm/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter, Table_userobject
+from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter
 from gnr.core.gnrdict import dictExtract
 import hashlib
 from datetime import datetime
--- projects/gnrcore/packages/flib/main.py
+++ projects/gnrcore/packages/flib/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter, Table_userobject, Table_recordtag, Table_recordtag_link
+from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage, Table_counter, Table_recordtag, Table_recordtag_link
 
 
 class Package(GnrDboPackage):
