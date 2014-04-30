d60d155ff229688c1a894e717339d5fa599b93b2
Michele Bertoldi <michele.bertoldi@softwell.it>
2014-04-30 13:13:25 +0200

    Test parallel dbsetup

--- gnrpy/pavement.py
+++ gnrpy/pavement.py
@@ -23,7 +23,7 @@ setup(
         license='LGPL',
         scripts=['../scripts/gnrdbsetup', '../scripts/gnrmkinstance', '../scripts/gnrmkthresource','../scripts/gnrmksite','../scripts/gnrxml2py', '../scripts/gnrheartbeat', '../scripts/gnrmkpackage',
                  '../scripts/gnrwsgiserve','../scripts/gnruwsgiserve', '../scripts/gnrmkapachesite','../scripts/gnrdaemon',
-                 '../scripts/gnrsendmail', '../scripts/gnrsitelocalize', '../scripts/gnrtrdaemon', '../scripts/gnrsync4d', '../scripts/gnrmkproject', '../scripts/gnrdbstruct', '../scripts/gnrdbgraph',
+                 '../scripts/gnrsendmail', '../scripts/gnrsitelocalize', '../scripts/gnrdbsetupparallel', '../scripts/gnrtrdaemon', '../scripts/gnrsync4d', '../scripts/gnrmkproject', '../scripts/gnrdbstruct', '../scripts/gnrdbgraph',
                  '../scripts/gnr'],
         packages=['gnr', 'gnr.core', 'gnr.app', 'gnr.web', 'gnr.sql'],
         data_files=data_files,
