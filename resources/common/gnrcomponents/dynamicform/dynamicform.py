e8bff21314be8e8fc03d0b66e8ea17a5469bae04
Francesco Porcari <francesco.porcari@softwell.it>
2014-05-16 12:35:05 +0200

    fix dynamicform code

--- resources/common/gnrcomponents/dynamicform/dynamicform.py
+++ resources/common/gnrcomponents/dynamicform/dynamicform.py
@@ -43,11 +43,11 @@ class Form(BaseComponent):
         fb = box.formbuilder(cols=3, border_spacing='4px',tdl_width='5em',width='100%')
         #tbl = pane.getInheritedAttributes()['table']
         fb.field('code',validate_notnull=True,validate_notnull_error='!!Required',width='8em', 
-                validate_regex='![^A-Za-z0-9_]', 
-                validate_regex_error='!!Invalid code: "." char is not allowed',#validate_case='l',
+               #validate_regex='![^A-Za-z0-9_]', 
+               #validate_regex_error='!!Invalid code: "." char is not allowed',#validate_case='l',
                 validate_nodup=True,validate_nodup_error='!!Already existing code',
                 validate_nodup_condition='$maintable_id=:fkey',validate_nodup_fkey='=#FORM.record.maintable_id',
-                validate_case='l',
+                #validate_case='l',
                 ghost='!!Field code')
         fb.field('description',validate_notnull=True,validate_notnull_error='!!Required',width='100%',colspan=2,
                     ghost='!!Field description')
