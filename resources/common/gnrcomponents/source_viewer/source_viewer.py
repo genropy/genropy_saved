b1e38903755d42786966acb23bc771d748f3ba3a
Francesco Porcari <francesco.porcari@softwell.it>
2014-06-04 17:28:22 +0200

    source_viewer improvements

--- resources/common/gnrcomponents/source_viewer/source_viewer.py
+++ resources/common/gnrcomponents/source_viewer/source_viewer.py
@@ -1,6 +1,5 @@
 from gnr.web.gnrbaseclasses import BaseComponent
 from gnr.core.gnrdecorator import public_method
-from gnr.core.gnrbag import Bag
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
@@ -11,7 +10,7 @@ import os
 
 class SourceViewer(BaseComponent):
     css_requires = 'gnrcomponents/source_viewer/source_viewer,gnrcomponents/source_viewer/pygmentcss/friendly'
-
+    js_requires = 'source_viewer'
     def rootWidget(self,root,**kwargs):
         frame = root.framePane(frameCode='sandbox',_class='sandbox',**kwargs)
         page = self.pageSource()
@@ -23,10 +22,6 @@ class SourceViewer(BaseComponent):
                         ).div(position='absolute',top='0',
                         right='0',bottom='0',left='0')
         sourceBox.remote(self.source_viewer_content)
-        page.dataController("""genro.wdgById('_docSource_floating').show();""",
-                            subscribe_editSourceDoc=True)
-        page.dataRpc('dummy',self.save_source_documentation,subscribe_sourceDocUpdate=True,
-                        rstdoc='=gnr.source_viewer.doc.rst')
         page.dataRpc('dummy',self.save_source_code,subscribe_sourceCodeUpdate=True,
                         sourceCode='=gnr.source_viewer.source',_if='sourceCode && _source_changed',
                         _source_changed='=gnr.source_viewer.changed_editor',
@@ -36,9 +31,6 @@ class SourceViewer(BaseComponent):
                                         }else{
                                             genro.publish('showCodeError',result);
                                         }""")
-        page.dataRpc('gnr.source_viewer.doc.html',self.source_viewer_rst2html,
-                    rstdoc='^gnr.source_viewer.doc.rst',
-                    _delay=500)
         page.dataController("""genro.src.updatePageSource('source_viewer_root')""",
                         subscribe_rebuildPage=True,_delay=100)
         page.dataController("""
@@ -60,17 +52,33 @@ class SourceViewer(BaseComponent):
         self.source_viewer_sourceDocPalette(page)
         return frame.center.contentPane(nodeId='source_viewer_root')
 
+    def source_viewer_docController(self,iframe,_onStart=None):
+        rstsource = self.__readsource('rst') or '**Documentation**'
+        page = self.pageSource()
+        page.dataController('iframe.domNode.contentWindow.document.body.innerHTML = rendering',
+                rendering='^gnr.source_viewer.doc.html',iframe=iframe,_onStart=_onStart)
+        page.data('gnr.source_viewer.doc.rst',rstsource)
+        page.data('gnr.source_viewer.doc.html',self.source_viewer_rst2html(rstsource))
+        page.dataController("alert(page);",subscribe_showPagePalette=True)
+        page.dataController("""genro.wdgById('_docSource_floating').show();""",
+                            subscribe_editSourceDoc=True)
+        page.dataRpc('gnr.source_viewer.doc.html',self.source_viewer_rst2html,
+                    rstdoc='^gnr.source_viewer.doc.rst',
+                    _delay=500)
+        page.dataRpc('dummy',self.save_source_documentation,subscribe_sourceDocUpdate=True,
+                        rstdoc='=gnr.source_viewer.doc.rst')
+
     @public_method
     def save_source_documentation(self,rstdoc=None,**kwargs):
         self.__writesource(rstdoc,'rst')
 
-    def __edit_allowed(self):
+    def source_viewer_edit_allowed(self):
         return self.site.remote_edit and self.isDeveloper()
 
     @public_method
     def save_source_code(self,sourceCode=None):
         sourceCode=str(sourceCode)
-        if not self.__edit_allowed():
+        if not self.source_viewer_edit_allowed():
             raise Exception('Not Allowed to write source code')
         try:
             compile('%s\n'%sourceCode, 'dummy', 'exec')
@@ -88,7 +96,7 @@ class SourceViewer(BaseComponent):
             return f.read()
 
     def __writesource(self,sourceCode,ext):
-        if self.__edit_allowed():
+        if self.source_viewer_edit_allowed():
             fname = self.source_viewer_docName(ext)
             with open(fname,'w') as f:
                 f.write(sourceCode)
@@ -104,25 +112,18 @@ class SourceViewer(BaseComponent):
                         _class='viewer_box')
         center = bc.framePane('sourcePane',region='center',_class='viewer_box')
         source = self.__readsource('py')
-        if self.__edit_allowed():
+        if self.source_viewer_edit_allowed():
             self.source_viewer_editor(center,source=source)
         else:
             self.source_viewer_html(center,source=source)
-        docslots = '5,vtitle,*,editbtn,5' if self.__edit_allowed() else '5,vtitle,*'
+        docslots = '5,vtitle,*,editbtn,5' if self.source_viewer_edit_allowed() else '5,vtitle,*'
         bar = top.top.slotToolbar(docslots,vtitle='Documentation',font_size='11px',font_weight='bold',height='20px')
-        if self.__edit_allowed():
+        if self.source_viewer_edit_allowed():
             bar.editbtn.slotButton('Edit',iconClass='iconbox pencil',
                                 action='PUBLISH editSourceDoc;')
         iframe = top.center.contentPane(overflow='hidden').htmliframe(height='100%',width='100%',border=0)
-        self.source_viewer_controller(pane,iframe)
-    
-    def source_viewer_controller(self,pane,iframe,_onStart=None):
-        pane.dataController('iframe.domNode.contentWindow.document.body.innerHTML = rendering',
-                rendering='^gnr.source_viewer.doc.html',iframe=iframe,_onStart=_onStart)
-        
-        rstsource = self.__readsource('rst') or '**Documentation**'
-        pane.data('gnr.source_viewer.doc.rst',rstsource)
-        pane.data('gnr.source_viewer.doc.html',self.source_viewer_rst2html(rstsource))
+        self.source_viewer_docController(iframe)
+
 
     def source_viewer_editor(self,frame,source=None):
         bar = frame.top.slotToolbar('5,vtitle,*,savebtn,revertbtn,5,readOnlyEditor,5',vtitle='Source',font_size='11px',font_weight='bold')
@@ -161,21 +162,49 @@ class SourceViewer(BaseComponent):
         palette = pane.palettePane(paletteCode='_docSource',dockTo='dummyDock',
                                     height='500px',width='600px',
                                     title='!!Source Documentation')
-        frame = palette.framePane(frameCode='_docEditorFrame')
-        frame.center.contentPane(overflow='hidden',_class='source_viewer',_lazyBuild=True).codemirror(value='^gnr.source_viewer.doc.rst',
+        self.source_viewer_docEditorFrame(palette,saveAndClose=True)
+
+    def source_viewer_docEditorFrame(self,parent,saveAndClose=False,**kwargs):
+        frame = parent.framePane(frameCode='_docEditorFrame',_lazyBuild=True,**kwargs)
+        frame.center.contentPane(overflow='hidden',_class='source_viewer').codemirror(value='^gnr.source_viewer.doc.rst',
                                 height='100%',config_lineNumbers=True,
                                 config_mode='rst',config_keyMap='softTab')
-        bar = frame.bottom.slotBar('*,saveDoc,5,saveAndClose,5',_class='slotbar_dialog_footer')
+        slots = '*,saveDoc,5,saveAndClose,5' if saveAndClose else '*,saveDoc,5'
+        bar = frame.bottom.slotBar(slots,_class='slotbar_dialog_footer',height='20px')
         bar.saveDoc.slotButton('!!Save',action='PUBLISH sourceDocUpdate;')
-        bar.saveAndClose.slotButton('!!Save and close',action="""
+        if saveAndClose:
+            bar.saveAndClose.slotButton('!!Save and close',action="""
             this.getParentWidget('floatingPane').hide()
             PUBLISH sourceDocUpdate;""")
 
         return frame
+
+class DocumentationEditor(SourceViewer):
+    def rootWidget(self,root,region=None,**kwargs):
+        frame = root.framePane('documentationFrame',region=region,background='white')
+        bar = frame.bottom.slotBar('0,documentationEditor,0',closable='close',height='500px',border_top='1px solid silver',
+                                    documentationEditor_width='100%',documentationEditor_overflow='hidden')
+        self.source_viewer_docEditorFrame(bar.documentationEditor,height='100%')
+        return frame.center.contentPane(overflow='hidden',**kwargs)
         
-class DocumentationPage(object):
+class DocumentationPage(SourceViewer):
+    def rootWidget(self,root,region=None,**kwargs):
+        frame = root.framePane('documentationFrame',region=region,background='white')
+        if self.source_viewer_edit_allowed():
+            bar = frame.bottom.slotBar('0,documentationEditor,0',closable='close',height='500px',border_top='1px solid silver',
+                                    documentationEditor_width='100%',documentationEditor_overflow='hidden')
+            self.source_viewer_docEditorFrame(bar.documentationEditor,height='100%')
+        return frame.center.contentPane(overflow='hidden',**kwargs)
+
     def main(self,root,**kwargs):
-        iframe = root.contentPane(overflow='hidden').htmliframe(height='100%',width='100%',border=0)
-        self.source_viewer_controller(root,iframe,_onStart=True)
+        iframe = root.htmliframe(height='100%',width='100%',border=0,
+                            onCreated="""
+                            widget.contentWindow.ondblclick=function(){
+                                genro.publish('editSourceDoc');
+                            }
+                            widget.contentWindow.viewer = function(page){
+                                genro.publish('showPagePalette',{page:page});
+                            }""")
+        self.source_viewer_docController(iframe,_onStart=True)
 
 
