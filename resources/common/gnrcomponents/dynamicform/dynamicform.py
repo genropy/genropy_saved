d2dbf90317261e42d9c8896d164252ad2f68edba
Francesco Porcari <francesco.porcari@softwell.it>
2014-06-14 13:49:54 +0200

    reversing a HUGE mistake

--- resources/common/gnrcomponents/dynamicform/dynamicform.py
+++ resources/common/gnrcomponents/dynamicform/dynamicform.py
@@ -319,8 +319,8 @@ class DynamicForm(BaseComponent):
     def df_fieldsBagGrid(self,pane,mastertable=None,**kwargs):
         rootcode = '%s_df' %mastertable.replace('.','_')
         bh = pane.contentPane(datapath='#FORM.%s' %rootcode,nodeId=rootcode,overflow='hidden')
-        pane.data('tickets.currents',Bag())
-        view = bh.bagGrid(frameCode='V_%s' %rootcode,storepath='tickets.currents',
+        
+        view = bh.bagGrid(frameCode='V_%s' %rootcode,storepath='#FORM.record.df_fields',
                     childname='view',struct=self.df_fieldsBagStruct,
                                 grid_selfDragRows=True,
                                datapath='.view',_class='frameGrid',gridEditor=False,
