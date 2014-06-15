fa0c1bfc8216a7b41e637aa23e081cdc0403ffce
gporcari <giovanni.porcari@gmail.com>
2014-06-15 10:08:30 +0200

    fixed psutil

--- resources/common/services/sysinfo/lib_psutil.py
+++ resources/common/services/sysinfo/lib_psutil.py
@@ -7,6 +7,8 @@
 
 from gnr.core.gnrbaseservice import GnrBaseService
 from gnr.core.gnrbag import Bag
+from gnr.core.gnrstring import splitAndStrip
+import re
 
 try:
     import psutil as ps
@@ -22,8 +24,26 @@ class Main(GnrBaseService):
         columns='user,nice,system,idle'.split(',')
         for k, core in enumerate(ps.cpu_times(True)):
             row = Bag()
+            row['core']=k+1
             for c in columns:
                 row.setItem(c,getattr(core,c))
-            row['core']=k+1
             result.setItem('r_%i'%k,row)
         return result
+        
+    def getProcessesBag(self,items=None,name=None,user=None):
+        
+        if isinstance(items,basestring):
+            items=splitAndStrip(items)
+    
+        def filteredProcess(p):
+            if user and user != p.username:
+                return False
+            return (not name) or re.match(name,p.name)
+
+        def bagFromProcess(p):
+            d=p.as_dict()
+            if items:
+                d = [(k,v) for k,v in d.items() if k in items]
+            return Bag(d)
+            
+        return Bag([('p_%s'%p.pid,bagFromProcess(p)) for p in ps.process_iter() if filteredProcess(p)])
