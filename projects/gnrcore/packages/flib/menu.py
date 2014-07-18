076edd1ec5f1f76f94549d43cb5a97a0bd838f92
Francesco Porcari <francesco.porcari@softwell.it>
2014-07-18 15:19:01 +0200

    no message

--- projects/gnrcore/packages/flib/menu.py
+++ projects/gnrcore/packages/flib/menu.py
@@ -2,6 +2,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 def config(root,application=None):
-    root.webpage("!!Categories", filepath="/flib/categories")
-    root.webpage("!!Item uploader", filepath="/flib/item_uploader")
-    root.thpage("!!Items", table="flib.item")
+    root.webpage(u"!!Categories", filepath="/flib/categories")
+    root.webpage(u"!!Item uploader", filepath="/flib/item_uploader")
+    root.thpage(u"!!Items", table="flib.item")
