9a31e479896be9ae03760dfc9982fe186e8cca78
Francesco Porcari <francesco.porcari@softwell.it>
2014-07-18 15:16:52 +0200

    unicode in label

--- legacy_packages/gnr/menu.py
+++ legacy_packages/gnr/menu.py
@@ -2,8 +2,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 def config(root,application=None):
-    gnrpkg = root.branch("gnrpkg", tags="_DEV_")
-    gnrpkg.thpage("!!Error", table="gnr.error")
-    gnrpkg.thpage("!!Sync_out", table="gnr.sync_out")
-    gnrpkg.thpage("!!Transaction", table="gnr.transaction")
+    gnrpkg = root.branch(u"gnrpkg", tags="_DEV_")
+    gnrpkg.thpage(u"!!Error", table="gnr.error")
+    gnrpkg.thpage(u"!!Sync_out", table="gnr.sync_out")
+    gnrpkg.thpage(u"!!Transaction", table="gnr.transaction")
 
--- projects/gnrcore/packages/adm/menu.py
+++ projects/gnrcore/packages/adm/menu.py
@@ -2,14 +2,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 def config(root,application=None):
-    administration = root.branch("!!Administration", tags="admin")
-    administration.webpage("!!Users", filepath="/adm/user_page")
-    administration.thpage("!!Auth tags", table="adm.htag")
-    administration.thpage("!!Letterheads", table="adm.htmltemplate")
-    administration.thpage("!!Notifications", table="adm.notification")
-    administration.thpage("!!Menu Manager", table="adm.menu")
-    administration.thpage("!!Menu Pages", table="adm.menu_page")
-    administration.thpage("!!Datacatalog", table="adm.datacatalog")
-    administration.lookups("!!Utility tables", lookup_manager="adm")
-    administration.thpage("!!Counter", table="adm.counter")
+    administration = root.branch(u"!!Administration", tags="admin")
+    administration.webpage(u"!!Users", filepath="/adm/user_page")
+    administration.thpage(u"!!Auth tags", table="adm.htag")
+    administration.thpage(u"!!Letterheads", table="adm.htmltemplate")
+    administration.thpage(u"!!Notifications", table="adm.notification")
+    administration.thpage(u"!!Menu Manager", table="adm.menu")
+    administration.thpage(u"!!Menu Pages", table="adm.menu_page")
+    administration.thpage(u"!!Datacatalog", table="adm.datacatalog")
+    administration.lookups(u"!!Utility tables", lookup_manager="adm")
+    administration.thpage(u"!!Counter", table="adm.counter")
 
