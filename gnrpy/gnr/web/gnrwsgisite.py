e641a021f1bc663064de49405275ff16f9939128
Francesco Porcari <francesco.porcari@softwell.it>
2014-10-01 18:09:18 +0200

    currentPage as dummyPage for static is create only for lazydoc

--- gnrpy/gnr/web/gnrwsgisite.py
+++ gnrpy/gnr/web/gnrwsgisite.py
@@ -679,7 +679,6 @@ class GnrWsgiSite(object):
         elif path_list and path_list[0].startswith('_'):
             self.log_print('%s : kwargs: %s' % (path_list, str(request_kwargs)), code='STATIC')
             try:
-                self.currentPage = self.dummyPage
                 return self.statics.static_dispatcher(path_list, environ, start_response, **request_kwargs)
             except Exception, exc:
                 return self.not_found_exception(environ,start_response)
