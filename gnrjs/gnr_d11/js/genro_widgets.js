27bdb8a8f0d9c4bbcd46bd7e7cdcd4671034572e
Francesco Porcari <francesco.porcari@softwell.it>
2014-11-12 12:38:03 +0100

    minor fix

--- gnrjs/gnr_d11/js/genro_widgets.js
+++ gnrjs/gnr_d11/js/genro_widgets.js
@@ -3115,7 +3115,7 @@ dojo.declare("gnr.widgets.DojoGrid", gnr.widgets.baseDojo, {
     creating_structure: function(attributes, sourceNode) {
         var structBag = sourceNode.getRelativeData(sourceNode.attr.structpath);
         if (structBag) {
-            if (genro.grid_configurator && sourceNode.attr.configurable) {
+            if (genro.grid_configurator) {
                  sourceNode.setRelativeData('.resource_structs.__baseview__',structBag.deepCopy(),{caption:_T('Base View')});
             }
         
