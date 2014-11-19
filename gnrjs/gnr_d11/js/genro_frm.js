2057f4eb7b9f7db87ef647c7e5ff4f3db558b66b
Saverio Porcari <saverio.porcari@softwell.it>
2014-11-19 11:27:18 +0100

    Merge branch 'hotfix/a' into develop
    Conflicts:
    	gnrjs/gnr_d11/js/genro_frm.js

--- gnrjs/gnr_d11/js/genro_frm.js
+++ gnrjs/gnr_d11/js/genro_frm.js
@@ -1166,7 +1166,7 @@ dojo.declare("gnr.GnrFrmHandler", null, {
     setCurrentPkey:function(pkey){
         var currentPkey = this.getCurrentPkey()
         this.sourceNode.setRelativeData(this.pkeyPath,pkey);
-        if(pkey!=currentPkey){
+        if(pkey!=currentPkey && this.store){
             this.store.loadedIndex = -1;
         }
     },
