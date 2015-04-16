00c1a91a796d3cd4048758f7910751405971c842
Francesco Porcari <francesco.porcari@softwell.it>
2015-04-16 15:31:03 +0200

    liveUpdate default

--- resources/common/th/th_view.py
+++ resources/common/th/th_view.py
@@ -464,7 +464,7 @@ class TableHandlerView(BaseComponent):
         options = self._th_hook('options',mangler=th_root)() or dict()
         pageOptions = self.pageOptions or dict()
         #liveUpdate: 'NO','LOCAL','PAGE'
-        liveUpdate = liveUpdate or options.get('liveUpdate') or pageOptions.get('liveUpdate') or 'LOCAL'
+        liveUpdate = liveUpdate or options.get('liveUpdate') or pageOptions.get('liveUpdate') or self.site.config['options?liveUpdate'] or 'LOCAL'
         store_kwargs.setdefault('liveUpdate',liveUpdate)
         hardQueryLimit = options.get('hardQueryLimit') or self.application.config['db?hardQueryLimit']
         allowLogicalDelete = store_kwargs.pop('allowLogicalDelete',None) or options.get('allowLogicalDelete')
