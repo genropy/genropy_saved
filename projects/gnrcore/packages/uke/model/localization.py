d42ddbf74cbd998860e8ac8db4ed0cdbe7c94800
Francesco Porcari <francesco.porcari@softwell.it>
2015-04-27 12:08:36 +0200

    uke localization

--- projects/gnrcore/packages/uke/model/localization.py
+++ projects/gnrcore/packages/uke/model/localization.py
@@ -1,10 +1,44 @@
 # encoding: utf-8
+from gnr.core.gnrbag import Bag
+from gnr.core.gnrdecorator import public_method
 
 class Table(object):
     def config_db(self, pkg):
-        tbl =  pkg.table('localization',pkey='code',name_long='!!Localization',
+        tbl =  pkg.table('localization',pkey='id',name_long='!!Localization',
                       name_plural='!!Localization')
-        self.sysFields(tbl,id=False)
-        tbl.column('code',name_long='!!Code')
-        tbl.column('topic',name_long='!!Topic')
-        tbl.column('identifier',name_long='!!Identifier')
+        self.sysFields(tbl)
+        tbl.column('package_identifier' ,group='_',name_long='!!Package identifier').relation('package.package_identifier',mode='foreignkey',onDelete='raise')
+        tbl.column('localization_key',name_long='!!Key')
+        tbl.column('localization_values',dtype='X',name_long='!!Localization Values')
+
+
+    def importLocalizationFile(self,package_identifier=None,filepath=None):
+        localization_bag = Bag(filepath)
+        self.deleteSelection(where='$package_identifier=:package_identifier',package_identifier=package_identifier)
+        for localization_key,attributes in localization_bag.digest('#a._key,#a'):
+            locrecord = dict(localization_key=localization_key or attributes.get('en'),package_identifier=package_identifier)
+            localization_values = Bag()
+            if attributes.get('it'):
+                localization_values['it'] = attributes.get('it')
+                localization_values['en'] = attributes.get('en') or localization_key
+            locrecord['localization_values'] = localization_values
+            self.insert(locrecord)
+
+
+
+    def getProjectLocalizations(self,project_code=None):
+        result = Bag()
+        rows = self.query(where='@package_identifier.project_code=:project_code',project_code=project_code,
+                                columns='$localization_key,$localization_values,$package_identifier',order_by='$package_identifier,$localization_key').fetch()
+
+        for r in rows:
+            localization_values = Bag(r['localization_values'])
+            localization_values['_package_identifier'] = r['package_identifier']
+            localization_values['_key'] = r['localization_key']
+            result[r['pkey']] = localization_values
+        return result
+
+
+    @public_method
+    def savePackageLocalizationBag(self,package_identifier=None,localizationBag=None):
+        pass
---
+++ projects/gnrcore/packages/uke/resources/tables/localization/action/import_localization_file.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+
+# test_special_action.py
+# Created by Francesco Porcari on 2010-07-02.
+# Copyright (c) 2011 Softwell. All rights reserved.
+
+
+
+from gnr.web.batch.btcaction import BaseResourceAction
+caption = '!!Import localization file'
+tags = 'superadmin,_DEV_'
+description = '!!Import localization file'
+
+class Main(BaseResourceAction):
+    batch_prefix = 'icnt'
+    batch_title = '!!Import localization file'
+    batch_cancellable = False
+    batch_delay = 0.5
+    batch_immediate = True
+    
+    def do(self):
+        package = self.batch_parameters['package']
+        package_identifier = '%s/%s'%(self.db.application.packages[package].project,package)
+        self.tblobj.importLocalizationFile(package_identifier=package_identifier,filepath=self.page.site.getStaticPath('pkg:%s'%package,'localization.xml'))
+        self.db.commit()
+
+    def table_script_parameters_pane(self, pane, table=None,**kwargs):
+        fb = pane.formbuilder(cols=1,border_spacing='10px')
+        fb.filteringSelect(value='^.package',lbl='!!Package identifier',
+                        values=','.join(self.db.packages.keys()))
+
+
+
+
+
