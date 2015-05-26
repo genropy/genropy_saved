be52c6eb09e95763e6b96bb4054346618ebac5bb
Francesco Porcari <francesco.porcari@softwell.it>
2015-05-26 18:59:55 +0200

    lookup table reloader

--- projects/gnrcore/packages/sys/webpages/lookuptables.py
+++ projects/gnrcore/packages/sys/webpages/lookuptables.py
@@ -10,14 +10,13 @@ from gnr.core.gnrstring import boolean
 
 class GnrCustomWebPage(object):
     py_requires='public:Public,th/th:TableHandler'
-    
     def main(self,root,th_public=None,**kwargs):
         callArgs = self.getCallArgs('th_pkg','th_table')
         public = boolean(th_public) if th_public else True
         root.attributes['datapath'] = 'main'
         pkg = callArgs.get('th_pkg')
         tbl = callArgs.get('th_table')
-        fixeed_table =False
+        fixed_table =False
         title = '!!Lookup Tables' 
         if pkg:
             title = '%s' %self.db.model.package(pkg).attributes.get('name_long')
@@ -42,10 +41,10 @@ class GnrCustomWebPage(object):
                                 storecaption='.description',disabled='^.package?=!#v')
         else:
             root.dataFormula('.table','tbl',tbl='%(th_pkg)s.%(th_table)s' %callArgs,_onStart=1)
-            fixeed_table = True
+            fixed_table = True
         root.dataController("""
                 SET main.current_table=table;""",table='^main.table',status='=main.mainth.view.grid.status')
-        frame.center.contentPane().remote(self.remoteTh,table='^main.current_table',fixeed_table=fixeed_table,_onRemote='FIRE main.load_data;')
+        frame.center.contentPane().remote(self.remoteTh,table='^main.current_table',fixed_table=fixed_table,_onRemote='FIRE main.load_data;')
 
     def lookupTablesDefaultStruct(self,struct):
         r = struct.view().rows()
@@ -57,26 +56,28 @@ class GnrCustomWebPage(object):
                 r.fieldcell(k,edit=attr['cell_edit'] if 'cell_edit' in attr else True)
 
     @public_method
-    def remoteTh(self,pane,table=None,fixeed_table=None):
+    def remoteTh(self,pane,table=None,fixed_table=None):
         pane.data('.mainth',Bag())
         if not table:
             pane.div('!!Select a table from the popup menu',margin_left='5em',margin_top='5px', color='#8a898a',text_align='center',font_size='large')
         else:
-            saveButton = not fixeed_table
-            semaphore = not fixeed_table
+            saveButton = not fixed_table
+            semaphore = not fixed_table
             tblobj= self.db.table(table)
             th = pane.inlineTableHandler(table=table,viewResource='LookupView',datapath='.mainth',autoSave=False,saveButton=saveButton,semaphore=semaphore,
                                     nodeId='mainth',configurable='*',
                                     view_structCb=self.lookupTablesDefaultStruct,condition_loaddata='^main.load_data',
                                     grid_selfDragRows=tblobj.attributes.get('counter'))
-            if fixeed_table:
+            if fixed_table:
                 bar = th.view.bottom.slotBar('10,revertbtn,*,cancel,savebtn,10',margin_bottom='2px',_class='slotbar_dialog_footer')
                 bar.revertbtn.slotButton('!!Revert',action='FIRE main.load_data;',disabled='==status!="changed"',status='^.grid.editor.status')
                 bar.cancel.slotButton('!!Cancel',action='genro.nodeById("lookup_root").publish("lookup_cancel");')
                 bar.savebtn.slotButton('!!Save',iconClass='editGrid_semaphore',publish='saveChangedRows',command='save',
                                disabled='==status!="changed"',status='^.grid.editor.status',showLabel=True)  
                 th.view.grid.attributes.update(selfsubscribe_savedRows='genro.nodeById("lookup_root").publish("lookup_cancel");')
-          
+            else:
+                bar = th.view.top.bar.replaceSlots('delrow','revertchanges,5,delrow')
+                bar.revertchanges.slotButton('!!Revert',iconClass='iconbox revert',action='FIRE main.load_data;',disabled='==status!="changed"',status='^.grid.editor.status')
 
 
     def getLookupTable(self,pkg=None):
