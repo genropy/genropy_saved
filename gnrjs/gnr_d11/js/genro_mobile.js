8507352c6b0e5dcad98ad025a0cf37dc05128725
Francesco Porcari <francesco.porcari@softwell.it>
2015-10-14 21:58:18 +0800

    fix date

--- gnrjs/gnr_d11/js/genro_mobile.js
+++ gnrjs/gnr_d11/js/genro_mobile.js
@@ -88,5 +88,66 @@ dojo.declare("gnr.GnrMobileHandler", null, {
             b = b + k + ':' + e[k] + '<br/>';
         }
         return b;
+    },
+
+    patch_splitter:function(){
+        dojo.require("dijit.layout.BorderContainer");
+        dojo.require("dijit.layout._LayoutWidget");
+        dijit.layout._Splitter.prototype.templateString = dijit.layout._Splitter.prototype.templateString.replace('onmousedown','ontouchstart');
+        //console.log('patch_splitter',dijit.layout._Splitter);
+        dijit.layout._Splitter.prototype._startDrag = function(e){
+            if(!this.cover){
+                this.cover = dojo.doc.createElement('div');
+                dojo.addClass(this.cover, "dijitSplitterCover");
+                dojo.place(this.cover, this.child.domNode, "after");
+            }else{
+                this.cover.style.zIndex = 1;
+            }
+
+            // Safeguard in case the stop event was missed.  Shouldn't be necessary if we always get the mouse up. 
+            if(this.fake){ dojo._destroyElement(this.fake); }
+            if(!(this._resize = this.live)){ //TODO: disable live for IE6?
+                // create fake splitter to display at old position while we drag
+                (this.fake = this.domNode.cloneNode(true)).removeAttribute("id");
+                dojo.addClass(this.domNode, "dijitSplitterShadow");
+                dojo.place(this.fake, this.domNode, "after");
+            }
+            dojo.addClass(this.domNode, "dijitSplitterActive");
+
+            //Performance: load data info local vars for onmousevent function closure
+            var factor = this._factor,
+                max = this._maxSize,
+                min = this._minSize || 10;
+            var axis = this.horizontal ? "pageY" : "pageX";
+            var pageStart = e[axis];
+            var splitterStyle = this.domNode.style;
+            var dim = this.horizontal ? 'h' : 'w';
+            var childStart = dojo.marginBox(this.child.domNode)[dim];
+            var splitterStart = parseInt(this.domNode.style[this.region]);
+            var resize = this._resize;
+            var region = this.region;
+            var mb = {};
+            var childNode = this.child.domNode;
+            var layoutFunc = dojo.hitch(this.container, this.container._layoutChildren);
+
+            var de = dojo.doc.body;
+            this._handlers = (this._handlers || []).concat([
+                dojo.connect(de, "ontouchmove", this._drag = function(e, forceResize){
+                    var delta = e[axis] - pageStart,
+                        childSize = factor * delta + childStart,
+                        boundChildSize = Math.max(Math.min(childSize, max), min);
+
+                    if(resize || forceResize){
+                        mb[dim] = boundChildSize;
+                        // TODO: inefficient; we set the marginBox here and then immediately layoutFunc() needs to query it
+                        dojo.marginBox(childNode, mb);
+                        layoutFunc(region);
+                    }
+                    splitterStyle[region] = factor * delta + splitterStart + (boundChildSize - childSize) + "px";
+                }),
+                dojo.connect(de, "ontouchend", this, "_stopDrag")
+            ]);
+            dojo.stopEvent(e);
+        };
     }
 });
