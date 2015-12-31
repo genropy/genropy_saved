1e7c24c5bbb02d5674630f6821870fd6cf5e35cf
Francesco Porcari <francesco.porcari@softwell.it>
2015-12-31 11:49:52 +0100

    minor fix

--- gnrpy/gnr/web/gnrwebpage_proxy/apphandler.py
+++ gnrpy/gnr/web/gnrwebpage_proxy/apphandler.py
@@ -1059,7 +1059,7 @@ class GnrWebAppHandler(GnrBaseProxy):
             attributes = kw.get('_attributes')
             colAttrs = selection.colAttrs
             for k,v in attributes.items():
-                if colAttrs.get(k,{}).get('dataType') == 'X':
+                if v and colAttrs.get(k,{}).get('dataType') == 'X':
                     attributes[k] = "%s::X" %v
             if attributes and '__value__' in attributes:
                 value = attributes.pop('__value__')
