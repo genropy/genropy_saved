d99317801c6f9f4194d112a7942a8fcc6fe5fed0
Francesco Porcari <francesco.porcari@softwell.it>
2016-02-15 10:49:16 +0100

    minor issues

--- projects/gnrcore/packages/adm/menu.py
+++ projects/gnrcore/packages/adm/menu.py
@@ -6,9 +6,9 @@ def config(root,application=None):
     administration.thpage(u"!!Auth tags", table="adm.htag")
     administration.thpage(u"!!Letterheads", table="adm.htmltemplate")
     administration.thpage(u"!!Notifications", table="adm.notification")
-    administration.thpage(u"!!Menu Manager", table="adm.menu")
-    administration.thpage(u"!!Menu Pages", table="adm.menu_page")
-    administration.thpage(u"!!Datacatalog", table="adm.datacatalog")
+    administration.thpage(u"!!Menu Manager", table="adm.menu",tags='_DEV_')
+    administration.thpage(u"!!Menu Pages", table="adm.menu_page",tags='_DEV_')
+    administration.thpage(u"!!Datacatalog", table="adm.datacatalog",tags='_DEV_')
     administration.lookups(u"!!Utility tables", lookup_manager="adm")
     administration.thpage(u"!!Counter", table="adm.counter")
 
