ad72ff3686d2980cfc061d91907e7c00df94b7b5
Francesco Porcari <francesco.porcari@softwell.it>
2016-02-15 15:59:40 +0100

    fix regression

--- gnrjs/gnr_d11/js/genro.js
+++ gnrjs/gnr_d11/js/genro.js
@@ -312,7 +312,7 @@ dojo.declare('gnr.GenroClient', null, {
 
     _registerUserEvents:function(){
         var cb = function(evt){
-            if (genro.wsk && genro.sendAllEvents){
+            if (genro.wsk.wsroot && genro.sendAllEvents){
                     genro.wsk.send('user_event',{event:{'type':evt.type,'modifiers':genro.dom.getEventModifiers(evt),'x':evt.x,'y':evt.y,
                                                 'keyCode':evt.keyCode,'keyChar':evt.keyChar,'timeStamp':evt.timeStamp,
                                                 'targetId':evt.target?evt.target.id:''}});
