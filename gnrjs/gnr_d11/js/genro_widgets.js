f850bf8d287756c523017f45f309f66c246d21bd
Francesco Porcari <francesco.porcari@softwell.it>
2016-07-27 13:19:14 +0200

    fix for new version of pyro

--- gnrjs/gnr_d11/js/genro_widgets.js
+++ gnrjs/gnr_d11/js/genro_widgets.js
@@ -4145,7 +4145,7 @@ dojo.declare("gnr.widgets.BaseSelect", null, {
             for (var k in related_setter){
                 values[related_setter[k]] = selectRow[k];
             }
-            grid.collectionStore().updateRow(this.editedRowIndex,values)
+            grid.collectionStore().updateRow(this.editedRowIndex,values);
         }
     }
 });
