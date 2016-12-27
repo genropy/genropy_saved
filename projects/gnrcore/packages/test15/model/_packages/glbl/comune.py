3b5c51f82857ca8096004c3196680de9c007a7dd
Francesco Porcari <francesco.porcari@softwell.it>
2016-12-27 15:41:45 +0100

    test totali voti

--- projects/gnrcore/packages/test15/model/_packages/glbl/comune.py
+++ projects/gnrcore/packages/test15/model/_packages/glbl/comune.py
@@ -7,8 +7,5 @@ class Table(object):
         tbl.column('voto_si',dtype='B',name_long=u'!!Sì')
         tbl.column('voto_no',dtype='B',name_long='!!No')
         tbl.column('voto_astenuto',dtype='B',name_long='!!Astenuto')
-        tbl.column('mill_si',dtype='L',name_long=u'!!Mill.Sì')
-        tbl.column('mill_no',dtype='L',name_long=u'!!Mill.No')
-        tbl.column('mill_ast',dtype='L',name_long=u'!!Mill.Ast.')
         tbl.formulaColumn('millesimi',select=dict(columns='SUM($popolazione_residente)',
                                                     where='$sigla_provincia=#THIS.sigla'),dtype='L')
--- projects/gnrcore/packages/test15/resources/tables/_packages/glbl/comune/th_comune.py
+++ projects/gnrcore/packages/test15/resources/tables/_packages/glbl/comune/th_comune.py
@@ -12,17 +12,33 @@ class TestViewVotoRadio(BaseComponent):
         r = struct.view().rows()
         r.fieldcell('denominazione', width='20em',edit=True
             )
-        r.fieldcell('popolazione_residente', width='10em',edit=True,
-            name='Popolazione')
+ 
         r.checkboxcolumn('voto_si',radioButton='voto',
             columnset='voto',name=u'Sì',width='4em')
         r.checkboxcolumn('voto_no',radioButton='voto',
             columnset='voto',name='No',width='4em')
         r.checkboxcolumn('voto_astenuto',radioButton='voto',
             columnset='voto',name='Ast.',width='4em')
+       
+        r.cell('n_voti_si',calculated=True,width='7em',
+                formula='voto_si?popolazione_residente:0',dtype='L',
+                columnset='po',
+                name=u'Sì',totalize=True)
 
+        r.cell('n_voti_no',calculated=True,width='7em',
+            columnset='po',
+                formula='voto_no?popolazione_residente:0',dtype='L',name='No',totalize=True)
+        r.cell('n_voti_astenuto',calculated=True,width='7em',
+            columnset='po',
+                formula='voto_astenuto?popolazione_residente:0',
+                    dtype='L',name='Astenuti',totalize=True)
+        r.fieldcell('popolazione_residente', width='7em',edit=True,
+            columnset='po',
+            name='Totale',totalize=True)
+        
     def th_order(self):
         return 'denominazione'
 
     def th_options(self):
-        return dict(columnset_voto='Votazione')
+        return dict(grid_columnset_voto='Votazione',grid_columnset_po='Popolazione',
+                    grid_footer='Totali Voto')
