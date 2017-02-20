2ab32101fde384e24533250d0435e62a8defff42
Francesco Porcari <francesco.porcari@softwell.it>
2017-02-20 11:31:22 +0100

    backport envattr fix in master

--- gnrjs/gnr_d11/js/genro_rpc.js
+++ gnrjs/gnr_d11/js/genro_rpc.js
@@ -240,6 +240,9 @@ dojo.declare("gnr.GnrRpcHandler", null, {
         var sourceNode = callKwargs['_sourceNode'];
         var sysrpc = objectPop(callKwargs,'sysrpc');
         callKwargs = this.serializeParameters(genro.src.dynamicParameters(callKwargs));
+        if(sourceNode){
+            objectUpdate(callKwargs,objectExtract(sourceNode.attr,'dbenv_*',true,true));
+        }
         objectPop(callKwargs, '_destFullpath');
        //if(genro.root_page_id){
        //    callKwargs._root_page_id = genro.root_page_id;
@@ -251,7 +254,6 @@ dojo.declare("gnr.GnrRpcHandler", null, {
         kw.url = kw.url || this.pageIndexUrl();
 
         if(sourceNode){
-            objectUpdate(callKwargs,objectExtract(sourceNode.attr,'dbenv_*',true,true));
             var req_dbstore = sourceNode.inheritedAttribute('context_dbstore');
             if (req_dbstore){
                 kw.url = '/'+req_dbstore+kw.url;
