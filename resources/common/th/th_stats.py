50509552ff680190805cab0cd78dba5e70eb9f0f
Francesco Porcari <francesco.porcari@softwell.it>
2017-06-30 18:05:14 +0200

    statspane

--- resources/common/th/th_stats.py
+++ resources/common/th/th_stats.py
@@ -22,91 +22,124 @@ from gnr.web.gnrbaseclasses import BaseComponent
 from gnr.web.gnrwebstruct import struct_method
 from gnr.core.gnrdecorator import extract_kwargs,public_method
 from gnr.core.gnrdict import dictExtract
+from gnr.xtnd.gnrpandas import pd,GnrDbDataframe
+
+
 
 from gnr.core.gnrbag import Bag
 
 class TableHandlerStats(BaseComponent):
-
+    js_requires='th/th_stats'
     @struct_method
-    def th_statsTableHandler(self,pane,field=None):
-        bc = pane.borderContainer()
-        self._statsTableHandlerLeft(bc.tabContainer(region='left',width='230px',margin='2px',
-                                                    drawer=True,splitter=True))
-        self._statsTableHandlerCenter(bc.framePane(region='center'))
-
-
-
-    def _statsTableHandlerLeft(self,tc):
-        pass
-        
-
-    def _statsTableHandlerCenter(self,frame):
-
-        sc = frame.center.stackContainer()
-        iframe = self._statsTableHandlerCenterHtml(sc.contentPane(title='Html'))
-
-        #tc = center.tabContainer()
-       # grid = tc.contentPane(title='Griglia').quickGrid('^.pivot_grid')
-        #grid.tools('export')
-        bar = center.top.slotToolbar('2,stackButtons,*,printStats,exportStats,5')
-        bar.printStats.slotButton('!!Print',action="genro.dom.iFramePrint(_iframe)",iconClass='iconbox print',
-                                _iframe=iframe.js_domNode)
-        bar.exportStats.slotButton('!!Export',iconClass='iconbox export',fire_xls='.run_pivot')
-        
-
-    
-    def dettaglioTotali(self,bc):
+    def th_tableHandlerStats(self,pane,table=None,relation_field=None,relation_value=None):
+        bc = pane.borderContainer(datapath='.stats_%s' %table.replace('.','_') if relation_value else None,
+                                _anchor=True)
         bc.dataController("""
             this.watch('totalizzatore_visibile',function(){
                 return genro.dom.isVisible(bcNode);
             },function(){
-                bcNode.fireEvent('.calcola_pivot',true);
+                bcNode.fireEvent('.stats.run_pivot',true);
             });
-        """,_fired='^#FORM.controller.loaded',
-            filtri_causali='^.causali',
-            filtri_anni_mesi='^.anni_mesi',
-            filtri_depositi_pkeys='^.depositi_pkeys',
-            stat_rows='^.raggruppamenti.store.rows',
-            stat_values='^.raggruppamenti.store.values',
-            stat_columns='^.raggruppamenti.store.columns',
+        """,store='^.store',
+            relation_value=relation_value,
+            filters='^.stats.filters',
+            stat_rows='^.stats.conf.rows',
+            stat_values='^.stats.conf.values',
+            stat_columns='^.stats.conf.columns',
             bcNode=bc)
-        
-        bc.data('.filtri',self.db.table('erpy_mag.articolo_totale').filtriTotalizzazione())
-        bc.dataRpc(None,self.db.table('erpy_mag.articolo_totale').getPivotTable,
-                    filtri_causali='=.causali',
-                    filtri_anni_mesi='=.anni_mesi',
-                    filtri_depositi_pkeys='=.depositi_pkeys',
-                    stat_rows='=.raggruppamenti.store.rows',
-                    stat_values='=.raggruppamenti.store.values',
-                    stat_columns='=.raggruppamenti.store.columns',
-                    articolo_id='=#FORM.pkey',
-                    outmode='^.calcola_pivot',
+        bc.dataController("FIRE .stats.run_pivot_do",_delay=1000,_fired='^.stats.run_pivot')
+        bc.dataRpc(None,self.ths_getPivotTable,
+                    table=table,
+                    relation_field=relation_field,
+                    relation_value=relation_value.replace('^','=') if relation_value else None,
+                    mainfilter = '=.stats.filters.%s' %relation_field,
+                    selectionName='=.store?selectionName',
+                    filters='=.stats.filters',
+                    stat_rows='=.stats.conf.rows',
+                    stat_values='=.stats.conf.values',
+                    stat_columns='=.stats.conf.columns',
+                    _lockScreen=True,
+                    _onCalling="""
+                        SET .stats.pivot_html = "";
+                        if (!(stat_values && stat_values.len() && stat_rows && stat_rows.len())){
+                            return false;
+                        }
+                    """,
+                    _if='',
+                    outmode='^.stats.run_pivot_do',
                     _onResult="""
                         result = result || new gnr.GnrBag();
                         //SET .pivot_grid = result.popNode('pivot_grid')
-                        SET .pivot_html = result.getItem('pivot_html')
+                        SET .stats.pivot_html = result.getItem('pivot_html')
                         if(result.getItem('xls_url')){
                             genro.download(result.getItem('xls_url'));
                         }
                     """)
-        self.dettaglioTotaliParametri(bc.tabContainer(region='left',width='250px',drawer=True,margin='2px'))
-        center = bc.roundedGroupFrame(title='Totalizzazioni',region='center')
-        iframe = self.dettaglioTotaliHtml(center.contentPane())
 
+        left = bc.tabContainer(region='left',width='230px',margin='2px',drawer=True,splitter=True)
+        self._ths_configPivotTree(left.framePane(title='!!Pivot'),table=table,relation_field=relation_field)
+        if not relation_value:
+            #part of grid
+            self._ths_mainFilter(left.contentPane(title='!!Main'),relation_field=relation_field)      
+        self._ths_filters(left.contentPane(title='!!Filters'),table=table,relation_field=relation_field)
+
+
+        self._ths_center(bc.framePane(region='center'))
+
+
+
+
+    def _ths_mainFilter(self,pane,relation_field=None):
+        inattr = pane.getInheritedAttributes()
+        table = inattr.get('table')
+        tblobj = self.db.table(table)
+        def struct(struct):
+            r = struct.view().rows()
+            r.cell(relation_field.replace('.','_').replace('@','_'), userSets=True, name=' ')
+            r.fieldcell(tblobj.attributes.get('caption_field'),
+                        width='100%',
+                        name=tblobj.attributes['name_long'])
+        pane.frameGrid(datapath='.stats.mainfilter',table=table,
+                        grid_store='%s_grid' %inattr['frameCode'],
+                        _newGrid=True,
+                        grid_userSets='#ANCHOR.stats.filters',
+                        struct=struct)
+
+    def _ths_filters(self,pane,table=None,relation_field=None):
+        tblobj = self.db.table(table)
+        if not hasattr(tblobj,'stats_filters'):
+            return
+        filtersbag = tblobj.stats_filters()
+        pane.data('.stats.source_filters',filtersbag)
+        for n in filtersbag:
+            v = n.value
+            label = n.label
+            title = n.attr.get('title') or tblobj.column(label).attributes.get('name_long') or label
+            titlepane = pane.titlePane(title=title,margin='2px')
+            if isinstance(v,basestring):
+                titlepane.checkBoxText(value='^.%s' %label,
+                                        values='^#ANCHOR.stats.source_filters.%s' %label,
+                                        datapath='.stats.filters',
+                                        cols=1)
+            if isinstance(v,Bag):
+                titlepane.tree(storepath='.stats.source_filters.%s' %label,
+                        checked_id='.stats.filters.%s' %label,
+                        labelAttribute='caption',hideValues=True,
+                        margin='2px')
+
+
+    def _ths_center(self,frame):
+        sc = frame.center.stackContainer()
+        iframe = self._ths_framehtml(sc.contentPane(title='Html'))
         #tc = center.tabContainer()
        # grid = tc.contentPane(title='Griglia').quickGrid('^.pivot_grid')
         #grid.tools('export')
-        bar = center.top.bar.replaceSlots('#','#,printStats,exportStats,5')
+        bar = frame.top.slotToolbar('2,stackButtons,*,printStats,exportStats,5')
         bar.printStats.slotButton('!!Print',action="genro.dom.iFramePrint(_iframe)",iconClass='iconbox print',
                                 _iframe=iframe.js_domNode)
-        bar.exportStats.slotButton('!!Export',iconClass='iconbox export',fire_xls='.calcola_pivot')
-        
+        bar.exportStats.slotButton('!!Export',iconClass='iconbox export',fire_xls='.stats.run_pivot_do')
 
-        #center.top.bar.replaceSlots('#','#,export,5')
-    
-
-    
-    def _statsTableHandlerCenterHtml(self,pane,**kwargs):
+    def _ths_framehtml(self,pane,**kwargs):
         iframe = pane.div(_class='scroll-wrapper').htmliframe(height='100%',width='100%',border=0)
         pane.dataController("""var cw = _iframe.contentWindow;
                         var cw_body = cw.document.body;
@@ -126,23 +159,92 @@ class TableHandlerStats(BaseComponent):
                     _if='htmlcontent',
                     _else="""_iframe.contentWindow.document.body.innerHTML ="<div class='document'>Vuoto</div>" """,
                     _iframe=iframe.js_domNode,styleurl=self.getResourceUri('js_plugins/statspane/report.css', add_mtime=True),
-                    htmlcontent='^.pivot_html')
-        return iframe
+                    htmlcontent='^.stats.pivot_html')
+        return iframe       
 
-    def dettaglioTotaliParametri(self,tc):
-        self.dettaglioTotaliRaggruppamenti(tc.framePane(title='Raggruppamenti'))
-        self.dettaglioTotaliParametriFiltri(tc.contentPane(title='Filtri',overflow='auto'))
+    def ths_getDataframe(self,table,filters=None,mainfilter=None,
+                                relation_field=None,relation_value=None,
+                                maintable=None,selectionName=None,**kwargs):
+        df = GnrDbDataframe('current_df_%s' %table.replace('.','_'),self.db)
+        where = []
+        where_kwargs = {}
+        for fkey,pkeys in filters.items():
+            if pkeys:
+                where.append(' $%s IN :filters_%s' %(fkey,fkey))
+                where_kwargs['filters_%s' %fkey] = pkeys.split(',')
+        if not mainfilter:
+            if selectionName:
+                pkeys = self.freezedPkeys(self.db.table(table),selectionName)
+            else:
+                pkeys = [relation_value]
+            where.append(' $%s IN :filters_%s' %(relation_field,relation_field))
+            where_kwargs['filters_%s' %relation_field] = pkeys
+        df.query(table,where=' AND '.join(where),**where_kwargs)
+        return df
+        
+    @public_method
+    def ths_getInfo(self,table=None,related_field=None,selectionName=None):
+        df = GnrDbDataframe('info_df_%s' %table.replace('.','_'),self.db)
+        df.query(table,where='$%s IN :freezed_pkeys' %related_field,
+                    freezed_pkeys=self.freezedPkeys(self.db.table(table),selectionName))
+        return df.getInfo()
+        
+    def ths_configPivotTreeData(self,table,relation_field=None):
+        df = GnrDbDataframe('current_df_%s' %table.replace('.','_'),self.db)
+        return df.configPivotTree(self.db.table(table),
+                                default_values=None,
+                                default_rows=None,
+                                default_columns=None)
 
-    def dettaglioTotaliRaggruppamenti(self,frame):
-        bar = frame.bottom.slotToolbar('*,calcola,5')
-        frame.data('.raggruppamenti.store',self.db.table('erpy_mag.articolo_totale').configPivotTree())
+    @public_method
+    def ths_getPivotTable(self,df=None,table=None,relation_field=None,relation_value=None,
+                        filters=None,mainfilter=None,selectionName=None,
+                        stat_rows=None,stat_columns=None,
+                        stat_values=None,outmode=None,
+                        filename=None,**kwargs):
+        stats_tableobj = self.db.table(table)
+        main_tableobj = self.db.table(table).column(relation_field).relatedColumn().table
+        if not df:
+            df = self.ths_getDataframe(table,filters=filters,mainfilter=None,
+                                        relation_field=relation_field,
+                                        relation_value=relation_value,
+                                        maintable=main_tableobj.fullname,
+                                        selectionName=selectionName)
+            if not df:
+                return
+        valuesbag = Bag()
+        if stat_values:
+            for k in stat_values.keys():
+                fieldname,agg = k.split('_')
+                vb = valuesbag[fieldname]
+                if vb:
+                    vb['aggregators'] = '%s,%s' %(vb['aggregators'],agg)
+                else:
+                    valuesbag[fieldname] = Bag(dict(aggregators=agg))
+        pivotdf,bagresult = df.pivotTableGrid(index=stat_rows.keys() if stat_rows else None,
+                                            values=valuesbag if valuesbag else None,
+                                            columns=stat_columns.keys() if stat_columns else None)
+        result = Bag()
+        result['pivot_html'] = pivotdf.to_html()
+        result['pivot_grid'] = bagresult
+        if outmode=='xls':
+            filename = filename or 'stats_%s' %main_tableobj.name
+            path = self.db.application.site.getStaticPath('page:xls_stats','%s.xls' %filename,autocreate=-1)
+            writer = pd.ExcelWriter(path)
+            pivotdf.to_excel(writer,'Sheet1')
+            writer.save()
+            result['xls_url'] = self.db.application.site.getStaticUrl('page:xls_stats','%s.xls' %filename)
+        return result   
+
+    def _ths_configPivotTree(self,frame,table=None,relation_field=None):
+        frame.data('.stats.conf',self.ths_configPivotTreeData(table,relation_field=relation_field))
         frame.center.contentPane(overflow='auto'
-                        ).tree(storepath='.raggruppamenti.store',margin='5px',
+                        ).tree(storepath='.stats.conf',margin='5px',
                                  _class="branchtree noIcon",
                                 labelAttribute='caption',hideValues=True,
                                 draggable=True,dragClass='draggedItem',
                                 dropTarget=True,
-                                nodeId='raggruppamenti_stats_articolo_%s' %id(frame),
+                                nodeId='conf_stats_%s' %id(frame),
                                 getLabelClass="""if (!node.attr.field){return "statfolder"}
                                         return node.attr.stat_type;""",
                                dropTargetCb="""
@@ -152,50 +254,4 @@ class TableHandlerStats(BaseComponent):
                                     return true;
                                 """,
                                 onDrag='dragValues["selfdrag_path"]= dragValues["treenode"]["relpath"];',
-                                onDrop_selfdrag_path=self.onDrop_raggruppamento())
-        bar.calcola.slotButton('Calcola',fire='.calcola_pivot')
-
-    def onDrop_raggruppamento(self):
-        return """if(!dropInfo.selfdrop){
-            return;
-        }
-        var put_before = dropInfo.modifiers == 'Shift';
-        var b = this.widget.storebag();
-        var destpath = dropInfo.treeItem.getFullpath(null,b);
-        if(destpath==data){
-            return;
-        }
-        var destNode = b.getNode(destpath);
-        var destBag;
-        var kw = {};
-        if(destNode.attr.field){
-            destBag = destNode.getParentBag();
-            kw._position = (put_before?'<':'>')+destNode.label;
-        }else{
-            destBag = destNode.getValue();
-        }
-        var dragNode = b.popNode(data);
-        destBag.setItem(dragNode.label,dragNode,null,kw);
-    """
-    def dettaglioTotaliParametriFiltri(self,tc):
-        self.filtroAnnoMese(tc.titlePane(title='Anni/mesi',margin='2px'))
-        self.filtroDepositi(tc.titlePane(title='Depositi',margin='2px'))
-        self.filtroCausali(tc.titlePane(title='Causali',margin='2px'))
-
-    def filtroAnnoMese(self,pane):
-        pane.tree(storepath='.filtri.anni_mesi',
-                  checked_anno_mese='.anni_mesi',
-                  labelAttribute='caption',hideValues=True,
-                  margin='2px')
-
-    def filtroDepositi(self,pane):
-        pane.tree(storepath='.filtri.depositi',
-                  checked_deposito_id='.depositi_pkeys',
-                  labelAttribute='caption',hideValues=True,
-                  margin='2px')
-
-    def filtroCausali(self,pane):
-        pane.data('.causali','VEND')
-        pane.checkBoxText(value='^.causali',
-                        values='^.filtri.causali',
-                        cols=1,parentForm=False)
+                                onDrop_selfdrag_path="th_stats_js.confTreeOnDrop(this,dropInfo,data)")
