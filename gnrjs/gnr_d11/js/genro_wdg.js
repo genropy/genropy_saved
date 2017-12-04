c72be775ef1d8a420c708090aa4976d1a4e9f554
Francesco Porcari <francesco.porcari@softwell.it>
2017-12-04 17:36:30 +0100

    fix qtooltip

--- gnrjs/gnr_d11/js/genro_wdg.js
+++ gnrjs/gnr_d11/js/genro_wdg.js
@@ -1429,7 +1429,7 @@ dojo.declare("gnr.GridEditor", null, {
                 var rowpah = this.widgetRootNode.absDatapath('.' + rowLabel);
                 genro.dlg.quickTooltipPane({datapath:rowpah,fields:attr.fields,domNode:cellNode,modal:attr.modal},
                                             funcCreate(attr.contentCb,'pane,kw',grid.sourceNode),
-                                            {rowDataNode:rowDataNode,grid:grid});
+                                            {rowDataNode:rowDataNode,grid:grid,cell:cell});
             }
             return
         }
