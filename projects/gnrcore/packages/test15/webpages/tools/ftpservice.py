289baab5227b422d37fb0e37b1b674f255043f6f
Francesco Porcari <francesco.porcari@softwell.it>
2017-12-15 14:20:17 +0100

    no message

--- projects/gnrcore/packages/test15/webpages/tools/ftpservice.py
+++ projects/gnrcore/packages/test15/webpages/tools/ftpservice.py
@@ -2,7 +2,7 @@
 # 
 
 from gnr.core.gnrdecorator import public_method
-from gnr.core.gnrbag import Bag
+from gnr.core.gnrbag import Bag,DirectoryResolver
 
 class GnrCustomWebPage(object):
     py_requires = "gnrcomponents/testhandler:TestHandlerFull"
@@ -18,7 +18,27 @@ class GnrCustomWebPage(object):
         pane.data('.root.genropy',resolver())
         pane.tree(storepath='.root',hideValues=True, selectedLabelClass='selectedTreeNode',
                       selected_abs_path='.abs_path',selected_file_ext='.file_ext',
+                      checked_abs_path='.checked_abs_path',
                       labelAttribute='nodecaption', autoCollapse=True)
+        
+
+    def test_3_dl(self, pane):
+        bc = pane.borderContainer(height='600px')
+        resolver = self.getService('ftp_genropy').sftpResolver()
+        left = bc.framePane(region='left',width='300px',datapath='.remote')
+        left.data('.tree.root',resolver())
+        left.center.contentPane(overflow='auto').tree(storepath='.tree',hideValues=True, selectedLabelClass='selectedTreeNode',
+                      selected_abs_path='.abs_path',selected_file_ext='.file_ext',
+                      checked_abs_path='.checked_abs_path',
+                      labelAttribute='nodecaption', autoCollapse=True)
+        right = bc.framePane(region='right',width='300px',datapath='.local')
+        resolver= DirectoryResolver(self.site.getStatic('site').path(),include='directory')
+        right.data('.tree.root',resolver())
+        right.center.contentPane(overflow='auto').tree(storepath='.tree',hideValues=True, selectedLabelClass='selectedTreeNode',
+                      selected_abs_path='.abs_path',selected_file_ext='.file_ext',
+                      labelAttribute='nodecaption', autoCollapse=True)
+        bar = right.bottom.slotToolbar('*,downloadbtn,5')
+        bar.downloadbtn.slotButton('Download')
 
     @public_method
     def ftplist(self):
