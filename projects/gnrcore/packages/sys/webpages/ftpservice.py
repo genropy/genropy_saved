d0304b8b93f7014e8ecc71a60d8bcd7b1c0f91f2
Francesco Porcari <francesco.porcari@softwell.it>
2017-12-20 15:13:29 +0100

    ftpservice sys page

---
+++ projects/gnrcore/packages/sys/webpages/ftpservice.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+# thpage.py
+# Created by Francesco Porcari on 2011-05-05.
+# Copyright (c) 2011 Softwell. All rights reserved.
+
+
+class GnrCustomWebPage(object):
+    py_requires = 'services/ftp/pysftp/component:SftpClient'
+    auth_main='admin'
+
+    #FOR ALTERNATE MAIN HOOKS LOOK AT public:TableHandlerMain component
+    def main(self,root,**kwargs):
+        callArgs = self.getCallArgs('ftpname')  
+        root.sftpClientLayout(callArgs['ftpname'],datapath='main')
