08436f5a1d583371af4d3fff2cd15ffa8e702649
Francesco Porcari <francesco.porcari@softwell.it>
2018-03-21 09:28:23 +0100

    no message

--- projects/gnrcore/packages/sys/webpages/desktop.py
+++ projects/gnrcore/packages/sys/webpages/desktop.py
@@ -32,8 +32,7 @@ class GnrCustomWebPage(object):
         <head>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
             <script>
-
-                    window.open('%s');
+                    window.location = '%s';
             </script>
             </head>
             <body>
