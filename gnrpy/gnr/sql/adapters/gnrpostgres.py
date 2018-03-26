4b6a94af7f9ba60c5b93b1fa5e5ee9e2a3b7c428
Francesco Porcari <francesco.porcari@softwell.it>
2018-03-26 13:05:35 +0200

    postgres adapter

--- gnrpy/gnr/sql/adapters/gnrpostgres.py
+++ gnrpy/gnr/sql/adapters/gnrpostgres.py
@@ -193,6 +193,8 @@ class SqlDbAdapter(SqlDbBaseAdapter):
                 'host':self.dbroot.host or 'localhost',
                 'port':self.dbroot.port or '5432'}
         extras = extras or []
+        if not filename.endswith('.pgd'):
+            filename = '%s.pgd' %filename
         #args = ['pg_dump', dbname, '-U', self.dbroot.user, '-f', filename]+extras
         args = ['pg_dump','--dbname=postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(dbname)s' %pars,'-Fc', '-f', filename]+extras
         return call(args)
@@ -204,7 +206,8 @@ class SqlDbAdapter(SqlDbBaseAdapter):
         :param filename: db name"""
         from subprocess import call
         dbname = dbname or self.dbroot.dbname
-        return call(['psql', "dbname=%s user=%s password=%s" % (dbname, self.dbroot.user, self.dbroot.password), '-f', filename])
+        command = 'psql' if not filename.endswith('.pgd') else 'pg_restore'
+        return call([command, "dbname=%s user=%s password=%s" % (dbname, self.dbroot.user, self.dbroot.password), '-f', filename])
         
     def createTableAs(self, sqltable, query, sqlparams):
         self.dbroot.execute("CREATE TABLE %s WITH OIDS AS %s;" % (sqltable, query), sqlparams)
