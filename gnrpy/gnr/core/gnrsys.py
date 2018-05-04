ef09c351db2de5c90138f14b64e56ccb561a3778
Francesco Porcari <francesco.porcari@softwell.it>
2018-05-04 08:27:20 +0200

    added progress for scripts

--- gnrpy/gnr/core/gnrsys.py
+++ gnrpy/gnr/core/gnrsys.py
@@ -23,6 +23,17 @@
 import os
 import sys
 
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush() 
+
+
 def mkdir(path, privileges=0777):
     """Create a directory named *path* with numeric mode *privileges*.
     
