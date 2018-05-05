fbe9bdaa33af6758f28c9826d66a1323c2e330a8
Francesco Porcari <francesco.porcari@softwell.it>
2018-05-05 12:13:13 +0200

    excluded dbevents in sqlargs

--- gnrpy/gnr/sql/gnrsql.py
+++ gnrpy/gnr/sql/gnrsql.py
@@ -415,7 +415,7 @@ class GnrSqlDb(GnrObject):
         """
         # transform list and tuple parameters in named values.
         # Eg.   WHERE foo IN:bar ----> WHERE foo in (:bar_1, :bar_2..., :bar_n)
-        envargs = dict([('env_%s' % k, v) for k, v in self.currentEnv.items()])
+        envargs = dict([('env_%s' % k, v) for k, v in self.currentEnv.items() if not k.startswith('dbevents')])
         if not 'env_workdate' in envargs:
             envargs['env_workdate'] = self.workdate
         envargs.update(sqlargs or {})
