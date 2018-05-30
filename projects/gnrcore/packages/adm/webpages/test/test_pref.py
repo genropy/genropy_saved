d1b3a9b43c865a58fc66bb70ffb928ff4e85242c
Francesco Porcari <francesco.porcari@softwell.it>
2018-05-30 09:08:18 +0200

    pref chache multidb

---
+++ projects/gnrcore/packages/adm/webpages/test/test_pref.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+#
+#  Preference
+#
+#  Created by Francesco Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+
+class GnrCustomWebPage(object):
+    py_requires="gnrcomponents/testhandler:TestHandlerFull"
+
+    def pageAuthTags(self, **kwargs):
+        return 'user'
+
+    def windowTitle(self):
+        return '!!Test pref'
+
+    def test_01_single_value(self, pane, **kwargs):
+        fb = pane.formbuilder()
+        fb.textbox(value='^.prefpkg',lbl='Pref pkg')
+        fb.textbox(value='^.prefpath',lbl='Pref path')
+        fb.button('Search',fire='.search')
+        fb.div('^.result',lbl='Result')
+        fb.dataRpc('.result',self.getAppPreference,pkg='=.prefpkg',prefpath='=.prefpath',_fired='^.search')
---
+++ projects/gnrcore/packages/sys/webpages/test/test_globalstore.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+#
+#  Preference
+#
+#  Created by Francesco Porcari on 2007-03-24.
+#  Copyright (c) 2007 Softwell. All rights reserved.
+#
+
+from gnr.core.gnrdecorator import public_method
+class GnrCustomWebPage(object):
+    py_requires="gnrcomponents/testhandler:TestHandlerFull"
+
+    def pageAuthTags(self, **kwargs):
+        return 'user'
+
+    def windowTitle(self):
+        return '!!Test pref'
+
+    def test_01_single_value(self, pane, **kwargs):
+        fb = pane.formbuilder()
+        fb.textbox(value='^.storepath',lbl='Path')
+        fb.button('Search',fire='.search')
+        fb.div('^.result',lbl='Result')
+        fb.dataRpc('.result',self.globalStoreValue,storepath='=.storepath',_fired='^.search')
+    
+    @public_method
+    def globalStoreValue(self,storepath=None):
+        return self.globalStore().getItem(storepath)
