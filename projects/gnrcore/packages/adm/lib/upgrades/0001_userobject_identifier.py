3823da0192c36c57cc7d45e8aac5ee6a135fe810
Francesco Porcari <francesco.porcari@softwell.it>
2018-07-04 11:55:07 +0200

    fix userobject fixer

--- projects/gnrcore/packages/adm/lib/upgrades/0001_userobject_identifier.py
+++ projects/gnrcore/packages/adm/lib/upgrades/0001_userobject_identifier.py
@@ -10,11 +10,12 @@ def main(db):
         if (r['pkg'] and not db.package(r['pkg'])) or  (r['tbl'] and not db.table(r['tbl'])):
             print 'deleting userobject',r,'because does not exist tbl or package'
             tblobj.delete(r)
-        identifier = '%s%s%s' %(r['pkg'] or r['tbl'] or '',r['objtype'],r['code'])
+        tbl = (r['tbl'] or r['pkg'] or '')
+        identifier = (tbl,r['objtype'],r['code'])
+        
         if identifier in addedidentifier:
-            print 'found duplicate for',identifier
             r['code'] = '%s_%i' %(r['code'],i)
-            identifier = '%s%s%s' %(r['pkg'] or r['tbl'] or '',r['objtype'],r['code'])
+            identifier = (tbl,r['objtype'],r['code'])
             if r['description']:
                 r['description'] = '%s [dup]' %r['description']
         tblobj.update(r,oldrec)
