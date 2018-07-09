f85ddc9d66a70984cedcae4946d6d2b96b873959
Francesco Porcari <francesco.porcari@softwell.it>
2018-07-09 09:02:34 +0200

    dulwich integration test

---
+++ gnrpy/gnr/core/gnrgit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+#  gnrgit.py
+#
+#  Created by Francesco Porcari
+#  Copyright (c) 2018 Softwell. All rights reserved.
+
+from dulwich.client import HttpGitClient
+from dulwich.repo import Repo
+from gnr.core.gnrbag import  Bag
+
+
+class GnrGit(object):
+    def __init__(self,repo_path,remote_origin=None,remote_user=None,remote_password=None):
+        self.repo = Repo(repo_path)
+        self.config = Bag(self.repo.get_config())
+        self.remote_origin = remote_origin
+        self.remote_user = remote_user
+        self.remote_password = remote_password
+        if self.remote_origin:
+            self.remote_url = self.config['remote.%s.url' %self.remote_origin]
+            self.remote_client = HttpGitClient(self.remote_url,username=remote_user,password=remote_password)
+
+    def get_refs(self,path):
+        self.remote_client.get_refs(path)
+    
+    
---
+++ projects/gnrcore/packages/test15/webpages/tools/gnrgit.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+# gettemplate.py
+# Created by Francesco Porcari on 2011-05-11.
+# Copyright (c) 2011 Softwell. All rights reserved.
+
+
+from gnr.core.gnrbag import  Bag
+from gnr.core.gnrgit import GnrGit
+from gnr.core.gnrdecorator import public_method
+
+class GnrCustomWebPage(object):
+    py_requires="""gnrcomponents/testhandler:TestHandlerFull"""
+
+    def test_0_config_tree(self,pane):
+        fb = pane.formbuilder()
+        fb.textbox(value='^.repo_path',lbl='Repository path',width='40em')
+        fb.tree(storepath='.repo_config',lbl='Config')
+        fb.dataRpc('.repo_config',self.getRepoConfig,repopath='^.repo_path',_if='repopath',_else='return gnr.GnrBag()')
+
+    @public_method
+    def getRepoConfig(self,repopath=None):
+        return GnrGit(repopath).config
