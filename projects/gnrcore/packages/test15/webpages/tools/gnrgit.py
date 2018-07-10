d0de0c3c1f7254e1f18d4537635484a6c3690d61
Francesco Porcari <francesco.porcari@softwell.it>
2018-07-10 08:49:50 +0200

    changed test

--- projects/gnrcore/packages/test15/webpages/tools/gnrgit.py
+++ projects/gnrcore/packages/test15/webpages/tools/gnrgit.py
@@ -15,9 +15,16 @@ class GnrCustomWebPage(object):
     def test_0_config_tree(self,pane):
         fb = pane.formbuilder()
         fb.textbox(value='^.repo_path',lbl='Repository path',width='40em')
+        fb.textbox(value='^.remote_origin',lbl='Origin',width='40em')
+        fb.textbox(value='^.remote_user',lbl='Username',width='40em')
+        fb.textbox(value='^.remote_password',lbl='Username',width='40em')
+
         fb.tree(storepath='.repo_config',lbl='Config')
         fb.dataRpc('.repo_config',self.getRepoConfig,repopath='^.repo_path',_if='repopath',_else='return gnr.GnrBag()')
 
     @public_method
     def getRepoConfig(self,repopath=None):
         return GnrGit(repopath).config
+
+    def test_2_config_tree(self,pane):
+        pass
