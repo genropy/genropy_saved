dcce408b9ee6c43272b1fed4edd8f858a3f9bc65
Francesco Porcari <francesco.porcari@softwell.it>
2018-07-10 08:48:47 +0200

    removed deploy from gnrcore now is in the repo gnrdeploy

--- projects/gnrcore/packages/deploy/model/wsgi_server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-class Table(object):
-    def config_db(self, pkg):
-        tbl = pkg.table('wsgi_server', pkey='code', name_long='!!WSGI server', name_plural='!!WSGI servers', lookup=True)
-        self.sysFields(tbl, id=False)
-        tbl.column('code' , size=':10',name_long='!!Code')
-        tbl.column('description',name_long='!!Description')
