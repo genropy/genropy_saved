97a5352828aff91c3aebc52c0a1f0df0b7a559d0
Francesco Porcari <francesco.porcari@softwell.it>
2018-07-11 17:49:00 +0200

    pkeyOnly changed syntax

--- gnrpy/gnr/sql/gnrsqldata.py
+++ gnrpy/gnr/sql/gnrsqldata.py
@@ -1023,7 +1023,7 @@ class SqlQuery(object):
         return [r[pkeyfield] for r in fetch]
 
         
-    def fetchAsDict(self, key=None, ordered=False, pkey_only=False):
+    def fetchAsDict(self, key=None, ordered=False, pkeyOnly=False):
         """Return the :meth:`~gnr.sql.gnrsqldata.SqlQuery.fetch` method as a dict with as key
         the parameter key you gave (or the pkey if you don't specify any key) and as value the
         record you get from the query
@@ -1031,7 +1031,7 @@ class SqlQuery(object):
         :param key: the key you give (if ``None``, it takes the pkey). 
         :param ordered: boolean. if ``True``, return the fetch using a :class:`OrderedDict`,
                         otherwise (``False``) return the fetch using a normal dict.
-        :pkey_only: boolean  if ``True``, the values of the dict are the pkeys and not the record"""
+        :pkeyOnly: boolean  if ``True``, the values of the dict are the pkeys and not the record"""
         
         fetch = self.fetch()
         key = key or self.dbtable.pkey
@@ -1039,7 +1039,7 @@ class SqlQuery(object):
             factory = OrderedDict
         else:
             factory = dict
-        if pkey_only:
+        if pkeyOnly:
             return factory([(r[key], r[self.dbtable.pkey]) for r in fetch])
         return factory([(r[key], r) for r in fetch])
         
