a9c1d7692142654f7cef8ee2de3171d0bf6bcc07
Francesco Porcari <francesco.porcari@softwell.it>
2018-07-24 12:16:26 +0200

    no message

--- projects/gnrcore/packages/sys/webpages/ecsdashboard.py
+++ projects/gnrcore/packages/sys/webpages/ecsdashboard.py
@@ -3,18 +3,12 @@ from gnr.core.gnrbag import DirectoryResolver
 from gnr.core.gnrdecorator import public_method
 
 class GnrCustomWebPage(object):
-    py_requires = 'public:Public,gnrcomponents/framegrid:FrameGrid'
+    py_requires = 'public:Public,gnrcomponents/framegrid:FrameGrid,gnrcomponents/gnride/gnride'
+
     auth_main = '_DEV_'
 
     def main(self,root,**kwargs):
         frame = root.framePane(datapath='main')
         bc = frame.center.borderContainer()
-        bc.contentPane(region='left',width='300px')
-        left = bc.contentPane(overflow='auto',region='left',width='300px') 
-        left.tree(storepath='.efs')
-        left.data('.efs.root',DirectoryResolver(self.site.getStaticPath('vol:efs'),name='EFS')())
-
-
-    @public_method
-    def efsResolver(self,pod=None,**kwargs):
-        return 
+        bc.gnrIdeFrame(region='center',nodeId='podIDE',datapath='main.podIDE',
+                                sourceFolders='/home')
