68ab9f19e7da38691366deb8f2aeaef55ccf4d91
Francesco Porcari <francesco.porcari@softwell.it>
2018-11-18 10:07:41 +0100

    minor fix

--- projects/gnrcore/packages/biz/resources/dashboard_items/standard/items_comparator.py
+++ projects/gnrcore/packages/biz/resources/dashboard_items/standard/items_comparator.py
@@ -26,9 +26,9 @@ from gnr.core.gnrdecorator import public_method
 
 caption = 'Compare items'
 description = 'Item comparator'
-objtype = 'dash_comparator'
+objtype = 'dash_items_comparator'
 
-item_parameters = [dict(value='^.item_1',lbl='Item 1',tag='callBackSelect',validate_notnull=True,
+item_parameters = [dict(value='^.item_1',lbl='Sheet 1',tag='callBackSelect',validate_notnull=True,
                         selected_caption='.item_1_caption',
                         callback="""function(kw){
                 var _id = kw._id;
@@ -46,7 +46,9 @@ item_parameters = [dict(value='^.item_1',lbl='Item 1',tag='callBackSelect',valid
                 }
                 data = data.filter(cbfilter);
                 return {headers:'',data:data}
-            }""",hasDownArrow=True),dict(value='^.item_2',lbl='Item 2',tag='callBackSelect',validate_notnull=True,
+            }""",hasDownArrow=True),
+            dict(value='^.compare_mode',lbl='Op',values='diff:-,perc_change:Perc.Change',tag='filteringSelect'),
+            dict(value='^.item_2',lbl='Sheet 2',tag='callBackSelect',validate_notnull=True,
                         selected_caption='.item_2_caption',
                         callback="""function(kw){
                 var _id = kw._id;
@@ -64,34 +66,63 @@ item_parameters = [dict(value='^.item_1',lbl='Item 1',tag='callBackSelect',valid
                 }
                 data = data.filter(cbfilter);
                 return {headers:'',data:data}
-            }""",hasDownArrow=True),
-            dict(value='^.compare_mode',lbl='Mode',values='diff,perc',tag='filteringSelect')]
+            }""",hasDownArrow=True)]
 
 
 class Main(BaseDashboardItem):
     title_template = '$title'
-    def content(self,pane,table=None,userobject_id=None,item_1=None,item_2=None,compare_mode=None,**kwargs):
-        frame = pane.bagGrid(frameCode=self.itemIdentifier,structpath='#%s_grid.struct' %item_1,datamode='attr',
-                            storepath='%s.store' %self.workpath,datapath=self.workpath,addrow=False,delrow=False)
-        frame.dataController("""
+    def content(self,pane,item_1=None,item_2=None,compare_mode=None,**kwargs):
+        frame = pane.bagGrid(frameCode=self.itemIdentifier,
+                            structpath='%s.struct' %self.workpath,datamode='attr',
+                            storepath='%s.store' %self.workpath,
+                            datapath=self.workpath,
+                            addrow=False,delrow=False)
+        pane.dataController("""
             var cmp_store = new gnr.GnrBag();
+            var structrow = basestruct.getItem('view_0.rows_0');
+            var cmp_struct = new gnr.GnrBag();
+            var cmp_struct_row = new gnr.GnrBag();
+            cmp_struct.setItem('view_0.rows_0',cmp_struct_row);
+            compare_mode = compare_mode || 'diff';
+
             store_1.forEach(function(n){
-                var row = objectUpdate({},n.attr);
-                var row2 = store_2.getNodeByAttr('_thgroup_pkey',row._thgroup_pkey);
+                var row1 = n.attr;
+                var row = objectUpdate({},row1);
+                var row2 = store_2.getNodeByAttr('_thgroup_pkey',row1._thgroup_pkey);
                 row2 = row2?row2.attr:{}
                 structrow.forEach(function(sn){
-                    if(sn.attr.group_aggr=='sum'){
-                        row[sn.attr.field+'_sum'] -= (row2[sn.attr.field+'_sum'] || 0)
+                    var cmp_struct_node = cmp_struct_row.setItem(sn.label,null,sn.attr);
+                    objectPop(cmp_struct_node.attr,'totalize');
+                    console.log('snattr',sn.attr);
+                    if('NLIRF'.indexOf(sn.attr.dtype)>=0 && !sn.attr.group_nobreak){
+                        var kf = sn.attr.field+'_sum';
+                        if(compare_mode=='diff'){
+                            row[kf] = (row1[kf] || 0) - (row2[kf] || 0)
+                        }else if(row2[kf]){
+                            cmp_struct_node.attr.format = '#,###.00;-;<span style="color:red">#,###.00</span>'
+                            row[kf] = (((row1[kf] || 0) - (row2[kf] || 0))/row2[kf]) *100;
+                        }else{
+                            row[kf] = null;
+                        }
+                        
                     }
                 })
                 cmp_store.setItem(n.label,null,row);
             });
-            SET .store = cmp_store;
+            SET %s.struct = cmp_struct;
+            SET %s.store = cmp_store;
 
-        """,store_1='^#%s.store' %item_1,
+        """ %(self.workpath,self.workpath),
+        store_1='^#%s.store' %item_1,
             store_2='^#%s.store' %item_2,
-            structrow='=#%s_grid.struct.view_0.rows_0' %item_1,
-            _fired='^.runItem',datapath=self.workpath,
-            compare_mode=compare_mode,_if='store_1&&store_2',
-            _else="SET .store = new gnr.GnrBag()")
+            basestruct='=#%s_grid.struct' %item_1,
+            _fired='^%s.runItem' %self.workpath,
+            _delay=100,
+            compare_mode='^.conf.compare_mode',_if='store_1 && store_2',
+            _else="SET %s.store = new gnr.GnrBag()" %self.workpath)
 
+    def configuration(self,pane,item_1=None,item_2=None,compare_mode=None,**kwargs):
+        fb = pane.formbuilder()
+        fb.filteringSelect(value='^.compare_mode',lbl='!!Compare mode',
+                        default_value=compare_mode,values='diff:-,perc_change:Perc.Change')
+        
