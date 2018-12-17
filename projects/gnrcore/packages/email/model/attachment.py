53cb97f3941ab85708f42ee1f41b4c018f6a37b2
Francesco Porcari <francesco.porcari@softwell.it>
2018-12-17 14:09:21 +0100

    imap using storageNode

--- projects/gnrcore/packages/email/model/attachment.py
+++ projects/gnrcore/packages/email/model/attachment.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+import os
 
 class Table(object):
 
@@ -14,3 +15,25 @@ class Table(object):
                                                                              onDelete_sql='cascade',
                                                                              onDelete='cascade',
                                                                              relation_name='attachments')
+
+
+    def getAttachmentNode(self,date=None,filename=None, message_id = None, account_id=None,atc_counter=None):
+        year = str(date.year)
+        month = '%02i' %date.month
+        filename = filename or 'attachment_%s' %atc_counter
+        storage_identifier = self.db.application.getPreference('attachment_storage',pkg='email')
+        site = self.db.application.site
+        if not storage_identifier:
+            storage = 'site'
+        else:
+            storage = storage.split('_',1)[1]
+        storage = '%s:mail' %storage
+        snode = site.storageNode(storage,account_id, year,month,message_id)
+        counter = 0
+        fname,ext = os.path.splitext(filename)
+
+        #avoiding dup
+        while snode.service.isfile(filename):
+            filename = '%s_%i%s'%(fname,counter,ext)
+            counter += 1
+        return site.storageNode(storage,account_id, year,month,message_id,filename)
