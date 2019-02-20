47a06d8c4cf17d75d72c2fcbe1779207a929b1e5
Francesco Porcari <francesco.porcari@softwell.it>
2019-02-20 08:16:04 +0100

    form in different dbstore depending from selection

--- gnrjs/gnr_d11/js/genro_frm.js
+++ gnrjs/gnr_d11/js/genro_frm.js
@@ -2488,6 +2488,12 @@ dojo.declare("gnr.formstores.Base", null, {
         virtual_columns = virtual_columns.concat(form_virtual_columns);
         kw['_sourceNode'] = form.sourceNode;
         kw.ignoreReadOnly = ignoreReadOnly;
+        var dbstoreField = this.parentStore?this.parentStore.storeNode.attr.dbstoreField:null;
+        if(dbstoreField){
+            var ps = this.parentStore;
+            var row = ps.rowFromItem(ps.rowBagNodeByIdentifier(currPkey));
+            this.form.sourceNode.attr.context_dbstore = row[dbstoreField];
+        }
         var deferred = genro.rpc.remoteCall(loader.rpcmethod ,objectUpdate({'pkey':currPkey,
                                                   'virtual_columns':arrayUniquify(virtual_columns).join(','),
                                                   'table':this.table, timeout:0},kw),null,'POST',null,maincb);
