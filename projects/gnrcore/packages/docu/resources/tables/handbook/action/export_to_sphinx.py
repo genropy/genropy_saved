dae3b3055bb214c80f29322485928b6fa5666d6c
Saverio Porcari <>
2019-02-22 15:36:37 +0100

    docu action to export handbooks to sphinx html sites

--- projects/gnrcore/packages/docu/resources/tables/handbook/action/export_to_sphinx.py
+++ projects/gnrcore/packages/docu/resources/tables/handbook/action/export_to_sphinx.py
@@ -25,21 +25,36 @@ class Main(BaseResourceBatch):
 
     def do(self):
         handbook_id = self.batch_parameters['extra_parameters']['handbook_id']
+        site=self.page.site
         self.handbook_record = self.tblobj.record(handbook_id).output('bag')
         self.doctable=self.db.table('docu.documentation')
         doc_data = self.doctable.getHierarchicalData(root_id=self.handbook_record['docroot_id'], condition='$is_published IS TRUE')['root']
-        default_path = '%s/%s' % (self.db.application.getPreference('.sphinx_path', pkg='docu'),self.handbook_record['name'])
-        self.destpath= self.handbook_record['sphinx_path'] or default_path
+        self.handbookNode= site.storageNode(self.handbook_record['sphinx_path']) #or default_path
+        self.sphinxNode = self.handbookNode.child('sphinx')
+        self.sourceDirNode = self.sphinxNode.child('source')
+        site.storageNode('rsrc:pkg_docu','sphinx_env','default_conf.py').copy(site.storageNode(self.sourceDirNode.child('conf.py')))
         self.imagesDict = dict()
         self.imagesPath='_static/images'
+        self.imagesDirNode = self.sourceDirNode.child(self.imagesPath)
+
         toc = self.prepare(doc_data,[])
         self.createFile(pathlist=[], name='index', title='Table of contents', rst='', toc=toc)
-        self.destNode = self.page.site.storageNode(self.destpath)
         for k,v in self.imagesDict.items():
             source_url = self.page.externalUrl(v) if v.startswith('/_vol') else v
-            child = self.destNode.child(k)
+            child = self.sourceDirNode.child(k)
             with child.open('wb') as f:
                 f.write(urllib.urlopen(source_url).read())
+
+        self.resultNode = self.sphinxNode.child('build')
+        site.shellCall('sphinx-build', self.sourceDirNode.internal_path , self.resultNode.internal_path)
+
+
+    def post_process(self):
+        self.zipNode = self.handbookNode.child('%s.zip' % self.handbook_record['name'])
+        self.page.site.zipFiles([self.resultNode.internal_path], self.zipNode.internal_path)
+        
+    def result_handler(self):
+        return 'Handbook zip', dict(url=self.zipNode.url())
         
     def prepare(self, data, pathlist):
         IMAGEFINDER = re.compile(r"\.\. image:: ([\w./]+)")
@@ -96,6 +111,6 @@ class Main(BaseResourceBatch):
             tocstring= ''
         content = '\n'.join([reference_label, title, '='*len(title), tocstring, '\n\n', rst])
         
-        storageNode = self.page.site.storageNode('/'.join([self.destpath]+pathlist))
+        storageNode = self.page.site.storageNode('/'.join([self.sourceDirNode.internal_path]+pathlist))
         with storageNode.child('%s.rst' % name).open('wb') as f:
             f.write(content)
