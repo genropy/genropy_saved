425ac92d089df8fa0b22213e03f5fbdc5a22b0d0
Francesco Porcari <francesco.porcari@softwell.it>
2020-01-17 15:17:48 +0100

    fix sqltablename

--- gnrpy/gnr/sql/gnrsqlmodel.py
+++ gnrpy/gnr/sql/gnrsqlmodel.py
@@ -813,7 +813,7 @@ class DbTableObj(DbModelObj):
         
     def _get_sqlfullname(self):
         """property. Returns the table's sqlfullname"""
-        return '%s.%s' % (self.sqlschema, self.adapted_sqlname)
+        return '%s.%s' % (self.sqlschema, self.adapted_sqlname) if self.sqlschema else self.adapted_sqlname
         
     sqlfullname = property(_get_sqlfullname)
         
