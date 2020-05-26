16ad3ecdc58c4769777592d72290869088f7714e
Francesco Porcari <francesco.porcari@softwell.it>
2020-05-26 15:27:27 +0200

    fix

--- projects/gnrcore/packages/docu/menu.py
+++ projects/gnrcore/packages/docu/menu.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def config(root,application=None):
-    docu = root.branch('Documentation')
+    docu = root.branch('Documentation',tags='_DOC_')
     docu.thpage('!!Documentation',table='docu.documentation')
     docu.thpage('!!Handbooks',table='docu.handbook')
     docu.lookups(u"!!Docu tables", lookup_manager="docu")
